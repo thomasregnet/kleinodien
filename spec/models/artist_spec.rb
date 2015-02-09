23683d880b6b0ae9cb808e34a526b5fa3076c799
ossi <ossi@ossgard.xx>
2015-02-09 20:49:56 +0100

    Artist validates presence of :name

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Artist, type: :model do
   it "is valid with with valid attributes" do
     expect(@artist).to be_valid
   end
+
+  it "is not valid without a name" do
+    @artist.name = nil
+    expect(@artist).not_to be_valid
+  end
 end
