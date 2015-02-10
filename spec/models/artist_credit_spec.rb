82b1917aaacdad04976feff5175880207ab8596f
ossi <ossi@ossgard.xx>
2015-02-10 21:46:11 +0100

    ArtistCredit must have a unique name

--- spec/models/artist_credit_spec.rb
+++ spec/models/artist_credit_spec.rb
@@ -13,4 +13,9 @@ RSpec.describe ArtistCredit, type: :model do
     @artist_credit.name = nil
     expect(@artist_credit).not_to be_valid
   end
+
+  it "must have a unique name" do
+    clone = ArtistCredit.new(name: @artist_credit.name)
+    expect(clone).not_to be_valid
+  end 
 end
