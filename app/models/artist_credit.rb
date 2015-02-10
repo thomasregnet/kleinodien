1403c1e69d3f7882c14510c66e0fa694bc4d700a
ossi <ossi@ossgard.xx>
2015-02-10 21:38:45 +0100

    ArtistCredit is not valid without a name

--- app/models/artist_credit.rb
+++ app/models/artist_credit.rb
@@ -1,2 +1,3 @@
 class ArtistCredit < ActiveRecord::Base
+  validates :name, presence: true
 end
--- spec/models/artist_credit_spec.rb
+++ spec/models/artist_credit_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ArtistCredit, type: :model do
   before(:each) do
-    FactoryGirl.create(:artist_credit)
+    @artist_credit = FactoryGirl.create(:artist_credit)
   end
 
-  it "is valid with valid attributes"
+  it "is valid with valid attributes" do
+    expect(@artist_credit).to be_valid
+  end
 
-  it "is not valid without a name"
+  it "is not valid without a name" do
+    @artist_credit.name = nil
+    expect(@artist_credit).not_to be_valid
+  end
 end
