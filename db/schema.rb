71fe68166f397afa233c768ee16f0c448dddc551
ossi <ossi@ossgard.xx>
2015-02-10 20:09:38 +0100

    Deleted artist_*_index migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209205003) do
+ActiveRecord::Schema.define(version: 20150209182752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
