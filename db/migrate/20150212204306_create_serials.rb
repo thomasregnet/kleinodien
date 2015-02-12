c95bc9667423a95a054cd6ad131c963076aee9e7
ossi <ossi@ossgard.xx>
2015-02-12 21:46:18 +0100

    Serials.name is NOT NULL, ran migration

--- db/migrate/20150212204306_create_serials.rb
+++ db/migrate/20150212204306_create_serials.rb
@@ -1,7 +1,7 @@
 class CreateSerials < ActiveRecord::Migration
   def change
     create_table :serials do |t|
-      t.string :title
+      t.string :title, null: false
       t.string :disambiguation
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212200749) do
+ActiveRecord::Schema.define(version: 20150212204306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 20150212200749) do
   end
 
   add_index "participants", ["no", "artist_credit_id"], name: "participants_no_artist_credit_id_idx", unique: true, using: :btree
+
+  create_table "serials", force: :cascade do |t|
+    t.string   "title",          null: false
+    t.string   "disambiguation"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   add_foreign_key "participants", "artist_credits", name: "participants_fk_artist_credits"
   add_foreign_key "participants", "artists", name: "participants_fk_artists"
