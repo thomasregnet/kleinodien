2e3fd613071e611f9a76face728a1c79efb9052f
ossi <ossi@ossgard.xx>
2015-02-12 20:40:20 +0100

    Artist_credit_spec uses "create" for factories

--- spec/models/artist_credit_spec.rb
+++ spec/models/artist_credit_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ArtistCredit, type: :model do
   before(:each) do
-    @artist_credit = FactoryGirl.build(:artist_credit)
+    @artist_credit = FactoryGirl.create(:artist_credit)
   end
 
   it { is_expected.to respond_to(:artists) }
@@ -22,8 +22,13 @@ RSpec.describe ArtistCredit, type: :model do
   end
 
   it "is not valid without a participant" do
+    @artist_credit = FactoryGirl.build(:artist_credit)
     @artist_credit.participants = []
     expect(@artist_credit).not_to be_valid
+  end
+
+  it "raises an error when it sets its participants to null" do
+    expect { @artist_credit.participants = [] }.to raise_error
   end
 
 end
