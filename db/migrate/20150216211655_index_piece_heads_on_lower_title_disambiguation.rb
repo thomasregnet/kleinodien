1701dba246d90f24a071121b9c1c25dbb7d0b6e5
ossi <ossi@ossgard.xx>
2015-02-16 22:24:19 +0100

    New index "index_piece_heads_on_lower_title_di..."
    index_piece_heads_on_lower_title_disambiguation

---
+++ db/migrate/20150216211655_index_piece_heads_on_lower_title_disambiguation.rb
@@ -0,0 +1,17 @@
+class IndexPieceHeadsOnLowerTitleDisambiguation < ActiveRecord::Migration
+  def change
+    reversible do |idx|
+      idx.up do
+        execute <<-DDL
+          CREATE UNIQUE INDEX index_piece_heads_on_lower_title_disambiguation
+            ON piece_heads (LOWER(title), LOWER(disambiguation))
+        DDL
+      end
+      idx.down do
+        remove_index(
+          :piece_heads, name: :index_piece_heads_on_lower_title_disambiguation
+        )
+      end
+    end
+  end
+end
