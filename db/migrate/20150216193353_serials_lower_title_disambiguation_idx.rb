6891932ee15750e2a02beb4f9141a306512d821b
ossi <ossi@ossgard.xx>
2015-02-16 20:39:49 +0100

    New index "serials_lower_title_disambiguation_idx"

---
+++ db/migrate/20150216193353_serials_lower_title_disambiguation_idx.rb
@@ -0,0 +1,15 @@
+class SerialsLowerTitleDisambiguationIdx < ActiveRecord::Migration
+  def change
+    reversible do |idx|
+      idx.up do
+        execute <<-DDL
+          CREATE UNIQUE INDEX serials_lower_title_disambiguation_idx
+            ON serials (LOWER(title), LOWER(disambiguation))
+        DDL
+      end
+      idx.down do
+        remove_index :serials, name: :serials_lower_title_disambiguation_idx
+      end
+    end
+  end
+end
