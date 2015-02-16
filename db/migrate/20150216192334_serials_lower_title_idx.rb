e8c6799285a78ee514c0df20979edb8da35990de
ossi <ossi@ossgard.xx>
2015-02-16 20:28:14 +0100

    New index "serials_lower_title_idx"

---
+++ db/migrate/20150216192334_serials_lower_title_idx.rb
@@ -0,0 +1,16 @@
+class SerialsLowerTitleIdx < ActiveRecord::Migration
+  def change
+    reversible do |idx|
+      idx.up do
+        execute <<-DDL
+          CREATE UNIQUE INDEX serials_lower_title_idx
+            ON serials (LOWER(title))
+              WHERE disambiguation IS NULL;
+        DDL
+      end
+      idx.down do
+        remove_index :serials, name: :serials_lower_title_idx
+      end
+    end
+  end
+end
