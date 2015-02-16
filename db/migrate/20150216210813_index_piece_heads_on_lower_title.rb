32cb549a392a7e9bcaf7a640d1e2569ed43892c0
ossi <ossi@ossgard.xx>
2015-02-16 22:13:46 +0100

    New index "index_piece_heads_on_lower_title"

---
+++ db/migrate/20150216210813_index_piece_heads_on_lower_title.rb
@@ -0,0 +1,16 @@
+class IndexPieceHeadsOnLowerTitle < ActiveRecord::Migration
+  def change
+    reversible do |idx|
+      idx.up do
+        execute <<-DDL
+          CREATE UNIQUE INDEX index_piece_heads_on_lower_title
+            ON piece_heads (LOWER(title))
+              WHERE disambiguation IS NULL;
+        DDL
+      end
+      idx.down do
+        remove_index(:piece_heads, name: :index_piece_heads_on_lower_title)
+      end
+    end
+  end
+end
