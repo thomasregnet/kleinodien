ce8f6e61eb37c0bc0a2a0019ed7739f4961f75c0
ossi <ossi@ossgard.xx>
2015-02-17 21:14:03 +0100

    Added failing specs for Station

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -8,4 +8,31 @@ RSpec.describe Station, type: :model do
   it "is valid with valid parameters" do
     expect(@station).to be_valid
   end
+
+  it "is not valid without a name" do
+    @station.name = nil
+    expect(@station).not_to be_valid
+  end
+
+  it "must have a unique name" do
+    clone = Station.new(name: @station.name)
+    expect { clone.save! }.to raise_error
+  end
+
+  it "must have a case insensitive unique name" do
+    clone = Station.new(name: @station.name.upcase)
+    expect { clone.save! }.to raise_error
+  end
+
+  it "is unique with a disambiguation" do
+    clone = Station.new(name: @station.name, disambiguation: 'other one')
+    expect { clone.save! }.not_to raise_error 
+  end
+
+  it "fails if a name-disambiguation pair already exists" do
+    @station = FactoryGirl.create(disambiguation: 'other one')
+    clone = Station.new(
+      name: @station.name, disambiguation: @station.disambiguation)
+    expect { clone.save! }.to raise_error
+  end
 end
