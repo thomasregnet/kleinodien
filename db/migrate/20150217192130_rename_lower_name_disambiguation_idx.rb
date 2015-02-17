ab2ab20ff0b9d879905bbe94804bee98ec65bf8c
ossi <ossi@ossgard.xx>
2015-02-17 20:28:10 +0100

    Renamed index artists_lower_name_disambiguation_idx
    ... to index_artists_lower_disambiguation_and_name.

---
+++ db/migrate/20150217192130_rename_lower_name_disambiguation_idx.rb
@@ -0,0 +1,14 @@
+class RenameLowerNameDisambiguationIdx < ActiveRecord::Migration
+  def change
+    old = 'artists_lower_name_disambiguation_idx'
+    new = 'index_artists_lower_disambiguation_and_name'
+    reversible do |rename|
+      rename.up do
+        execute "ALTER INDEX #{old} RENAME TO #{new}"
+      end
+      rename.down do
+        execute "ALTER INDEX #{new} RENAME TO #{old}"
+      end
+    end
+  end
+end
