289546e6c1c87f8d5a07f6244cca36b68b846de1
ossi <ossi@ossgard.xx>
2015-02-17 20:19:54 +0100

    Renamed index artist_lower_name_idx...
    ...to index_artists_on_lower_name.

---
+++ db/migrate/20150217191223_rename_artist_unique_lower_name_idx.rb
@@ -0,0 +1,14 @@
+class RenameArtistUniqueLowerNameIdx < ActiveRecord::Migration
+  def change
+    old = 'artist_lower_name_idx'
+    new = 'index_artists_on_lower_name'
+    reversible do |rename|
+      rename.up do
+        execute "ALTER INDEX #{old} RENAME TO #{new}"
+      end
+      rename.down do
+        execute "ALTER INDEX #{new} RENAME TO #{old}"
+      end
+    end
+  end
+end
