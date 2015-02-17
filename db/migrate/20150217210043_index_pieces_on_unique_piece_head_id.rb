07f526c4a287b3dc80ac76492edb549242d0464b
ossi <ossi@ossgard.xx>
2015-02-17 22:04:56 +0100

    New index index_piece_on_unique_piece_head_id

---
+++ db/migrate/20150217210043_index_pieces_on_unique_piece_head_id.rb
@@ -0,0 +1,16 @@
+class IndexPiecesOnUniquePieceHeadId < ActiveRecord::Migration
+  def change
+    reversible do |idx|
+      idx.up do
+        execute <<-DDL
+          CREATE UNIQUE INDEX index_piece_on_unique_piece_head_id
+            ON pieces (piece_head_id)
+              WHERE version IS NULL;
+        DDL
+      end
+      idx.down do
+        remove_index(:pieces, name: :index_piece_on_unique_piece_head_id)
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217204538) do
+ActiveRecord::Schema.define(version: 20150217210043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 20150217204538) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+
+  add_index "pieces", ["piece_head_id"], name: "index_piece_on_unique_piece_head_id", unique: true, where: "(version IS NULL)", using: :btree
 
   create_table "seasons", force: :cascade do |t|
     t.integer  "serial_id",  null: false
