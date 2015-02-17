022c714ff1fecb27d5acbce5c2f06dabb9dc7d30
ossi <ossi@ossgard.xx>
2015-02-17 20:50:25 +0100

    Renamed index serials_lower_title_idx
    ... to index_serials_lower_title.

---
+++ db/migrate/20150217194106_rename_serials_lower_title_disambiguation_idx.rb
@@ -0,0 +1,14 @@
+class RenameSerialsLowerTitleDisambiguationIdx < ActiveRecord::Migration
+  def change
+    old = 'serials_lower_title_disambiguation_idx'
+    new = 'index_serials_lower_disambiguation_and_title'
+    reversible do |rename|
+      rename.up do
+        execute "ALTER INDEX #{old} RENAME TO #{new}"
+      end
+      rename.down do
+        execute "ALTER INDEX #{new} RENAME TO #{old}"
+      end
+    end
+  end
+end
---
+++ db/migrate/20150217194613_rename_serials_lower_title_idx.rb
@@ -0,0 +1,14 @@
+class RenameSerialsLowerTitleIdx < ActiveRecord::Migration
+  def change
+    old = 'serials_lower_title_idx'
+    new = 'index_serials_lower_title'
+    reversible do |rename|
+      rename.up do
+        execute "ALTER INDEX #{old} RENAME TO #{new}"
+      end
+      rename.down do
+        execute "ALTER INDEX #{new} RENAME TO #{old}"
+      end
+    end
+  end
+end
