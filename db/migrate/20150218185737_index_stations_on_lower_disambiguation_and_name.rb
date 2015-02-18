5e12e5e6df5247fa5fdcd2fdc1631ea164c86da9
ossi <ossi@ossgard.xx>
2015-02-18 20:02:24 +0100

    New index_stations_on_lower_disambiguation_and_name

---
+++ db/migrate/20150218185737_index_stations_on_lower_disambiguation_and_name.rb
@@ -0,0 +1,16 @@
+class IndexStationsOnLowerDisambiguationAndName < ActiveRecord::Migration
+  def change
+    reversible do |idx|
+      idx.up do
+        execute <<-DDL
+          CREATE UNIQUE INDEX index_stations_on_lower_disambiguation_and_name
+            ON stations (LOWER(disambiguation), LOWER(name));
+        DDL
+      end
+      idx.down do
+        remove_index(:stations,
+                     name: :index_stations_on_lower_disambiguation_and_name)
+      end
+    end
+  end
+end
