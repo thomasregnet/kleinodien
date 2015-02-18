69210d353ab226218591b51e44623f7c3ceb0a63
ossi <ossi@ossgard.xx>
2015-02-18 21:55:54 +0100

    Code cleanup in artist_spec.rb

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Artist, type: :model do
   before(:each) do
     @artist = FactoryGirl.create(:artist)
   end
+  
+  it "is valid with with valid attributes" do
+    expect(@artist).to be_valid
+  end
 
   it_behaves_like "a model with disambiguations" do
     let(:factory) { :artist }
     let(:naming) { 'name' }
-  end
-  
-  it "is valid with with valid attributes" do
-    expect(@artist).to be_valid
   end
 end
