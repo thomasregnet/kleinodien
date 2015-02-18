87ddedf85ee477d26823e81253b7d9b75a818c49
ossi <ossi@ossgard.xx>
2015-02-18 21:38:17 +0100

    Shared example "a model with disambiguations"

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,38 +1,44 @@
 require 'rails_helper'
+require 'shared_examples_for_disambiguations'
 
 RSpec.describe Artist, type: :model do
   before(:each) do
     @artist = FactoryGirl.create(:artist)
   end
 
+  it_behaves_like "a model with disambiguations" do
+    let(:factory) { :artist }
+    let(:naming) { 'name' }
+  end
+  
   it "is valid with with valid attributes" do
     expect(@artist).to be_valid
   end
 
-  it "is not valid without a name" do
-    @artist.name = nil
-    expect(@artist).not_to be_valid
-  end
+  # it "is not valid without a name" do
+  #   @artist.name = nil
+  #   expect(@artist).not_to be_valid
+  # end
 
-  it "must have a unique name" do
-    clone = Artist.new(name: @artist.name)
-    expect { clone.save! }.to raise_error
-  end
+  # it "must have a unique name" do
+  #   clone = Artist.new(name: @artist.name)
+  #   expect { clone.save! }.to raise_error
+  # end
 
-  it "must have a case insensitive unique name" do
-    clone = Artist.new(name: @artist.name.upcase)
-    expect { clone.save! }.to raise_error
-  end
+  # it "must have a case insensitive unique name" do
+  #   clone = Artist.new(name: @artist.name.upcase)
+  #   expect { clone.save! }.to raise_error
+  # end
 
-  it "is unique with a disambiguation" do
-    clone = Artist.new(name: @artist.name, disambiguation: 'other one')
-    expect { clone.save! }.not_to raise_error 
-  end
+  # it "is unique with a disambiguation" do
+  #   clone = Artist.new(name: @artist.name, disambiguation: 'other one')
+  #   expect { clone.save! }.not_to raise_error 
+  # end
 
-  it "fails if a name-disambiguation pair already exists" do
-    @artist = FactoryGirl.create(:artist_with_disambiguation)
-    clone = Artist.new(
-      name: @artist.name, disambiguation: @artist.disambiguation)
-    expect { clone.save! }.to raise_error
-  end
+  # it "fails if a name-disambiguation pair already exists" do
+  #   @artist = FactoryGirl.create(:artist_with_disambiguation)
+  #   clone = Artist.new(
+  #     name: @artist.name, disambiguation: @artist.disambiguation)
+  #   expect { clone.save! }.to raise_error
+  # end
 end
---
+++ spec/shared_examples_for_disambiguations.rb
@@ -0,0 +1,43 @@
+
+RSpec.shared_examples "a model with disambiguations" do
+  before(:each) do
+    @model = FactoryGirl.create(factory)
+  end
+
+  let(:get_name) { naming }
+  let(:set_name) { naming + '=' }
+  let(:disambiguation) { 'another one' }
+  
+  it "is not valid without a naming}" do
+    @model.send set_name, nil
+    expect(@model).not_to be_valid
+  end
+
+  it "must have a unique naming" do
+    clone = FactoryGirl.build(factory)
+    clone.send set_name, @model.send(get_name)
+    expect { clone.save! }.to raise_error
+  end
+
+  it "must have a case insensitive unique naming" do
+    clone = FactoryGirl.build(factory)
+    clone.send(set_name, @model.send(get_name).upcase)
+    expect { clone.save! }.to raise_error
+  end
+
+  it "is unique with a disambiguation" do
+    clone = FactoryGirl.build(factory)
+    clone.send(set_name, @model.send(get_name))
+    clone.disambiguation = disambiguation
+    expect { clone.save! }.not_to raise_error 
+  end
+
+  it "fails if a name-disambiguation pair already exists" do
+    @model.disambiguation = disambiguation
+    @model.save!
+    clone = FactoryGirl.build(factory)
+    clone.send(set_name, @model.send(get_name))
+    clone.disambiguation = disambiguation
+    expect { clone.save! }.to raise_error
+  end  
+end
