a53577b607437acca37c40acf316fde4b31dce2b
ossi <ossi@ossgard.xx>
2015-02-19 20:13:16 +0100

    Replaces many FactoryGirl.build calls with one let
    shared_examples_for_disambiguations.rb

--- spec/shared_examples_for_disambiguations.rb
+++ spec/shared_examples_for_disambiguations.rb
@@ -6,8 +6,9 @@ RSpec.shared_examples "a model with disambiguations" do
 
   let(:get_name) { naming }
   let(:set_name) { naming + '=' }
+  let(:clone) { FactoryGirl.build(factory) }
   let(:disambiguation) { 'another one' }
-  
+
   it "is not valid without a naming" do
     @model.send set_name, nil
     expect(@model).not_to be_valid
@@ -15,15 +16,13 @@ RSpec.shared_examples "a model with disambiguations" do
   end
 
   it "must have a unique naming" do
-    clone = FactoryGirl.build(factory)
-    clone.send set_name, @model.send(get_name)
+    clone.send(set_name, @model.send(get_name))
     expect(clone).not_to be_valid
     expect { clone.save! validate: false }.to raise_error(
                                                 ActiveRecord::RecordNotUnique)
   end
 
   it "must have a case insensitive unique naming" do
-    clone = FactoryGirl.build(factory)
     clone.send(set_name, @model.send(get_name).upcase)
     expect(clone).not_to be_valid
     expect { clone.save! validate: false }.to raise_error(
@@ -31,7 +30,6 @@ RSpec.shared_examples "a model with disambiguations" do
   end
 
   it "is unique with a disambiguation" do
-    clone = FactoryGirl.build(factory)
     clone.send(set_name, @model.send(get_name))
     clone.disambiguation = disambiguation
     expect(clone).to be_valid
@@ -40,8 +38,7 @@ RSpec.shared_examples "a model with disambiguations" do
 
   it "fails if a name-disambiguation pair already exists" do
     @model.disambiguation = disambiguation
-    @model.save!
-    clone = FactoryGirl.build(factory)
+    @model.save
     clone.send(set_name, @model.send(get_name))
     clone.disambiguation = disambiguation
     expect { clone.save! validate: false }.to raise_error(
