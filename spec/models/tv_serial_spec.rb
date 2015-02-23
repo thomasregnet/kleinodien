da9cbff5a7aeccb2212a0f4d372e7af94dd213e0
ossi <ossi@ossgard.xx>
2015-02-23 21:46:41 +0100

    Added context "without seasons" to tv_serial_spec

--- spec/models/tv_serial_spec.rb
+++ spec/models/tv_serial_spec.rb
@@ -2,17 +2,20 @@ require 'rails_helper'
 require 'shared_examples_for_disambiguations'
 
 RSpec.describe TvSerial, type: :model do
-  before(:each) do
-    @tv_serial = FactoryGirl.create(:tv_serial)
-  end
 
-  it "is valid with valid attributes" do
-    expect(@tv_serial).to be_valid
-  end
+  context "without seasons" do
+    before(:each) do
+      @tv_serial = FactoryGirl.create(:tv_serial)
+    end
 
-  it_behaves_like "a model with disambiguations" do
-    let(:factory) { :tv_serial }
-    let(:object) { @tv_serial }
-    let(:naming) { 'title' }
-  end  
+    it "is valid with valid attributes" do
+      expect(@tv_serial).to be_valid
+    end
+
+    it_behaves_like "a model with disambiguations" do
+      let(:factory) { :tv_serial }
+      let(:object) { @tv_serial }
+      let(:naming) { 'title' }
+    end
+  end
 end
