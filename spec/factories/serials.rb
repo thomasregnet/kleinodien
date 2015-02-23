1758cd18ffeac8eb4b6427bead5fda28d462fb45
ossi <ossi@ossgard.xx>
2015-02-23 20:33:06 +0100

    Specs and factory for Serial

--- spec/factories/serials.rb
+++ spec/factories/serials.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :serial do
     sequence(:title) { |n| "serial ##{n}" }
     type 'Serial'
+
+    factory :tv_serial, class: TvSerial do
+      type 'TvSerial'
+    end
   end
 end
