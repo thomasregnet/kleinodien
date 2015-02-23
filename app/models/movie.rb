729c12202efb88bc69c38c47d60daa836d507daf
ossi <ossi@ossgard.xx>
2015-02-23 20:09:06 +0100

    Movie behaves like a piece

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < Piece
   belongs_to :head, class_name: 'MovieHead', foreign_key: :piece_head_id
+  validates :head, presence: true
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'shared_examples_for_pieces'
 
 RSpec.describe Movie, type: :model do
   before(:each) do
@@ -7,5 +8,9 @@ RSpec.describe Movie, type: :model do
 
   it "is valid with valid attributes" do
     expect(@movie).to be_valid
+  end
+
+  it_behaves_like "a piece" do
+    let(:piece) { @movie }
   end
 end
