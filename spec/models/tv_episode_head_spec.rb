8f16290b6413d5348008ac02a14699d7b4cb28f3
ossi <ossi@ossgard.xx>
2015-02-23 21:24:03 +0100

    TvEpisodeHead behaves like a model with disambi...

--- spec/models/tv_episode_head_spec.rb
+++ spec/models/tv_episode_head_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'shared_examples_for_disambiguations'
 
 RSpec.describe TvEpisodeHead, type: :model do
   before(:each) do
@@ -7,5 +8,11 @@ RSpec.describe TvEpisodeHead, type: :model do
 
   it "is valid with valid attributes" do
     expect(@tv_episode_head).to be_valid
+  end
+
+  it_behaves_like "a model with disambiguations" do
+    let(:factory) { :tv_episode_head }
+    let(:object) { @tv_episode_head }
+    let(:naming) { 'title' }
   end
 end
