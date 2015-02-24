ef327de872f8bce11cc8e4c29307a2fb75875e4c
ossi <ossi@ossgard.xx>
2015-02-24 20:40:05 +0100

    Failing specs with season-episodes
    New factory :season_with_tv_episode_heads.

--- spec/factories/seasons.rb
+++ spec/factories/seasons.rb
@@ -3,5 +3,15 @@ FactoryGirl.define do
     serial
     sequence(:no) { |n| n }
     type 'Season'
+
+    factory :season_with_tv_episode_heads do
+      transient do
+        episodes_count 5
+      end
+
+      after(:create) do |season, elevator|
+        create_list(:tv_episode_head, elevator.episodes_count, season: season)
+      end
+    end
   end
 end
--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -3,21 +3,33 @@ require 'rails_helper'
 RSpec.describe Season, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
 
-  before(:each) do
-    @season = FactoryGirl.create(:season)
+  context "without seasons" do
+    before(:each) do
+      @season = FactoryGirl.create(:season)
+    end
+
+    it "is valid with valid attributes" do
+      expect(@season).to be_valid
+    end
+
+    it "is not valid without a no" do
+      @season.no = nil
+      expect(@season).not_to be_valid
+    end
+
+    it "is not valid without a serial" do
+      @season.serial = nil
+      expect(@season).not_to be_valid
+    end
   end
 
-  it "is valid with valid attributes" do
-    expect(@season).to be_valid
-  end
+  context "with episodes" do
+    before(:each) do
+      @season = FactoryGirl.create(:season_with_tv_episode_heads)
+    end
 
-  it "is not valid without a no" do
-    @season.no = nil
-    expect(@season).not_to be_valid
-  end
-
-  it "is not valid without a serial" do
-    @season.serial = nil
-    expect(@season).not_to be_valid
+    it "is nice" do
+      expect(@season).to be_valid
+    end
   end
 end
