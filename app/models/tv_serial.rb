ab1d89f76e4bf65ed38235a2fc3d686ccda0e533
ossi <ossi@ossgard.xx>
2015-02-24 20:22:18 +0100

    Deleted some comments

--- app/models/tv_serial.rb
+++ app/models/tv_serial.rb
@@ -1,6 +1,2 @@
 class TvSerial < Serial
-  # has_many(:seasons,
-  #          class_name: 'TvSeason',
-  #          inverse_of: :serial,
-  #          foreign_key: :serial_id)
 end
--- spec/factories/seasons.rb
+++ spec/factories/seasons.rb
@@ -3,9 +3,5 @@ FactoryGirl.define do
     serial
     sequence(:no) { |n| n }
     type 'Season'
-
-    # factory :tv_season, class: TvSeason do
-    #   type 'TvSeason'
-    # end
   end
 end
