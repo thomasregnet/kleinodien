91f01ca2b4f31c6166a077aa5d858070add0e606
ossi <ossi@ossgard.xx>
2015-02-25 20:28:13 +0100

    belongs_to attr class_name: works without quotes

--- app/models/tv_episode_release.rb
+++ app/models/tv_episode_release.rb
@@ -1,4 +1,4 @@
 class TvEpisodeRelease < EpisodeRelease
-  belongs_to :head, class_name: 'TvEpisodeHead', foreign_key: :piece_head_id
+  belongs_to :head, class_name: TvEpisodeHead, foreign_key: :piece_head_id
   validates :head, presence: true
 end
