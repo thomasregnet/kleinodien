faae34a0ed5ff75fa2eb39c52e0565ec21b507f7
ossi <ossi@ossgard.xx>
2015-02-25 21:28:42 +0100

    More Specs for ZeroSeason

--- app/models/zero_season.rb
+++ app/models/zero_season.rb
@@ -1,2 +1,5 @@
 class ZeroSeason < Season
+  belongs_to :serial, class_name: PodcastSerial, foreign_key: :serial_id
+  validates :serial, presence: true
+  validates :no, inclusion: { in: [0] }
 end
--- spec/models/zero_season_spec.rb
+++ spec/models/zero_season_spec.rb
@@ -5,7 +5,19 @@ RSpec.describe ZeroSeason, type: :model do
     @zero_season = FactoryGirl.create(:zero_season)
   end
 
+  it { should respond_to(:episodes) }
+  
   it "is valid with valid attributes" do
     expect(@zero_season).to be_valid
+  end
+
+  it "is not valid with another 'no' than 0" do
+    @zero_season.no = 1
+    expect(@zero_season).not_to be_valid
+  end
+
+  it "is not valid without a 'no'" do
+    @zero_season.no = nil
+    expect(@zero_season).not_to be_valid
   end
 end
