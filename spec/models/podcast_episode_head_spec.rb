59741a735dd3c6a070d7a2e8f0adc4c6a0f11a23
ossi <ossi@ossgard.xx>
2015-02-25 21:45:04 +0100

    PodcastEpisodeHead behaves_like a model with disa...

--- spec/models/podcast_episode_head_spec.rb
+++ spec/models/podcast_episode_head_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'shared_examples_for_disambiguations'
 
 RSpec.describe PodcastEpisodeHead, type: :model do
   before(:each) do
@@ -7,5 +8,11 @@ RSpec.describe PodcastEpisodeHead, type: :model do
 
   it "is valid with valid attributes" do
     expect(@pe_head).to be_valid
+  end
+
+  it_behaves_like "a model with disambiguations" do
+    let(:factory) { :podcast_episode_head }
+    let(:object) { @pe_head }
+    let(:naming) { 'title' }
   end
 end
