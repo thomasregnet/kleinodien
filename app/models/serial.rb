83aabfc527da074cf3abb7d85a57e1d91ed61f20
ossi <ossi@ossgard.xx>
2015-03-02 10:09:34 +0100

    Serial has_many :episodes through: :seasons
    Not tested now.

--- app/models/serial.rb
+++ app/models/serial.rb
@@ -2,4 +2,5 @@ class Serial < ActiveRecord::Base
   validates :title, presence: true
   validates_uniqueness_of :title, scope: :disambiguation, case_sensitive: false
   has_many :seasons, inverse_of: :serial
+  has_many :episodes, through: :seasons
 end
