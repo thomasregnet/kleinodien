6a2eca423c27416d56ba49b4e51d5f326a3936d6
ossi <ossi@ossgard.xx>
2015-03-02 10:48:33 +0100

    More specs for EpisodeHead

--- spec/models/episode_head_spec.rb
+++ spec/models/episode_head_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe EpisodeHead, type: :model do
   it "is valid with valid attributes" do
     expect(@episode_head).to be_valid
   end
+
+  it "knows its season" do
+    expect(@episode_head.season).to be_instance_of(Season)
+  end
+
+  it "knows its serial" do
+    expect(@episode_head.serial).to be_instance_of(Serial)
+  end
 end
