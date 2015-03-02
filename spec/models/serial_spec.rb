1b6ac0c4b5462ff9432177964dc3e5c12e4b31d0
ossi <ossi@ossgard.xx>
2015-03-02 10:24:18 +0100

    Specs for Serial has_many :episodes

--- spec/models/serial_spec.rb
+++ spec/models/serial_spec.rb
@@ -2,17 +2,34 @@ require 'rails_helper'
 require 'shared_examples_for_disambiguations'
 
 RSpec.describe Serial, type: :model do
-  before(:each) do
-    @serial = FactoryGirl.create(:serial)
+  context "without seasons" do
+    before(:each) do
+      @serial = FactoryGirl.create(:serial)
+    end
+
+    it "is valid with valid attributes" do
+      expect(@serial).to be_valid
+    end
+
+    it_behaves_like "a model with disambiguations" do
+      let(:factory) { :serial }
+      let(:object) { @serial }
+      let(:naming) { 'title' }
+    end
   end
 
-  it "is valid with valid attributes" do
-    expect(@serial).to be_valid
-  end
+  context "with seasons and episodes" do
+    before(:each) do
+      @season = FactoryGirl.create(:season_with_tv_episode_heads)
+      @serial = @season.serial
+    end
 
-  it_behaves_like "a model with disambiguations" do
-    let(:factory) { :serial }
-    let(:object) { @serial }
-    let(:naming) { 'title' }
-  end  
+    it "has seasons" do
+      expect(@serial.seasons.count).to eq(1)
+    end
+
+    it "has episodes" do
+      expect(@serial.episodes.count).to eq(5)
+    end
+  end
 end
