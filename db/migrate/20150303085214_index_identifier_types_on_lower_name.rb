7c59578163298a2156c8e79982a6ca57a153c9d0
ossi <ossi@ossgard.xx>
2015-03-03 09:58:24 +0100

    New index "index_identifier_types_on_lower_name"
    All tests pass.

---
+++ db/migrate/20150303085214_index_identifier_types_on_lower_name.rb
@@ -0,0 +1,16 @@
+class IndexIdentifierTypesOnLowerName < ActiveRecord::Migration
+  def change
+    reversible do |idx|
+      idx.up do
+        execute <<-DDL
+          CREATE UNIQUE INDEX index_identifier_types_on_lower_name
+            ON identifier_types (LOWER(name));
+        DDL
+      end
+      idx.down do
+        remove_index(:identifier_types,
+                     name: :index_identifier_types_on_lower_name)
+      end
+    end
+  end
+end
