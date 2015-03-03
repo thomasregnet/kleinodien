db05917b773cb8b8b354fd5a11926d61b7064ed7
ossi <ossi@ossgard.xx>
2015-03-03 10:54:58 +0100

    New index on CompilationRelease
    Full name:
    index_compilation_releases_on_compilation_head_id_lower_version

---
+++ db/migrate/20150303094838_index_compilation_releases_on_compilation_head_id_lower_version.rb
@@ -0,0 +1,19 @@
+class IndexCompilationReleasesOnCompilationHeadIdLowerVersion < ActiveRecord::Migration
+  def change
+    reversible do |idx|
+      idx.up do
+        execute <<-DDL
+          CREATE UNIQUE INDEX
+            index_compilation_releases_on_compilation_head_id_lower_version
+              ON compilation_releases (compilation_head_id, LOWER(version));
+        DDL
+      end
+      idx.down do
+        remove_index(
+          :compilation_releases,
+          name: :index_compilation_releases_on_compilation_head_id_lower_version
+        )
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303090246) do
+ActiveRecord::Schema.define(version: 20150303094838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20150303090246) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
   end
+
+  add_index "compilation_releases", ["compilation_head_id"], name: "index_compilation_releases_on_compilation_head_id_lower_version", unique: true, using: :btree
 
   create_table "formats", force: :cascade do |t|
     t.string   "name",        null: false
