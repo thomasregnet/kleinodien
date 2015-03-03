55fb867157f12bc01bcae645d10c098398587215
ossi <ossi@ossgard.xx>
2015-03-03 08:36:42 +0100

    First specs for CompilationHead

--- spec/models/compilation_head_spec.rb
+++ spec/models/compilation_head_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
+require 'shared_examples_for_disambiguations'
 
 RSpec.describe CompilationHead, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @c_head = FactoryGirl.create(:compilation_head)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@c_head).to be_valid
+  end
 end
