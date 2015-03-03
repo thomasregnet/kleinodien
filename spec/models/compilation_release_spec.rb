1fc25a51e64a34fc676aeb093bde4fe7a7115e22
ossi <ossi@ossgard.xx>
2015-03-03 10:23:49 +0100

    CompilationRelease must have a unique head

--- spec/models/compilation_release_spec.rb
+++ spec/models/compilation_release_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe CompilationRelease, type: :model do
   it "is valid with valid attributes" do
     expect(@c_release).to be_valid
   end
+
+  it "has a unique head" do
+    other_release = CompilationRelease.new(head: @c_release.head)
+    expect(other_release).not_to be_valid
+    # TODO: save! without validation
+  end
 end
