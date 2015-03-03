c6a1a4383a50d635c4a7e0a6a3f31be1067f1403
ossi <ossi@ossgard.xx>
2015-03-03 10:36:31 +0100

    CompilationRelease versions must be unique

--- app/models/compilation_release.rb
+++ app/models/compilation_release.rb
@@ -2,5 +2,5 @@ class CompilationRelease < ActiveRecord::Base
   belongs_to(:head,
              class_name: CompilationHead,
              foreign_key: :compilation_head_id)
-  validates_uniqueness_of :head, scope: :version, case_sensitive: false
+  validates_uniqueness_of :version, scope: :head, case_sensitive: false
 end
--- spec/models/compilation_release_spec.rb
+++ spec/models/compilation_release_spec.rb
@@ -9,6 +9,9 @@ RSpec.describe CompilationRelease, type: :model do
     expect(@c_release).to be_valid
   end
 
+  it "is not unique without a head"
+  it "is not unique without a type"
+  
   it "has a unique head" do
     other_release = CompilationRelease.new(head: @c_release.head)
     expect(other_release).not_to be_valid
@@ -19,5 +22,21 @@ RSpec.describe CompilationRelease, type: :model do
     other_release = CompilationRelease.new(head: @c_release.head)
     other_release.version = 'other one'
     expect(other_release).to be_valid
+  end
+
+  it "is not valid with a duplicate head and duplicate version" do
+    @c_release.version = 'version 1'
+    @c_release.save!
+    other_release = CompilationRelease.new(
+      head: @c_release.head, version: @c_release.version)
+    expect(other_release).not_to be_valid
+  end
+
+    it "is not valid with a duplicate head and duplicate upcase version" do
+    @c_release.version = 'version 1'
+    @c_release.save!
+    other_release = CompilationRelease.new(
+      head: @c_release.head, version: @c_release.version.upcase)
+    expect(other_release).not_to be_valid
   end
 end
