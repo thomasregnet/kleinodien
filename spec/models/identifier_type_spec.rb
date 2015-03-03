4dd1eec24308fe84a5472ac159fa7462689375ce
ossi <ossi@ossgard.xx>
2015-03-03 09:50:49 +0100

    Validations and more specs for IdentifierType

--- spec/models/identifier_type_spec.rb
+++ spec/models/identifier_type_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe IdentifierType, type: :model do
   it "is valid with valid attributes" do
     expect(@id_type).to be_valid
   end
+
+  it "is not valid without a name" do
+    @id_type.name = nil
+    expect(@id_type).not_to be_valid
+  end
+
+  it "must have a unique name" do
+    other_id_type = IdentifierType.new(name: @id_type.name)
+    expect(other_id_type).not_to be_valid
+    expect { other_id_type.save! validate: false }.to raise_error
+  end
+
+  it "must have a case insensitive unique name" do
+    other_id_type = IdentifierType.new(name: @id_type.name.upcase)
+    expect(other_id_type).not_to be_valid
+    expect { other_id_type.save! validate: false }.to raise_error    
+  end
 end
