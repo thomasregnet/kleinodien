856cc25ee06c40c2811a2ae1629b00ffd30c4c8b
ossi <ossi@ossgard.xx>
2015-03-04 11:38:26 +0100

    Spec for CompilationSection

--- spec/models/compilation_section_spec.rb
+++ spec/models/compilation_section_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe CompilationSection, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @section = FactoryGirl.create(:compilation_section)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@section).to be_valid
+  end
 end
