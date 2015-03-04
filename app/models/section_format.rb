b9e99d1239b0d46420c37a23ddde51625c312170
ossi <ossi@ossgard.xx>
2015-03-04 11:21:26 +0100

    SectionFormat must have a name and an abbr

--- app/models/section_format.rb
+++ app/models/section_format.rb
@@ -1,2 +1,4 @@
 class SectionFormat < ActiveRecord::Base
+  validates :name, presence: true, blank: false
+  validates :abbr, presence: true, blank: false
 end
--- spec/models/section_format_spec.rb
+++ spec/models/section_format_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe SectionFormat, type: :model do
   it "is valid with valid attributes" do
     expect(@format).to be_valid
   end
+
+  it "is not valid without a name" do
+    @format.name = nil
+    expect(@format).not_to be_valid
+  end
+  
+  it "is not valid withaut an abbr" do
+    @format.abbr = nil
+    expect(@format).not_to be_valid
+  end
 end
