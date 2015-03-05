4d7ac281c9014c1ae8a4b2387a1f4d6bb23b1a4b
ossi <ossi@ossgard.xx>
2015-03-05 11:59:54 +0100

    DiscogsImprter creates a medium
    ...but every time just one!

--- app/models/compilation_release.rb
+++ app/models/compilation_release.rb
@@ -8,5 +8,9 @@ class CompilationRelease < ActiveRecord::Base
   has_many(
     :identifiers,
     class_name: CompilationIdentifier)
+  has_many(
+    :media,
+    class_name: CompilationMedium,
+    foreign_key: :compilation_release_id)
   validates_uniqueness_of :version, scope: :head, case_sensitive: false
 end
