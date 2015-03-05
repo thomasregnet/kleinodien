880a9bcf7dcf46f85c1d132b6091eb9d5e6ce40a
ossi <ossi@ossgard.xx>
2015-03-05 10:44:32 +0100

    DatabaseCleaner.start and clean let test pass

--- spec/models/discogs_importer_spec.rb
+++ spec/models/discogs_importer_spec.rb
@@ -5,11 +5,15 @@ RSpec.describe DiscogsImporter, type: :model do
   describe "import releases" do
     context "import a CD release of one artist"
 
+    DatabaseCleaner.start
+    
     json = DiscogsTestHelper.get_discogs_release_data('940468.json')
     release = DiscogsImporter.import_release(json)
 
     it "has imported the album" do
       expect(release).to be_instance_of(AlbumRelease)
     end
+
+    DatabaseCleaner.clean
   end
 end
