984ffa59173a0fa9cdcfb1f684a3a32f6b98fcb8
ossi <ossi@ossgard.xx>
2015-03-05 11:02:05 +0100

    New model AlbumHead

---
+++ app/models/album_head.rb
@@ -0,0 +1,2 @@
+class AlbumHead < CompilationHead
+end
--- app/models/artist_credit.rb
+++ app/models/artist_credit.rb
@@ -1,6 +1,7 @@
 class ArtistCredit < ActiveRecord::Base
   has_many :artists, through: :participants
   has_many :participants, inverse_of: :artist_credit
+  has_many :compilations, class_name: CompilationHead
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :participants, presence: true
 
