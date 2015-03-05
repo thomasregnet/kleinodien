b801669120a42731bce3688e48fc6a615dea3752
ossi <ossi@ossgard.xx>
2015-03-05 11:36:23 +0100

    DiscogsImporter#import_songs

--- app/models/artist_credit.rb
+++ app/models/artist_credit.rb
@@ -2,6 +2,7 @@ class ArtistCredit < ActiveRecord::Base
   has_many :artists, through: :participants
   has_many :participants, inverse_of: :artist_credit
   has_many :compilations, class_name: CompilationHead
+  has_many :pieces, class_name: PieceHead
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :participants, presence: true
 
