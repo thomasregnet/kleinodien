bccb96d1bdf92405214729a00cf47ac4c55a7ceb
ossi <ossi@ossgard.xx>
2015-03-05 10:25:57 +0100

    Import of artist_credit fails due to foreign keys

--- app/models/artist_credit.rb
+++ app/models/artist_credit.rb
@@ -3,4 +3,15 @@ class ArtistCredit < ActiveRecord::Base
   has_many :participants, inverse_of: :artist_credit
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :participants, presence: true
+
+  before_save { self.name = name }
+  
+  def name
+    names = []
+    participants.each do |p|
+      names << p.artist.name
+      names << p.joinparse unless p.joinparse.blank?
+    end
+    names.join(' ')
+  end
 end
