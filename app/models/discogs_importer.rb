5d3c3aa1fec6797aee5fd50761db7adb23888a40
ossi <ossi@ossgard.xx>
2015-03-05 10:03:31 +0100

    New method DiscogsImpoter#import_artist_credit
    Tests fail because ArtistCredit needs a name, which is not given for now.

--- app/models/discogs_importer.rb
+++ app/models/discogs_importer.rb
@@ -1,5 +1,24 @@
 class DiscogsImporter
   def self.import_release(json)
+    raw_release = JSON.parse(json, symbolize_names: true)
+
+    artist_credit = import_artist_credit(raw_release[:artists])
     AlbumRelease.new
+  end
+
+  def self.import_artist_credit(raw_artists)
+    artist_credit = Artist.new
+
+    raw_artists.each_with_index do |a, no|
+      artist = Artist.find_or_create_by!(name: a[:name])
+      participant = artist_credit.participants.build do |p|
+        p.joinparse = a[:join] unless a[:join].blank?
+        p.no        = no
+        p.artist    = artist
+      end
+    end
+
+    artist_credit.save!
+    artist_credit
   end
 end
