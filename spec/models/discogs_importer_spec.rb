0a369edae95c12518794ea54a347e589413d2ffb
ossi <ossi@ossgard.xx>
2015-03-06 09:52:12 +0100

    Expect imported releases not to be new_record

--- spec/models/discogs_importer_spec.rb
+++ spec/models/discogs_importer_spec.rb
@@ -3,17 +3,19 @@ require 'discogs_test_helper'
 
 RSpec.describe DiscogsImporter, type: :model do
   describe "import releases" do
-    context "import a CD release of one artist"
-    before do
-      json = DiscogsTestHelper.get_discogs_release_data(940468)
-      @release = DiscogsImporter.import_release(json)
+    context "import a CD release of one artist (AC/DC Highway to Hell)" do
+      before do
+        json = DiscogsTestHelper.get_discogs_release_data(940468)
+        @release = DiscogsImporter.import_release(json)
+      end
+
+      it "has imported the album" do
+        expect(@release).to be_instance_of(AlbumRelease)
+        expect(@release).not_to be_new_record
+      end
     end
 
-    it "has imported the album" do
-      expect(@release).to be_instance_of(AlbumRelease)
-    end
-
-    context "import 'Dead Human Collection'" do
+    context "import a Compilation (Cannibal Corpse - Dead Human Collection)" do
       before do
         json = DiscogsTestHelper.get_discogs_release_data(4462260)
         @release = DiscogsImporter.import_release(json)
@@ -21,7 +23,7 @@ RSpec.describe DiscogsImporter, type: :model do
 
       it "has imported the album" do
         expect(@release).to be_instance_of(AlbumRelease)
-        #expect(@require).not_to be_new_record
+        expect(@release).not_to be_new_record
       end
     end
   end
