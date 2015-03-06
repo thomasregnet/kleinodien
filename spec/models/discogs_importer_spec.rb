7f324430147dcd517852ec412c8dc36e6abd1820
ossi <ossi@ossgard.xx>
2015-03-06 08:50:21 +0100

    Removed DatabaseCleaner.start and stop
    ...from discogs_importer_spec.rb. The creation of the test-object is
    now done in a before-block and DatabaseCleaner works as expected.

--- spec/models/discogs_importer_spec.rb
+++ spec/models/discogs_importer_spec.rb
@@ -4,16 +4,13 @@ require 'discogs_test_helper'
 RSpec.describe DiscogsImporter, type: :model do
   describe "import releases" do
     context "import a CD release of one artist"
-
-    DatabaseCleaner.start
-    
-    json = DiscogsTestHelper.get_discogs_release_data('940468.json')
-    release = DiscogsImporter.import_release(json)
-
-    it "has imported the album" do
-      expect(release).to be_instance_of(AlbumRelease)
+    before do
+      json = DiscogsTestHelper.get_discogs_release_data(940468)
+      @release = DiscogsImporter.import_release(json)
     end
 
-    DatabaseCleaner.clean
+    it "has imported the album" do
+      expect(@release).to be_instance_of(AlbumRelease)
+    end
   end
 end
