6f98b21cd28123bbf6ac4b2adb963b686a29236a
ossi <ossi@ossgard.xx>
2015-03-09 11:40:08 +0100

    Tests pass after deleting compilation_sections.no

--- spec/factories/compilation_sections.rb
+++ spec/factories/compilation_sections.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :compilation_section do
     association :format, factory: :section_format
     association :medium, factory: :compilation_medium
-    sequence(:no) { |n| n }
 
     factory :compilation_section_with_side do
       side 'A'
