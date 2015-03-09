7a85c02f6baeb0e3ce2e0865e79b904fa46404ac
ossi <ossi@ossgard.xx>
2015-03-09 12:38:12 +0100

    Specs and factory for AlbumHead

--- spec/factories/compilation_heads.rb
+++ spec/factories/compilation_heads.rb
@@ -2,5 +2,10 @@ FactoryGirl.define do
   factory :compilation_head do
     sequence(:title) { |n| "compilation head ##{n}"}
     type 'CompilationHead'
+
+    factory :album_head, class: AlbumHead do
+      artist_credit
+      type 'AlbumHead'
+    end
   end
 end
