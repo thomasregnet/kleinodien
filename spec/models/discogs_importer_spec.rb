a43c248cfccd555d07d72fe321b92eca80cff9fc
ossi <ossi@ossgard.xx>
2015-03-10 11:45:01 +0100

    replaced before-blocks with let

--- spec/models/discogs_importer_spec.rb
+++ spec/models/discogs_importer_spec.rb
@@ -4,42 +4,35 @@ require 'discogs_test_helper'
 RSpec.describe DiscogsImporter, type: :model do
   describe "import releases" do
     context "import a CD release of one artist (AC/DC Highway to Hell)" do
-      before do
-        json = DiscogsTestHelper.get_discogs_release_data(940468)
-        @release = DiscogsImporter.import_release(json)
-      end
-
+      let(:json) { DiscogsTestHelper.get_discogs_release_data(940468) }
+      let(:release) { DiscogsImporter.import_release(json) }
+      
       it "has imported the album" do
-        expect(@release).to be_instance_of(AlbumRelease)
-        expect(@release).not_to be_new_record
+        expect(release).to be_instance_of(AlbumRelease)
+        expect(release).not_to be_new_record
       end
     end
 
     context "import a Compilation (Cannibal Corpse - Dead Human Collection)" do
-      before do
-        json = DiscogsTestHelper.get_discogs_release_data(4462260)
-        @release = DiscogsImporter.import_release(json)
-      end
+      let(:json) { DiscogsTestHelper.get_discogs_release_data(4462260) }
+      let(:release) { DiscogsImporter.import_release(json) }
 
       it "has imported the album" do
-        expect(@release).to be_instance_of(AlbumRelease)
-        expect(@release).not_to be_new_record
+        expect(release).to be_instance_of(AlbumRelease)
+        expect(release).not_to be_new_record
       end
     end
 
     context "import a double album (Aphrodite's Child - 666)" do
-      before do
-        json = DiscogsTestHelper.get_discogs_release_data(4298844)
-        @release = DiscogsImporter.import_release(json)
+      let(:json) { DiscogsTestHelper.get_discogs_release_data(4298844) }
+      let(:release) { DiscogsImporter.import_release(json) }
+
+      it "has imported the album" do
+        expect(release).not_to be_new_record
       end
 
-      # it "has imported the album" do
-      #   expect(@release).not_to be_new_record
-      # end
-
       it "has the song 'Hit Et Nunc' at medium two side two" do
-        #byebug
-        track = @release.media[1].sections[1].tracks[0]
+        track = release.media[1].sections[1].tracks[0]
         expect(track.release.head.title).to eq('All The Seats Were Occupied')
       end
     end
