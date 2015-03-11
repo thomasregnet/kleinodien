b633efb125128b7938326c57a507a45d9a6792f7
ossi <ossi@ossgard.xx>
2015-03-11 10:55:46 +0100

    ImdbImporter returns a MovieHead with title

--- spec/models/imdb_importer_spec.rb
+++ spec/models/imdb_importer_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 require 'imdb_test_helper'
 
 RSpec.describe ImdbImporter, type: :model do
-  context "Live of Brian" do
+  context "Life of Brian" do
     let(:html) { ImdbTestHelper.get_movie_data('tt0079470.html') }
     let(:movie_head) { ImdbImporter.import_movie(html) }
 
     it "should have the right title" do
-      expect(movie_head.title).to eq('Live Of Brian')
+      expect(movie_head.title).to eq('Life of Brian')
     end
   end
 end
