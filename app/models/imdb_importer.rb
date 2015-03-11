0b527bdffd6cf3c4f6550e6e4e1087741ece389f
ossi <ossi@ossgard.xx>
2015-03-11 12:20:22 +0100

    Refactored ImdbImporter

--- app/models/imdb_importer.rb
+++ app/models/imdb_importer.rb
@@ -3,15 +3,16 @@ require 'cgi'
 class ImdbImporter < ActiveRecord::Base
   def self.import_movie(html)
     doc = Nokogiri::HTML(html)
-    title = doc.at('h1').inner_html.split('<span').first.strip
-    title = CGI.unescapeHTML(title)
-    MovieHead.create!(title: title)
+    MovieHead.create!(title: title(doc))
   end
 
   def self.import_tv_serial(html)
     doc = Nokogiri::HTML(html)
+    TvSerial.create!(title: title(doc))
+  end
+
+  def self.title(doc)
     title = doc.at('h1').inner_html.split('<span').first.strip
     title = CGI.unescapeHTML(title)
-    TvSerial.create!(title: title)
   end
 end
