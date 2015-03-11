046fb6208722b7949a4442d925669a16b175f7bf
ossi <ossi@ossgard.xx>
2015-03-11 10:24:03 +0100

    New method ImdbTestHelper#get_movie_data

--- spec/models/imdb_importer_spec.rb
+++ spec/models/imdb_importer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
+require 'imdb_test_helper'
 
 RSpec.describe ImdbImporter, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
   context "Live of Brian" do
     let(:html) { ImdbTestHelper.get_movie_data('tt0079470.html') }
     let(:movie_head) { ImdbImporter.import_movie(html) }
