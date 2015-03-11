3cb76ce30fa8a5390840302213022d2cc5e0d6d0
ossi <ossi@ossgard.xx>
2015-03-11 09:09:12 +0100

    New method DiscogsTestHelper#import_release
    ...used by discogs_importer_spec.rb.

--- spec/discogs_test_helper.rb
+++ spec/discogs_test_helper.rb
@@ -6,4 +6,13 @@ module DiscogsTestHelper
     file_path = File.join('fixtures', 'discogs', 'releases', name)
     File.open(file_path).read
   end
+
+  def self.import_release(name)
+    if ( name !~ /\.json$/ )
+      name = name.to_s + '.json'
+    end
+    file_path = File.join('fixtures', 'discogs', 'releases', name)
+    json = File.open(file_path).read
+    DiscogsImporter.import_release(json)
+  end
 end
--- spec/models/discogs_importer_spec.rb
+++ spec/models/discogs_importer_spec.rb
@@ -4,9 +4,7 @@ require 'discogs_test_helper'
 RSpec.describe DiscogsImporter, type: :model do
   describe "import releases" do
     context "import a CD release of one artist (AC/DC Highway to Hell)" do
-      let(:json) { DiscogsTestHelper.get_discogs_release_data(940468) }
-      let(:release) { DiscogsImporter.import_release(json) }
-      
+      let(:release) { DiscogsTestHelper.import_release(940468) }
       it "has imported the album" do
         expect(release).to be_instance_of(AlbumRelease)
         expect(release).not_to be_new_record
@@ -14,8 +12,7 @@ RSpec.describe DiscogsImporter, type: :model do
     end
 
     context "import a Compilation (Cannibal Corpse - Dead Human Collection)" do
-      let(:json) { DiscogsTestHelper.get_discogs_release_data(4462260) }
-      let(:release) { DiscogsImporter.import_release(json) }
+      let(:release) { DiscogsTestHelper.import_release(4462260) }
 
       it "has imported the album" do
         expect(release).to be_instance_of(AlbumRelease)
@@ -24,8 +21,7 @@ RSpec.describe DiscogsImporter, type: :model do
     end
 
     context "import a double album (Aphrodite's Child - 666)" do
-      let(:json) { DiscogsTestHelper.get_discogs_release_data(4298844) }
-      let(:release) { DiscogsImporter.import_release(json) }
+      let(:release) { DiscogsTestHelper.import_release(4298844) }
 
       it "has imported the album" do
         expect(release).not_to be_new_record
