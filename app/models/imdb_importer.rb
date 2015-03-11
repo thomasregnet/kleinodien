af56ef7e8035d0b5c2c480a3564d870b5eee67fb
ossi <ossi@ossgard.xx>
2015-03-11 11:31:41 +0100

    ImdbImporter creates a MovieHead

--- app/models/imdb_importer.rb
+++ app/models/imdb_importer.rb
@@ -3,9 +3,8 @@ require 'cgi'
 class ImdbImporter < ActiveRecord::Base
   def self.import_movie(html)
     doc = Nokogiri::HTML(html)
-    #byebug
     title = doc.at('h1').inner_html.split('<span').first.strip
     title = CGI.unescapeHTML(title)
-    movie_head = MovieHead.new(title: title)
+    movie_head = MovieHead.create!(title: title)
   end
 end
