f0fa45e90c2e7dec0caf61a58f1a526ed69003ba
ossi <ossi@ossgard.xx>
2015-03-11 12:08:14 +0100

    Rearranged fixtures/imdb

--- spec/imdb_test_helper.rb
+++ spec/imdb_test_helper.rb
@@ -1,5 +1,5 @@
 module ImdbTestHelper
   def self.get_movie_data(name)
-    File.open(File.join('fixtures', 'imdb', 'movies', name)).read
+    File.open(File.join('fixtures', 'imdb', name)).read
   end
 end
