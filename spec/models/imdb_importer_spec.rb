6c13b44faab1a58a4271a5e5c3fa9db8699c7b09
ossi <ossi@ossgard.xx>
2015-03-11 10:20:35 +0100

    Wrote failing specs for ImdbImporter

--- spec/models/imdb_importer_spec.rb
+++ spec/models/imdb_importer_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ImdbImporter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  context "Live of Brian" do
+    let(:html) { ImdbTestHelper.get_movie_data('tt0079470.html') }
+    let(:movie_head) { ImdbImporter.import_movie(html) }
+
+    it "should have the right title" do
+      expect(movie_head.title).to eq('Live Of Brian')
+    end
+  end
 end
