ad19b88eaa4b0a090d13f8eaceab8c37967a5ed6
ossi <ossi@ossgard.xx>
2015-03-12 09:25:01 +0100

    ImdbImporter creates episodes

--- spec/models/imdb_importer_spec.rb
+++ spec/models/imdb_importer_spec.rb
@@ -27,15 +27,37 @@ RSpec.describe ImdbImporter, type: :model do
       @tv_serial = ImdbImporter.import_tv_serial(html)
 
       season_html = ImdbTestHelper.get_season_data('tt0106179-1.html')
-      ImdbImporter.import_tv_serial_season(@tv_serial, season_html)
+      @season = ImdbImporter.import_tv_serial_season(@tv_serial, 1, season_html)
     end
 
     it "has the right title" do
       expect(@tv_serial.title).to eq('"The X Files"')
     end
+
+    it "has imported the seasons" do
+      expect(@tv_serial.seasons.count).to eq(1)
+    end
+
+    it "has imported the episodes" do
+      expect(@tv_serial.seasons.first.episodes.count).to eq(24)
+    end
+
+    it "has imported the episodes" do
+      expect(@tv_serial.seasons.first.episodes[0].title).to eq('Pilot')
+      expect(@tv_serial.seasons.first.episodes[0].no).to eq(0)
+
+      expect(@tv_serial.seasons.first.episodes[9].title).to eq('Fallen Angel')
+      expect(@tv_serial.seasons.first.episodes[9].no).to eq(9)
+
+      expect(@tv_serial.seasons.first.episodes.last.title).
+        to eq('The Erlenmeyer Flask')
+      expect(@tv_serial.seasons.first.episodes.last.no).to eq(23)
+    end
     
     after(:all) do
-      @tv_serial.delete
+      @season.episodes.each { |e| e.destroy }
+      @season.destroy
+      @tv_serial.destroy
     end
   end
 end
