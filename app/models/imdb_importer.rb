5b354f7009b2e0dccb737e54c07b6b7944a14c45
ossi <ossi@ossgard.xx>
2015-03-12 09:25:01 +0100

    ImdbImporter creates episodes

--- app/models/imdb_importer.rb
+++ app/models/imdb_importer.rb
@@ -11,14 +11,20 @@ class ImdbImporter < ActiveRecord::Base
     TvSerial.create!(title: title(doc))
   end
 
-  def self.import_tv_serial_season(tv_serial, html)
+  def self.import_tv_serial_season(tv_serial, s_no, html)
+    season = tv_serial.seasons.create!(no: s_no, type: 'Season')
     doc = Nokogiri::HTML(html)
     doc.search("div.eplist div[@itemprop*='episode']").each do |div|
       link = div.search("a[@itemprop*='name']").first
       title = link.content.strip
-      no = div.search("meta[@itemprop*='episodeNumber']").first[:content].to_i
-      #byebug
+      e_no = div.search("meta[@itemprop*='episodeNumber']").first[:content].to_i
+      epi = season.episodes.create!(
+        title: title,
+        no: e_no
+      )
+      puts epi
     end
+    season
   end
   
   def self.title(doc)
