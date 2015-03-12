15b80abdccf9efdbcfb6e909f4d68a28f8ac2bbc
ossi <ossi@ossgard.xx>
2015-03-12 10:04:59 +0100

    Before(:all)-blocks for discogs_importer_spec.rb

--- spec/models/discogs_importer_spec.rb
+++ spec/models/discogs_importer_spec.rb
@@ -4,33 +4,49 @@ require 'discogs_test_helper'
 RSpec.describe DiscogsImporter, type: :model do
   describe "import releases" do
     context "import a CD release of one artist (AC/DC Highway to Hell)" do
-      let(:release) { DiscogsTestHelper.import_release(940468) }
-      it "has imported the album" do
-        expect(release).to be_instance_of(AlbumRelease)
-        expect(release).not_to be_new_record
+      before(:all) do
+        DatabaseCleaner.start
+        @release = DiscogsTestHelper.import_release(940468)
       end
+      
+      it "has imported the album" do
+        expect(@release).to be_instance_of(AlbumRelease)
+        expect(@release).not_to be_new_record
+      end
+
+      after(:all) { DatabaseCleaner.clean }
     end
 
     context "import a Compilation (Cannibal Corpse - Dead Human Collection)" do
-      let(:release) { DiscogsTestHelper.import_release(4462260) }
-
-      it "has imported the album" do
-        expect(release).to be_instance_of(AlbumRelease)
-        expect(release).not_to be_new_record
+      before(:all) do
+        DatabaseCleaner.start
+        @release = DiscogsTestHelper.import_release(4462260)
       end
+      
+      it "has imported the album" do
+        expect(@release).to be_instance_of(AlbumRelease)
+        expect(@release).not_to be_new_record
+      end
+
+      after(:all) { DatabaseCleaner.clean }
     end
 
     context "import a double album (Aphrodite's Child - 666)" do
-      let(:release) { DiscogsTestHelper.import_release(4298844) }
+      before(:all) do
+        DatabaseCleaner.start
+        @release = DiscogsTestHelper.import_release(4298844)
+      end
 
       it "has imported the album" do
-        expect(release).not_to be_new_record
+        expect(@release).not_to be_new_record
       end
 
       it "has the song 'Hit Et Nunc' at medium two side two" do
-        track = release.media[1].sections[1].tracks[0]
+        track = @release.media[1].sections[1].tracks[0]
         expect(track.release.head.title).to eq('All The Seats Were Occupied')
       end
+
+      after(:all) { DatabaseCleaner.clean }
     end
   end
 end
