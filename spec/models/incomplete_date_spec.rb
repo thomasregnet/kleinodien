c4f50ae6d3179629bc89ed7949ab23d6b2fe6246
ossi <ossi@ossgard.xx>
2015-03-16 21:09:52 +0100

    Specs and methods for IncompleteDate

--- spec/models/incomplete_date_spec.rb
+++ spec/models/incomplete_date_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe IncompleteDate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "with a complete date" do
+    before(:each) do
+      @idate = IncompleteDate.new('2015-03-16')
+    end
+
+    it "returns a date-object" do
+      expect(@idate.date).to be_instance_of(Date)
+    end
+
+    it "returns 7 as mask" do
+      expect(@idate.mask).to eq(7)
+    end
+  end
+
+  context "with a incomplete date" do
+  end
 end
