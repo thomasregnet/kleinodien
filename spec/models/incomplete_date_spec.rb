63c362e047c0a814c488cce8cc69786966c23538
ossi <ossi@ossgard.xx>
2015-03-16 21:26:45 +0100

    Context "with a number" for IncompleteDate

--- spec/models/incomplete_date_spec.rb
+++ spec/models/incomplete_date_spec.rb
@@ -14,6 +14,25 @@ RSpec.describe IncompleteDate, type: :model do
       expect(@idate.mask).to eq(7)
     end
   end
+
+  context "with a number" do
+    before(:each) do
+      @idate = IncompleteDate.new(2015)
+    end
+
+    it "returns a date object" do
+      expect(@idate.date).to be_instance_of(Date)
+    end
+
+    it "returns 4 as mask" do
+      expect(@idate.mask).to eq(4)
+    end
+    
+    it "has set month and day to 1" do
+      expect(@idate.date.month).to eq(1)
+      expect(@idate.date.day).to eq(1)
+    end
+  end
   
   context "with a complete date-string" do
     before(:each) do
