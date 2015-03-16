a4e28613515e2ca1711fe3812bef4db38fc81915
ossi <ossi@ossgard.xx>
2015-03-16 21:09:52 +0100

    Specs and methods for IncompleteDate

--- app/models/incomplete_date.rb
+++ app/models/incomplete_date.rb
@@ -1,2 +1,44 @@
 class IncompleteDate
+  attr_reader :date, :mask
+
+  def initialize(date, mask = nil)
+    init_date(date)
+    init_mask(date)
+  end 
+
+  def date_from_string(string)
+    return Date.iso8601(string) if /^\d\d\d\d-\d\d-\d\d$/.match(string)
+    return Date.iso8601(string + '-01') if /^\d\d\d\d-\d\d$/.match(string)
+    return Date.iso8601(string + '-01-01') if /^\d\d\d\d$/.match(string)
+    nil
+  end
+
+  def mask_from_string(string)
+    return 7 if /\d\d\d\d-\d\d-\d\d/.match(string)
+    return 6 if /^\d\d\d\d-\d\d$/.match(string)
+    return 4 if /^\d\d\d\d$/.match(string)
+    nil
+  end
+    
+  private
+
+  def init_date(date)
+    if date.class == Date
+      @date = date
+    elsif date.class == Fixnum
+      @date = Date.new(date, 1, 1)
+    elsif date.class == String
+      @date = date_from_string(date)
+    end
+  end
+
+  def init_mask(date)
+    if date.class == Date
+      @mask = 7
+    elsif date.class == Fixnum
+      @mask = 4
+    elsif date.class == String
+      @mask = mask_from_string(date)
+    end
+  end
 end
--- spec/factories/incomplete_dates.rb
+++ spec/factories/incomplete_dates.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :incomplete_date do
-    
+    date '2015-03-16'
   end
-
 end
