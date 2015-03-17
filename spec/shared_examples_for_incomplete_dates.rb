0d94690b23cefacd40b3177c76c3c0be4005dd43
ossi <ossi@ossgard.xx>
2015-03-17 21:18:36 +0100

    Refactored shared_examples_for_incomplete_dates
    Now working with send to date_getter and date_setter.
    Piece_release_spec.rb fails

--- spec/shared_examples_for_incomplete_dates.rb
+++ spec/shared_examples_for_incomplete_dates.rb
@@ -1,34 +1,36 @@
 RSpec.shared_examples "a model with an IncompleteDate" do
+  let(:date_getter) { date_naming }
+  let(:date_setter) { date_naming + '=' }
+  
   before(:each) do
     @object = FactoryGirl.create(factory)
   end
 
   it "works with a Fixnum as IncompleteDate" do
-    idate = IncompleteDate.new(2015)
-    @object.date = idate
-    expect(@object.date.to_s).to eq('2015-01-01')
-    expect(@object.date.mask).to eq(4)
+    @object.send(date_setter, IncompleteDate.new(2015))
+    expect(@object.send(date_getter).to_s).to eq('2015-01-01')
+    expect(@object.send(date_getter).mask).to eq(4)
     expect(@object.save).to be true
   end
 
   it "works with a String representing only a year" do
     @object.date = IncompleteDate.new('2015')
-    expect(@object.date.to_s).to eq('2015-01-01')
-    expect(@object.date.mask).to eq(4)
+    expect(@object.send(date_getter).to_s).to eq('2015-01-01')
+    expect(@object.send(date_getter).mask).to eq(4)
     expect(@object.save).to be true
   end
   
   it "works with a String representing only year and month" do
     @object.date = IncompleteDate.new('2015-03')
-    expect(@object.date.to_s).to eq('2015-03-01')
-    expect(@object.date.mask).to eq(6)
+    expect(@object.send(date_getter).to_s).to eq('2015-03-01')
+    expect(@object.send(date_getter).mask).to eq(6)
     expect(@object.save).to be true
   end
 
   it "works with a String representing a iso-date" do
     @object.date = IncompleteDate.new('2015-03-13')
-    expect(@object.date.to_s).to eq('2015-03-13')
-    expect(@object.date.mask).to eq(7)
+    expect(@object.send(date_getter).to_s).to eq('2015-03-13')
+    expect(@object.send(date_getter).mask).to eq(7)
     expect(@object.save).to be true
   end
 end
