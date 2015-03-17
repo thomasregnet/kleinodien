5b8dcf954dfb1fb80a761b405c1dca797406fbf4
ossi <ossi@ossgard.xx>
2015-03-17 20:49:52 +0100

    shared_examples_for_incomplete_dates saves objects

--- spec/shared_examples_for_incomplete_dates.rb
+++ spec/shared_examples_for_incomplete_dates.rb
@@ -8,23 +8,27 @@ RSpec.shared_examples "a model with an IncompleteDate" do
     @object.date = idate
     expect(@object.date.to_s).to eq('2015-01-01')
     expect(@object.date.mask).to eq(4)
+    expect(@object.save).to be true
   end
 
   it "works with a String representing only a year" do
     @object.date = IncompleteDate.new('2015')
     expect(@object.date.to_s).to eq('2015-01-01')
     expect(@object.date.mask).to eq(4)
+    expect(@object.save).to be true
   end
   
   it "works with a String representing only year and month" do
     @object.date = IncompleteDate.new('2015-03')
     expect(@object.date.to_s).to eq('2015-03-01')
     expect(@object.date.mask).to eq(6)
+    expect(@object.save).to be true
   end
 
   it "works with a String representing a iso-date" do
     @object.date = IncompleteDate.new('2015-03-13')
     expect(@object.date.to_s).to eq('2015-03-13')
     expect(@object.date.mask).to eq(7)
+    expect(@object.save).to be true
   end
 end
