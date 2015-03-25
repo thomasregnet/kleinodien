42d9863afae01d45ca89bec5ca40277cf9d9e28d
ossi <ossi@ossgard.xx>
2015-03-25 21:09:22 +0100

    Associations for CrFormat

--- app/models/cr_format.rb
+++ app/models/cr_format.rb
@@ -1,2 +1,10 @@
 class CrFormat < ActiveRecord::Base
+  belongs_to(
+    :release,
+    class_name: CompilationRelease,
+    foreign_key: :compilation_release_id)
+  belongs_to(
+    :format_kind,
+    class_name: FormatKind,
+    foreign_key: :format_kind_id)
 end
