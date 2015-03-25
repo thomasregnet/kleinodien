9d9fdb8ee63541daf2209061b8ce6bffcb356cc4
ossi <ossi@ossgard.xx>
2015-03-25 20:47:51 +0100

    FormatKind validates_uniqueness_of name
    ... in scope "type".

--- app/models/format_kind.rb
+++ app/models/format_kind.rb
@@ -1,4 +1,5 @@
 class FormatKind < ActiveRecord::Base
   validates :name, presence: true, blank: false
   validates :type, presence: true, blank: false
+  validates_uniqueness_of :name, scope: 'type', case_sensitive: false
 end
