76c7698ae3412850069b755dd219602083e53bf8
ossi <ossi@ossgard.xx>
2015-03-25 20:22:55 +0100

    Modified FormatKind migration and ran it
    name, null:false
    type, null:false

--- db/migrate/20150325192035_create_format_kinds.rb
+++ db/migrate/20150325192035_create_format_kinds.rb
@@ -1,10 +1,10 @@
 class CreateFormatKinds < ActiveRecord::Migration
   def change
     create_table :format_kinds do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :abbr
       t.string :note
-      t.string :type
+      t.string :type, null: false
 
       t.timestamps null: false
     end
