78afd3bb699f02f8aff19be58d6cce048a662186
ossi <ossi@ossgard.xx>
2015-03-25 21:12:12 +0100

    Unfulfilled specs for CrFormat

--- spec/models/cr_format_spec.rb
+++ spec/models/cr_format_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe CrFormat, type: :model do
   it "is valid with valid attributes" do
     expect(@cr_format).to be_valid
   end
+
+  it "is not valid without a release"
+  it "is not valid without a format_kind"
+  it "is not valid without a quantity"
+  it "is not valid without a no"
+
+  it "must have a unique combination of release and no"
 end
