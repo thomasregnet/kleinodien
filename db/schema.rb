c63ee91cd9e28561663265ae02fd9034b66f80d5
ossi <ossi@ossgard.xx>
2015-03-25 20:33:10 +0100

    New: "index_format_kinds_on_lower_name_and_type"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325192035) do
+ActiveRecord::Schema.define(version: 20150325192619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 20150325192035) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "format_kinds", ["type"], name: "index_format_kinds_on_lower_name_and_type", unique: true, using: :btree
 
   create_table "formats", force: :cascade do |t|
     t.string   "name",        null: false
