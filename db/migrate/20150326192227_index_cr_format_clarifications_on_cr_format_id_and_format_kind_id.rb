e37d765390718a1611e1348b8bb9a07f8681d4d5
ossi <ossi@ossgard.xx>
2015-03-26 20:25:46 +0100

    Added unique index to cr_format_clarifications
    ...on cr_format_id and no

--- db/migrate/20150326192227_index_cr_format_clarifications_on_cr_format_id_and_format_kind_id.rb
+++ db/migrate/20150326192227_index_cr_format_clarifications_on_cr_format_id_and_format_kind_id.rb
@@ -1,4 +1,7 @@
 class IndexCrFormatClarificationsOnCrFormatIdAndFormatKindId < ActiveRecord::Migration
   def change
+    add_index(
+      :cr_format_clarifications, [:cr_format_id, :no],  unique: true
+    )
   end
 end
