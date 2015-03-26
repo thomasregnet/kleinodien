d0e30d74c2094476dc6968ce455fca48a571c79f
ossi <ossi@ossgard.xx>
2015-03-26 20:34:41 +0100

    CrFormatClarification needs unique format and no

--- app/models/cr_format_clarification.rb
+++ app/models/cr_format_clarification.rb
@@ -2,7 +2,7 @@ class CrFormatClarification < ActiveRecord::Base
   validates :no, presence: true
   validates :format, presence: true
   validates :format_kind, presence: true
-
+  validates_uniqueness_of :format, scope: :no
   belongs_to :format, class_name: CrFormat, foreign_key: :cr_format_id
   belongs_to :format_kind
 end
