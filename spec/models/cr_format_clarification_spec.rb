62753719b233287ab5e39e47dbc1563b0bc90b36
ossi <ossi@ossgard.xx>
2015-03-26 20:17:34 +0100

    Validations for CrFormatClarification

--- spec/models/cr_format_clarification_spec.rb
+++ spec/models/cr_format_clarification_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe CrFormatClarification, type: :model do
   it "is valid with valid attributes" do
     expect(@clarification).to be_valid
   end
+
+  it "is not valid without a format" do
+    @clarification.format = nil
+    expect(@clarification).not_to be_valid
+  end
+  
+  it "is not valid without a format_kind" do
+    @clarification.format_kind = nil
+    expect(@clarification).not_to be_valid
+  end
+  
+  it "is not valid without a no" do
+    @clarification.no = nil
+    expect(@clarification).not_to be_valid
+  end
 end
