a566324b6b72a089e1ebd75aa3c7164004a5d27f
ossi <ossi@ossgard.xx>
2015-03-26 20:17:34 +0100

    Validations for CrFormatClarification

--- app/models/cr_format_clarification.rb
+++ app/models/cr_format_clarification.rb
@@ -1,4 +1,8 @@
 class CrFormatClarification < ActiveRecord::Base
+  validates :no, presence: true
+  validates :format, presence: true
+  validates :format_kind, presence: true
+
   belongs_to :format, class_name: CrFormat, foreign_key: :cr_format_id
   belongs_to :format_kind
 end
