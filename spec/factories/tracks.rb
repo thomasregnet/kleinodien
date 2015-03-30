586ada965ce76c35e8dc565c8b51cc37475b1a2e
ossi <ossi@ossgard.xx>
2015-03-30 21:25:53 +0200

    Track belongs_to Compilation

--- spec/factories/tracks.rb
+++ spec/factories/tracks.rb
@@ -3,8 +3,11 @@ FactoryGirl.define do
     association :release, factory: :piece_release
     format
 
-    factory :track_with_section do
-      association :section, factory: :compilation_section
+    factory :track_with_compilation_release do
+      association :compilation, factory: :compilation_release
     end
+    # factory :track_with_section do
+    #   association :section, factory: :compilation_section
+    # end
   end
 end
