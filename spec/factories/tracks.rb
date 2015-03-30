0235e6f7210835876815610471fbf42ec5e502fc
ossi <ossi@ossgard.xx>
2015-03-30 21:26:50 +0200

    Cleanup in Track and its spec

--- spec/factories/tracks.rb
+++ spec/factories/tracks.rb
@@ -6,8 +6,5 @@ FactoryGirl.define do
     factory :track_with_compilation_release do
       association :compilation, factory: :compilation_release
     end
-    # factory :track_with_section do
-    #   association :section, factory: :compilation_section
-    # end
   end
 end
