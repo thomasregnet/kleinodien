b94ac833da1e40ea2efa2bcd74a739ee4d65e5d0
ossi <ossi@ossgard.xx>
2015-03-31 21:44:08 +0200

    TrackHeading#heading must be present

--- spec/models/track_heading_spec.rb
+++ spec/models/track_heading_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe TrackHeading, type: :model do
   it "is valid with valid attributes" do
     expect(@heading).to be_valid
   end
+
+  it "is not valid without a heading" do
+    @heading.heading = ''
+    expect(@heading).not_to be_valid
+
+    @heading.heading = nil
+    expect(@heading).not_to be_valid
+  end
 end
