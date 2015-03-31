c21331ce669d56aacb7414ad599f1bc071cd4c7e
ossi <ossi@ossgard.xx>
2015-03-31 21:33:04 +0200

    Specs and factory for TrackHeading

--- spec/factories/track_headings.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :track_heading do
-    
-  end
-
-end
--- spec/factories/tracks.rb
+++ spec/factories/tracks.rb
@@ -2,6 +2,10 @@ FactoryGirl.define do
   factory :track do
     association :release, factory: :piece_release
 
+    factory :track_heading, class: TrackHeading do
+      association :compilation, factory: :compilation_release
+    end
+    
     factory :track_with_compilation_release do
       association :compilation, factory: :compilation_release
     end
