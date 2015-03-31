e9808a6a62030f19d5e4a4f6421303e863f61142
ossi <ossi@ossgard.xx>
2015-03-31 20:23:23 +0200

    Track_spec works after removing format relations

--- spec/factories/tracks.rb
+++ spec/factories/tracks.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :track do
     association :release, factory: :piece_release
-    format
 
     factory :track_with_compilation_release do
       association :compilation, factory: :compilation_release
