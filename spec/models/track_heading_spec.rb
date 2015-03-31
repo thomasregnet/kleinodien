b343d56ba934867fe6c4bd105e65e9638ef9fd89
ossi <ossi@ossgard.xx>
2015-03-31 21:33:04 +0200

    Specs and factory for TrackHeading

--- spec/models/track_heading_spec.rb
+++ spec/models/track_heading_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe TrackHeading, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @heading = FactoryGirl.create(:track_heading)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@heading).to be_valid
+  end
 end
