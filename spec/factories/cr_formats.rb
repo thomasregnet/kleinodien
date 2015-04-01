88abdb36e19de4aa222af840d2945477b6566cbd
ossi <ossi@ossgard.xx>
2015-04-01 20:50:35 +0200

    Cr_format_spec passes

--- spec/factories/cr_formats.rb
+++ spec/factories/cr_formats.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :cr_format do
     association :release, factory: :compilation_release
-    association :format_kind, factory: :format_kind
+    association :format, factory: :format
     quantity 1
     no 1
 
