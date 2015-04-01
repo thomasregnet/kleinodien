db8b1c029fce85cd80b7b3681b3338599fb987bc
ossi <ossi@ossgard.xx>
2015-04-01 20:17:56 +0200

    Dropped table "formats", some tests fail
    /spec/models/format_spec.rb fails.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331192611) do
+ActiveRecord::Schema.define(version: 20150401181529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,14 +94,6 @@ ActiveRecord::Schema.define(version: 20150331192611) do
   end
 
   add_index "format_kinds", ["type"], name: "index_format_kinds_on_lower_name_and_type", unique: true, using: :btree
-
-  create_table "formats", force: :cascade do |t|
-    t.string   "name",        null: false
-    t.string   "abbr"
-    t.string   "explanation"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
 
   create_table "identifier_types", force: :cascade do |t|
     t.string   "name",        null: false
