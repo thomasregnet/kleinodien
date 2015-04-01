ef4bdae6c53284da2f4cf6fc9b4ee4c108a3b43d
ossi <ossi@ossgard.xx>
2015-04-01 21:32:20 +0200

    Renamed cr_format_clarification.format_kind_id
    ...to format_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401191204) do
+ActiveRecord::Schema.define(version: 20150401192652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,11 +64,11 @@ ActiveRecord::Schema.define(version: 20150401191204) do
   add_index "compilation_releases", ["compilation_head_id"], name: "index_compilation_releases_on_compilation_head_id_lower_version", unique: true, using: :btree
 
   create_table "cr_format_clarifications", force: :cascade do |t|
-    t.integer  "cr_format_id",   null: false
-    t.integer  "format_kind_id", null: false
-    t.integer  "no",             null: false
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.integer  "cr_format_id", null: false
+    t.integer  "format_id",    null: false
+    t.integer  "no",           null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   add_index "cr_format_clarifications", ["cr_format_id", "no"], name: "index_cr_format_clarifications_on_cr_format_id_and_no", unique: true, using: :btree
