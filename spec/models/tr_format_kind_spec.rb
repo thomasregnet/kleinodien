b7e4cdc6940b127b32bfc08c79aede007ecabd29
ossi <ossi@ossgard.xx>
2015-04-08 21:35:13 +0200

    TrFormatKind behaves like a format kind

--- spec/models/tr_format_kind_spec.rb
+++ spec/models/tr_format_kind_spec.rb
@@ -1,28 +1,8 @@
 require 'rails_helper'
+require 'shared_examples_for_format_kinds'
 
 RSpec.describe TrFormatKind, type: :model do
-  before(:each) do
-    @trfk = FactoryGirl.create(:tr_format_kind)
-  end
-
-  it "is valid with valid attributes" do
-    expect(@trfk).to be_valid
-  end
-
-  it "is not valid without a name" do
-    @trfk.name = ''
-    expect(@trfk).not_to be_valid
-    
-    @trfk.name = nil
-    expect(@trfk).not_to be_valid
-  end
-
-  it "must have a unique name" do
-    clone = TrFormatKind.new(name: @trfk.name)
-    expect(clone).not_to be_valid
-
-    clone = TrFormatKind.new(name: @trfk.name.upcase)
-    expect(clone).not_to be_valid
-    expect { clone.save! validate: false }.to raise_error
+  it_behaves_like "a format kind" do
+    let(:factory) { :tr_format_kind }
   end
 end
---
+++ spec/shared_examples_for_format_kinds.rb
@@ -0,0 +1,28 @@
+RSpec.shared_examples "a format kind" do
+  before(:each) do
+    @format_kind = FactoryGirl.create(factory)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@format_kind).to be_valid
+  end
+
+  it "is not valid without a name" do
+    @format_kind.name = ''
+    expect(@format_kind).not_to be_valid
+    
+    @format_kind.name = nil
+    expect(@format_kind).not_to be_valid
+  end
+
+  it "must have a unique name" do
+    clone = FactoryGirl.create(factory)
+    clone.name = @format_kind.name
+    expect(clone).not_to be_valid
+
+    clone = FactoryGirl.create(factory)    
+    clone.name = @format_kind.name.upcase
+    expect(clone).not_to be_valid
+    expect { clone.save! validate: false }.to raise_error
+  end
+end  
