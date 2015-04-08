8c2d6113de62a890c518c6ad6144ead6b66194e4
ossi <ossi@ossgard.xx>
2015-04-08 20:27:50 +0200

    Spec and factory for TrFormatKind

--- spec/models/tr_format_kind_spec.rb
+++ spec/models/tr_format_kind_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TrFormatKind, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @trfk = FactoryGirl.create(:tr_format_kind)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@trfk).to be_valid
+  end
 end
