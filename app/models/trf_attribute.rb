bace812c3d06709414a6327930438c06c12eea8c
ossi <ossi@ossgard.xx>
2015-04-08 22:01:20 +0200

    Associations for TrfAttribute

--- app/models/trf_attribute.rb
+++ app/models/trf_attribute.rb
@@ -1,2 +1,8 @@
 class TrfAttribute < ActiveRecord::Base
+  belongs_to :track
+  belongs_to(
+    :kind,
+    class_name: TrfAttributeKind,
+    foreign_key: :trf_attribute_kind_id
+  )
 end
--- spec/models/trf_attribute_spec.rb
+++ spec/models/trf_attribute_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe TrfAttribute, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = FactoryGirl.create(:trf_attribute)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@attr).to be_valid
+  end
+
+  it "is not valid without a track"
+  it "is not valid without a kind"
+  it "is not valid without a no"
 end
