22a18dc98cf5f70fb2fdfba493ca6d66b7d62137
ossi <ossi@ossgard.xx>
2015-04-08 21:44:54 +0200

    Added index to pass trf_attribute_kinds_spec but..
    ... it fails due to a bad factory

---
+++ db/migrate/20150408194132_index_trf_attribute_kinds_on_lower_name.rb
@@ -0,0 +1,18 @@
+class IndexTrfAttributeKindsOnLowerName < ActiveRecord::Migration
+  def change
+    reversible do |idx|
+      idx.up do
+        execute <<-DDL
+          CREATE UNIQUE INDEX index_trf_attribute_kinds_on_lower_name
+            ON trf_attribute_kinds (LOWER(name));
+        DDL
+      end
+      idx.down do
+        remove_index(
+          :trf_attribute_kinds,
+          name: :index_trf_attribute_kinds_on_lower_name
+        )
+      end
+    end
+  end
+end
