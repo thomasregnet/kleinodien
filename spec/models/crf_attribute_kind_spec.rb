f507a9df4b4e894412fc53a3b20184a0fbb0d665
ossi <ossi@ossgard.xx>
2015-04-09 21:19:07 +0200

    Failing specs and factory for CrfAttributeKinds

--- spec/models/crf_attribute_kind_spec.rb
+++ spec/models/crf_attribute_kind_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
+require 'shared_examples_for_format_kinds'
 
 RSpec.describe CrfAttributeKind, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like "a format kind" do
+    let(:factory) { :crf_attribute_kind }
+  end
 end
