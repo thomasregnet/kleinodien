1323c19dfc111e1a088fa7f96258a9fbb66fd4fb
ossi <ossi@ossgard.xx>
2015-04-09 20:33:08 +0200

    Failing specs and factory for CrFormatKind

--- spec/factories/cr_format_kinds.rb
+++ spec/factories/cr_format_kinds.rb
@@ -1,8 +1,5 @@
 FactoryGirl.define do
   factory :cr_format_kind do
-    name "MyString"
-abbr "MyString"
-note "MyString"
+    sequence(:name) { |n| "compilation release format kind ##[n]" }
   end
-
 end
