963d7be3589f5c4bbdffd23b86ed9f7a77889b9e
ossi <ossi@ossgard.xx>
2015-04-09 21:54:27 +0200

    Added NOT NULL constraints for crf_attributes

---
+++ db/migrate/20150409195031_not_null_constraints_for_crf_attributes.rb
@@ -0,0 +1,22 @@
+class NotNullConstraintsForCrfAttributes < ActiveRecord::Migration
+  def change
+    change_column(
+      :crf_attributes,
+      :cr_format_id,
+      :integer,
+      null: false
+    )
+    change_column(
+      :crf_attributes,
+      :crf_attribute_kind_id,
+      :integer,
+      null: false
+    )
+    change_column(
+      :crf_attributes,
+      :no,
+      :integer,
+      null: false
+    )
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409193705) do
+ActiveRecord::Schema.define(version: 20150409195031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,9 +101,9 @@ ActiveRecord::Schema.define(version: 20150409193705) do
   end
 
   create_table "crf_attributes", force: :cascade do |t|
-    t.integer  "cr_format_id"
-    t.integer  "crf_attribute_kind_id"
-    t.integer  "no"
+    t.integer  "cr_format_id",          null: false
+    t.integer  "crf_attribute_kind_id", null: false
+    t.integer  "no",                    null: false
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
   end
