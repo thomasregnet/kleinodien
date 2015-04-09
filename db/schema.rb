785f99ca17a746e4e6d346976c46160849b7f08f
ossi <ossi@ossgard.xx>
2015-04-09 20:10:26 +0200

    Added "index_trf_attributes_on_track_id_and_no"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408195157) do
+ActiveRecord::Schema.define(version: 20150409180730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -208,6 +208,8 @@ ActiveRecord::Schema.define(version: 20150408195157) do
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
   end
+
+  add_index "trf_attributes", ["track_id", "no"], name: "index_trf_attributes_on_track_id_and_no", unique: true, using: :btree
 
   add_foreign_key "participants", "artist_credits", name: "participants_fk_artist_credits"
   add_foreign_key "participants", "artists", name: "participants_fk_artists"
