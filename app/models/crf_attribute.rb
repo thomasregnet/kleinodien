bb6508749779986ee666e92cc3d764f2d2b03c03
ossi <ossi@ossgard.xx>
2015-04-09 21:49:40 +0200

    Failing specs and factory for CrfAttribute

--- app/models/crf_attribute.rb
+++ app/models/crf_attribute.rb
@@ -1,2 +1,15 @@
 class CrfAttribute < ActiveRecord::Base
+  belongs_to(
+    :format,
+    class_name: CrFormat,
+    foreign_key: :cr_format_id
+  )
+  belongs_to(
+    :kind,
+    class_name: CrfAttributeKind,
+    foreign_key: :crf_attribute_kind_id
+  )
+  validates :format, presence: true
+  validates :kind,   presence: true
+  validates :no,     presence: true
 end
