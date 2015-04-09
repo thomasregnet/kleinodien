8fa8f1cabcf202c2d9f78dc64c3738e510f87871
ossi <ossi@ossgard.xx>
2015-04-09 20:48:31 +0200

    Renamed cr_formats.format_id to cr_format_kind_id
    Many tests fail.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409183650) do
+ActiveRecord::Schema.define(version: 20150409184600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 20150409183650) do
 
   create_table "cr_formats", force: :cascade do |t|
     t.integer  "compilation_release_id", null: false
-    t.integer  "format_id",              null: false
+    t.integer  "cr_format_kind_id",      null: false
     t.integer  "quantity",               null: false
     t.integer  "no",                     null: false
     t.datetime "created_at",             null: false
