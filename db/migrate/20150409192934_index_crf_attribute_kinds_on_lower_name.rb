67dd3ecd09954141eb829b6017b038f4a14c36be
ossi <ossi@ossgard.xx>
2015-04-09 21:32:41 +0200

    Added unique index on crf_attribute_kinds.name
    All tests pass.

---
+++ db/migrate/20150409192934_index_crf_attribute_kinds_on_lower_name.rb
@@ -0,0 +1,18 @@
+class IndexCrfAttributeKindsOnLowerName < ActiveRecord::Migration
+  def change
+     reversible do |idx|
+      idx.up do
+        execute <<-DDL
+          CREATE UNIQUE INDEX index_crf_attribute_kinds_on_lower_name
+            ON crf_attribute_kinds (LOWER(name));
+        DDL
+      end
+      idx.down do
+        remove_index(
+          :crf_attribute_kinds,
+          name: :index_crf_attribute_kinds_on_lower_name
+        )
+      end
+    end   
+  end
+end
