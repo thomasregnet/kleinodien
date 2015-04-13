3379ed92505d72d9aaeaa38b53c36f3cb9b98b95
ossi <ossi@ossgard.xx>
2015-04-13 19:37:13 +0200

    Tests pass after dropping cr_format_clarifications

--- spec/factories/cr_formats.rb
+++ spec/factories/cr_formats.rb
@@ -5,20 +5,6 @@ FactoryGirl.define do
     quantity 1
     no 1
 
-    factory :cr_format_with_clarifications do
-      transient do
-        clarifications_count 3
-      end
-
-      after(:create) do |cr_format, evaluator|
-        create_list(
-          :cr_format_clarification,
-          evaluator.clarifications_count,
-          format: cr_format
-        )
-      end
-    end
-
     factory :cr_format_with_format_attributes do
       transient do
         attributes_count 3
