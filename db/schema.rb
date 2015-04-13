2e3540b0607901fe3c34fdcc3e8c67a56d25b8d0
ossi <ossi@ossgard.xx>
2015-04-13 21:02:32 +0200

    Renamed identifier_types.explanation to note

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413183115) do
+ActiveRecord::Schema.define(version: 20150413190041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,10 +113,10 @@ ActiveRecord::Schema.define(version: 20150413183115) do
   add_index "formats", ["type"], name: "index_format_kinds_on_lower_name_and_type", unique: true, using: :btree
 
   create_table "identifier_types", force: :cascade do |t|
-    t.string   "name",        null: false
-    t.string   "explanation"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "name",       null: false
+    t.string   "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "participants", force: :cascade do |t|
