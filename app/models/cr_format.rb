16745985674b9cbabfc9a8a67657c8d65cda18c4
ossi <ossi@ossgard.xx>
2015-04-13 19:18:23 +0200

    Cleanup in CrFormat

--- app/models/cr_format.rb
+++ app/models/cr_format.rb
@@ -8,9 +8,17 @@ class CrFormat < ActiveRecord::Base
     :release,
     class_name: CompilationRelease,
     foreign_key: :compilation_release_id)
-  #belongs_to :format
-  belongs_to :kind, class_name: CrFormatKind, foreign_key: :cr_format_kind_id
-  
-  has_many(:clarifications, class_name: CrFormatClarification)
-  has_many(:format_attributes, class_name: CrfAttribute)
+  belongs_to(
+    :kind,
+    class_name: CrFormatKind,
+    foreign_key: :cr_format_kind_id
+  )
+  has_many(
+    :clarifications,
+    class_name: CrFormatClarification
+  )
+  has_many(
+    :format_attributes,
+    class_name: CrfAttribute
+  )
 end
