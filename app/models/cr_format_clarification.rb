1a305820e82f8d8a92187555824f85cc2b082e6c
ossi <ossi@ossgard.xx>
2015-04-13 19:29:00 +0200

    Removed CrFormatClarification files

--- app/models/cr_format_clarification.rb
+++
@@ -1,8 +0,0 @@
-class CrFormatClarification < ActiveRecord::Base
-  validates :no, presence: true
-  validates :format, presence: true
-  validates :kind, presence: true
-  validates_uniqueness_of :format, scope: :no
-  belongs_to :format, class_name: CrFormat, foreign_key: :cr_format_id
-  belongs_to :kind, class_name: Format, foreign_key: :format_id
-end
--- spec/factories/cr_format_clarifications.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :cr_format_clarification do
-    association :format, factory: :cr_format
-    association :kind, factory: :format
-    sequence(:no) { |n| n }
-  end
-end
