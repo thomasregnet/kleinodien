619b41503e01d1a27c48ad4b0a70f1d8a2367ff0
ossi <ossi@ossgard.xx>
2015-04-14 19:47:33 +0200

    Drop title & disambiguation indexes on pice_heads
    * index_piece_heads_on_lower_title
    * index_piece_heads_on_lower_title_disambiguation

---
+++ db/migrate/20150414174328_remove_disambiguation_indices_on_piece_head.rb
@@ -0,0 +1,12 @@
+class RemoveDisambiguationIndicesOnPieceHead < ActiveRecord::Migration
+  def change
+    remove_index(
+      :piece_heads,
+      name: :index_piece_heads_on_lower_title
+    )
+    remove_index(
+      :piece_heads,
+      name: :index_piece_heads_on_lower_title_disambiguation
+    )
+  end
+end
