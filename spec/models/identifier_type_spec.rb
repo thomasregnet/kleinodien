6cec4fc08012f19fcf401ece5b42a5b9369310c4
ossi <ossi@ossgard.xx>
2015-09-14 10:57:17 +0200

    No more warnings in identifier_type_spec.rb

--- spec/models/identifier_type_spec.rb
+++ spec/models/identifier_type_spec.rb
@@ -17,12 +17,14 @@ RSpec.describe IdentifierType, type: :model do
   it "must have a unique name" do
     other_id_type = IdentifierType.new(name: @id_type.name)
     expect(other_id_type).not_to be_valid
-    expect { other_id_type.save! validate: false }.to raise_error
+    expect { other_id_type.save! validate: false }
+      .to raise_error(/duplicate key value violates unique constraint "index_identifier_types_on_lower_name"/)
   end
 
   it "must have a case insensitive unique name" do
     other_id_type = IdentifierType.new(name: @id_type.name.upcase)
     expect(other_id_type).not_to be_valid
-    expect { other_id_type.save! validate: false }.to raise_error    
+    expect { other_id_type.save! validate: false }
+      .to raise_error(/duplicate key value violates unique constraint "index_identifier_types_on_lower_name"/)
   end
 end
