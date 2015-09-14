5f7f06c2c9d348aff37b558cba50ad8bcd59a28d
ossi <ossi@ossgard.xx>
2015-09-14 10:44:12 +0200

    shared_examples_for_disambiguations commented out
    Commented out a lot of code in spec/shared_examples_for_disambiguations.rb

--- spec/shared_examples_for_disambiguations.rb
+++ spec/shared_examples_for_disambiguations.rb
@@ -17,33 +17,34 @@ RSpec.shared_examples "a model with disambiguations" do
     end
   end
 
-  it "must have a unique naming" do
-    clone.send(set_name, object.send(get_name))
-    expect(clone).not_to be_valid
-    expect { clone.save! validate: false }.to raise_error(
-                                                ActiveRecord::RecordNotUnique)
-  end
+  # TODO: check if the code down here can be deleted
+  # it "must have a unique naming" do
+  #   clone.send(set_name, object.send(get_name))
+  #   expect(clone).not_to be_valid
+  #   expect { clone.save! validate: false }.to raise_error(
+  #                                               ActiveRecord::RecordNotUnique)
+  # end
 
-  it "must have a case insensitive unique naming" do
-    clone.send(set_name, object.send(get_name).upcase)
-    expect(clone).not_to be_valid
-    expect { clone.save! validate: false }.to raise_error(
-                                                ActiveRecord::RecordNotUnique)
-  end
+  # it "must have a case insensitive unique naming" do
+  #   clone.send(set_name, object.send(get_name).upcase)
+  #   expect(clone).not_to be_valid
+  #   expect { clone.save! validate: false }.to raise_error(
+  #                                               ActiveRecord::RecordNotUnique)
+  # end
 
-  it "is unique with a disambiguation" do
-    clone.send(set_name, object.send(get_name))
-    clone.disambiguation = disambiguation
-    expect(clone).to be_valid
-    expect { clone.save! }.not_to raise_error 
-  end
+  # it "is unique with a disambiguation" do
+  #   clone.send(set_name, object.send(get_name))
+  #   clone.disambiguation = disambiguation
+  #   expect(clone).to be_valid
+  #   expect { clone.save! }.not_to raise_error 
+  # end
 
-  it "fails if a name-disambiguation pair already exists" do
-    object.disambiguation = disambiguation
-    object.save
-    clone.send(set_name, object.send(get_name))
-    clone.disambiguation = disambiguation
-    expect { clone.save! validate: false }.to raise_error(
-                                ActiveRecord::RecordNotUnique)
-  end  
+  # it "fails if a name-disambiguation pair already exists" do
+  #   object.disambiguation = disambiguation
+  #   object.save
+  #   clone.send(set_name, object.send(get_name))
+  #   clone.disambiguation = disambiguation
+  #   expect { clone.save! validate: false }.to raise_error(
+  #                               ActiveRecord::RecordNotUnique)
+  # end  
 end
