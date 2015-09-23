d1724f92944bf9b1d9e4865a522be79d206df483
ossi <ossi@ossgard.xx>
2015-09-23 15:14:25 +0200

    More specs for CrCompany

--- spec/models/cr_company_spec.rb
+++ spec/models/cr_company_spec.rb
@@ -11,5 +11,31 @@ RSpec.describe CrCompany, type: :model do
       expect(@cr_company).to be_valid
     end
   end
-      
+
+  describe "without a company" do
+    before(:each) do
+      @cr_company = FactoryGirl.create(:cr_company)
+      @cr_company.company = nil
+    end
+
+    it "is not valid" do
+      expect(@cr_company).not_to be_valid
+      expect { @cr_company.save! validate: false }
+        .to raise_error(/null value.+"company_id".+not-null constraint/)
+    end
+  end
+
+    describe "without a company_role" do
+    before(:each) do
+      @cr_company = FactoryGirl.create(:cr_company)
+      @cr_company.company_role = nil
+    end
+
+    it "is not valid" do
+      expect(@cr_company).not_to be_valid
+      expect { @cr_company.save! validate: false }
+        .to raise_error(/null value.+"company_role_id".+not-null constraint/)
+    end
+  end
+    
 end
