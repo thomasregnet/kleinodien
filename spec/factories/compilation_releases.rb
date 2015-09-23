e8d3d6e2e8a129fc4af0f4d7f85eb2d4e9041981
ossi <ossi@ossgard.xx>
2015-09-23 20:34:57 +0200

    New factory :compilation_release_with_companies
    ...used by compilation_release_spec.rb

--- spec/factories/compilation_releases.rb
+++ spec/factories/compilation_releases.rb
@@ -2,5 +2,19 @@ FactoryGirl.define do
   factory :compilation_release do
     association :head, factory: :compilation_head
     type "CompilationRelease"
+
+    factory :compilation_release_with_companies do
+      transient do
+        companies_count 2
+      end
+
+      after(:create) do |compilation_release, elevator|
+        create_list(
+          :cr_company,
+          elevator.companies_count,
+          compilation_release: compilation_release
+        )
+      end
+    end
   end
 end
