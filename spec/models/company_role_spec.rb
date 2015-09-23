52375fce62ddf4b996f711c8aa25f15e0919d3f9
ossi <ossi@ossgard.xx>
2015-09-23 11:32:16 +0200

    Failing specs for CompanyRole

--- spec/models/company_role_spec.rb
+++ spec/models/company_role_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe CompanyRole, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @company_role = FactoryGirl.create(:company_role)
+  end
+
+  context "with a name set" do
+    it "is valid" do
+      expect(@company_role).to be_valid
+    end
+  end
+
+  context "with no name set" do
+    before(:each) do
+      @company_role = CompanyRole.new
+    end
+
+    it "is not valid" do
+      expect(@company_role).not_to be_valid
+    end
+  end
 end
