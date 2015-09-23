555ab34c92232106238963d5f291d28359a5d866
ossi <ossi@ossgard.xx>
2015-09-23 20:23:46 +0200

    Deleted commented code form CompilationRelease

--- app/models/compilation_release.rb
+++ app/models/compilation_release.rb
@@ -12,35 +12,11 @@ class CompilationRelease < ActiveRecord::Base
   has_many(
     :identifiers,
     class_name: CompilationIdentifier)
-  # has_many(
-  #   :media,
-  #   class_name: CompilationMedium,
-  #   foreign_key: :compilation_release_id)
   has_many(:tracks)
-    
   has_many(
     :formats,
     class_name: CrFormat,
     foreign_key: :compilation_release_id)
   validates_uniqueness_of :version, scope: :head, case_sensitive: false
   delegate :title, to: :head
-
-  # def date=(date)
-  #   if date.class == Fixnum
-  #     write_attribute(:date, date.to_s + '-01-01')
-  #     write_attribute(:date_mask, 4)
-  #   elsif date =~ /^\d\d\d\d$/
-  #     write_attribute(:date, date + '-01-01')
-  #     write_attribute(:date_mask, 4)
-  #   elsif date =~ /^\d\d\d\d-\d\d$/
-  #     write_attribute(:date, date + '-01')
-  #     write_attribute(:date_mask, 6)
-  #   elsif date =~ /^\d\d\d\d-\d\d-\d\d$/
-  #     write_attribute(:date, date)
-  #     write_attribute(:date_mask, 7)
-  #   else
-  #     write_attribute(:date, date)
-  #   end
-  #   date
-  # end
 end
