7f7d33940e6a3c203e67b43383a9641a6799cf43
ossi <ossi@ossgard.xx>
2015-09-24 09:45:43 +0200

    First specs for "Country"

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  context "with valid values" do
+    before(:each) do
+      @country = FactoryGirl.create(:country)
+    end
+
+    it "is valid" do
+      expect(@country).to be_valid
+    end
+  end
 end
