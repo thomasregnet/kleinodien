ec1307bc87894e1512681450f88ab814d67848bd
ossi <ossi@ossgard.xx>
2015-09-24 10:36:39 +0200

    Company_role_spec shared_examples_for_unique_names

--- spec/models/company_role_spec.rb
+++ spec/models/company_role_spec.rb
@@ -1,48 +1,8 @@
 require 'rails_helper'
+require 'shared_examples_for_unique_names'
 
 RSpec.describe CompanyRole, type: :model do
-  before(:each) do
-    @company_role = FactoryGirl.create(:company_role)
+  it_behaves_like "an entity with an unique name" do
+    let(:factory) { :company_role }
   end
-
-  context "with a name set" do
-    it "is valid" do
-      expect(@company_role).to be_valid
-    end
-  end
-
-  context "with no name set" do
-    before(:each) do
-      @company_role = CompanyRole.new
-    end
-
-    it "is not valid" do
-      expect(@company_role).not_to be_valid
-    end
-  end
-
-  context "with name set to a blank value" do
-    before(:each) do
-      @company_role = CompanyRole.new(name: "")
-    end
-
-    it "is not valid" do
-      expect(@company_role).not_to be_valid
-    end
-  end
-
-  context "two entities with the same name" do
-    before(:each) do
-      @company_role = FactoryGirl.create(:company_role)
-      @clone_role   = FactoryGirl.build(:company_role)
-      @clone_role.name = @company_role.name
-    end
-
-    it "is not valid when the name already exists" do
-      expect(@clone_role).not_to be_valid
-      expect{ @clone_role.save! validate: false }
-        .to raise_error(/duplicate.+ violates.+"index_company_roles_on_name"/)
-    end
-  end
-  
 end
