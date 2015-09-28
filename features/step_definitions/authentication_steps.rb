4c2bddfa9ab6391f281c5400ce73d93414f09499
ossi <ossi@ossgard.xx>
2015-09-28 10:45:26 +0200

    Added "Sign out" link to application.html.erb
    All steps of Scenario: "Sign up" are passing.

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,3 +1,26 @@
 Given(/^I have not signed up$/) do
   # TODO: how to test that an user has not signed up?
 end
+
+When(/^I visit the sign\-up\-path$/) do
+  visit 'users/sign_up'
+end
+
+When(/^I fill in appropriate values for email and password$/) do
+  password = 'topSecret'
+  fill_in 'Email', with: 'foo@example.com'
+  fill_in 'Password', with: password
+  fill_in 'Password confirmation', with: password
+end
+
+When(/^I press the Sign up Button$/) do
+  click_button 'Sign up'
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  expect(page).to have_content 'signed up successfully'
+end
+
+Then(/^I should see the logout link$/) do
+  expect(page).to have_link 'Sign out'
+end
