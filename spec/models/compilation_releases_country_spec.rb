1f210f4f688581d404f106b201d3c389a9be7f3d
ossi <ossi@ossgard.xx>
2015-10-01 11:51:38 +0200

    Unique Release & Country  CompilationReleasesCo...

--- spec/models/compilation_releases_country_spec.rb
+++ spec/models/compilation_releases_country_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe CompilationReleasesCountry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "the combination of Release and Country must be unique" do
+    before(:each) do
+      @country = FactoryGirl.create(:country)
+      @release = FactoryGirl.create(:album_release)
+      @release_country = CompilationReleasesCountry.create!(
+        country:             @country,
+        compilation_release: @release,
+      )
+    end
+
+    it "fails when the combination of country and release already exists" do
+      other_release_country = CompilationReleasesCountry.new(
+        country:             @country,
+        compilation_release: @release,
+      )
+      expect(other_release_country).not_to be_valid
+    end
+  end
 end
