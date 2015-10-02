68f7ecf145caab6d52d3d02416ed136790dfa765
ossi <ossi@ossgard.xx>
2015-10-02 10:23:46 +0200

    Other association compilationReleases & Countries
    The models are now associated with a `has_and_belongs_to_many` association.
    Deleted model "CompilationReleasesCountry".

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,6 +2,7 @@ class Country < ActiveRecord::Base
   validates :name, presence: true, blank: false
   validates_uniqueness_of :name, case_sensitive: false
 
-  has_many :compilation_releases, through: :compilation_releases_countries
-  has_many :compilation_releases_countries, inverse_of: :country
+  #has_many :compilation_releases, through: :compilation_releases_countries
+  #has_many :compilation_releases_countries, inverse_of: :country
+  has_and_belongs_to_many :compilation_releases
 end
