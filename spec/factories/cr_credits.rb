8508d3f21756535680ff3fad7f77973949a05e0c
ossi <ossi@ossgard.xx>
2015-10-05 20:29:45 +0200

    More specs for CrCredit

--- spec/factories/cr_credits.rb
+++ spec/factories/cr_credits.rb
@@ -2,6 +2,14 @@ FactoryGirl.define do
   factory :cr_credit do
     artist_credit
     compilation_release
+
+    factory :cr_credit_with_role do
+      role 'a role'
+    end
+
+    factory :cr_credit_with_job do
+      association :job, factory: :job
+    end
   end
 
 end
