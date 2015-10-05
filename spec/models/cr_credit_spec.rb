3a6b7746f85e1495d6358a1e412c0dbc76882a19
ossi <ossi@ossgard.xx>
2015-10-05 20:40:50 +0200

    Validations and more specs for CrCredit

--- spec/models/cr_credit_spec.rb
+++ spec/models/cr_credit_spec.rb
@@ -31,4 +31,31 @@ RSpec.describe CrCredit, type: :model do
       expect(@cr_credit).to be_valid
     end
   end
+
+  describe "without a compilation_release" do
+    before(:each) do
+      @cr_credit = FactoryGirl.create(:cr_credit_with_job)
+      @cr_credit.compilation_release = nil
+    end
+
+    it "is not valid" do
+      expect(@cr_credit).not_to be_valid
+      expect { @cr_credit.save! validate: false }
+        .to raise_error(/ERROR:\s+null value in column.+_id/)
+    end
+  end
+
+  describe "without a artist_credit" do
+    before(:each) do
+      @cr_credit = FactoryGirl.create(:cr_credit_with_job)
+      @cr_credit.artist_credit = nil
+    end
+
+    it "is not valid" do
+      expect(@cr_credit).not_to be_valid
+      expect { @cr_credit.save! validate: false }
+        .to raise_error(/ERROR:\s+null value in column "artist_credit_id"/)
+    end
+  end
+    
 end
