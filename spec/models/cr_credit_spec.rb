3f7c0b8fcee443de5c437025aad314934c43ab9e
ossi <ossi@ossgard.xx>
2015-10-05 20:29:45 +0200

    More specs for CrCredit

--- spec/models/cr_credit_spec.rb
+++ spec/models/cr_credit_spec.rb
@@ -1,11 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe CrCredit, type: :model do
-  before(:each) do
-    @cr_credit = FactoryGirl.create(:cr_credit)
+
+  describe "with the mininum of attributes" do
+    before(:each) do
+      @cr_credit = FactoryGirl.create(:cr_credit)
+    end
+
+    it "is valid" do
+      expect(@cr_credit).to be_valid
+    end
   end
 
-  it "is valid with valid attributes" do
-    expect(@cr_credit).to be_valid
+  describe "with a role" do
+    before(:each) do
+      @cr_credit = FactoryGirl.create(:cr_credit_with_role)
+    end
+
+    it "is valid" do
+      expect(@cr_credit).to be_valid
+    end
+  end
+
+  describe "with a job" do
+    before(:each) do
+      @cr_credit = FactoryGirl.create(:cr_credit_with_job)
+    end
+
+    it "is valid" do
+      expect(@cr_credit).to be_valid
+    end
   end
 end
