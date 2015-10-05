d94f01b3199f1f975bd5ba3ee1b3d661ba864580
ossi <ossi@ossgard.xx>
2015-10-05 20:40:50 +0200

    Validations and more specs for CrCredit

--- app/models/cr_credit.rb
+++ app/models/cr_credit.rb
@@ -2,4 +2,6 @@ class CrCredit < ActiveRecord::Base
   belongs_to :artist_credit
   belongs_to :compilation_release
   belongs_to :job
+  validates :artist_credit, presence: true
+  validates :compilation_release, presence: true
 end
