33284b8d1ba008e944edba8e44d7486e580fa1ea
ossi <ossi@ossgard.xx>
2015-10-05 19:52:06 +0200

    Refined and migrated "jobs", spec fails

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
+require 'shared_examples_for_unique_names'
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like "an entity with an unique name" do
+    let(:factory) { :job }
+  end
 end
