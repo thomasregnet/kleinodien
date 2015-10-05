c35fe8330ba5ec837f783a5f82e655951c5c1d97
ossi <ossi@ossgard.xx>
2015-10-05 19:52:06 +0200

    Refined and migrated "jobs", spec fails

--- db/migrate/20151005174738_create_jobs.rb
+++ db/migrate/20151005174738_create_jobs.rb
@@ -1,7 +1,7 @@
 class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002074436) do
+ActiveRecord::Schema.define(version: 20151005174738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,6 +152,12 @@ ActiveRecord::Schema.define(version: 20151002074436) do
   create_table "identifier_types", force: :cascade do |t|
     t.string   "name",       null: false
     t.string   "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
