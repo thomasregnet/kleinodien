4d814d93db4ba5be4495bce9130a49cce9425834
ossi <ossi@ossgard.xx>
2015-10-05 20:22:16 +0200

    Specs and factory for CrCredit

--- spec/models/cr_credit_spec.rb
+++ spec/models/cr_credit_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe CrCredit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @cr_credit = FactoryGirl.create(:cr_credit)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@cr_credit).to be_valid
+  end
 end
