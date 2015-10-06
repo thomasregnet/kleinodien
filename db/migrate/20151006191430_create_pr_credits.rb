6eee4256e44a0776ac63baa1e38d613d3d848a92
ossi <ossi@ossgard.xx>
2015-10-06 21:17:20 +0200

    New model "PrCredit"
    $(rails generate model PrCredit
        artist_credit:references
        piece_release:references
        job:references
        role:string
    )

---
+++ db/migrate/20151006191430_create_pr_credits.rb
@@ -0,0 +1,12 @@
+class CreatePrCredits < ActiveRecord::Migration
+  def change
+    create_table :pr_credits do |t|
+      t.references :artist_credit, index: true, foreign_key: true, null: false
+      t.references :piece_release, index: true, foreign_key: true, null: false
+      t.references :job, index: true, foreign_key: true
+      t.string :role
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/pr_credits.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :pr_credit do
+    artist_credit nil
+piece_release nil
+job nil
+role "MyString"
+  end
+
+end
