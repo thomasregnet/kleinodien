49b96face4566d09698805261af66a549b96c5f7
ossi <ossi@ossgard.xx>
2015-10-07 21:44:48 +0200

    PhCredit specs are passing
    Needed to rework the "..._create_ph_credits.rb" migration.

--- app/models/ph_credit.rb
+++ app/models/ph_credit.rb
@@ -2,4 +2,6 @@ class PhCredit < ActiveRecord::Base
   belongs_to :artist_credit
   belongs_to :piece_head
   belongs_to :job
+  validates :artist_credit, presence: true
+  validates :piece_head, presence: true
 end
