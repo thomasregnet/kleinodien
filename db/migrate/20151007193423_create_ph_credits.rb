2c6854ac7c070dca063768e6a5ac6ebca1aecfb5
ossi <ossi@ossgard.xx>
2015-10-07 21:35:15 +0200

    New model "PhCredit"
    $(rails generate model PhCredit
      artist_credit:references
      piece_head:references
      job:references
      role:string
    )

---
+++ db/migrate/20151007193423_create_ph_credits.rb
@@ -0,0 +1,12 @@
+class CreatePhCredits < ActiveRecord::Migration
+  def change
+    create_table :ph_credits do |t|
+      t.references :artist_credit, index: true, foreign_key: true
+      t.references :piece_head, index: true, foreign_key: true
+      t.references :job, index: true, foreign_key: true
+      t.string :role
+
+      t.timestamps null: false
+    end
+  end
+end
