969606dc30ba58adea0438bf482a92e69acb010b
ossi <ossi@ossgard.xx>
2015-10-07 21:38:35 +0200

    Failing specs for PhCredit

--- spec/models/ph_credit_spec.rb
+++ spec/models/ph_credit_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
+require 'shared_examples_for_credits'
 
 RSpec.describe PhCredit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it_behaves_like "a credit" do
+    let(:factory)           { :ph_credit }
+    let(:factory_with_job)  { :ph_credit_with_job }
+    let(:factory_with_role) { :ph_credit_with_role }
+    let(:owner_setter)      { 'piece_head=' }
+    end
+    
 end
