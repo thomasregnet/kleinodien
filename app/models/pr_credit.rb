505c00ea18408b9256be9dc501924ae6fb208739
ossi <ossi@ossgard.xx>
2015-10-07 20:01:09 +0200

    Pr_credit_spec pass

--- app/models/pr_credit.rb
+++ app/models/pr_credit.rb
@@ -2,4 +2,6 @@ class PrCredit < ActiveRecord::Base
   belongs_to :artist_credit
   belongs_to :piece_release
   belongs_to :job
+  validates :artist_credit, presence: true
+  validates :piece_release, presence: true
 end
