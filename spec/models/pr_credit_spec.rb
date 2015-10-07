83e9545042b7d760361641668de530eee583c269
ossi <ossi@ossgard.xx>
2015-10-07 19:51:24 +0200

    Fabulous failing specs for PrCredit
    Using it_behaves_like "an entity with credits".

--- spec/models/pr_credit_spec.rb
+++ spec/models/pr_credit_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
+require 'shared_examples_for_credits'
 
 RSpec.describe PrCredit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it_behaves_like "an entity with credits" do
+    let(:factory)           { :pr_credit }
+    let(:factory_with_job)  { :pr_credit_with_job }
+    let(:factory_with_role) { :pr_credit_with_role }
+  end
+
 end
