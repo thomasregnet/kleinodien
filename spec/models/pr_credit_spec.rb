522c893fce8a497ab7c199423736982c26a11bfe
ossi <ossi@ossgard.xx>
2015-10-07 21:31:00 +0200

    Renamed "an entity with credits" to "a credit"

--- spec/models/pr_credit_spec.rb
+++ spec/models/pr_credit_spec.rb
@@ -3,7 +3,7 @@ require 'shared_examples_for_credits'
 
 RSpec.describe PrCredit, type: :model do
 
-  it_behaves_like "an entity with credits" do
+  it_behaves_like "a credit" do
     let(:factory)           { :pr_credit }
     let(:factory_with_job)  { :pr_credit_with_job }
     let(:factory_with_role) { :pr_credit_with_role }
