35021067d1ddde9b0a5eb4f95bc72a2eb0c8e366
ossi <ossi@ossgard.xx>
2015-10-08 21:51:38 +0200

    MovieHead.coutries are set by OmdbImporter

--- app/models/omdb_importer.rb
+++ app/models/omdb_importer.rb
@@ -2,8 +2,15 @@ class OmdbImporter < ActiveRecord::Base
   def self.import_movie(xml)
     omdb_movie = KleinodienOmdb.parse_movie(xml)
     movie_head = MovieHead.create!(title: omdb_movie.name)
+    import_countries(omdb_movie.countries, movie_head)
     import_departments(omdb_movie.departments, movie_head)
     movie_head
+  end
+
+  def self.import_countries(countries, movie_head)
+    countries.each do |c|
+      movie_head.countries << Country.find_or_create_by(name: c)
+    end
   end
 
   def self.import_departments(departments, movie_head)
