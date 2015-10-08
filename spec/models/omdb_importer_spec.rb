f5f1b8594a54ea9d4f53eb91b168cfef65d90d3d
ossi <ossi@ossgard.xx>
2015-10-08 20:39:10 +0200

    More specs on MovieHead.credits imported from OMDB

--- spec/models/omdb_importer_spec.rb
+++ spec/models/omdb_importer_spec.rb
@@ -15,6 +15,14 @@ RSpec.describe OmdbImporter, type: :model do
           .to eq('Braindead - Der Zombie-Rasenmähermann')
 
         expect(@movie_head.credits.length).to eq(19)
+        
+        expect(@movie_head.credits[0].artist_credit.name).to eq('Peter Jackson')
+        expect(@movie_head.credits[0].role).to be_nil
+        expect(@movie_head.credits[0].job.name).to eq('Director')
+
+        expect(@movie_head.credits[18].artist_credit.name).to eq('Lewis Rowe')
+        expect(@movie_head.credits[18].role).to eq('Mr. Matheson')
+        expect(@movie_head.credits[18].job.name).to eq('Akteur')
       end
 
       after(:all) do
