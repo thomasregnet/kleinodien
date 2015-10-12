2f7ca071cc1335c3c649dae16ce0cf29811bd761
ossi <ossi@ossgard.xx>
2015-10-12 19:35:18 +0200

    Failing specs for "ChCredit"

--- spec/models/ch_credit_spec.rb
+++ spec/models/ch_credit_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
+require 'shared_examples_for_credits'
 
 RSpec.describe ChCredit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it_behaves_like "a credit" do
+    let(:factory)           { :ch_credit }
+    let(:factory_with_job)  { :ch_credit_with_job }
+    let(:factory_with_role) { :ch_credit_with_role }
+    let(:owner_setter)      { 'compilation_head=' }
+  end
+
 end
