a094dcb8aa62adcc41ec886ab138cafe135dcb33
ossi <ossi@ossgard.xx>
2015-10-12 19:43:16 +0200

    All specs of ChCredit are passing

--- app/models/ch_credit.rb
+++ app/models/ch_credit.rb
@@ -2,4 +2,6 @@ class ChCredit < ActiveRecord::Base
   belongs_to :artist_credit
   belongs_to :compilation_head
   belongs_to :job
+  validates :artist_credit, presence: true
+  validates :compilation_head, presence: true  
 end
