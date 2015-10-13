263db323143f9c6595673467ca6443b6bdf2a7b6
ossi <ossi@ossgard.xx>
2015-10-13 20:27:59 +0200

    Omdb_importer_spec makes rubocop happy

--- spec/models/omdb_importer_spec.rb
+++ spec/models/omdb_importer_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 require 'omdb_test_helper'
 
 RSpec.describe OmdbImporter, type: :model do
-  describe "import movies" do
-    context "Braindead" do
+  describe 'import movies' do
+    context 'Braindead' do
       before(:all) do
         DatabaseCleaner.start
         @movie_head = OmdbTestHelper.import_movie(763)
       end
 
-      specify "was imported" do
+      specify 'was imported' do
         expect(@movie_head.title)
           .to eq('Braindead - Der Zombie-Rasenmähermann')
 
@@ -18,7 +18,7 @@ RSpec.describe OmdbImporter, type: :model do
         expect(@movie_head.countries[0].name).to eq('NZ')
 
         expect(@movie_head.credits.length).to eq(19)
-        
+
         expect(@movie_head.credits[0].artist_credit.name).to eq('Peter Jackson')
         expect(@movie_head.credits[0].role).to be_nil
         expect(@movie_head.credits[0].job.name).to eq('Director')
