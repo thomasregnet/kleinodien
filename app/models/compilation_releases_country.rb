a972f632343011bacac2800c0b8f6177ec47517f
ossi <ossi@ossgard.xx>
2015-10-14 19:59:47 +0200

    Deleted CompilationReleasesCountry

--- app/models/compilation_releases_country.rb
+++
@@ -1,5 +0,0 @@
-class CompilationReleasesCountry < ActiveRecord::Base
-  belongs_to :compilation_release, inverse_of: :compilation_releases_countries
-  belongs_to :country, inverse_of: :compilation_releases_countries
-  validates_uniqueness_of :compilation_release, scope: :country
-end
