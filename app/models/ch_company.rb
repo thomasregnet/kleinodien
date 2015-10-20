33388d1652ea9ff86d5340325d5e5c10111a7acc
ossi <ossi@ossgard.xx>
2015-10-20 19:36:56 +0200

    Ch_company_spec uses shared_examples_for_companies
    Also added the "owner_setter" parameter for shared_examples_for_companies.

--- app/models/ch_company.rb
+++ app/models/ch_company.rb
@@ -4,5 +4,5 @@ class ChCompany < ActiveRecord::Base
   belongs_to :company_role
   validates :compilation_head, presence: true
   validates :company, presence: true
-  validates :company, presence: true
+  validates :company_role, presence: true
 end
