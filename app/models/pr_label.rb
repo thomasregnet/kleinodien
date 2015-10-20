2acae08bffc398c6a27238ec02a37c496e3cc43d
ossi <ossi@ossgard.xx>
2015-10-20 21:28:09 +0200

    Specs, validations and factories for PrLabel

--- app/models/pr_label.rb
+++ app/models/pr_label.rb
@@ -1,4 +1,6 @@
 class PrLabel < ActiveRecord::Base
   belongs_to :piece_release
   belongs_to :company
+  validates :piece_release, presence: true
+  validates :company, presence: true
 end
--- spec/factories/pr_labels.rb
+++ spec/factories/pr_labels.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
   factory :pr_label do
-    piece_release nil
-company nil
-catalog_no "MyString"
+    piece_release
+    company
+
+    factory :pr_label_with_catalog_no do
+      sequence(:catalog_no) { |n| "catalog-##{n}" }
+    end
   end
 
 end
