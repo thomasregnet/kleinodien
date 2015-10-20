bc47e0eb4563808712527014ca7cb46d16b86473
ossi <ossi@ossgard.xx>
2015-10-20 21:42:39 +0200

    PieceRelease has_many Companies

--- app/models/piece_release.rb
+++ app/models/piece_release.rb
@@ -4,6 +4,7 @@ class PieceRelease < ActiveRecord::Base
               mapping: [ %w(date date), %w(date_mask mask) ]
   belongs_to :head, class_name: PieceHead, foreign_key: :piece_head_id
   has_many :credits, class_name: PrCredit
+  has_many :companies, class_name: PrCompany
   has_many :labels, class_name: PrLabel
   has_many :tracks
   has_and_belongs_to_many :countries
