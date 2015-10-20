f5e6802ff0b00eebfd0955786831d6f883d66ef2
ossi <ossi@ossgard.xx>
2015-10-20 20:55:15 +0200

    Specs, validations and factories for CrLabel

--- app/models/cr_label.rb
+++ app/models/cr_label.rb
@@ -1,4 +1,6 @@
 class CrLabel < ActiveRecord::Base
   belongs_to :compilation_release
   belongs_to :company
+  validates :company, presence: true
+  validates :compilation_release, presence: true
 end
--- spec/models/cr_label_spec.rb
+++ spec/models/cr_label_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
+require 'shared_examples_for_labels'
 
 RSpec.describe CrLabel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like "a label" do
+    let(:factory)                 { :cr_label }
+    let(:with_catalog_no_factory) { :cr_label_with_catalog_no }
+    let(:owner_setter)            { 'compilation_release=' }
+  end
 end
