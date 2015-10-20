0544f949c05ba8c1cb3fc1edfd2ab3262c5ae267
ossi <ossi@ossgard.xx>
2015-10-20 19:41:55 +0200

    Ph_company_spec uses shared_examples_for_companies
    Also added validations for PhCompany.

--- spec/models/ph_company_spec.rb
+++ spec/models/ph_company_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
+require 'shared_examples_for_companies'
 
 RSpec.describe PhCompany, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like "a company" do
+    let(:factory)      { :ph_company }
+    let(:owner_setter) { 'piece_head=' }
+  end
 end
