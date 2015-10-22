b0f57e0fa1a3fdff1f575fcca854d566fc878a5a
ossi <ossi@ossgard.xx>
2015-10-22 20:20:02 +0200

    More specs and validations for SourceIdentifier

--- app/models/source_identifier.rb
+++ app/models/source_identifier.rb
@@ -1,3 +1,6 @@
 class SourceIdentifier < ActiveRecord::Base
   belongs_to :data_source
+  validates :data_source, presence: true
+  validates :identifier, presence: true, blank: false
+  validates :type, presence: true, blank: false
 end
--- spec/models/source_identifier_spec.rb
+++ spec/models/source_identifier_spec.rb
@@ -1,12 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe SourceIdentifier, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
   before(:each) do
     @src_ident = FactoryGirl.create(:source_identifier)
   end
 
   it 'is valid with valid attributes' do
     expect(@src_ident).to be_valid
+  end
+
+  describe 'without a DataSource' do
+    before(:each) do
+      @src_ident.data_source = nil
+    end
+
+    it 'is not valid' do
+      expect(@src_ident).not_to be_valid
+      expect { @src_ident.save! validate: false }
+        .to raise_error(/PG::NotNullViolation:.+"data_source_id".+not-null/)
+    end
+  end
+
+  describe 'without a identifier' do
+    before(:each) do
+      @src_ident.identifier = nil
+    end
+
+    it 'is not valid' do
+      expect(@src_ident).not_to be_valid
+      expect { @src_ident.save! validate: false }
+        .to raise_error(/PG::NotNullViolation:.+"identifier".+not-null/)
+    end
+  end
+
+  describe 'with a blank identifier' do
+    before(:each) do
+      @src_ident.identifier = ''
+    end
+
+    it 'is not valid' do
+      expect(@src_ident).not_to be_valid
+    end
+  end
+
+    describe 'without a type' do
+    before(:each) do
+      @src_ident.type = nil
+    end
+
+    it 'is not valid' do
+      expect(@src_ident).not_to be_valid
+      expect { @src_ident.save! validate: false }
+        .to raise_error(/PG::NotNullViolation:.+"type".+not-null/)
+    end
+  end
+
+  describe 'with a blank type' do
+    before(:each) do
+      @src_ident.type = ''
+    end
+
+    it 'is not valid' do
+      expect(@src_ident).not_to be_valid
+    end
   end
 end
