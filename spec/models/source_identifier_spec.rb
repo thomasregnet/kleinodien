5d793278e2f4967dbf1d291c12c0b35eacfffd23
ossi <ossi@ossgard.xx>
2015-10-22 20:04:50 +0200

    Specs and factory for SourceIdentifier

--- spec/models/source_identifier_spec.rb
+++ spec/models/source_identifier_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe SourceIdentifier, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @src_ident = FactoryGirl.create(:source_identifier)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@src_ident).to be_valid
+  end
 end
