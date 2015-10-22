31ec494ff244e68c8781fabc0e89958a8b4dd37c
ossi <ossi@ossgard.xx>
2015-10-22 20:04:50 +0200

    Specs and factory for SourceIdentifier

--- spec/factories/source_identifiers.rb
+++ spec/factories/source_identifiers.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :source_identifier do
-    data_source nil
-identifier "MyString"
-type ""
+    data_source
+    sequence(:identifier) { |n| "abc_#{n}_defg" }
+    type 'GenericSourceIdentifier'
   end
 
 end
