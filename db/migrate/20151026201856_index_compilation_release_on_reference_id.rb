374ba402e04157211c9ca486f453896b8812219b
ossi <ossi@ossgard.xx>
2015-10-26 21:26:50 +0100

    New index on compilation_releases.reference_id
    This index is unique to prevent duplicate entries for one Reference.

---
+++ db/migrate/20151026201856_index_compilation_release_on_reference_id.rb
@@ -0,0 +1,19 @@
+class IndexCompilationReleaseOnReferenceId < ActiveRecord::Migration
+  def change
+    reversible do |idx|
+      idx.up do
+        execute <<-DDL.gsub /^\s+/, ''
+          CREATE UNIQUE INDEX compilation_releases_reference_id
+            ON compilation_releases (reference_id)
+              WHERE reference_id IS NOT NULL;
+        DDL
+      end
+      idx.down do
+        remove_index(
+          :compilation_releases,
+          name: :compilation_releases_reference_id
+        )
+      end
+    end
+  end
+end
