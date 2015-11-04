3dd52df5f5dbd0dd030d461cf0e2f78e4db55df3
ossi <ossi@ossgard.xx>
2015-11-04 20:42:50 +0100

    Added format_quantity_multiplied_name_for

--- app/helpers/album_releases_helper.rb
+++ app/helpers/album_releases_helper.rb
@@ -3,4 +3,9 @@ module AlbumReleasesHelper
     return label.company.name unless label.catalog_no
     "#{label.company.name} - #{label.catalog_no}"
   end
+
+  def format_quantity_multiplied_name_for(format)
+    return format.kind.name unless format.quantity
+    "#{format.quantity} x #{format.kind.name}"
+  end
 end
