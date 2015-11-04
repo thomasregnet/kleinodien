f791962e5646a174b2bd468f788441f5905da3ed
ossi <ossi@ossgard.xx>
2015-11-04 20:34:09 +0100

    Added AlbumReleasesHelper.label_and_catalog_no_for

--- app/helpers/album_releases_helper.rb
+++ app/helpers/album_releases_helper.rb
@@ -1,2 +1,6 @@
 module AlbumReleasesHelper
+  def label_and_catalog_no_for(label)
+    return label.company.name unless label.catalog_no
+    "#{label.company.name} - #{label.catalog_no}"
+  end
 end
