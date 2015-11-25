b68f1f5eabc492c4283f401d2ed2b6b011016b2c
ossi <ossi@asgard.ossgard.xx>
2015-11-25 22:10:22 +0100

    Insert::Discogs::Release.run returns useless data

--- spec/models/insert/discogs/release_spec.rb
+++ spec/models/insert/discogs/release_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
+require 'discogs_test_helper'
 
 RSpec.describe Insert::Discogs::Release, type: :model do
-  context 'whithout data' do
+  context 'Highway To Hell' do
     before(:all) do
-      @release = Insert::Discogs::Release.new
+      json = DiscogsTestHelper.get_discogs_data('releases', 940468)
+      @release = Insert::Discogs::Release.new(json)
     end
-    
+
     it 'responds to "run"' do
       expect(@release).to respond_to(:run)
+    end
+
+    it 'returns a kleinodien_discogs-release' do
+      expect(@release.run).to be_instance_of(KleinodienDiscogs::Release)
     end
   end
 end
