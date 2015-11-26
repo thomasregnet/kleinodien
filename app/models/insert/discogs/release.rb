b3390186dce0429982eaeb2010ed94dd5636023a
ossi <ossi@asgard.ossgard.xx>
2015-11-26 20:57:21 +0100

    New model Insert::Discogs::ArtistCredit
    No unit tests but used by Insert::Discogs::Release.

--- app/models/insert/discogs/release.rb
+++ app/models/insert/discogs/release.rb
@@ -5,7 +5,7 @@ class Insert::Discogs::Release
   end
   
   def run
-    artist_credit = insert_artist_credit(@dc_release.artists)
+    artist_credit = Insert::Discogs::ArtistCredit.new(@dc_release.artists).run
     album_release = insert_album_release(artist_credit)
   end
 
@@ -20,28 +20,4 @@ class Insert::Discogs::Release
     )
   end
   
-  def insert_artist_credit(artists)
-    ac_name =  KleinodienDiscogs.join_artist_names(artists)
-    ArtistCredit.find_by(name: ac_name) || create_artist_credit(artists)
-  end
-
-  def create_artist_credit(artists)
-    artist_credit = ArtistCredit.new
-    artists.each_with_index do |artist, no|
-      insert_participant(artist, no, artist_credit)
-    end
-    artist_credit.save!
-    artist_credit
-  end
-
-   def insert_participant(dc_artist, no, artist_credit)
-     artist = Artist.find_or_create_by!(name: dc_artist.name)
-     joinparse = dc_artist.join  unless dc_artist.join.blank?
-     artist_credit.participants.build(
-       artist:    artist,
-       joinparse: joinparse,
-       no:        no
-     )
-   end
-   
 end
---
+++ spec/factories/insert_discogs_artist_credits.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :insert_discogs_artist_credit, :class => 'Insert::Discogs::ArtistCredit' do
+    
+  end
+
+end
