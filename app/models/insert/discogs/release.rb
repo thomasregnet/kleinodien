7e7e34e8ad62a9f7f3d56af37316786954983a05
ossi <ossi@asgard.ossgard.xx>
2015-11-26 20:26:54 +0100

    Insert::Discogs::Release.run returns ArtistCredit
    What is of course logically wrong but works for this moment.

--- app/models/insert/discogs/release.rb
+++ app/models/insert/discogs/release.rb
@@ -5,6 +5,31 @@ class Insert::Discogs::Release
   end
   
   def run
-    @dc_release
+    artist_credit = insert_artist_credit(@dc_release.artists)
   end
+
+  def insert_artist_credit(artists)
+    ac_name =  KleinodienDiscogs.join_artist_names(artists)
+    ArtistCredit.find_by(name: ac_name) || create_artist_credit(artists)
+  end
+
+  def create_artist_credit(artists)
+    artist_credit = ArtistCredit.new
+    artists.each_with_index do |artist, no|
+      insert_participant(artist, no, artist_credit)
+    end
+    artist_credit.save!
+    artist_credit
+  end
+
+   def insert_participant(dc_artist, no, artist_credit)
+     artist = Artist.find_or_create_by!(name: dc_artist.name)
+     joinparse = dc_artist.join  unless dc_artist.join.blank?
+     artist_credit.participants.build(
+       artist:    artist,
+       joinparse: joinparse,
+      no:        no
+     )
+   end
+   
 end
