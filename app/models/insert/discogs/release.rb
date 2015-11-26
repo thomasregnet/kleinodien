f05d1970d31a4a516a26cb13d904909472ac2c51
ossi <ossi@asgard.ossgard.xx>
2015-11-26 21:04:18 +0100

    New: Insert::Discogs::Release.insert_album_head

--- app/models/insert/discogs/release.rb
+++ app/models/insert/discogs/release.rb
@@ -10,14 +10,16 @@ class Insert::Discogs::Release
   end
 
   def insert_album_release(artist_credit)
-    album_head = artist_credit.compilations.create!(
-      title: @dc_release.title,
-      type:  AlbumHead.to_s
-    )
-
+    album_head = insert_album_head(artist_credit)
     album_head.releases.create!(
       date: IncompleteDate.new(@dc_release.released)
     )
   end
-  
+
+  def insert_album_head(artist_credit)
+    artist_credit.compilations.create!(
+      title: @dc_release.title,
+      type:  AlbumHead.to_s
+    )    
+  end
 end
