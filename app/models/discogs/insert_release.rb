22c6f1105a92995444f97942d7f98ffb4f76a0b2
ossi <ossi@asgard.ossgard.xx>
2015-11-30 20:35:52 +0100

    Constructor & parameter for Discogs::InsertRelease

--- app/models/discogs/insert_release.rb
+++ app/models/discogs/insert_release.rb
@@ -1,8 +1,13 @@
 class Discogs::InsertRelease
 
-  def self.perform
+  def self.perform(dc_release)
+    new(dc_release).perform
   end
 
+  def initialize(dc_release)
+    @dc_release = dc_release
+  end
+  
   def perform
   end  
 end
