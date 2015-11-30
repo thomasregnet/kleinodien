1c4f0f89dba13e0dfa86a82c5c1f4510b4ef9c5a
ossi <ossi@asgard.ossgard.xx>
2015-11-30 20:08:09 +0100

    New model Discogs::InsertRelease
    $(rails generate model Discogs::InsertRelease --no-migration)

---
+++ app/models/discogs.rb
@@ -0,0 +1,5 @@
+module Discogs
+  def self.table_name_prefix
+    'discogs_'
+  end
+end
---
+++ spec/factories/discogs_insert_releases.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :discogs_insert_release, :class => 'Discogs::InsertRelease' do
+    
+  end
+
+end
