d0aacb98bf27fc82adedf991fae3f281cf1df643
ossi <ossi@asgard.ossgard.xx>
2015-11-30 22:21:25 +0100

    Discogs::InsertRelease uses Discogs::InsertArtists

--- app/models/discogs/insert_release.rb
+++ app/models/discogs/insert_release.rb
@@ -7,9 +7,9 @@ class Discogs::InsertRelease
   def initialize(dc_release)
     @dc_release = dc_release
   end
-  
+
   def perform
-    @artist_credit = insert_artist_credit
+    artist_credit
     album_head
     @release = @album_head.releases.create!(
       date: IncompleteDate.new(@dc_release.released)
@@ -18,36 +18,14 @@ class Discogs::InsertRelease
 
   private
 
+  def artist_credit
+    @artist_credit = Discogs::InsertArtists.perform(@dc_release.artists)
+  end
+
   def album_head
     @album_head = @artist_credit.compilations.create!(
       title: @dc_release.title,
       type:  AlbumHead.to_s
-    )
-
-  end
-  # TODO: Refactor following to own class
-  def insert_artist_credit
-    ac_name =  KleinodienDiscogs.join_artist_names(@dc_release.artists)
-    ::ArtistCredit.find_by(name: ac_name) || create_artist_credit
-  end
-
-  def create_artist_credit
-    artist_credit = ArtistCredit.new
-    #@dc_artists.each_with_index do |artist, no|
-    @dc_release.artists.each_with_index do |artist, no|
-      insert_participant(artist, no, artist_credit)
-    end
-    artist_credit.save!
-    artist_credit
-  end
-
-  def insert_participant(dc_artist, no, artist_credit)
-    artist = Artist.find_or_create_by!(name: dc_artist.name)
-    joinparse = dc_artist.join  unless dc_artist.join.blank?
-    artist_credit.participants.build(
-      artist:    artist,
-      joinparse: joinparse,
-      no:        no
     )
   end
 end
