e0651837d6116361cff79a6753fc900e007bca07
ossi <ossi@asgard.ossgard.xx>
2015-11-30 20:53:39 +0100

    AlbumRelease from Discogs::InsertRelease.perform

--- app/models/discogs/insert_release.rb
+++ app/models/discogs/insert_release.rb
@@ -9,5 +9,44 @@ class Discogs::InsertRelease
   end
   
   def perform
-  end  
+    @artist_credit = insert_artist_credit
+    album_head
+    @release = @album_head.releases.create!(
+      date: IncompleteDate.new(@dc_release.released)
+    )
+  end
+
+  private
+
+  def album_head
+    @album_head = @artist_credit.compilations.create!(
+      title: @dc_release.title,
+      type:  AlbumHead.to_s
+    )
+
+  end
+  # TODO: Refactor following to own class
+  def insert_artist_credit
+    ac_name =  KleinodienDiscogs.join_artist_names(@dc_release.artists)
+    ::ArtistCredit.find_by(name: ac_name) || create_artist_credit
+  end
+
+  def create_artist_credit
+    artist_credit = ArtistCredit.new
+    @dc_artists.each_with_index do |artist, no|
+      insert_participant(artist, no, artist_credit)
+    end
+    artist_credit.save!
+    artist_credit
+  end
+
+  def insert_participant(dc_artist, no, artist_credit)
+    artist = Artist.find_or_create_by!(name: dc_artist.name)
+    joinparse = dc_artist.join  unless dc_artist.join.blank?
+    artist_credit.participants.build(
+      artist:    artist,
+      joinparse: joinparse,
+      no:        no
+    )
+  end
 end
