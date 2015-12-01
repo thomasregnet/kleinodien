94f98ce075c7c9ea4b47a5c9ec155241f419e986
ossi <ossi@asgard.ossgard.xx>
2015-12-01 22:56:17 +0100

    Discogs::InsertRelease inserts credits

--- spec/models/discogs/insert_extraartists_spec.rb
+++ spec/models/discogs/insert_extraartists_spec.rb
@@ -9,10 +9,25 @@ RSpec.describe Discogs::InsertExtraartists, type: :model do
       json = DiscogsTestHelper.get_discogs_data('releases', 940468)
       dc_release = KleinodienDiscogs.get_release(json)
       @release = Discogs::InsertRelease.perform(dc_release)
+      @credits = @release.credits
     end
 
     it 'has the extraartists set' do
+      credit = @credits[0]
+      expect(credit.artist_credit.name).to eq('Cliff Williams')
+      expect(credit.job.name).to eq('Bass')
       
+      credit = @credits[3]
+      expect(credit.artist_credit.name).to eq('Angus Young')
+      expect(credit.job.name).to eq('Guitar')
+
+      credit = @credits[8]
+      expect(credit.artist_credit.name).to eq('Bon Scott')
+      expect(credit.job.name).to eq('Vocals')
+
+      credit = @credits[9]
+      expect(credit.artist_credit.name).to eq('Angus Young')
+      expect(credit.job.name).to eq('Written-By')
     end
 
     after(:all) { DatabaseCleaner.clean }
