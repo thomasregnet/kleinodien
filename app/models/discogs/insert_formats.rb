3d539fe63cebf1ab301e4a9861ac5dc670260713
ossi <ossi@asgard.ossgard.xx>
2015-12-01 20:05:16 +0100

    Failing specs for Discogs::InsertFormats

--- app/models/discogs/insert_formats.rb
+++ app/models/discogs/insert_formats.rb
@@ -1,2 +1,13 @@
-class Discogs::InsertFormats < ActiveRecord::Base
+class Discogs::InsertFormats
+  def self.perform(dc_formats, album_release)
+    new(dc_formats).perform
+  end
+
+  def initialize(dc_formats, album_release)
+    @dc_formats    = dc_formats
+    @album_release = album_release
+  end
+
+  def perform
+  end
 end
--- spec/models/discogs/insert_formats_spec.rb
+++ spec/models/discogs/insert_formats_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
+require 'discogs_test_helper'
 
 RSpec.describe Discogs::InsertFormats, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Dead Human Collection' do
+    before(:all) do
+      DatabaseCleaner.start
+      
+      json = DiscogsTestHelper.get_discogs_data('releases', 4462260)
+      dc_release = KleinodienDiscogs.get_release(json)
+      @album_release = Discogs::InsertRelease.perform(dc_release)
+      @formats = @album_release.formats
+    end
+
+    it 'has the formats set' do
+      expect(@formats[0].name).to eq('foo')
+    end
+
+    after(:all) { DatabaseCleaner.clean }
+  end
 end
