7ee30a7dd91fb50e18dd3b1372d48276c72a7b54
ossi <ossi@asgard.ossgard.xx>
2015-12-01 22:46:39 +0100

    Fixed bug in Discogs::InsertArtists
    if an ArtistCredit exists it is used.

--- spec/models/discogs/insert_extraartists_spec.rb
+++ spec/models/discogs/insert_extraartists_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
+require 'discogs_test_helper'
 
 RSpec.describe Discogs::InsertExtraartists, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'AC/DC - Highway To Hell' do
+    before(:all) do
+      DatabaseCleaner.start
+      
+      json = DiscogsTestHelper.get_discogs_data('releases', 940468)
+      dc_release = KleinodienDiscogs.get_release(json)
+      @release = Discogs::InsertRelease.perform(dc_release)
+    end
+
+    it 'has the extraartists set' do
+      
+    end
+
+    after(:all) { DatabaseCleaner.clean }
+  end
 end
