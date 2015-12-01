4c6192779193581705262f2af7b62465df77424e
ossi <ossi@asgard.ossgard.xx>
2015-12-01 21:04:23 +0100

    New model Discogs::InsertCompanies
    $(rails generate model Discogs::InsertCompanies --no-migration --force-plural)

---
+++ app/models/discogs/insert_companies.rb
@@ -0,0 +1,2 @@
+class Discogs::InsertCompanies < ActiveRecord::Base
+end
---
+++ spec/factories/discogs_insert_companies.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :discogs_insert_company, :class => 'Discogs::InsertCompanies' do
+    
+  end
+
+end
