ec8bd004dd42861ad769575705d062e71a058c16
ossi <ossi@asgard.ossgard.xx>
2015-12-01 22:08:54 +0100

    Discogs::InsertRelease inserts Labels

--- app/models/discogs/insert_labels.rb
+++ app/models/discogs/insert_labels.rb
@@ -1,2 +1,30 @@
-class Discogs::InsertLabels < ActiveRecord::Base
+class Discogs::InsertLabels
+  def self.perform(dc_labels, album_release)
+    new(dc_labels, album_release).perform
+  end
+
+  def initialize(dc_labels, album_release)
+    @dc_labels = dc_labels
+    @album_release = album_release
+  end
+
+  def perform
+    labels
+  end
+
+  private
+
+  def labels
+    @dc_labels.each do |dc_label|
+      label(dc_label)
+    end
+  end
+
+  def label(dc_label)
+    company = Company.find_or_create_by!(name: dc_label.name)
+    @album_release.labels.create!(
+      company: company,
+      catalog_no: dc_label.catno
+    )
+  end
 end
