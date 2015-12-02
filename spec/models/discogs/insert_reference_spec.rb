b35e09c58e0162bbd936a4ce067d7f20a2688c36
ossi <ossi@asgard.ossgard.xx>
2015-12-02 22:39:06 +0100

    Discogs::InsertReference works

--- spec/models/discogs/insert_reference_spec.rb
+++ spec/models/discogs/insert_reference_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Discogs::InsertReference, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  context 'compilation_head' do
+    before(:all) do
+      DatabaseCleaner.start
+      @album_head = FactoryGirl.create(:album_head)
+    end
+
+    it 'sets the reference' do
+      Discogs::InsertReference.perform(123, @album_head, ChReference)
+      expect(@album_head.reference.identifier).to eq('123')
+      expect(@album_head.reference.supplier.name).to eq('Discogs')
+    end
+
+    after(:all) { DatabaseCleaner.clean }
+  end
+
+  context 'compilation_release' do
+    before(:all) do
+      DatabaseCleaner.start
+      @album_release = FactoryGirl.create(:album_release)
+    end
+
+    it 'sets the reference' do
+      Discogs::InsertReference.perform('abc', @album_release, CrReference)
+      expect(@album_release.reference.identifier).to eq('abc')
+      expect(@album_release.reference.supplier.name).to eq('Discogs')
+    end
+
+    after(:all) { DatabaseCleaner.clean }
+  end
 end
