ae877fa6c1901a7235367cb8681fd803387ef0b6
ossi <ossi@asgard.ossgard.xx>
2015-12-02 22:39:06 +0100

    Discogs::InsertReference works

--- app/models/discogs/insert_reference.rb
+++ app/models/discogs/insert_reference.rb
@@ -1,2 +1,26 @@
-class Discogs::InsertReference < ActiveRecord::Base
+class Discogs::InsertReference
+  def self.perform(identifier, owner, klass)
+    new(identifier, owner, klass).perform
+  end
+
+  def initialize(identifier, owner, klass)
+    @identifier = identifier
+    @owner      = owner
+    @klass      = klass
+  end
+
+  def perform
+    reference
+  end
+
+  private
+
+  def reference
+    supplier = DataSupplier.find_or_create_by!(name: 'Discogs')
+    reference = @klass.create!(
+      identifier: @identifier,
+      supplier:   supplier
+    )
+    @owner.reference = reference
+  end
 end
