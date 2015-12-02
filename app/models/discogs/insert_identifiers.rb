ee7abd912b40e0148715b6a5099cfad1ea27fcce
ossi <ossi@asgard.ossgard.xx>
2015-12-02 21:16:32 +0100

    New model Discogs::InsertIdentifiers
    $(rails generate model Discogs::InsertIdentifiers --no-migration --force-plural)

---
+++ app/models/discogs/insert_identifiers.rb
@@ -0,0 +1,2 @@
+class Discogs::InsertIdentifiers < ActiveRecord::Base
+end
---
+++ spec/factories/discogs_insert_identifiers.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :discogs_insert_identifier, :class => 'Discogs::InsertIdentifiers' do
+    
+  end
+
+end
