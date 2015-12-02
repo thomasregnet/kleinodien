355653304f7fa91dbef549f843238fe41cdc42e2
ossi <ossi@asgard.ossgard.xx>
2015-12-02 20:12:09 +0100

    New model Discogs::InsertTracklist
    $(rails generate model Discogs::InsertTracklist --no-migration)

---
+++ app/models/discogs/insert_tracklist.rb
@@ -0,0 +1,2 @@
+class Discogs::InsertTracklist < ActiveRecord::Base
+end
---
+++ spec/factories/discogs_insert_tracklists.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :discogs_insert_tracklist, :class => 'Discogs::InsertTracklist' do
+    
+  end
+
+end
