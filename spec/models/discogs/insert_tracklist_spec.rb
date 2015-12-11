789ae450e08fb92578add8b101ecc19fd2ec92e2
ossi <ossi@asgard.ossgard.xx>
2015-12-11 10:53:02 +0100

    Discogs::InsertTracklist inserts credits

--- spec/models/discogs/insert_tracklist_spec.rb
+++ spec/models/discogs/insert_tracklist_spec.rb
@@ -2,33 +2,64 @@ require 'rails_helper'
 require 'discogs_test_helper'
 
 RSpec.describe Discogs::InsertTracklist, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
-  context 'AC/DC - Highway To Hell' do
+  # context 'AC/DC - Highway To Hell' do
+  #   before(:all) do
+  #     DatabaseCleaner.start
+      
+  #     json = DiscogsTestHelper.get_discogs_data('releases', 940468)
+  #     dc_release = KleinodienDiscogs.get_release(json)
+  #     @album_release = FactoryGirl.create(:album_release)
+  #     Discogs::InsertTracklist.perform(dc_release.get_media, @album_release)
+  #     @tracks = @album_release.tracks
+  #   end
+
+  #   it 'has the tracks set' do
+  #     track = @tracks[0]
+  #     expect(track.position).to eq('1')
+  #     expect(track.release.title).to eq('Highway To Hell')
+
+  #     track = @tracks[4]
+  #     expect(track.position).to eq('5')
+  #     expect(track.release.title).to eq('Beating Around The Bush')
+      
+  #     track = @tracks[9]
+  #     expect(track.position).to eq('10')
+  #     expect(track.release.title).to eq('Night Prowler')      
+  #   end
+    
+  #   after(:all) { DatabaseCleaner.clean }
+  # end
+
+  context 'Jello Biafra With Nomeansno - The Sky is Fallin And ...' do
     before(:all) do
       DatabaseCleaner.start
       
-      json = DiscogsTestHelper.get_discogs_data('releases', 940468)
+      json = DiscogsTestHelper.get_discogs_data('releases', 1083888)
       dc_release = KleinodienDiscogs.get_release(json)
       @album_release = FactoryGirl.create(:album_release)
-      #@release = Discogs::InsertRelease.perform(dc_release)
       Discogs::InsertTracklist.perform(dc_release.get_media, @album_release)
       @tracks = @album_release.tracks
     end
 
-    it 'has the tracks set' do
+    it 'has inserted the tracks and its extraartists' do
       track = @tracks[0]
-      expect(track.position).to eq('1')
-      expect(track.release.title).to eq('Highway To Hell')
+      expect(track.release.title)
+        .to eq('The Sky Is Falling, And I Want My Mommy (Falling Space Junk)')
+      credit = track.release.credits[0]
+      expect(credit.artist_credit.name).to eq('Nomeansno')
 
-      track = @tracks[4]
-      expect(track.position).to eq('5')
-      expect(track.release.title).to eq('Beating Around The Bush')
-      
-      track = @tracks[9]
-      expect(track.position).to eq('10')
-      expect(track.release.title).to eq('Night Prowler')      
+      credit = @tracks[1].release.credits[0]
+      expect(credit).to be_nil
+
+      track = @tracks[2]
+      expect(track.release.title).to eq("Bruce's Diary")
+      credits = track.release.credits
+      expect(credits[0].artist_credit.name).to eq('John Wright')
+
+      expect(credits[1].artist_credit.name).to eq('The Totaliterrortones')
+      expect(credits[1].job.name).to eq('Horns')
     end
-    
+
     after(:all) { DatabaseCleaner.clean }
   end
 end
