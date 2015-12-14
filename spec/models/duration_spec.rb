670abdc7397a02657c6d6605bbb86edf4dae7fd1
ossi <ossi@asgard.ossgard.xx>
2015-12-14 20:24:15 +0100

    Attr_reader and constructor for Duration

--- spec/models/duration_spec.rb
+++ spec/models/duration_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Duration, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  describe 'constructor' do
+    context 'without accuracy' do
+      before(:all) do
+        @duration = Duration.new(300123)
+      end
+
+      it 'has the expected values set' do
+        expect(@duration.milliseconds).to eq(300123)
+        expect(@duration.accuracy).to eq('millisecond')
+      end
+    end
+  end
 end
