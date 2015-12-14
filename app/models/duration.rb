f5ab2e6a6c57b0155f2a26f965e7ff3cbb35b337
ossi <ossi@asgard.ossgard.xx>
2015-12-14 21:05:14 +0100

    More constructors for Duration

--- app/models/duration.rb
+++ app/models/duration.rb
@@ -12,6 +12,24 @@ class Duration
   def self.hours(hours)
     new(hours * 3_600_000, 'hour')
   end
+
+  def self.mmss(mmss)
+    mm, ss = mmss.split(':')
+    milliseconds = (mm.to_i * 60_000) + (ss.to_i * 1000)
+    new(milliseconds, 'second')
+  end
+
+  def self.hhmm(hhmm)
+    hh, mm = hhmm.split(':')
+    milliseconds = (hh.to_i * 3_600_000) + (mm.to_i * 60_000)
+    new(milliseconds, 'minute')
+  end
+
+  def self.hhmmss(hhmmss)
+    hh, mm, ss = hhmmss.split(':')
+    milliseconds = (hh.to_i * 3_600_000) + (mm.to_i * 60_000) + (ss.to_i * 1000)
+    new(milliseconds, 'second')
+  end
   
   def initialize(milliseconds, accuracy = 'millisecond')
     @milliseconds = milliseconds
--- spec/models/duration_spec.rb
+++ spec/models/duration_spec.rb
@@ -44,8 +44,40 @@ RSpec.describe Duration, type: :model do
 
     it 'has the expected values set' do
       expect(@duration.milliseconds).to eq(25200000)
-      expect(@duration.milliseconds).to eq(25200000)
       expect(@duration.accuracy).to eq('hour')
+    end
+  end
+
+  describe 'constructor "mmss"' do
+    before(:all) do
+      @duration = Duration.mmss('3:11')
+    end
+
+    it 'has the expected values set' do
+      expect(@duration.milliseconds).to eq(191_000)
+      expect(@duration.accuracy).to eq('second')
+    end
+  end
+
+  describe 'constructor "mmss"' do
+    before(:all) do
+      @duration = Duration.hhmm('4:56')
+    end
+
+    it 'has the expected values set' do
+      expect(@duration.milliseconds).to eq(17760000)
+      expect(@duration.accuracy).to eq('minute')
+    end
+  end
+
+  describe 'constructor "hhmmss"' do
+    before(:all) do
+      @duration = Duration.hhmmss('19:44:13')
+    end
+
+    it 'has the expected values set' do
+      expect(@duration.milliseconds).to eq(71053000)
+      expect(@duration.accuracy).to eq('second')
     end
   end
 end
