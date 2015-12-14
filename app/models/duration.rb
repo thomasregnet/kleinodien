30647498a20308114227c20fea7921d0b0bd35ea
ossi <ossi@asgard.ossgard.xx>
2015-12-14 21:12:30 +0100

    Duration uses constant instead of bare integers

--- app/models/duration.rb
+++ app/models/duration.rb
@@ -1,33 +1,39 @@
 class Duration
+  SECOND_MS = 1000
+  MINUTE_MS = 60_000
+  HOUR_MS   = 3_600_000
+
   attr_reader :milliseconds, :accuracy
 
   def self.seconds(seconds)
-    new(seconds * 1000, 'second')
+    new(seconds * SECOND_MS, 'second')
   end
 
   def self.minutes(minutes)
-    new(minutes * 60_000, 'minute')
+    new(minutes * MINUTE_MS, 'minute')
   end
 
   def self.hours(hours)
-    new(hours * 3_600_000, 'hour')
+    new(hours * HOUR_MS, 'hour')
   end
 
   def self.mmss(mmss)
     mm, ss = mmss.split(':')
-    milliseconds = (mm.to_i * 60_000) + (ss.to_i * 1000)
+    milliseconds = (mm.to_i * MINUTE_MS) + (ss.to_i * SECOND_MS)
     new(milliseconds, 'second')
   end
 
   def self.hhmm(hhmm)
     hh, mm = hhmm.split(':')
-    milliseconds = (hh.to_i * 3_600_000) + (mm.to_i * 60_000)
+    milliseconds = (hh.to_i * HOUR_MS) + (mm.to_i * MINUTE_MS)
     new(milliseconds, 'minute')
   end
 
   def self.hhmmss(hhmmss)
     hh, mm, ss = hhmmss.split(':')
-    milliseconds = (hh.to_i * 3_600_000) + (mm.to_i * 60_000) + (ss.to_i * 1000)
+    milliseconds = (hh.to_i * HOUR_MS) +
+                   (mm.to_i * MINUTE_MS) +
+                   (ss.to_i * SECOND_MS)
     new(milliseconds, 'second')
   end
   
