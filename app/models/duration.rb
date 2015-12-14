4b4fb8458e3300cadc4e33ff4a5d0f32b95ce9d9
ossi <ossi@asgard.ossgard.xx>
2015-12-14 20:39:36 +0100

    Alternative constructors for Duration

--- app/models/duration.rb
+++ app/models/duration.rb
@@ -1,6 +1,18 @@
 class Duration
   attr_reader :milliseconds, :accuracy
 
+  def self.seconds(seconds)
+    new(seconds * 1000, 'second')
+  end
+
+  def self.minutes(minutes)
+    new(minutes * 60_000, 'minute')
+  end
+
+  def self.hours(hours)
+    new(hours * 3_600_000, 'hour')
+  end
+  
   def initialize(milliseconds, accuracy = 'millisecond')
     @milliseconds = milliseconds
     @accuracy     = accuracy
