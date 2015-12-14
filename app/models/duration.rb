f1624b8e7499e8a48f82dfad066c3a7ac5dbaff0
ossi <ossi@asgard.ossgard.xx>
2015-12-14 20:24:15 +0100

    Attr_reader and constructor for Duration

--- app/models/duration.rb
+++ app/models/duration.rb
@@ -1,2 +1,8 @@
-class Duration < ActiveRecord::Base
+class Duration
+  attr_reader :milliseconds, :accuracy
+
+  def initialize(milliseconds, accuracy = 'millisecond')
+    @milliseconds = milliseconds
+    @accuracy     = accuracy
+  end
 end
