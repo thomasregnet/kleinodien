ff915439744c5f9af0bf664ee9e0649baa32e549
ossi <ossi@asgard.ossgard.xx>
2015-12-15 20:55:21 +0100

    Re-factored constructor specs in duration_spec.rb

--- spec/models/duration_spec.rb
+++ spec/models/duration_spec.rb
@@ -1,93 +1,109 @@
 require 'rails_helper'
 
 RSpec.describe Duration, type: :model do
-  describe 'constructor' do
+  describe '.new' do
     context 'without accuracy' do
-      before(:all) do
-        @duration = Duration.new(300123)
+      duration = Duration.new(300123)
+
+      it 'has the milliseconds set' do
+        expect(duration.milliseconds).to eq(300123)
       end
 
-      it 'has the expected values set' do
-        expect(@duration.milliseconds).to eq(300123)
-        expect(@duration.accuracy).to eq('millisecond')
+      it 'has the accuracy set' do
+        expect(duration.accuracy).to eq('millisecond')
+      end
+    end
+
+    context 'with accuracy' do
+      duration = Duration.new(123456, 'second')
+
+      it 'uses the given accuracy' do
+        expect(duration.accuracy).to eq('second')
       end
     end
   end
 
-  describe 'construtor "seconds"' do
-    before(:all) do
-      @duration = Duration.seconds(322)
+  describe '.hours' do
+    duration = Duration.hours(7)
+
+    it 'has the milliseconds set' do
+      expect(duration.milliseconds).to eq(25200000)
     end
 
-    it 'has the expected values set' do
-      expect(@duration.milliseconds).to eq(322000)
-      expect(@duration.accuracy).to eq('second')
-    end
-  end
-
-  describe 'constructor "milliseconds"' do
-    before(:all) do
-      @duration = Duration.new(999_987_123)
-    end
-
-    it 'has the expected values set' do
-      expect(@duration.milliseconds).to eq(999_987_123)
-      expect(@duration.accuracy).to eq('millisecond')
+    it 'has the accuracy set' do
+      expect(duration.accuracy).to eq('hour')
     end
   end
 
-  describe 'construtor "seconds"' do
-    before(:all) do
-      @duration = Duration.minutes(10)
+  describe '.minutes' do
+    duration = Duration.minutes(10)
+
+    it 'has the milliseconds set' do
+      expect(duration.milliseconds).to eq(600_000)
     end
 
-    it 'has the expected values set' do
-      expect(@duration.milliseconds).to eq(600_000)
-      expect(@duration.accuracy).to eq('minute')
+    it 'has the accuracy set' do
+      expect(duration.accuracy).to eq('minute')
+    end
+  end
+    
+  describe '.seconds' do
+    duration = Duration.seconds(322)
+
+    it 'has the milliseconds set' do
+      expect(duration.milliseconds).to eq(322000)
+    end
+
+    it 'has the accuracy set' do
+      expect(duration.accuracy).to eq('second')
     end
   end
 
-  describe 'constructor "hours"' do
-    before(:all) do
-      @duration = Duration.hours(7)
+  describe '.milliseconds' do
+    duration = Duration.new(999_987_123)
+
+    it 'has the milliseconds set' do
+      expect(duration.milliseconds).to eq(999_987_123)
     end
 
-    it 'has the expected values set' do
-      expect(@duration.milliseconds).to eq(25200000)
-      expect(@duration.accuracy).to eq('hour')
-    end
-  end
-
-  describe 'constructor "mmss"' do
-    before(:all) do
-      @duration = Duration.mmss('3:11')
-    end
-
-    it 'has the expected values set' do
-      expect(@duration.milliseconds).to eq(191_000)
-      expect(@duration.accuracy).to eq('second')
+    it 'has the accuracy set' do
+      expect(duration.accuracy).to eq('millisecond')
     end
   end
 
-  describe 'constructor "mmss"' do
-    before(:all) do
-      @duration = Duration.hhmm('4:56')
+  describe '.mmss' do
+    duration = Duration.mmss('3:11')
+
+    it 'has the milliseconds set' do
+      expect(duration.milliseconds).to eq(191_000)
     end
 
-    it 'has the expected values set' do
-      expect(@duration.milliseconds).to eq(17760000)
-      expect(@duration.accuracy).to eq('minute')
+    it 'has the accuracy set' do
+      expect(duration.accuracy).to eq('second')
     end
   end
 
-  describe 'constructor "hhmmss"' do
-    before(:all) do
-      @duration = Duration.hhmmss('19:44:13')
+  describe '.mmss' do
+    duration = Duration.hhmm('4:56')
+
+    it 'has the milliseconds set' do
+      expect(duration.milliseconds).to eq(17760000)
     end
 
-    it 'has the expected values set' do
-      expect(@duration.milliseconds).to eq(71053000)
-      expect(@duration.accuracy).to eq('second')
+    it 'has the accuracy set' do
+      expect(duration.accuracy).to eq('minute')
+    end
+  end
+
+  describe '.hhmmss' do
+    duration = Duration.hhmmss('19:44:13')
+
+    it 'has the milliseconds set' do
+      expect(duration.milliseconds).to eq(71053000)
+    end
+    
+    it 'has the accuracy set' do
+      expect(duration.accuracy).to eq('second')
     end
   end
 
