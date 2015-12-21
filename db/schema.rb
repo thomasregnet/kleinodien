85459d365cfa002e1b7317d2d3a806b4b1ea54b9
ossi <ossi@asgard.ossgard.xx>
2015-12-21 10:42:58 +0100

    Specs for Track#duration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103202204) do
+ActiveRecord::Schema.define(version: 20151221090925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -497,6 +497,8 @@ ActiveRecord::Schema.define(version: 20151103202204) do
     t.string   "position"
     t.string   "heading"
     t.integer  "tr_format_kind_id"
+    t.integer  "milliseconds"
+    t.string   "accuracy"
   end
 
   create_table "users", force: :cascade do |t|
