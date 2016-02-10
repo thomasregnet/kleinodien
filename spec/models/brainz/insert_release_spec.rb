4e9820c3d551551a0186286fe56ed3918977de33
ossi <ossi@asgard.ossgard.xx>
2016-02-10 21:27:16 +0100

    New model Brainz::InsertArtistCredit
    $(rails generate model Brainz::InsertArtistCredit
            --no-force-plural
            --no-migration)

--- spec/models/brainz/insert_release_spec.rb
+++ spec/models/brainz/insert_release_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
+require 'brainz_test_helper'
 
 RSpec.describe Brainz::InsertRelease, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'AC/DC - Highway to Hell' do
+    before(:all) do
+      DatabaseCleaner.start
+      @brz_release = BrainzTestHelper
+                     .get_release('c8f7094d-ce27-365e-961f-0af27321be08')
+      @release = Brainz::InsertRelease.perform(@brz_release)
+    end
+
+    it 'works'
+    
+    after(:all) { DatabaseCleaner.clean }
+  end
 end
