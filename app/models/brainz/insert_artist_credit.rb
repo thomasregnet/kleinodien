2c98e47c320d0f8284efde7fcca2f2d6ac5711b5
ossi <ossi@asgard.ossgard.xx>
2016-02-10 21:57:43 +0100

    Brainz::InsertArtistCredit seems to work

--- app/models/brainz/insert_artist_credit.rb
+++ app/models/brainz/insert_artist_credit.rb
@@ -1,2 +1,38 @@
-class Brainz::InsertArtistCredit < ActiveRecord::Base
+class Brainz::InsertArtistCredit
+  def self.perform(brz_artist_credit)
+    new(brz_artist_credit).perform
+  end
+
+  def initialize(brz_artist_credit)
+    @brz_artist_credit = brz_artist_credit
+  end
+
+  def perform
+    ac_name = @brz_artist_credit.joined_artists
+    ArtistCredit.find_by(name: ac_name) || create_artist_credit
+  end
+
+  private
+  
+  def create_artist_credit
+    @artist_credit = ArtistCredit.new
+    @brz_artist_credit.name_credits.each_with_index do |brz_name_credit, no|
+      participant(brz_name_credit, no)
+    end
+    @artist_credit.save!
+    @artist_credit
+  end
+
+  def participant(brz_name_credit, no)
+    brz_artist = brz_name_credit.artist
+    artist = Artist.where('lower(name) = ?', brz_artist.name.downcase).first
+    artist = Artist.create!(name: brz_artist.name) unless artist
+
+    join_phrase = brz_name_credit.joinphrase
+    @artist_credit.participants.build(
+      artist:      artist,
+      join_phrase: join_phrase,
+      no:          no
+    )
+  end
 end
--- spec/models/brainz/insert_artist_credit_spec.rb
+++ spec/models/brainz/insert_artist_credit_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
+require 'brainz_test_helper'
 
 RSpec.describe Brainz::InsertArtistCredit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'AC/DC - Highway to Hell' do
+    before(:all) do
+      DatabaseCleaner.start
+      brz_release = BrainzTestHelper
+                    .get_release('c8f7094d-ce27-365e-961f-0af27321be08')
+      @artist_credit = Brainz::InsertArtistCredit
+                       .perform(brz_release.artist_credit)
+    end
+
+    specify '#name' do
+      expect(@artist_credit.name).to eq 'AC/DC'
+    end
+
+    after(:all) { DatabaseCleaner.clean }
+  end
 end
