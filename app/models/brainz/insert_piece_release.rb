e3783183973e61fe9cf51ab5b059df5447a58329
ossi <ossi@asgard.ossgard.xx>
2016-02-15 20:23:33 +0100

    Skeletal structure for Brainz::InsertPieceReleases

--- app/models/brainz/insert_piece_release.rb
+++ app/models/brainz/insert_piece_release.rb
@@ -1,2 +1,13 @@
-class Brainz::InsertPieceRelease < ActiveRecord::Base
+class Brainz::InsertPieceRelease
+  def self.perform(brz_release, release)
+    new(brz_release, release).perform
+  end
+
+  def initialize(brz_release, release)
+    @brz_release = brz_release
+    @release     = release
+  end
+
+  def perform
+  end
 end
