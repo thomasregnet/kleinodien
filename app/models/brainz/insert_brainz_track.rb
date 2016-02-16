909f2a875f19def08b0e447e715b5ba2415df5e0
ossi <ossi@asgard.ossgard.xx>
2016-02-16 21:46:26 +0100

    Started with Brainz::InsertBrainzTrack

--- app/models/brainz/insert_brainz_track.rb
+++ app/models/brainz/insert_brainz_track.rb
@@ -1,2 +1,37 @@
-class Brainz::InsertBrainzTrack < ActiveRecord::Base
+class Brainz::InsertBrainzTrack
+  def self.perform(brz_track, release, no, side_name)
+    new(brz_track, release, no, side_name).perform
+  end
+
+  def initialize(brz_track, release, no, side_name)
+    @brz_track = brz_track
+    @release   = release
+    @no        = no
+    @side_name = side_name
+  end
+
+  def perform
+    brz_recording = @brz_track.recording
+    # TODO: real ArtistCredit for SongHead
+    @song_head = SongHead.create!(
+      artist_credit: @release.head.artist_credit,
+      title:         brz_recording.title
+    )
+
+    perform_song_release
+    t = @release.tracks.create!(
+      release:  @song_release,
+      no:       @no,
+      side:     @side_name,
+      position: @brz_track.number
+    )
+
+    #byebug
+  end
+
+  private
+
+  def perform_song_release
+    @song_release = @song_head.releases.create!
+  end
 end
