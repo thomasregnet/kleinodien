c9ae64afcbb026b5870d8db3b44bdb08e679a5bb
ossi <ossi@asgard.ossgard.xx>
2016-02-17 20:12:37 +0100

    Started refactoring of MusicBrainz Track insertion

--- app/models/brainz/insert_brainz_track.rb
+++ app/models/brainz/insert_brainz_track.rb
@@ -1,35 +1,33 @@
 class Brainz::InsertBrainzTrack
-  def self.perform(brz_track, release, no, side_name)
-    new(brz_track, release, no, side_name).perform
+  def self.perform(brz_track, artist_credit)
+    new(brz_track, artist_credit).perform
   end
 
-  def initialize(brz_track, release, no, side_name)
-    @brz_track = brz_track
-    @release   = release
-    @no        = no
-    @side_name = side_name
+  def initialize(brz_track, artist_credit)
+    @brz_track     = brz_track
+    @artist_credit = artist_credit
   end
 
-  def perform
-    brz_recording = @brz_track.recording
-    # TODO: real ArtistCredit for SongHead
+  def perform 
+   brz_recording = @brz_track.recording
+
     @song_head = SongHead.create!(
-      artist_credit: @release.head.artist_credit,
+      artist_credit: artist_credit,
       title:         brz_recording.title
     )
 
     perform_song_release
-    t = @release.tracks.create!(
-      release:  @song_release,
-      no:       @no,
-      side:     @side_name,
-      position: @brz_track.number
-    )
-
-    #byebug
   end
 
   private
+
+  def artist_credit
+    brz_artist_credit = @brz_track.recording.artist_credit
+    # TODO: ArtistCredit of the Recording for SongHead
+    # TODO: find better way to determinate if brz_artist_credit is nil
+    return @artist_credit unless brz_artist_credit.node
+    Brainz::InsertArtistCredit.perform(brz_artist_credit)
+  end
 
   def perform_song_release
     @song_release = @song_head.releases.create!
