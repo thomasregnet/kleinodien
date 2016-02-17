13fc9adb324d29b4feab55d56bd04a5a11301077
ossi <ossi@asgard.ossgard.xx>
2016-02-17 20:21:00 +0100

    Little refactoring in module Brainz

--- app/models/brainz/insert_brainz_track.rb
+++ app/models/brainz/insert_brainz_track.rb
@@ -1,35 +1,38 @@
-class Brainz::InsertBrainzTrack
-  def self.perform(brz_track, artist_credit)
-    new(brz_track, artist_credit).perform
-  end
+module Brainz
+  # Insert a MusicBrainz-Track, returns a SongRelease
+  class InsertBrainzTrack
+    def self.perform(brz_track, artist_credit)
+      new(brz_track, artist_credit).perform
+    end
 
-  def initialize(brz_track, artist_credit)
-    @brz_track     = brz_track
-    @artist_credit = artist_credit
-  end
+    def initialize(brz_track, artist_credit)
+      @brz_track     = brz_track
+      @artist_credit = artist_credit
+    end
 
-  def perform 
-   brz_recording = @brz_track.recording
+    def perform
+      brz_recording = @brz_track.recording
 
-    @song_head = SongHead.create!(
-      artist_credit: artist_credit,
-      title:         brz_recording.title
-    )
+      @song_head = SongHead.create!(
+        artist_credit: artist_credit,
+        title:         brz_recording.title
+      )
 
-    perform_song_release
-  end
+      perform_song_release
+    end
 
-  private
+    private
 
-  def artist_credit
-    brz_artist_credit = @brz_track.recording.artist_credit
-    # TODO: ArtistCredit of the Recording for SongHead
-    # TODO: find better way to determinate if brz_artist_credit is nil
-    return @artist_credit unless brz_artist_credit.node
-    Brainz::InsertArtistCredit.perform(brz_artist_credit)
-  end
+    def artist_credit
+      brz_artist_credit = @brz_track.recording.artist_credit
+      # TODO: ArtistCredit of the Recording for SongHead
+      # TODO: find better way to determinate if brz_artist_credit is nil
+      return @artist_credit unless brz_artist_credit.node
+      Brainz::InsertArtistCredit.perform(brz_artist_credit)
+    end
 
-  def perform_song_release
-    @song_release = @song_head.releases.create!
+    def perform_song_release
+      @song_release = @song_head.releases.create!
+    end
   end
 end
--- app/models/brainz/insert_piece_release.rb
+++ app/models/brainz/insert_piece_release.rb
@@ -1,13 +1,16 @@
-class Brainz::InsertPieceRelease
-  def self.perform(brz_release, release)
-    new(brz_release, release).perform
-  end
+module Brainz
+  # Insert a song from MusicBrainz
+  class InsertPieceRelease
+    def self.perform(brz_release, release)
+      new(brz_release, release).perform
+    end
 
-  def initialize(brz_release, release)
-    @brz_release = brz_release
-    @release     = release
-  end
+    def initialize(brz_release, release)
+      @brz_release = brz_release
+      @release     = release
+    end
 
-  def perform
+    def perform
+    end
   end
 end
