13f44893dbf63622745774a7c9d8d642c9106521
ossi <ossi@asgard.ossgard.xx>
2016-02-22 21:39:37 +0100

    Extracted Discogs::InsertParticipant from ...
    Discogs::InsertArtists

---
+++ app/models/discogs/insert_participant.rb
@@ -0,0 +1,29 @@
+module Discogs
+  class InsertParticipant
+    def self.perform(dc_artist, no, artist_credit)
+      new(dc_artist, no, artist_credit).perform
+    end
+
+    def initialize(dc_artist, no, artist_credit)
+      @dc_artist     = dc_artist
+      @no            = no
+      @artist_credit = artist_credit
+    end
+
+    def perform
+      # TODO: move finding of unique case insensitive
+      #       artists to the Artist model
+
+      dc_artist_name = @dc_artist.name
+      artist = Artist.where('lower(name) = ?', dc_artist_name.downcase).first
+      artist = Artist.create!(name: dc_artist_name) unless artist
+
+      join_phrase = @dc_artist.join  unless @dc_artist.join.blank?
+      @artist_credit.participants.build(
+        artist:      artist,
+        join_phrase: join_phrase,
+        no:          @no
+      )
+    end
+  end
+end
