11d7292674812bc9100975543c1b6197ae3c4bee
ossi <ossi@asgard.ossgard.xx>
2016-02-22 21:39:37 +0100

    Extracted Discogs::InsertParticipant from ...
    Discogs::InsertArtists

--- app/models/discogs/insert_artists.rb
+++ app/models/discogs/insert_artists.rb
@@ -23,24 +23,10 @@ module Discogs
     def new_artist_credit
       artist_credit = ArtistCredit.new
       @artists.each_with_index do |artist, no|
-        participant(artist, no, artist_credit)
+        Discogs::InsertParticipant.perform(artist, no, artist_credit)
       end
       artist_credit.save!
       artist_credit
-    end
-
-    def participant(dc_artist, no, artist_credit)
-      # TODO: move finding of unique case insensitive artists to the Artist model
-
-      artist = Artist.where('lower(name) = ?', dc_artist.name.downcase).first
-      artist = Artist.create!(name: dc_artist.name) unless artist
-
-      join_phrase = dc_artist.join  unless dc_artist.join.blank?
-      artist_credit.participants.build(
-        artist:      artist,
-        join_phrase: join_phrase,
-        no:          no
-      )
     end
   end
 end
