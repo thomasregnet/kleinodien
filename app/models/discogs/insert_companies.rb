a69e75c6d9f22210176a2ab6193f48b831157276
ossi <ossi@asgard.ossgard.xx>
2016-02-22 22:19:24 +0100

    Extracted Discogs::InsertCompany form ...
    Discogs::InsertCompanies

--- app/models/discogs/insert_companies.rb
+++ app/models/discogs/insert_companies.rb
@@ -1,35 +1,26 @@
-class Discogs::InsertCompanies
-  def self.perform(dc_companies, album_release)
-    new(dc_companies, album_release).perform
-  end
-
-  def initialize(dc_companies, album_release)
-    @dc_companies  = dc_companies
-    @album_release = album_release
-  end
-
-  def perform
-    companies
-  end
-
-  private
-
-  def companies
-    @dc_companies.each do |dc_company|
-      company(dc_company)
+module Discogs
+  # Inserts Companies from Discogs
+  class InsertCompanies
+    def self.perform(dc_companies, album_release)
+      new(dc_companies, album_release).perform
     end
-  end
-  
-  def company(dc_company)
-    company = Company.find_or_create_by!(name: dc_company.name)
-    @album_release.companies.create!(
-      catalog_no:   dc_company.catno,
-      company:      company,
-      company_role: role(dc_company.entity_type_name)
-    )
-  end
 
-  def role(entity_type_name)
-    CompanyRole.find_or_create_by!(name: entity_type_name)
+    def initialize(dc_companies, album_release)
+      @dc_companies  = dc_companies
+      @album_release = album_release
+    end
+
+    def perform
+      companies
+    end
+
+    private
+
+    def companies
+      @dc_companies.each do |dc_company|
+        @dc_company = dc_company
+        Discogs::InsertCompany.perform(dc_company, @album_release)
+      end
+    end
   end
 end
