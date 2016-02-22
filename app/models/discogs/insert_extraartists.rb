090707bbc3ba19444557ab3eaedd2683c587e22d
ossi <ossi@asgard.ossgard.xx>
2016-02-22 22:26:55 +0100

    Refactored Discogs::InsertExtraartists

--- app/models/discogs/insert_extraartists.rb
+++ app/models/discogs/insert_extraartists.rb
@@ -1,40 +1,35 @@
-class Discogs::InsertExtraartists
-  def self.perform(dc_extraartists, owner)
-    new(dc_extraartists, owner).perform
-  end
-
-  def initialize(dc_extraartists, owner)
-    @dc_extraartists = dc_extraartists
-    @owner   = owner
-  end
-
-  def perform
-    return unless @dc_extraartists
-    extraartists
-  end
-
-  private
-
-  def extraartists
-    @dc_extraartists.each do |dc_artist|
-      artist_credit = artist_credit(dc_artist)
-      job           = job(dc_artist.role)
-      extraartist(artist_credit, job)
+module Discogs
+  # Insert Extraartists for Tracks and Releases
+  class InsertExtraartists
+    def self.perform(dc_extraartists, owner)
+      new(dc_extraartists, owner).perform
     end
-  end
 
-  def extraartist(artist_credit, job)
-    @owner.credits.create!(
-      artist_credit: artist_credit,
-      job:           job
-    )
-  end
+    def initialize(dc_extraartists, owner)
+      @dc_extraartists = dc_extraartists
+      @owner           = owner
+    end
 
-  def artist_credit(dc_artist)
-    Discogs::InsertArtists.perform([dc_artist])
-  end
+    def perform
+      return unless @dc_extraartists
+      extraartists
+    end
 
-  def job(role)
-    Job.find_or_create_by!(name: role)
+    private
+
+    def extraartists
+      @dc_extraartists.each do |dc_artist|
+        artist_credit = Discogs::InsertArtists.perform([dc_artist])
+        job           = Job.find_or_create_by!(name: dc_artist.role)
+        extraartist(artist_credit, job)
+      end
+    end
+
+    def extraartist(artist_credit, job)
+      @owner.credits.create!(
+        artist_credit: artist_credit,
+        job:           job
+      )
+    end
   end
 end
