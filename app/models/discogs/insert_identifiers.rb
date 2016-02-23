86989390bcc3335fcb70436a00d29e7112611acc
ossi <ossi@asgard.ossgard.xx>
2016-02-23 20:11:33 +0100

    Refactored Discogs::InsertIdentifiers

--- app/models/discogs/insert_identifiers.rb
+++ app/models/discogs/insert_identifiers.rb
@@ -1,32 +1,26 @@
-class Discogs::InsertIdentifiers
-  def self.perform(dc_identifiers, album_release)
-    new(dc_identifiers, album_release).perform
-  end
-
-  def initialize(dc_identifiers, album_release)
-    @dc_identifiers = dc_identifiers
-    @album_release  = album_release
-  end
-
-  def perform
-    return unless @dc_identifiers
-    identifiers
-  end
-
-  private
-
-  def identifiers
-    @dc_identifiers.each do |dc_identifier|
-      type = identifier_type(dc_identifier.type)
-      @album_release.identifiers.create!(
-        code: dc_identifier.value,
-        type: type,
-        disambiguation: dc_identifier.description
-      )
+module Discogs
+  # Insets Identifiers like barcode, isbn ...
+  class InsertIdentifiers
+    def self.perform(dc_identifiers, album_release)
+      new(dc_identifiers, album_release).perform
     end
-  end
 
-  def identifier_type(name)
-    IdentifierType.find_or_create_by!(name: name)
+    def initialize(dc_identifiers, album_release)
+      @dc_identifiers = dc_identifiers
+      @album_release  = album_release
+    end
+
+    def perform
+      return unless @dc_identifiers
+
+      @dc_identifiers.each do |dc_identifier|
+        type = IdentifierType.find_or_create_by!(name: dc_identifier.type)
+        @album_release.identifiers.create!(
+          code:           dc_identifier.value,
+          type:           type,
+          disambiguation: dc_identifier.description
+        )
+      end
+    end
   end
 end
