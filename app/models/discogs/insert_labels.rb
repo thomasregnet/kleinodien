2bbfc4259a1b19f893e89af72d9f2667eeba0157
ossi <ossi@asgard.ossgard.xx>
2016-02-23 20:39:47 +0100

    Refactored Discogs::InsertLabels

--- app/models/discogs/insert_labels.rb
+++ app/models/discogs/insert_labels.rb
@@ -1,30 +1,29 @@
-class Discogs::InsertLabels
-  def self.perform(dc_labels, album_release)
-    new(dc_labels, album_release).perform
-  end
-
-  def initialize(dc_labels, album_release)
-    @dc_labels = dc_labels
-    @album_release = album_release
-  end
-
-  def perform
-    labels
-  end
-
-  private
-
-  def labels
-    @dc_labels.each do |dc_label|
-      label(dc_label)
+module Discogs
+  # Inserts Labels
+  class InsertLabels
+    def self.perform(dc_labels, album_release)
+      new(dc_labels, album_release).perform
     end
-  end
 
-  def label(dc_label)
-    company = Company.find_or_create_by!(name: dc_label.name)
-    @album_release.labels.create!(
-      company: company,
-      catalog_no: dc_label.catno
-    )
+    def initialize(dc_labels, album_release)
+      @dc_labels = dc_labels
+      @album_release = album_release
+    end
+
+    def perform
+      labels
+    end
+
+    private
+
+    def labels
+      @dc_labels.each do |dc_label|
+        company = Company.find_or_create_by!(name: dc_label.name)
+        @album_release.labels.create!(
+          company:    company,
+          catalog_no: dc_label.catno
+        )
+      end
+    end
   end
 end
