faaea7c3ef4d4bdedd5f7af74b188c9dc5e7c38e
ossi <ossi@asgard.ossgard.xx>
2016-02-29 10:20:35 +0100

    Failing insert release from Brainz and Discogs

---
+++ spec/models/brainz_and_discogs_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'brainz_test_helper'
+require 'discogs_test_helper'
+
+RSpec.describe 'insert a release from Discogs and MusicBrainz' do
+  before(:all) do DatabaseCleaner.start
+    release = BrainzTestHelper
+              .get_release('c8f7094d-ce27-365e-961f-0af27321be08')
+    @brz_release = Brainz::InsertRelease.perform(release)
+
+    json = DiscogsTestHelper.get_discogs_data('releases', 940468)
+    release = KleinodienDiscogs.get_release(json)
+    @dc_release = Discogs::InsertRelease.perform(release)
+  end
+
+  it 'works'
+
+  after(:all) {      DatabaseCleaner.clean }
+end
