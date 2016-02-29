c8d6e9bc287b127fb5fc584d989e48cdcaf4585f
ossi <ossi@asgard.ossgard.xx>
2016-02-29 09:12:20 +0100

    Drop table formats

--- app/models/format.rb
+++
@@ -1,5 +0,0 @@
-class Format < ActiveRecord::Base
-  validates :name, presence: true, blank: false
-  validates :type, presence: true, blank: false
-  validates_uniqueness_of :name, scope: 'type', case_sensitive: false
-end
