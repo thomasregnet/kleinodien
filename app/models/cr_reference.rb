b1a0e102bb8a30a9a2d368404dbba8d7fbf9add6
ossi <ossi@asgard.ossgard.xx>
2016-03-02 12:44:05 +0100

    New method CrReference.create_with_supplier_name!

--- app/models/cr_reference.rb
+++ app/models/cr_reference.rb
@@ -1,3 +1,8 @@
 class CrReference < Reference
   has_one :compilation_release, foreign_key: 'reference_id'
+
+  def self.create_with_supplier_name!(foreign_id, supplier_name)
+    data_supplier = DataSupplier.find_or_create_by(name: supplier_name)
+    create!(identifier: foreign_id, supplier: data_supplier)
+  end
 end
