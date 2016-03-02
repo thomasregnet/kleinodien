0bb9920768546922de3fbddb8be495017b010543
ossi <ossi@asgard.ossgard.xx>
2016-03-02 12:52:47 +0100

    New method Reference.create_with_supplier_name!
    used by ChReference and CrReference

--- app/models/ch_reference.rb
+++ app/models/ch_reference.rb
@@ -2,7 +2,6 @@ class ChReference < Reference
   has_one :compilation_head, foreign_key: 'reference_id'
 
   def self.create_with_supplier_name!(foreign_id, supplier_name)
-    data_supplier = DataSupplier.find_or_create_by(name: supplier_name)
-    create!(identifier: foreign_id, supplier: data_supplier)
+    Reference.create_with_supplier_name!(self, foreign_id, supplier_name)
   end
 end
