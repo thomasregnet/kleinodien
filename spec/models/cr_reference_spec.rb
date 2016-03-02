bbccf8f7c22248b31c262ee5ebf31002a4c21eeb
ossi <ossi@asgard.ossgard.xx>
2016-03-02 14:45:26 +0100

    shared_examples 'a reference'

--- spec/models/cr_reference_spec.rb
+++ spec/models/cr_reference_spec.rb
@@ -1,25 +1,29 @@
 require 'rails_helper'
+require 'shared_examples_for_references'
 
 RSpec.describe CrReference, type: :model do
-  describe '.create_with_supplier_name!' do
-    before(:all) do
-      DatabaseCleaner.start
-      @foreign_id    = 'abc-123'
-      @supplier_name = 'DataSupplier'
-      @reference     = CrReference.create_with_supplier_name!(
-        @foreign_id,
-        @supplier_name
-      )
-    end
-
-    specify '#identifier' do
-      expect(@reference.identifier).to eq @foreign_id
-    end
-
-    specify '#supplier' do
-      expect(@reference.supplier.name).to eq @supplier_name
-    end
-
-    after(:all) { DatabaseCleaner.clean }
+  it_behaves_like 'a reference' do
+    let(:class_name) { CrReference }
   end
+  # describe '.create_with_supplier_name!' do
+  #   before(:all) do
+  #     DatabaseCleaner.start
+  #     @foreign_id    = 'abc-123'
+  #     @supplier_name = 'DataSupplier'
+  #     @reference     = CrReference.create_with_supplier_name!(
+  #       @foreign_id,
+  #       @supplier_name
+  #     )
+  #   end
+
+  #   specify '#identifier' do
+  #     expect(@reference.identifier).to eq @foreign_id
+  #   end
+
+  #   specify '#supplier' do
+  #     expect(@reference.supplier.name).to eq @supplier_name
+  #   end
+
+  #   after(:all) { DatabaseCleaner.clean }
+  # end
 end
