3bc3805cad0eb1459fa0eecb6fb5a598f92cd258
ossi <ossi@asgard.ossgard.xx>
2016-03-02 12:52:47 +0100

    New method Reference.create_with_supplier_name!
    used by ChReference and CrReference

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -3,4 +3,9 @@ class Reference < ActiveRecord::Base
   validates :supplier, presence: true
   validates :identifier, presence: true, blank: false
   validates :type, presence: true, blank: false
+
+  def self.create_with_supplier_name!(klass, foreign_id, supplier_name)
+    data_supplier = DataSupplier.find_or_create_by(name: supplier_name)
+    klass.create!(identifier: foreign_id, supplier: data_supplier)
+  end
 end
