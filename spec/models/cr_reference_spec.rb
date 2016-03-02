0f2a1506eac7095e8fef70d0930b9f44d0bf0fab
ossi <ossi@asgard.ossgard.xx>
2016-03-02 14:29:47 +0100

    Specs for CrReference.create_with_supplier_name!

--- spec/models/cr_reference_spec.rb
+++ spec/models/cr_reference_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe CrReference, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.create_with_supplier_name!' do
+    before(:all) do
+      DatabaseCleaner.start
+      @foreign_id    = 'abc-123'
+      @supplier_name = 'DataSupplier'
+      @reference     = CrReference.create_with_supplier_name!(
+        @foreign_id,
+        @supplier_name
+      )
+    end
+
+    specify '#identifier' do
+      expect(@reference.identifier).to eq @foreign_id
+    end
+
+    specify '#supplier' do
+      expect(@reference.supplier.name).to eq @supplier_name
+    end
+
+    after(:all) { DatabaseCleaner.clean }
+  end
 end
