5403a7f6d7345733e25c3101aedcd0d2935b6e71
ossi <ossi@asgard.ossgard.xx>
2016-03-03 11:13:56 +0100

    Renamed two indexes on piece_releases

---
+++ db/migrate/20160303100947_rename_some_indxes_on_piece_releases.rb
@@ -0,0 +1,10 @@
+class RenameSomeIndxesOnPieceReleases < ActiveRecord::Migration
+  def change
+    rename_index :piece_releases,
+                 :index_piece_on_piece_head_id_and_lower_version,
+                 :index_piece_releases_on_piece_head_id_and_lower_version
+    rename_index :piece_releases,
+                 :index_piece_on_unique_piece_head_id,
+                 :index_piece_releases_on_unique_piece_head_id
+  end
+end
