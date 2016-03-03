91fc9de991821ed02d1ac1fe2c0d21c028b469a2
ossi <ossi@asgard.ossgard.xx>
2016-03-03 10:33:00 +0100

    Deleted comments from CrfDetail

--- app/models/crf_detail.rb
+++ app/models/crf_detail.rb
@@ -2,12 +2,10 @@ class CrfDetail < ActiveRecord::Base
   belongs_to :format,
              class_name: CrFormat,
              foreign_key: :cr_format_id
-  # belongs_to :kind,
-  #            class_name: CrfDetailKind,
-  #            foreign_key: :crf_attribute_kind_id
   belongs_to :kind,
              class_name: CrfDetailKind,
              foreign_key: :crf_detail_kind_id
+
   validates :format, presence: true
   validates :kind,   presence: true
   validates :no,     presence: true
