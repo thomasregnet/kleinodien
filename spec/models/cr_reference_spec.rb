1c47160e0b4451ca84c607e9d16127bb15b98d4b
ossi <ossi@asgard.ossgard.xx>
2016-03-03 09:08:51 +0100

    Deleted comments from cr_reference_spec.rb

--- spec/models/cr_reference_spec.rb
+++ spec/models/cr_reference_spec.rb
@@ -5,25 +5,4 @@ RSpec.describe CrReference, type: :model do
   it_behaves_like 'a reference' do
     let(:class_name) { CrReference }
   end
-  # describe '.create_with_supplier_name!' do
-  #   before(:all) do
-  #     DatabaseCleaner.start
-  #     @foreign_id    = 'abc-123'
-  #     @supplier_name = 'DataSupplier'
-  #     @reference     = CrReference.create_with_supplier_name!(
-  #       @foreign_id,
-  #       @supplier_name
-  #     )
-  #   end
-
-  #   specify '#identifier' do
-  #     expect(@reference.identifier).to eq @foreign_id
-  #   end
-
-  #   specify '#supplier' do
-  #     expect(@reference.supplier.name).to eq @supplier_name
-  #   end
-
-  #   after(:all) { DatabaseCleaner.clean }
-  # end
 end
