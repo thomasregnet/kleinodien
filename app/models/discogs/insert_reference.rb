e1127e4e2efa3a6cbfec4f14021e945e3a615071
ossi <ossi@asgard.ossgard.xx>
2016-03-03 08:53:13 +0100

    Commented out class Discogs::InsertReference
    ... and its specs.

--- app/models/discogs/insert_reference.rb
+++ app/models/discogs/insert_reference.rb
@@ -1,34 +1,34 @@
 module Discogs
   # creates a Reference to Discgs as DataSupplier
   class InsertReference
-    def self.perform(identifier, owner, klass)
-      new(identifier, owner, klass).perform
-    end
+  #   def self.perform(identifier, owner, klass)
+  #     new(identifier, owner, klass).perform
+  #   end
 
-    def initialize(identifier, owner, klass)
-      @identifier = identifier
-      @owner      = owner
-      @klass      = klass
-    end
+  #   def initialize(identifier, owner, klass)
+  #     @identifier = identifier
+  #     @owner      = owner
+  #     @klass      = klass
+  #   end
 
-    def perform
-      return unless @identifier
-      reference
-    end
+  #   def perform
+  #     return unless @identifier
+  #     reference
+  #   end
 
-    private
+  #   private
 
-    def reference
-      supplier = DataSupplier.find_or_create_by!(name: 'Discogs')
-      reference = create_reference(supplier)
-      @owner.reference = reference
-    end
+  #   def reference
+  #     supplier = DataSupplier.find_or_create_by!(name: 'Discogs')
+  #     reference = create_reference(supplier)
+  #     @owner.reference = reference
+  #   end
 
-    def create_reference(supplier)
-      @klass.create!(
-        identifier: @identifier,
-        supplier:   supplier
-      )
-    end
+  #   def create_reference(supplier)
+  #     @klass.create!(
+  #       identifier: @identifier,
+  #       supplier:   supplier
+  #     )
+  #   end
   end
 end
