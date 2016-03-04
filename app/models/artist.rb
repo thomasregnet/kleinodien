7d711fe7e30f2fdcb81642d3665ee3fe25b75199
ossi <ossi@asgard.ossgard.xx>
2016-03-04 10:06:02 +0100

    Artist behaves_like 'a model with many References'

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,10 @@
 class Artist < ActiveRecord::Base
   belongs_to :reference, class_name: ArtistReference
   has_many :participants, inverse_of: :artist
+  has_and_belongs_to_many :references,
+                          class_name: ArtistReference,
+                          join_table: :artists_references,
+                          association_foreign_key: :reference_id
 
   validates :name, presence: true
   validates_uniqueness_of :name, scope: :disambiguation, case_sensitive: false
--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -9,5 +9,15 @@ FactoryGirl.define do
     factory :artist_with_a_reference do
       association :reference, factory: :artist_reference
     end
+
+    factory :artist_with_many_references do
+      after(:create) do |artist|
+        2.times do
+          artist.references << FactoryGirl.create(:artist_reference)
+        end
+        # artist.references << FactoryGirl.create(:artist_reference)
+        # artist.references << FactoryGirl.create(:artist_reference)
+      end
+    end
   end
 end
