711677c0908c3eecafb2132fbdbae86ecfcf72cd
ossi <ossi@asgard.ossgard.xx>
2016-03-04 09:58:11 +0100

    New table "artists_references"

---
+++ db/migrate/20160304085500_create_artists_references.rb
@@ -0,0 +1,8 @@
+class CreateArtistsReferences < ActiveRecord::Migration
+  def change
+    create_table :artists_references, id: false do |t|
+      t.references :artist,    index: true, foreign_key: true
+      t.references :reference, index: true, foreign_key: true
+    end
+  end
+end
