db0828e254a5aad1dffa48f2ff4a2d4aa27bf91a
ossi <ossi@asgard.ossgard.xx>
2016-03-07 20:46:42 +0100

    ArtistCredit belongs_to DataSupplier

--- spec/factories/artist_credits.rb
+++ spec/factories/artist_credits.rb
@@ -4,5 +4,10 @@ FactoryGirl.define do
     after(:build) do |ac|
       ac.participants << FactoryGirl.build(:participant_without_artist_credit)
     end
+
+    factory :artist_credit_with_data_supplier do
+      #association :data_supplier, factory: :data_supplier
+      data_supplier
+    end
   end
 end
--- spec/models/artist_credit_spec.rb
+++ spec/models/artist_credit_spec.rb
@@ -1,29 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe ArtistCredit, type: :model do
-  before(:each) do
-    @artist_credit = FactoryGirl.create(:artist_credit)
+  context 'without data_supplier' do
+    before(:each) do
+      @artist_credit = FactoryGirl.create(:artist_credit)
+    end
+
+    it { is_expected.to respond_to(:artists) }
+
+    it 'is valid with valid attributes' do
+      expect(@artist_credit).to be_valid
+    end
+
+    it 'must have a unique name' do
+      clone = ArtistCredit.new(name: @artist_credit.name)
+      expect(clone).not_to be_valid
+    end
+
+    it 'is not valid without a participant' do
+      @artist_credit = FactoryGirl.build(:artist_credit)
+      @artist_credit.participants = []
+      expect(@artist_credit).not_to be_valid
+    end
+
+    it 'raises an error when it sets its participants to null' do
+      expect { @artist_credit.participants = [] }
+        .to raise_error(/null value in column "artist_credit_id"/)
+    end
   end
 
-  it { is_expected.to respond_to(:artists) }
+  context 'with data_supplier' do
+    before(:all) do
+      @artist_credit = FactoryGirl.create(
+        :artist_credit_with_data_supplier
+      )
+    end
 
-  it 'is valid with valid attributes' do
-    expect(@artist_credit).to be_valid
-  end
-
-  it 'must have a unique name' do
-    clone = ArtistCredit.new(name: @artist_credit.name)
-    expect(clone).not_to be_valid
-  end
-
-  it 'is not valid without a participant' do
-    @artist_credit = FactoryGirl.build(:artist_credit)
-    @artist_credit.participants = []
-    expect(@artist_credit).not_to be_valid
-  end
-
-  it 'raises an error when it sets its participants to null' do
-    expect { @artist_credit.participants = [] }
-      .to raise_error(/null value in column "artist_credit_id"/)
+    it 'is valid' do
+      expect(@artist_credit).to be_valid
+    end
   end
 end
