9ae5eddf251785a9ed465e73fc86f091e24c985f
ossi <ossi@asgard.ossgard.xx>
2016-03-10 20:02:49 +0100

    New model Source

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307205804) do
+ActiveRecord::Schema.define(version: 20160310185226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -466,6 +466,12 @@ ActiveRecord::Schema.define(version: 20160307205804) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "type",           null: false
+  end
+
+  create_table "sources", primary_key: "name", force: :cascade do |t|
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "stations", force: :cascade do |t|
