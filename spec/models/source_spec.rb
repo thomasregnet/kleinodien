bca87cf46ee95123faeb33e8d3293efb2a9640fd
ossi <ossi@asgard.ossgard.xx>
2016-03-10 21:28:36 +0100

    Added constants representing sources

--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Source, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'constants' do
+    specify 'Source::Discogs' do
+      expect(Source::Discogs).to be_instance_of Source
+    end
+
+    specify 'Source::MusicBrainz' do
+      expect(Source::MusicBrainz).to be_instance_of Source
+    end
+
+    specify 'Source::Omdb' do
+      expect(Source::Omdb).to be_instance_of Source
+    end
+
+    specify 'Source::User' do
+      expect(Source::User).to be_instance_of Source
+    end
+  end
 end
