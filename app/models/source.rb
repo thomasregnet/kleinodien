1aaa7a0a20bec0e33b0f7a53b4a31b02b1ca4eff
ossi <ossi@asgard.ossgard.xx>
2016-03-10 21:28:36 +0100

    Added constants representing sources

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,9 @@
+# Data Source for db entries
 class Source < ActiveRecord::Base
   self.primary_key = 'name'
+
+  Discogs     = find('Discogs')
+  MusicBrainz = find('MusicBrainz')
+  Omdb        = find('Omdb')
+  User        = find('User')
 end
