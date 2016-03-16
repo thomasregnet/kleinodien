e397c73e3af8ba9cedf3f28343ca6b541b89c79a
ossi <ossi@asgard.ossgard.xx>
2016-03-16 21:35:35 +0100

    IncompleteDate now is an empty subclass of ...
    ... KleinodienDateTime::IncompleteDate

--- spec/shared_examples_for_incomplete_dates.rb
+++ spec/shared_examples_for_incomplete_dates.rb
@@ -7,32 +7,21 @@ RSpec.shared_examples "a model with an IncompleteDate" do
   end
 
   it "works with a Fixnum as IncompleteDate" do
-    #@object.send(date_setter, IncompleteDate.new(2015))
-    @object.send(date_setter, IncompleteDate.new('2015'))
-    expect(@object.send(date_getter).to_s).to eq('2015-01-01')
-    #expect(@object.send(date_getter).to_s).to eq('2015')
+    @object.send(date_setter, IncompleteDate.from_string('2015'))
+    expect(@object.send(date_getter).to_s).to eq '2015-01-01'
     expect(@object.send(date_getter).mask).to eq(4)
     expect(@object.save).to be true
   end
 
-  it "works with a String representing only a year" do
-    @object.date = IncompleteDate.new('2015')
-    expect(@object.send(date_getter).to_s).to eq('2015-01-01')
-    #expect(@object.send(date_getter).to_s).to eq('2015')
-    expect(@object.send(date_getter).mask).to eq(4)
-    expect(@object.save).to be true
-  end
-  
   it "works with a String representing only year and month" do
-    @object.date = IncompleteDate.new('2015-03')
+    @object.date = IncompleteDate.from_string('2015-03')
     expect(@object.send(date_getter).to_s).to eq('2015-03-01')
-    #expect(@object.send(date_getter).to_s).to eq('2015-03')
     expect(@object.send(date_getter).mask).to eq(6)
     expect(@object.save).to be true
   end
 
   it "works with a String representing a iso-date" do
-    @object.date = IncompleteDate.new('2015-03-13')
+    @object.date = IncompleteDate.from_string('2015-03-13')
     expect(@object.send(date_getter).to_s).to eq('2015-03-13')
     expect(@object.send(date_getter).mask).to eq(7)
     expect(@object.save).to be true
