4e912b339962b451ed97c91e3f6d7701532717c2
ossi <ossi@asgard.ossgard.xx>
2016-03-21 21:30:28 +0100

    Little code improvements for two model classes

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,3 +1,4 @@
+# Season of a serial, has many episodes
 class Season < ActiveRecord::Base
   belongs_to :serial, inverse_of: :seasons
   has_many :episodes, class_name: EpisodeHead, inverse_of: :season
