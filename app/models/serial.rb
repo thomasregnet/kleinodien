1ee2e317f2a43e593b8a815369a69e50a89796f0
ossi <ossi@asgard.ossgard.xx>
2016-03-22 20:16:42 +0100

    Little code improvements for Serial

--- app/models/serial.rb
+++ app/models/serial.rb
@@ -1,9 +1,12 @@
+# Base class for serials like TV-serials or podcast-serials
 class Serial < ActiveRecord::Base
   has_many :seasons, inverse_of: :serial
   has_many :episodes, through: :seasons
 
-  validates :title, presence: true
-  validates_uniqueness_of :title,
-                          scope: :disambiguation,
-                          case_sensitive: false
+  validates :title,
+            presence: true,
+            uniqueness: {
+              scope:          :disambiguation,
+              case_sensitive: false
+            }
 end
