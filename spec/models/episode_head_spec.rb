cc58521d345f648dac62ca8829fbd2f5e048541d
ossi <ossi@asgard.ossgard.xx>
2016-03-23 20:39:15 +0100

    rubocoop -a on some spec/models/*_spec.rb files

--- spec/models/episode_head_spec.rb
+++ spec/models/episode_head_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe EpisodeHead, type: :model do
     @episode_head = FactoryGirl.create(:episode_head)
   end
 
-  it "is valid with valid attributes" do
+  it 'is valid with valid attributes' do
     expect(@episode_head).to be_valid
   end
 
-  it "knows its season" do
+  it 'knows its season' do
     expect(@episode_head.season).to be_instance_of(Season)
   end
 
-  it "knows its serial" do
+  it 'knows its serial' do
     expect(@episode_head.serial).to be_instance_of(Serial)
   end
 end
