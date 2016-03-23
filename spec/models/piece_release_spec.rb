8a65c33105214a7b136cdcc323cd270d28281fd5
ossi <ossi@asgard.ossgard.xx>
2016-03-23 20:06:42 +0100

    piece_release_spec uses single-quoted strings

--- spec/models/piece_release_spec.rb
+++ spec/models/piece_release_spec.rb
@@ -8,55 +8,55 @@ require 'shared_examples_for_models_with_many_references'
 require 'shared_examples_for_incomplete_dates'
 
 RSpec.describe PieceRelease, type: :model do
-  context "without tracks" do
+  context 'without tracks' do
     before(:each) do
       @piece_release = FactoryGirl.create(:piece_release)
     end
 
-    it "is valid with valid attributes" do
+    it 'is valid with valid attributes' do
       expect(@piece_release).to be_valid
     end
 
-    it "delegates title to its head" do
+    it 'delegates title to its head' do
       expect(@piece_release.title).to eq(@piece_release.head.title)
     end
   end
 
-  context "with tracks" do
+  context 'with tracks' do
     before(:each) do
       @piece_release = FactoryGirl.create(:piece_release_with_tracks)
     end
 
-    it "has some tracks" do
+    it 'has some tracks' do
       expect(@piece_release.tracks.count).to be > 0
     end
   end
 
-  it_behaves_like "a model with companies" do
+  it_behaves_like 'a model with companies' do
     let(:factory) { :piece_release_with_companies }
   end
 
-  it_behaves_like "a model with countries" do
+  it_behaves_like 'a model with countries' do
     let(:factory) { :piece_release_with_countries }
   end
 
-  it_behaves_like "a model with credits" do
+  it_behaves_like 'a model with credits' do
     let(:factory) { :piece_release_with_credits }
   end
 
-  it_behaves_like "a model with labels" do
+  it_behaves_like 'a model with labels' do
     let(:factory) { :piece_release_with_labels }
   end
 
-  it_behaves_like "a model with a Reference" do
+  it_behaves_like 'a model with a Reference' do
     let(:factory) { :piece_release_with_a_reference }
   end
 
-  it_behaves_like "a model with many References" do
+  it_behaves_like 'a model with many References' do
     let(:factory) { :piece_release_with_many_references }
   end
 
-  it_behaves_like "a model with an IncompleteDate" do
+  it_behaves_like 'a model with an IncompleteDate' do
     let(:factory) { :piece_release }
     let(:date_naming) { 'date' }
   end
