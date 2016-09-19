07f1f95f961b2b480a2bb65fb7387a50b16ac8a5
Ossi <ossi@localdomain>
2016-09-19 11:07:16 +0200

    Deleted test from discogs/insert_artists_spec.rb

--- spec/models/discogs/insert_artists_spec.rb
+++ spec/models/discogs/insert_artists_spec.rb
@@ -2,24 +2,6 @@ require 'rails_helper'
 require 'discogs_test_helper'
 
 RSpec.describe Discogs::InsertArtists, type: :model do
-  context 'AC/DC' do
-    before(:all) do
-      DatabaseCleaner.start
-
-      json = DiscogsTestHelper.get_discogs_data('releases', 940_468)
-      dc_release = KleinodienDiscogs.get_release(json)
-      @artist_credit = Discogs::InsertArtists.perform(dc_release.artists)
-    end
-
-    it 'has the right name set' do
-      expect(@artist_credit.name).to eq('AC/DC')
-    end
-
-    after(:all) do
-      DatabaseCleaner.clean
-    end
-  end
-
   context 'Jello Biafra With Nomeansno' do
     before(:all) do
       DatabaseCleaner.start
