70aa289a48c2c167570234bb53b7986ac28a039e
Ossi <ossi@localdomain>
2016-09-20 10:37:39 +0200

    Wrote spec that inserts an artist with a source

--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -2,6 +2,11 @@ FactoryGirl.define do
   factory :artist do
     sequence(:name) { |n| "artist ##{n}" }
 
+    factory :artist_brainz do
+      source_name Source::MusicBrainz.name
+      sequence(:source_ident) { |n| "looks-like-an-artist-uuid-#{n}" }
+    end
+
     factory :artist_with_disambiguation do
       sequence(:disambiguation) { |n| "artist disambiguation ##{n}" }
     end
