0dcd491c4a8e4a4614e739bec739cfc916d91939
Ossi <ossi@localdomain>
2016-09-30 09:58:58 +0200

    compilation_track_spec.rb works again

--- app/models/track_detail.rb
+++ app/models/track_detail.rb
@@ -3,7 +3,7 @@ class TrackDetail < ActiveRecord::Base
   belongs_to :kind,
              class_name: TrackDetailKind,
              foreign_key: :trf_attribute_kind_id
-  belongs_to :track
+  belongs_to :track, class_name: CompilationTrack
 
   validates :kind,  presence: true
   validates :no,    presence: true
--- spec/factories/compilation_tracks.rb
+++ spec/factories/compilation_tracks.rb
@@ -1,20 +1,20 @@
 FactoryGirl.define do
-  factory :track do
+  factory :compilation_track do
     association :release, factory: :piece_release
 
-    factory :track_with_compilation_release do
+    factory :compilation_track_with_compilation_release do
       association :compilation, factory: :compilation_release
     end
 
-    factory :track_with_duration do
+    factory :compilation_track_with_duration do
       association :duration, factory: :duration
     end
 
-    factory :track_with_format do
+    factory :compilation_track_with_format do
       association :format, factory: :tr_format_kind
     end
 
-    factory :track_with_details do
+    factory :compilation_track_with_details do
       transient do
         details_count 3
       end
