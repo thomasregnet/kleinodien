4a43d0fe51e9ee71eddd928ee28ed73088c25815
Ossi <ossi@localdomain>
2016-09-30 10:19:53 +0200

    Tests pass after renaming to compilation_tracks

--- spec/factories/track_details.rb
+++ spec/factories/track_details.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :track_detail do
-    association :track, factory: :track
+    association :track, factory: :compilation_track
     association :kind, factory: :track_detail_kind
     sequence(:no) { |n| n }
   end
