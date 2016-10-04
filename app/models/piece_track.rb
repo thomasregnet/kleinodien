1a5ae272b912b7ff9e444a12a4e3ae6fed08d36a
Ossi <ossi@localdomain>
2016-10-04 10:12:24 +0200

    Added "duration" to PieceTrack

--- app/models/piece_track.rb
+++ app/models/piece_track.rb
@@ -1,4 +1,8 @@
 class PieceTrack < ApplicationRecord
   belongs_to :release, class_name: PieceRelease, foreign_key: :piece_release_id
   belongs_to :tr_format_kinds
+
+  composed_of :duration,
+              class_name: 'Duration',
+              mapping: [%w(milliseconds milliseconds), %w(accuracy accuracy)]
 end
--- spec/models/piece_track_spec.rb
+++ spec/models/piece_track_spec.rb
@@ -1,12 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe PieceTrack, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
   before(:each) do
     @track = FactoryGirl.create(:piece_track)
   end
 
   it 'is valid with valid attributes' do
     expect(@track).to be_valid
+  end
+
+  # TODO: shared_exampes_for_duration
+  # this spec was cut and pasted from compilation_tracks
+  context 'with duration' do
+    before(:each) do
+      @track = FactoryGirl.create(:piece_track)
+      @track.duration = Duration.new(311_000, 'second')
+    end
+
+    it 'is valid' do
+      expect(@track).to be_valid
+    end
+
+    it 'can be saved' do
+      expect { @track.save! }.not_to raise_error
+    end
+
+    it 'returns the duration in mm:ss format' do
+      expect(@track.duration.mmss).to eq('5:11')
+    end
   end
 end
