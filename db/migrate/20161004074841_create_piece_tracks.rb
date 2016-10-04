69764b951360950dfab6f6e5e0b5082c90daecf4
Ossi <ossi@localdomain>
2016-10-04 10:05:13 +0200

    Corrected piece_tracks migration
    Accidental plural form for foreign keys.

--- db/migrate/20161004074841_create_piece_tracks.rb
+++ db/migrate/20161004074841_create_piece_tracks.rb
@@ -1,8 +1,8 @@
 class CreatePieceTracks < ActiveRecord::Migration[5.0]
   def change
     create_table :piece_tracks do |t|
-      t.references :piece_releases, foreign_key: true, null: false
-      t.references :tr_format_kinds, foreign_key: true
+      t.references :piece_release, foreign_key: true, null: false
+      t.references :tr_format_kind, foreign_key: true
       t.integer :milliseconds
       t.text :accuracy
 
