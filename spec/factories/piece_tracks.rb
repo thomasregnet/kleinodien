cceb31df4c38647155dab710c3702cbad2a5aa6a
Ossi <ossi@localdomain>
2016-10-04 10:08:46 +0200

    First specs for PieceTrack

---
+++ spec/factories/piece_tracks.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :piece_track do
+    association :release, factory: :piece_release
+  end
+end
--- spec/models/piece_track_spec.rb
+++ spec/models/piece_track_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe PieceTrack, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @track = FactoryGirl.create(:piece_track)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@track).to be_valid
+  end
 end
