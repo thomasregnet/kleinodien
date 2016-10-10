e42679c4c5b958ada9cb65641d03c85a98b399bc
Ossi <ossi@localdomain>
2016-10-10 20:46:26 +0200

    A RepositoryPosition is only valid with an User

--- app/models/repository_position.rb
+++ app/models/repository_position.rb
@@ -3,6 +3,10 @@ class RepositoryPosition < ApplicationRecord
              primary_key: [:id, :compilation_release_id],
              foreign_key: [:compilation_track_id, :compilation_release_id]
 
+  belongs_to :user
+
+  validates :user, presence: true
+
   def compilation_track=(ct)
     self.compilation_track_id   = ct.id
     self.compilation_release_id = ct.compilation_release_id
--- spec/models/repository_position_spec.rb
+++ spec/models/repository_position_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe RepositoryPosition, type: :model do
   it 'is valid' do
     expect(@position).to be_valid
   end
+
+  it 'is not valid without an user' do
+    @position.user = nil
+    expect(@position).not_to be_valid
+  end
 end
