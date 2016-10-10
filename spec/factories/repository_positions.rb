a2757175e4196ae9994fcf8f43021c311a4d352d
Ossi <ossi@localdomain>
2016-10-10 21:46:47 +0200

    A RepositoryPosition is valid with a Repository

--- spec/factories/repository_positions.rb
+++ spec/factories/repository_positions.rb
@@ -5,6 +5,11 @@ FactoryGirl.define do
       association :compilation_track,
                   factory: :compilation_track_with_compilation_release
       association :user, factory: :user
+
+      after(:build) do |r_pos|
+        user = FactoryGirl.create(:user)
+        r_pos.repository = FactoryGirl.create(:repository, user: r_pos.user)
+      end
     end
   end
 end
