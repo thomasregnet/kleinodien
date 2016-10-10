fd9b6f41b8881a628eb1be8d176e5c65d2618fba
Ossi <ossi@localdomain>
2016-10-10 20:46:26 +0200

    A RepositoryPosition is only valid with an User

--- spec/factories/repository_positions.rb
+++ spec/factories/repository_positions.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     factory :repository_position_with_compilation_track do
       association :compilation_track,
                   factory: :compilation_track_with_compilation_release
+      association :user, factory: :user
     end
   end
 end
