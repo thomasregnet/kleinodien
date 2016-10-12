d21c1453e4722f7f3ce5cddd06a724198b632d8c
Ossi <ossi@localdomain>
2016-10-12 20:56:20 +0200

    Refactored factory repository_positions.rb

--- spec/factories/repository_positions.rb
+++ spec/factories/repository_positions.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :repository_position do
+    association :user, factory: :user
 
     factory :repository_position_with_compilation_track do
       association :compilation_track,
@@ -7,11 +8,9 @@ FactoryGirl.define do
       association :user, factory: :user
 
       after(:build) do |r_pos|
-        user = FactoryGirl.create(:user)
         r_pos.repository = create(:repository, user: r_pos.user)
         r_pos.compilation_copy = create(
           :compilation_copy,
-          # TODO: user is generated twice a time
           user: r_pos.user,
           compilation_release_id: r_pos.compilation_track.compilation_release_id
         )
@@ -20,7 +19,6 @@ FactoryGirl.define do
 
     factory :repository_position_with_piece_track do
       association :piece_track, factory: :piece_track
-      association :user, factory: :user
 
       after(:build) do |r_pos|
         r_pos.repository = create(:repository, user: r_pos.user)
