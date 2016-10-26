5c7cf8c85772ec6edda62fb3be17a764b2d2a700
Ossi <ossi@localdomain>
2016-10-26 20:34:36 +0200

    Specs and definitions for ReFormat

--- app/models/re_format.rb
+++ app/models/re_format.rb
@@ -1,2 +1,9 @@
 class ReFormat < ApplicationRecord
+  self.primary_key = :name
+
+  belongs_to :format, primary_key: :name, foreign_key: :name
+
+  validates :name, presence: true
+
+  delegate :abbr, to: :format
 end
--- spec/models/re_format_spec.rb
+++ spec/models/re_format_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe ReFormat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'from db' do
+    before(:all) do
+      @format = ReFormat.find('CDr')
+    end
+
+    it 'has the right abbr' do
+      expect(@format.abbr).to eq 'CDr'
+    end
+
+    it 'has the format set' do
+      expect(@format.format.name).to eq 'CDr'
+    end
+  end
+
+  context 'without a name' do
+    before(:all) do
+      @format = ReFormat.new
+    end
+
+    it 'is not valid' do
+      expect(@format).not_to be_valid
+    end
+  end
 end
