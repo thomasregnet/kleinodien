cf0ebe68dc3e4adf8fa262b02cf0deb15560cdfc
Ossi <ossi@localdomain>
2016-10-26 21:49:12 +0200

    RefAttribute behaves_like 'a format'

--- spec/models/ref_attribute_spec.rb
+++ spec/models/ref_attribute_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
+require 'shared_examples_for_formats'
 
 RSpec.describe RefAttribute, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like 'a format' do
+    let(:klass) { RefAttribute }
+    let(:format) { RefAttribute.find('FLAC') }
+    let(:name) { 'FLAC' }
+    let(:abbr) { 'FLAC' }
+  end
 end
