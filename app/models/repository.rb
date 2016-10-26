87f2ba971b2bb8a4e79c704e884b5c3d865b21a5
Ossi <ossi@localdomain>
2016-10-26 21:05:32 +0200

    Repository#format

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,4 +1,8 @@
 class Repository < ApplicationRecord
+  belongs_to :format,
+             class_name: ReFormat,
+             primary_key: :name,
+             foreign_key: :re_format_name
   belongs_to :user
 
   validates :name, presence: true
