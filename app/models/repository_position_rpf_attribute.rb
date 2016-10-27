e424e36938bdf60099e21db0ae603cd92d96c507
Ossi <ossi@localdomain>
2016-10-27 19:36:45 +0200

    RepositoryPositionRpfAttribute: spec & validations

--- app/models/repository_position_rpf_attribute.rb
+++ app/models/repository_position_rpf_attribute.rb
@@ -1,2 +1,9 @@
 class RepositoryPositionRpfAttribute < ApplicationRecord
+  self.primary_key = [:ref_attribute_name, :repository_id, :no]
+
+  belongs_to :attrib,
+             class_name: RefAttribute,
+             primary_key: :name,
+             foreign_key: :ref_attribute_name
+  belongs_to :repository
 end
--- spec/models/repository_position_rpf_attribute_spec.rb
+++ spec/models/repository_position_rpf_attribute_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe RepositoryPositionRpfAttribute, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    DatabaseCleaner.start
+    @attr = RepositoryPositionRpfAttribute.new(
+      repository: FactoryGirl.build(:repository),
+      attrib:     RefAttribute.find('FLAC'),
+      no:         0
+    )
+  end
+
+  it 'works' do
+    expect(@attr).to be_valid
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean
+  end
 end
