815390f3da0ccb6207d0ed2639ccef5349ad1c56
Ossi <ossi@localdomain>
2016-10-31 19:39:54 +0100

    RepositoryRefDetail#abbr delegated to RefDetail

--- app/models/repository_ref_detail.rb
+++ app/models/repository_ref_detail.rb
@@ -1,9 +1,11 @@
 class RepositoryRefDetail < ApplicationRecord
   self.primary_key = [:name, :repository_id, :no]
 
-  belongs_to :attrib,
+  belongs_to :detail,
              class_name: RefDetail,
              primary_key: :name,
              foreign_key: :name
   belongs_to :repository
+
+  delegate :abbr, to: :detail
 end
