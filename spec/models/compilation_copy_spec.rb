b48a306e9c78f53c404a7d1182e6ac4a0aea2eb7
Ossi <ossi@localdomain>
2016-11-18 11:35:47 +0100

    Ugly spec creating an AlbumRelease-copy

--- spec/models/compilation_copy_spec.rb
+++ spec/models/compilation_copy_spec.rb
@@ -1,33 +1,71 @@
 require 'rails_helper'
+require 'brainz_test_helper'
 
 RSpec.describe CompilationCopy, type: :model do
-  before(:each) do
-    @copy = FactoryGirl.create(:compilation_copy)
+  context 'test in a lab' do
+    before(:each) do
+      @copy = FactoryGirl.create(:compilation_copy)
+    end
+
+    it 'is valid with valid attributes' do
+      expect(@copy).to be_valid
+    end
+
+    it 'is not valid without a release' do
+      @copy.release = nil
+      expect(@copy).not_to be_valid
+    end
+
+    it 'raises without a release' do
+      @copy.release = nil
+      expect { @copy.save! validate: false }
+        .to raise_error /PG::NotNullViolation: ERROR/
+    end
+
+    it 'is not valid without an user' do
+      @copy.user = nil
+      expect(@copy).not_to be_valid
+    end
+
+    it 'raises without a user' do
+      @copy.user = nil
+      expect { @copy.save! validate: false }
+        .to raise_error /PG::NotNullViolation: ERROR/
+    end
   end
 
-  it 'is valid with valid attributes' do
-    expect(@copy).to be_valid
-  end
+  context 'dirty tests on real release' do
+    before(:all) do
+      DatabaseCleaner.start
+      @brz_release = BrainzTestHelper
+                     .get_release('c8f7094d-ce27-365e-961f-0af27321be08')
+      @x_release = Brainz::InsertRelease.perform(@brz_release)
+      @release = AlbumRelease.find(@x_release.id)
+      @repository = FactoryGirl.create(:repository)
+      @co_copy    = FactoryGirl.create(
+        :compilation_copy,
+        release: @release,
+        user:    @repository.user
+      )
+    end
 
-  it 'is not valid without a release' do
-    @copy.release = nil
-    expect(@copy).not_to be_valid
-  end
+    # TODO: This is just a proove of concept, code and structure...
+    # TODO: ...realy suck!
+    it 'works' do
+      @release.tracks.each do |track|
+        puts track.piece_release.title
+        puts track.compilation_release.id
+        rp = RepositoryPosition.new(
+          release:           @release,
+          repository:        @repository,
+          compilation_track: track,
+          compilation_copy:  @co_copy
+        )
+      end
+    end
 
-  it 'raises without a release' do
-    @copy.release = nil
-    expect { @copy.save! validate: false }
-      .to raise_error /PG::NotNullViolation: ERROR/
-  end
-
-  it 'is not valid without an user' do
-    @copy.user = nil
-    expect(@copy).not_to be_valid
-  end
-
-  it 'raises without a user' do
-    @copy.user = nil
-    expect { @copy.save! validate: false }
-      .to raise_error /PG::NotNullViolation: ERROR/
+    after(:all) do
+      DatabaseCleaner.clean
+    end
   end
 end
