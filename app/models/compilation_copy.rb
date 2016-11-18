c1c0df4e6cd49017c5e5ad49cc9be7edd82b7f7b
Ossi <ossi@localdomain>
2016-11-18 09:50:31 +0100

    Feature "Add a CompilationCopy" small improvements

--- app/models/compilation_copy.rb
+++ app/models/compilation_copy.rb
@@ -3,12 +3,20 @@ class CompilationCopy < ApplicationRecord
   belongs_to :release,
              class_name:  CompilationRelease,
              foreign_key: :compilation_release_id
-  belongs_to :user
+  belongs_to :user, inverse_of: :compilation_copies
 
   validates :release, presence: true
   validates :user, presence: true
 
   has_many :repositories, through: :user
+
+  accepts_nested_attributes_for :repositories
   # def release(release)
   #   self.
+
+  # def repository_ids
+  # end
+
+  # def repository_ids=
+  # end
 end
