bd9beb38e592f819d4f4eb5156ee7c01b732e6de
ossi <ossi@example.com>
2016-11-29 21:25:15 +0100

    Little code improvements on ChLabel

--- app/models/ch_label.rb
+++ app/models/ch_label.rb
@@ -1,7 +1,7 @@
 # A Label involved in a CompilationHead
 class ChLabel < ActiveRecord::Base
-  belongs_to :compilation_head
   belongs_to :company
+  belongs_to :compilation_head
 
   validates :company, presence: true
   validates :compilation_head, presence: true
