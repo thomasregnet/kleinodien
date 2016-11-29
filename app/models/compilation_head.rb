6ea289849b8961ca3f9369bbafbbde875cfcdddc
ossi <ossi@example.com>
2016-11-29 22:05:26 +0100

    Little improvements for CompilationHead

--- app/models/compilation_head.rb
+++ app/models/compilation_head.rb
@@ -6,11 +6,11 @@ class CompilationHead < ActiveRecord::Base
   has_many :labels, class_name: ChLabel
   has_and_belongs_to_many :countries
 
-  validates :title, presence: true
   validates :type, presence: true
   validates :source_ident,
             uniqueness: { allow_blank: true, scope: [:source_name, :type] }
   validates :title,
+            presence: true,
             uniqueness: {
               scope:          [:type, :disambiguation, :source],
               case_sensitive: false
