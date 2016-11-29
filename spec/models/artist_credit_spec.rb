3f25a33e9090e47f041a63aa8e337eeb64908932
ossi <ossi@example.com>
2016-11-29 21:10:30 +0100

    Code improvements to artist_credit_spec.rb

--- spec/models/artist_credit_spec.rb
+++ spec/models/artist_credit_spec.rb
@@ -1,40 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe ArtistCredit, type: :model do
-  context 'without data_supplier' do
-    before(:each) do
-      @artist_credit = FactoryGirl.create(:artist_credit)
+  context 'without DataSupplier' do
+    context 'not saved to the database' do
+      before(:each) do
+        @artist_credit = FactoryGirl.build(:artist_credit)
+      end
+
+      it { is_expected.to respond_to(:artists) }
+
+      it 'is valid with valid attributes' do
+        expect(@artist_credit).to be_valid
+      end
+
+      it 'is not valid without a participant' do
+        @artist_credit.participants = []
+        expect(@artist_credit).not_to be_valid
+      end
     end
 
-    it { is_expected.to respond_to(:artists) }
+    context 'saved to the database' do
+      before(:each) do
+        @artist_credit = FactoryGirl.create(:artist_credit)
+      end
+      it 'raises an error when it sets its participants to null' do
+        expect { @artist_credit.participants = [] }
+          .to raise_error(/null value in column "artist_credit_id"/)
+      end
 
-    it 'is valid with valid attributes' do
-      expect(@artist_credit).to be_valid
-    end
-
-    it 'must have a unique name' do
-      clone = ArtistCredit.new(participants: @artist_credit.participants)
-      expect(clone).not_to be_valid
-    end
-
-    it 'is not valid without a participant' do
-      @artist_credit = FactoryGirl.build(:artist_credit)
-      @artist_credit.participants = []
-      expect(@artist_credit).not_to be_valid
-    end
-
-    it 'raises an error when it sets its participants to null' do
-      expect { @artist_credit.participants = [] }
-        .to raise_error(/null value in column "artist_credit_id"/)
+      it 'must have a unique name' do
+        clone = ArtistCredit.new(participants: @artist_credit.participants)
+        expect(clone).not_to be_valid
+      end
     end
   end
 
-  context 'with data_supplier' do
+  context 'with DataSupplier' do
     before(:all) do
       DatabaseCleaner.start
-      @artist_credit = FactoryGirl.create(
-        :artist_credit_with_data_supplier
-      )
+      @artist_credit = FactoryGirl.create(:artist_credit_with_data_supplier)
     end
 
     it 'is valid' do
@@ -54,6 +58,8 @@ RSpec.describe ArtistCredit, type: :model do
       expect { clone.save! }.not_to raise_error
     end
 
-    after(:all) { DatabaseCleaner.clean }
+    after(:all) do
+      DatabaseCleaner.clean
+    end
   end
 end
