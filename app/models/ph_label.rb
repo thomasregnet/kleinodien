d3c5bbc21ce2cfba23c14e5eca6cec9f093486b9
ossi <ossi@example.com>
2016-11-30 21:30:46 +0100

    Minimal improvement for PhLabel

--- app/models/ph_label.rb
+++ app/models/ph_label.rb
@@ -1,7 +1,7 @@
 # PieceHead Label
 class PhLabel < ActiveRecord::Base
-  belongs_to :piece_head
   belongs_to :company
-  validates :company, presence: true
+  belongs_to :piece_head
+  validates :company,    presence: true
   validates :piece_head, presence: true
 end
