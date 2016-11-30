95caffc830f06a8944c2370e3e4fbec4a7db4164
ossi <ossi@example.com>
2016-11-30 21:26:26 +0100

    Minimal improvement for PhCredit

--- app/models/ph_credit.rb
+++ app/models/ph_credit.rb
@@ -1,8 +1,8 @@
 # PieceHead Credit
 class PhCredit < ActiveRecord::Base
   belongs_to :artist_credit
-  belongs_to :piece_head
   belongs_to :job
+  belongs_to :piece_head
   validates :artist_credit, presence: true
   validates :piece_head, presence: true
 end
