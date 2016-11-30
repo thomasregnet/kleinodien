894415eac241503b11f5eb1c28cfcd6e38702bc3
ossi <ossi@example.com>
2016-11-30 20:24:22 +0100

    Little improvements for CompilationTrack

--- app/models/compilation_track.rb
+++ app/models/compilation_track.rb
@@ -6,30 +6,30 @@ class CompilationTrack < ActiveRecord::Base
               class_name: 'Duration',
               mapping: [%w(milliseconds milliseconds), %w(accuracy accuracy)]
 
-  # belongs_to :release,
-  belongs_to :piece_release,
-             class_name:  PieceRelease,
-             foreign_key: :piece_release_id
   belongs_to :compilation_release,
              class_name:  CompilationRelease,
              foreign_key: :compilation_release_id
   belongs_to :format,
              class_name:  TrFormatKind,
              foreign_key: :tr_format_kind_id
-  has_many :repository_positions,
-           inverse_of: :compilation_track,
-           primary_key: [:id, :compilation_release_id],
-           foreign_key: [:compilation_track_id, :compilation_release_id]
-  has_many :details,
-           class_name:  CompilationTrackDetail,
-           foreign_key: :track_id
+  belongs_to :piece_release,
+             class_name:  PieceRelease,
+             foreign_key: :piece_release_id
 
+  # TODO: new_format must be deleted
   belongs_to :new_format,
              class_name: CtFormat,
              foreign_key: :format_name,
              primary_key: :name
-  
+
+  has_many :details,
+           class_name:  CompilationTrackDetail,
+           foreign_key: :track_id
+  has_many :repository_positions,
+           inverse_of: :compilation_track,
+           primary_key: [:id, :compilation_release_id],
+           foreign_key: [:compilation_track_id, :compilation_release_id]
+
   validates :compilation_release, presence: true
-  # validates :release,     presence: true
-  validates :piece_release,     presence: true
+  validates :piece_release,       presence: true
 end
