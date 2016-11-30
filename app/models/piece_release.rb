74c9298a22d831a283a78815014d4bdb02ca49d7
ossi <ossi@example.com>
2016-11-30 21:48:56 +0100

    Little improvements for PieceRelease

--- app/models/piece_release.rb
+++ app/models/piece_release.rb
@@ -6,11 +6,13 @@ class PieceRelease < ActiveRecord::Base
 
   belongs_to :head, class_name: PieceHead, foreign_key: :piece_head_id
   belongs_to :source, primary_key: :name, foreign_key: :source_name
+
   has_many :companies, class_name: PrCompany
   has_many :credits, class_name: PrCredit
   has_many :labels, class_name: PrLabel
   # TODO: must be PieceTrack when table piece_tracks exists
   has_many :tracks, class_name: CompilationTrack
+
   has_and_belongs_to_many :countries
 
   delegate :title, to: :head
