4488dd732442306bcacc818b54cd7c37816a2870
ossi <ossi@example.com>
2016-11-30 21:58:40 +0100

    Minimal improvement for PrCredit

--- app/models/pr_credit.rb
+++ app/models/pr_credit.rb
@@ -1,8 +1,9 @@
 # PieceRelease Credit
 class PrCredit < ActiveRecord::Base
   belongs_to :artist_credit
-  belongs_to :piece_release
   belongs_to :job
+  belongs_to :piece_release
+
   validates :artist_credit, presence: true
   validates :piece_release, presence: true
 end
