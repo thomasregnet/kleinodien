5c111ced77a4eb6337a4b803a413401260f06b13
ossi <ossi@example.com>
2016-11-30 22:02:01 +0100

    Minimal improvement for PrLabel

--- app/models/pr_label.rb
+++ app/models/pr_label.rb
@@ -1,7 +1,8 @@
 # PieceRelease Label
 class PrLabel < ActiveRecord::Base
-  belongs_to :piece_release
   belongs_to :company
+  belongs_to :piece_release
+
   validates :piece_release, presence: true
   validates :company, presence: true
 end
