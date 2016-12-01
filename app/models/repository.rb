ecbfe1d874c3926fdc74af09c997c69ec5f89c51
ossi <ossi@example.com>
2016-12-01 19:37:59 +0100

    Little improvements for Repository

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,18 +1,17 @@
+# A Repository is used to store parts of an users collection
 class Repository < ApplicationRecord
   belongs_to :format,
              class_name: ReFormat,
              primary_key: :name,
              foreign_key: :format_name
   belongs_to :user
+
   has_many :format_details,
            class_name: RepositoryRefDetail
 
+  # TODO: check if accepts_nested_attributes_for :format_details is needed
   accepts_nested_attributes_for :format_details
-  
+
   validates :name, presence: true
   validates :user, presence: true
-
-  # def all_format_details(detail_names = [])
-  #   byebug
-  # end
 end
