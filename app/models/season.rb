463524928f8c5271a28f1b48ce95e7510cccb50e
ossi <ossi@example.com>
2016-12-01 20:35:30 +0100

    Little improvements for Season

--- app/models/season.rb
+++ app/models/season.rb
@@ -2,6 +2,7 @@
 class Season < ActiveRecord::Base
   belongs_to :serial, inverse_of: :seasons
   has_many :episodes, class_name: EpisodeHead, inverse_of: :season
-  validates :serial, presence: true
+
   validates :no,     presence: true
+  validates :serial, presence: true
 end
