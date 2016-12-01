4cb1dbe4e27c2aa24e0abc92ddcb52ea73e185e7
ossi <ossi@example.com>
2016-12-01 19:32:16 +0100

    Minimal improvement for RefDetail

--- app/models/ref_detail.rb
+++ app/models/ref_detail.rb
@@ -1,9 +1,7 @@
+# Repository format detail
 class RefDetail < ApplicationRecord
   self.primary_key = :name
-
   belongs_to :format, primary_key: :name, foreign_key: :name
-             
   validates :name, presence: true
-
   delegate :abbr, to: :format
 end
