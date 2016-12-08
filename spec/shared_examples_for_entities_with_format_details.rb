83a42c7df4a34fa31893f53386086cdf5f6c9ac9
ossi <ossi@example.com>
2016-12-08 15:01:08 +0100

    Shared_examples for entities with format_details
    ...currently only used by repository_spec.rb

---
+++ spec/shared_examples_for_entities_with_format_details.rb
@@ -0,0 +1,13 @@
+RSpec.shared_examples 'an entity with format_details' do
+  it 'is valid' do
+    expect(entity).to be_valid
+  end
+
+  it 'has set at least one FormatDetail' do
+    expect(entity.format_details.length).to be >= 1
+  end
+
+  it 'can be saved to the database' do
+    expect(entity.save).to be true
+  end
+end
