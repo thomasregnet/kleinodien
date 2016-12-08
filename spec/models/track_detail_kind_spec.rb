314a75c45ce8b25d348de4936246e27f7c59cfeb
ossi <ossi@example.com>
2016-12-08 12:11:31 +0100

    Dropped unused tables and removed their classes

--- spec/models/track_detail_kind_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-require 'shared_examples_for_format_kinds'
-
-RSpec.describe TrackDetailKind do
-  it_behaves_like 'a format kind' do
-    let(:factory) { :track_detail_kind }
-  end
-end
