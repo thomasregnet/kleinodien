3059f5357d0711be4d0772777b8b1f44fadd0a70
ossi <ossi@example.com>
2016-12-08 14:44:32 +0100

    Small improvements for repositories_controller.rb

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,19 +7,15 @@ class RepositoriesController < ApplicationController
   end
 
   def create
-
     params = repository_params
     format_detail_names = params.delete(:format_details)
-    # byebug
     @repository = Repository.new(params)
     detail_names = format_detail_names.select { |name| !name.blank? }
     detail_names.each_with_index do |name, no|
-      
-      #RepositoryRefDetail.create(name: name, no: no,
       @repository.format_details.build(name: name, no: no)
     end
     @repository.user = current_user
-    
+
     if @repository.save!
       redirect_to @repository
     else
@@ -39,7 +35,6 @@ class RepositoriesController < ApplicationController
 
   def repository_params
     params.require(:repository).permit(
-      #:name, :id, :format_name, format_details: []
       :name, :id, :format_abbr, format_details: []
     )
   end
