c7879beafc369a72f8f8dad1824ac8a22a0a2ff2
ossi <ossi@example.com>
2016-12-14 20:32:39 +0100

    ArtistResource has_one source

--- app/resources/api/v01/artist_resource.rb
+++ app/resources/api/v01/artist_resource.rb
@@ -1,3 +1,6 @@
 class Api::V01::ArtistResource < JSONAPI::Resource
   attributes :name, :disambiguation, :source_name, :source_ident
+
+  has_one :source, foreign_key: :source_name
+  key_type :integer
 end
