4c9615d414cc78398608f7e270d13dbc0cc7c704
ossi <ossi@example.com>
2016-12-19 21:09:21 +0100

    Added spec/api_helper.rb

--- spec/requests/api/v01/artists_spec.rb
+++ spec/requests/api/v01/artists_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
+require 'api_helper'
+
+include ApiHelper
 
 RSpec.describe 'Artists API' do
   it 'sends a list of artists' do
     FactoryGirl.create_list(:artist, 2)
 
-    get '/api/v01/artists',
-        headers: { 'Accept' => 'application/vnd.api+json' }
+    api_get '/api/v01/artists'
 
     expect(response).to be_success
 
-    json = JSON.parse(response.body)
     expect(json['data'].length).to eq 2
   end
 end
