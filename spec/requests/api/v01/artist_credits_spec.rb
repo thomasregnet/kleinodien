67b3c793f866d6a31d7b8c142f036c1cfe8e6421
ossi <ossi@example.com>
2016-12-21 20:10:23 +0100

    ArtistCreditResource has_many :participants

--- spec/requests/api/v01/artist_credits_spec.rb
+++ spec/requests/api/v01/artist_credits_spec.rb
@@ -3,14 +3,35 @@ require 'api_helper'
 
 include ApiHelper
 
-RSpec.describe Api::V01::ArtistsController, :type => :controller do
-  it 'sends a list of artist_credits' do
-    FactoryGirl.create_list(:artist_credit, 2)
+RSpec.describe Api::V01::ArtistCreditsController, :type => :request do
+  context '#get' do
+    before(:all) do
+      DatabaseCleaner.start
+      @credits = FactoryGirl.create_list(:artist_credit, 2)
+    end
 
-    get :index
+    it 'sends a list of artist_credits' do
+      headers = {
+        'Accept'       => 'application/vnd.api+json'
+      }
+      get '/api/v01/artist-credits', headers: headers
 
-    expect(response).to be_success
+      expect(response).to be_success
 
-    expect(json['data'].length).to eq 2
+      expect(json['data'].length).to eq 2
+    end
+
+    it 'sends a list of artist_credits with participants' do
+      id = @credits.first.id
+      get "/api/v01/artist-credits/#{id}?include=participants",
+          headers: { 'Accept' => 'application/vnd.api+json' }
+
+      #byebug
+      expect(response).to be_success
+    end
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean
   end
 end
