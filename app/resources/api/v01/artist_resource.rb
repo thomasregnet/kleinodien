173a224083d00bf2411f317654f2c09a87263ecc
ossi <ossi@example.com>
2017-01-10 20:56:54 +0100

    All tests pass after changing table "sources"

--- app/resources/api/v01/artist_resource.rb
+++ app/resources/api/v01/artist_resource.rb
@@ -1,6 +1,6 @@
 class Api::V01::ArtistResource < JSONAPI::Resource
-  attributes :name, :disambiguation, :source_name, :source_ident
+  attributes :name, :disambiguation, :source_ident
 
-  has_one :source, foreign_key: :source_name
+  has_one :source
   key_type :integer
 end
