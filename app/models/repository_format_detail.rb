e9704769585a58e318355c711a368d794d632c25
ossi <ossi@example.com>
2017-01-11 21:49:16 +0100

    All rspec tests pass, cucumber not

--- app/models/repository_format_detail.rb
+++ app/models/repository_format_detail.rb
@@ -1,3 +1,4 @@
 class RepositoryFormatDetail < ApplicationRecord
   belongs_to :repository, inverse_of: :format_details
+  belongs_to :detail, class_name: FormatDetail, foreign_key: :format_detail_id
 end
