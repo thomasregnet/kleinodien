27da0fd4dda306e72417cf3d0c35f3f08f236ac1
ossi <ossi@example.com>
2017-01-11 21:37:07 +0100

    Compilation_track_spec.rb passes again

--- app/models/ct_format_detail.rb
+++ app/models/ct_format_detail.rb
@@ -2,7 +2,7 @@
 class CtFormatDetail < ApplicationRecord
   belongs_to :compilation_track, inverse_of: :format_details
   belongs_to :detail,
-             class_name: FormatDetail,
-             primary_key: :abbr,
-             foreign_key: :abbr
+             class_name: FormatDetail, foreign_key: :format_detail_id
+             #primary_key: :abbr,
+             #foreign_key: :abbr
 end
