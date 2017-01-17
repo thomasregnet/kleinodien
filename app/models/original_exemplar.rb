c6ec00770abffcad89278ac90fdafa7ed92cac91
ossi <ossi@example.com>
2017-01-17 20:58:41 +0100

    Spec, factory and validations for OriginalExemplar

--- app/models/original_exemplar.rb
+++ app/models/original_exemplar.rb
@@ -1,4 +1,8 @@
 class OriginalExemplar < ApplicationRecord
   belongs_to :compilation_release
   belongs_to :user
+
+  validates :compilation_release, presence: true
+  validates :disambiguation,      presence: true
+  validates :user,                presence: true
 end
