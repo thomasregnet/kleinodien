17432f07a0672fd45bcba6e88e088ba5f01fab49
ossi <ossi@example.com>
2017-01-17 22:07:53 +0100

    Added original_exemplars_controller.rb

---
+++ app/controllers/original_exemplars_controller.rb
@@ -0,0 +1,39 @@
+class OriginalExemplarsController < ApplicationController
+  def new
+    @original_exemplar      = OriginalExemplar.new
+    @compilation_release_id = params[:compilation_release_id]
+  end
+
+  def create
+    @original_exemplar = OriginalExemplar.new(original_exemplar_params)
+    @original_exemplar.user = current_user
+
+    if @original_exemplar.save
+      redirect_to @original_exemplar
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def original_exemplar_params
+    params.require(:original_exemplar)
+      .permit(:compilation_release_id, :disambiguation)
+  end
+end
---
+++ app/helpers/original_exemplar_helper.rb
@@ -0,0 +1,2 @@
+module OriginalExemplarHelper
+end
