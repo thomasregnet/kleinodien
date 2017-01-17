01740ab125cf4689a47788aec225d39dfe993769
ossi <ossi@example.com>
2017-01-17 20:27:08 +0100

    Added a failing step for "Original Exemplar"

--- features/step_definitions/original_exemplar_steps.rb
+++ features/step_definitions/original_exemplar_steps.rb
@@ -5,3 +5,11 @@ end
 When(/^I click the link to add it as original to my collection$/) do
   click_link 'add as original'
 end
+
+When(/^I fill in a disambiguation$/) do
+  fill_in 'Disambiguation', with: 'my nice exemplar'
+end
+
+Then(/^I will see my new original exemplar$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
