ec743f2e1274780867ba8739232f79f05d3463ae
ossi <ossi@example.com>
2017-01-17 20:58:41 +0100

    Spec, factory and validations for OriginalExemplar

---
+++ spec/factories/original_exemplars.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :original_exemplar do
+    association :compilation_release, factory: :album_release
+    association :user, factory: :user
+    sequence(:disambiguation) { |n| "my original #{n}" }
+  end
+end
--- spec/models/original_exemplar_spec.rb
+++ spec/models/original_exemplar_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe OriginalExemplar, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @original = FactoryGirl.build(:original_exemplar)
+  end
+
+  it 'is valid with valid parameters' do
+    expect(@original).to be_valid
+  end
+
+  it 'is not valid without a compilation_release' do
+    @original.compilation_release = nil
+    expect(@original).not_to be_valid
+  end
+
+  it 'is not valid without a user' do
+    @original.user = nil
+    expect(@original).not_to be_valid
+  end
+
+  it 'is not valid without a disambiguation' do
+    @original.disambiguation = nil
+    expect(@original).not_to be_valid
+  end
 end
