d62abfa26634bae382680de6a7a087ecb26a69ca
ossi <ossi@example.com>
2017-01-18 20:51:24 +0100

    Specs and validations for "Comment"

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :artist
+  belongs_to :artist_credit
+  belongs_to :artist
+  belongs_to :compilation_head
+  belongs_to :compilation_release
+  belongs_to :piece_head
+  belongs_to :piece_release
+  belongs_to :repository
+  belongs_to :season
+  belongs_to :serial
+  belongs_to :station
+
+  validates :text, presence: true
+  validates :user, presence: true
 end
