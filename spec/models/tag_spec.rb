22b5dbe48856658081c0ecb529a55f167e77f1a9
ossi <ossi@example.com>
2017-01-24 21:18:08 +0100

    Specs, validations and factory for "Tag"

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'without associations' do
+    before(:each) do
+      DatabaseCleaner.start
+      @tag = FactoryGirl.create(:tag)
+    end
+
+    it 'is valid' do
+      expect(@tag).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      @tag.name = nil
+      expect(@tag).not_to be_valid
+    end
+
+    it 'must have an unique name' do
+      clone = FactoryGirl.build(:tag, name: @tag.name)
+      expect(clone).not_to be_valid
+    end
+
+    after(:each) { DatabaseCleaner.clean }
+  end
 end
