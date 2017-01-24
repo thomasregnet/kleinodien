c1e33a076b6301ee9d04ec66e3a8f989227ccc0e
ossi <ossi@example.com>
2017-01-24 22:03:03 +0100

    New has_and_belongs_to_many associations on Tag
    Not implemented yet!

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,4 +5,12 @@ class Tag < ApplicationRecord
             uniqueness: { case_sensitive: false }
 
   has_and_belongs_to_many :artist_credits
+  has_and_belongs_to_many :artists
+  has_and_belongs_to_many :compilation_heads
+  has_and_belongs_to_many :compilation_releases
+  has_and_belongs_to_many :piece_heads
+  has_and_belongs_to_many :piece_releases
+  has_and_belongs_to_many :seasons
+  has_and_belongs_to_many :serials
+  has_and_belongs_to_many :stations
 end
