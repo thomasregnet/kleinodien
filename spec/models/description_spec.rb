74e3a2c98240cf2caf51a7dcbcd8684b4f71fb2f
ossi <ossi@example.com>
2017-01-25 21:58:28 +0100

    Specs for "exact_one_content_on_descriptions"

--- spec/models/description_spec.rb
+++ spec/models/description_spec.rb
@@ -1,43 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Description, type: :model do
-  before(:each) do
-    DatabaseCleaner.start
-    @description = FactoryGirl.build(:artist_credit_description)
-  end
-
-  it 'is valid with valid attributes' do
-    expect(@description).to be_valid
-  end
-
-  it 'is not valid without text' do
-    @description.text = nil
-    expect(@description).not_to be_valid
-  end
-
-  it 'is not valid with blank text' do
-    @description.text = ''
-    expect(@description).not_to be_valid
-  end
-
-  after(:all) { DatabaseCleaner.clean }
-
-  describe 'with more than one content' do
+  context 'with exact one content' do
     before(:each) do
       DatabaseCleaner.start
       @description = FactoryGirl.build(:artist_credit_description)
     end
 
-    contents = [
+    it 'is valid with valid attributes' do
+      expect(@description).to be_valid
+    end
+
+    it 'is not valid without text' do
+      @description.text = nil
+      expect(@description).not_to be_valid
+    end
+
+    it 'is not valid with blank text' do
+      @description.text = ''
+      expect(@description).not_to be_valid
+    end
+
+    after(:all) { DatabaseCleaner.clean }
+  end
+  context 'with more than one content' do
+    before(:each) do
+      DatabaseCleaner.start
+      @description = FactoryGirl.create(:artist_credit_description)
+    end
+
+    factories = [
       :artist, :compilation_head, :compilation_release, :country,
       :piece_head, :piece_release, :season, :serial, :station
     ]
 
-    contents.each do |content|
+    factories.each do |factory|
       it 'is not valid' do
-        setter = content.to_s + '='
-        @description.send setter, FactoryGirl.create(content)
+        setter = factory.to_s + '='
+        @description.send setter, FactoryGirl.create(factory)
         expect(@description).not_to be_valid
+        expect { @description.save! validate: false }
+          .to raise_error /exact_one_content_on_descriptions/
       end
     end
 
