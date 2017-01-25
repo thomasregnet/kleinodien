deb82ca9af385124f9e74fab6df67862b482b088
ossi <ossi@example.com>
2017-01-25 21:30:44 +0100

    Specs, validations and factories for "Description"

--- app/models/description.rb
+++ app/models/description.rb
@@ -1,4 +1,9 @@
 class Description < ApplicationRecord
+  POSSIBLE_CONTENTS = [
+       :artist_credit, :artist, :compilation_head, :compilation_release,
+       :country, :piece_head, :piece_release, :season, :serial, :station
+  ]
+
   belongs_to :user
   belongs_to :source
 
@@ -12,4 +17,18 @@ class Description < ApplicationRecord
   belongs_to :season
   belongs_to :serial
   belongs_to :station
+
+  validate :only_one_content
+
+  validates :text, presence: true, blank: false
+
+  private
+
+  def only_one_content
+    count = 0
+    POSSIBLE_CONTENTS.each { |content| count += 1 if send content }
+    if count != 1
+      errors.add :description, 'must contain exact one content model'
+    end
+  end
 end
--- spec/models/description_spec.rb
+++ spec/models/description_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Description, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    DatabaseCleaner.start
+    @description = FactoryGirl.build(:artist_credit_description)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@description).to be_valid
+  end
+
+  it 'is not valid without text' do
+    @description.text = nil
+    expect(@description).not_to be_valid
+  end
+
+  it 'is not valid with blank text' do
+    @description.text = ''
+    expect(@description).not_to be_valid
+  end
+
+  after(:all) { DatabaseCleaner.clean }
+
+  describe 'with more than one content' do
+    before(:each) do
+      DatabaseCleaner.start
+      @description = FactoryGirl.build(:artist_credit_description)
+    end
+
+    contents = [
+      :artist, :compilation_head, :compilation_release, :country,
+      :piece_head, :piece_release, :season, :serial, :station
+    ]
+
+    contents.each do |content|
+      it 'is not valid' do
+        setter = content.to_s + '='
+        @description.send setter, FactoryGirl.create(content)
+        expect(@description).not_to be_valid
+      end
+    end
+
+    after(:all) { DatabaseCleaner.clean }
+  end
 end
