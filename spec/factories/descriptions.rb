3b1e03441861b5fcbdb209ef37a5f1b91db7a48c
ossi <ossi@example.com>
2017-01-25 21:30:44 +0100

    Specs, validations and factories for "Description"

---
+++ spec/factories/descriptions.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :description do
+    text 'a nice description'
+
+    factory :artist_credit_description do
+      association :artist_credit, factory: :artist_credit
+    end
+  end
+end
