65f34c328e5b320973ad3ff6202629235cd4ca1a
ossi <ossi@example.com>
2017-02-01 21:36:35 +0100

    ArtistIdentifier has_one :artist

--- app/models/artist_identifier.rb
+++ app/models/artist_identifier.rb
@@ -1,6 +1,6 @@
 class ArtistIdentifier < ApplicationRecord
   belongs_to :source
-
+  has_one :artist
   validates :source, presence: true
   validates :value, presence: true, blank: false
 end
--- spec/factories/artist_identifiers.rb
+++ spec/factories/artist_identifiers.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :artist_identifier do
     sequence(:value) { |n| "artist-identifier-#{n}" }
     association :source, factory: :source
+
+    factory :artist_identifier_with_artist do
+      association :artist, factory: :artist
+    end
   end
 end
