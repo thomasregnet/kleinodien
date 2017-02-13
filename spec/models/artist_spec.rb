5a09453ac95e01b7b431fc8b56b094ee4af6cc3b
ossi <ossi@example.com>
2017-02-13 20:22:31 +0100

    Artist_spec.rb works again

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -6,6 +6,7 @@ require 'shared_examples_for_incomplete_dates'
 require 'shared_examples_for_tagable_models'
 
 RSpec.describe Artist, type: :model do
+  # TODO: spec Artist has_many: identifiers
   specify '#descriptions' do
     expect(subject).to respond_to(:descriptions)
   end
@@ -76,47 +77,4 @@ RSpec.describe Artist, type: :model do
       let(:naming) { 'name' }
     end
   end
-
-  context 'with an identifier' do
-    before(:each) do
-      @artist = FactoryGirl.build(:artist_with_identifier)
-    end
-
-    it 'gets the identifier' do
-      expect(@artist.identifier).to be_instance_of ArtistIdentifier
-    end
-  end
-
-  context '#alt_identifiers' do
-    before(:all) do
-      DatabaseCleaner.start
-      @artist = FactoryGirl.create(:artist)
-    end
-
-    it 'takes an alternative identifier' do
-      @artist.alt_identifiers << FactoryGirl.create(:artist_identifier)
-      expect(@artist.alt_identifiers.length).to eq 1
-    end
-
-    after(:all) { DatabaseCleaner.clean }
-  end
-
-  # 2017-02-07 dropped columns artists.source_ident and artists.source_id
-  # context 'with a source' do
-  #   before(:each) do
-  #     @artist = FactoryGirl.build(:artist_brainz)
-  #   end
-
-  #   it 'has the source set' do
-  #     expect(@artist.source_name).to eq('MusicBrainz')
-  #   end
-
-  #   it 'delegates #source_name to source.name' do
-  #     expect(@artist.source.name).to eq('MusicBrainz')
-  #   end
-
-  #   specify '#source_ident looks like an artist uuid' do
-  #     expect(@artist.source_ident).to match(/^looks-like-an-artist-uuid-/)
-  #   end
-  # end
 end
