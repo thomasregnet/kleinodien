66f0897322c16e866b1d693166b1ef903bd53738
ossi <ossi@example.com>
2017-02-13 20:30:02 +0100

    Specs of ArtistIdentifier pass again

--- spec/models/artist_identifier_spec.rb
+++ spec/models/artist_identifier_spec.rb
@@ -10,18 +10,23 @@ RSpec.describe ArtistIdentifier, type: :model do
       expect(@identifier).to be_valid
     end
 
-    it 'is not valid without a source' do
-      @identifier.source = nil
-      expect(@identifier).not_to be_valid
-    end
-
     it 'is not valid without a value' do
       @identifier.value = nil
       expect(@identifier).not_to be_valid
     end
 
+    it 'is not valid without an artist' do
+      @identifier.artist = nil
+      expect(@identifier).not_to be_valid
+    end
+
     it 'is not valid with a blank value' do
       @identifier.value = ''
+      expect(@identifier).not_to be_valid
+    end
+
+    it 'is not valid without a source' do
+      @identifier.source = nil
       expect(@identifier).not_to be_valid
     end
   end
@@ -31,30 +36,15 @@ RSpec.describe ArtistIdentifier, type: :model do
       DatabaseCleaner.start
       @identifier = FactoryGirl.create(:artist)
     end
-    
+
     it 'has one artist' do
       expect(@identifier).to be_valid
     end
 
     specify '#identified' do
-
       expect(@identifier).to be_instance_of Artist
     end
 
     after(:each) { DatabaseCleaner.clean }
-  end
-
-  context 'with an alternative identified' do
-    before(:all) do
-      DatabaseCleaner.start
-      @identifier = FactoryGirl.create(:artist_identifier)
-    end
-
-    specify '#alt_identified' do
-      @identifier.alt_identified << FactoryGirl.create(:artist)
-      expect(@identifier.alt_identified.length).to eq 1
-    end
-
-    after(:all) { DatabaseCleaner.clean }
   end
 end
