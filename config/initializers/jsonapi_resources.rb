0b7adf0f01cd6ef862373ae8b5b852eb8030b11e
ossi <ossi@example.com>
2017-02-21 20:46:50 +0100

    After tribulations accept artists from api-client
    The error was on client side.
    Also added :sort_name to Api::V01::ArtistResource.

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -1,2 +1,7 @@
 JSONAPI.configure do |config|
+  # TODO: using underscored_key as json_key_format does not meet the JSON Api
+  # TODO: ... naming recommendations, it would be better to let the client
+  # TODO: ... sent valid keys
+  # http://jsonapi-resources.com/v0.8/guide/formating.html#Key-Format
+  #config.json_key_format = :underscored_key
 end
