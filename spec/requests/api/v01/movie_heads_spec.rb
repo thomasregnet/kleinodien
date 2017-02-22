3ae76e4a644c591609cd5c179550748d7d70390a
ossi <ossi@example.com>
2017-02-22 21:32:56 +0100

    Spec for Api::V01::MovieHeadResource

---
+++ spec/requests/api/v01/movie_heads_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+require 'api_helper'
+
+include ApiHelper
+
+RSpec.describe Api::V01::MovieHeadsController, type: :request do
+  it 'accepts an MovieHead' do
+    api_post '/api/v01/movie-heads',
+             Hash[
+               data: {
+                 type: 'movie_heads',
+                 attributes: {
+                   title: 'some movie',
+                   disambiguation: 'the other one'
+                 }
+               }
+             ]
+
+    expect(response).to be_success
+    expect(response).to have_http_status(:created)
+  end
+end
