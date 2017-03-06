58b87ac9d742eab062cab216cce232dfa740d315
ossi <ossi@example.com>
2017-03-06 20:37:43 +0100

    Filters for ArtistIdentifierResource

--- app/resources/api/v01/artist_identifier_resource.rb
+++ app/resources/api/v01/artist_identifier_resource.rb
@@ -1,6 +1,8 @@
 class Api::V01::ArtistIdentifierResource < JSONAPI::Resource
   attributes :value, :artist_id, :source_id
 
+  filters :artist_id, :source_id, :value
+
   has_one :artist
   has_one :source
 end
