66cd3549fe1376a2ac611e592aa1713d96fcd49d
ossi <ossi@example.com>
2017-03-13 21:31:46 +0100

    ArtistCredit must no longer have participants
    In order to let JSONAPI add artist_credits is was necessary to allow
    them to be created without participants.

--- app/models/artist_credit.rb
+++ app/models/artist_credit.rb
@@ -17,16 +17,37 @@ class ArtistCredit < ActiveRecord::Base
             blank:    false,
             uniqueness: { case_sensitive: false, scope: :source }
 
-  validates :participants, presence: true
+  #validates :participants, presence: true
 
-  before_save { write_attribute(:name, name) }
-  before_validation { self.name = name }
+  #before_save { write_attribute(:name, forced_name) }
+  before_save { name = forced_name }
+  before_validation { self.name = forced_name }
 
-  def name
+  def forced_name
+    return name if name
+    combined_name
+  end
+
+  def combined_name
     return unless participants
     return if participants.empty?
     KleinodienUtil::JoinNames.perform(
       participants
     )
   end
+  # def name
+  #   if !participants || participants.empty?
+  #     byebug
+  #     return self.name
+  #   end
+  #   name_by_participants
+  # end
+
+  # def name_by_participants
+  #   return unless participants
+  #   return if participants.empty?
+  #   KleinodienUtil::JoinNames.perform(
+  #     participants
+  #   )
+  # end
 end
