9c8b17660085de825c36c220721f583e085ef3db
ossi <ossi@example.com>
2017-03-27 22:02:09 +0200

    Fixed bug in MovieHeadResource
    It was inexplicably no longer possible to create a MovieHead by
    using MovieHeadResource. The solution was to add
    
    `model_name 'MovieHead'`
    
    to Api::V01::MovieHeadResource.

--- spec/requests/api/v01/movie_heads_spec.rb
+++ spec/requests/api/v01/movie_heads_spec.rb
@@ -5,17 +5,19 @@ include ApiHelper
 
 RSpec.describe Api::V01::MovieHeadsController, type: :request do
   it 'accepts an MovieHead' do
-    api_post '/api/v01/movie-heads',
+    api_post( '/api/v01/movie-heads',
              Hash[
                data: {
                  type: 'movie_heads',
                  attributes: {
-                   title: 'some movie',
-                   disambiguation: 'the other one'
+                   title:          'some movie',
+                   disambiguation: 'distict from the others'
                  }
                }
              ]
-
+            )
+    #byebug
+    puts response.body
     expect(response).to be_success
     expect(response).to have_http_status(:created)
   end
