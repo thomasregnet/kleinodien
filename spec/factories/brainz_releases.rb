2f8d18b84c86bb445e7695fb3e2bc5a2b9cd5fa8
ossi <ossi@example.com>
2017-03-27 22:53:20 +0200

    Specs, validations and factory for BrainzRelease

---
+++ spec/factories/brainz_releases.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :brainz_release, :class => 'BrainzRelease' do
+    mbid SecureRandom.uuid.to_s
+    sequence(:url) { |n| "http://fake/url#{n}" }
+    data '<fake>data</fake>'
+  end
+end
--- spec/models/brainz_release_spec.rb
+++ spec/models/brainz_release_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe BrainzRelease, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @brainz_release = FactoryGirl.build(:brainz_release)
+  end
+
+  it 'is not valid without a mbid' do
+    @brainz_release.mbid = nil
+    expect(@brainz_release).not_to be_valid
+  end
+
+  it 'is not valid without an url' do
+    @brainz_release.url = nil
+    expect(@brainz_release).not_to be_valid
+  end
+
+  it 'is not valid with a blank url' do
+    @brainz_release.url = ''
+    expect(@brainz_release).not_to be_valid
+  end
+
+  it 'is not valid without data' do
+    @brainz_release.data = nil
+    expect(@brainz_release).not_to be_valid
+  end
+
+  it 'is not valid with blank data' do
+    @brainz_release.data = ''
+    expect(@brainz_release).not_to be_valid
+  end
+
+  it 'is saveable' do
+    expect(@brainz_release.save).to be true
+  end
 end
