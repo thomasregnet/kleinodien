040bda9b2bb0e1f58ec79253825298db4ae61f82
ossi <ossi@example.com>
2017-04-03 20:37:51 +0200

    New method BrainzTestHelper.get_xml

--- spec/brainz_test_helper.rb
+++ spec/brainz_test_helper.rb
@@ -4,4 +4,10 @@ module BrainzTestHelper
     xml = File.open(file_name)
     KleinodienBrainz::Model::Release.xml(xml)
   end
+
+  def self.get_xml(type, mbid)
+    file_name = File.join('fixtures', 'music_brainz', type.to_s, mbid)
+    file_name += '.xml'
+    File.open(file_name).read
+  end
 end
