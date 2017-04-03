3face427ceca104cb3be2b2ab95b8ccfcb62c186
ossi <ossi@example.com>
2017-04-03 22:25:39 +0200

    Import::BrainzRelease returns an ArtistCredit

--- spec/models/import/brainz_release_spec.rb
+++ spec/models/import/brainz_release_spec.rb
@@ -3,14 +3,19 @@ require 'brainz_test_helper'
 
 RSpec.describe Import::BrainzRelease, type: :model do
   before(:each) do
+    DatabaseCleaner.start
     @xml = BrainzTestHelper.get_xml(
       :release,
       '28e723f2-1c0a-38a0-8109-038cca05ffca'
     )
   end
 
-  it 'returns a hash' do
-    expect(Import::BrainzRelease.perform(@xml))
-      .to be_instance_of MashedBrainz::Release
+  it 'returns an ArtistCredit' do
+    artist_credit = Import::BrainzRelease.perform(@xml)
+    expect(artist_credit).to be_instance_of ArtistCredit
+  end
+
+  after(:each) do
+    DatabaseCleaner.clean
   end
 end
