675f12f9c19297c898133e2258b8622fc17d29bf
ossi <ossi@example.com>
2017-04-04 22:42:46 +0200

    Import::BrainzRelease creates artists ...
    ... and participants.

--- app/models/import/brainz_release.rb
+++ app/models/import/brainz_release.rb
@@ -21,16 +21,39 @@ module Import
       ac_name = artist_credit_name
       ac = ArtistCredit.find_by(name: ac_name)
       return ac if ac
-      ArtistCredit.create!(name: ac_name)
+      ac = ArtistCredit.create!(name: ac_name)
+      artists = import_artists(data.artist_credit.name_credits)
+      participants(ac, artists, data.artist_credit.name_credits)
+      ac
     end
 
     def artist_credit_name
-      # FIXME: evil hack here
-      # TODO: join compound ArtistCredit names
-      data.artist_credit.name_credit[0].artist.name
+      data.artist_credit.name
     end
 
-    def artist
+    def import_artists(name_credits)
+      name_credits.map { |credit| import_artist(credit.artist) }
+    end
+
+    def import_artist(artist)
+      # TODO: check if the artist already exists
+      Artist.create!(
+        name:           artist.name,
+        sort_name:      artist.sort_name,
+        disambiguation: artist.disambiguation
+      )
+    end
+
+    def participants(artist_credit, artists, name_credits)
+      artists.each_with_index do |artist, no|
+        join_phrase = name_credits[no].stripped_joinphrase
+        Participant.create!(
+          artist:        artist,
+          artist_credit: artist_credit,
+          join_phrase:   join_phrase,
+          position:      no
+        )
+      end
     end
   end
 end
--- app/models/mashed_brainz/artist_credit.rb
+++ app/models/mashed_brainz/artist_credit.rb
@@ -5,6 +5,10 @@ module MashedBrainz
         name_credit,
         join_phrase: 'stripped_joinphrase')
     end
+
+    def name_credits
+      name_credit
+    end
   end
 end
     
