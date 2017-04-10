11d624e91b089d88e2b44e62109aee970129dd32
ossi <ossi@example.com>
2017-04-10 21:50:07 +0200

    Specs for ChIdentifier

--- spec/factories/cr_identifiers.rb
+++ spec/factories/cr_identifiers.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :cr_identifier do
-    sequence(:value) { |n| "compilation-head-identifier-#{n}" }
+    sequence(:value) { |n| "compilation-release-identifier-#{n}" }
     association :compilation_release, factory: :compilation_release
     association :source, factory: :source
   end
