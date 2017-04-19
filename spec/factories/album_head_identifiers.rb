63db30252e16d4eca732ba5dfd54e986bc4a4dce
ossi <ossi@example.com>
2017-04-19 20:40:54 +0200

    Specs and factory for AlbumHeadIdentifier

---
+++ spec/factories/album_head_identifiers.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :album_head_identifier do
+    sequence(:value) { |n| "album-head-identifier-#{n}" }
+    association :identified, factory: :album_head
+    association :source, factory: :source
+  end
+end
