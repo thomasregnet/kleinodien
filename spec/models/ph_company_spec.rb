5ad14ad81e49709a2d9f2a5aa8f4b124d050713c
ossi <ossi@example.com>
2017-04-24 20:42:14 +0200

    Refactored shared_examples_for_companies
    No more before() blocks.

--- spec/models/ph_company_spec.rb
+++ spec/models/ph_company_spec.rb
@@ -3,7 +3,7 @@ require 'shared_examples_for_companies'
 
 RSpec.describe PhCompany, type: :model do
   it_behaves_like 'a company' do
-    let(:factory)      { :ph_company }
+    let(:company)      { FactoryGirl.create(:ph_company) }
     let(:owner_setter) { 'piece_head=' }
   end
 end
