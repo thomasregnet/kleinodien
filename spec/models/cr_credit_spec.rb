e3027dff42492dc528bc2649cb7f37ed0460bc13
ossi <ossi@example.com>
2017-04-24 20:56:15 +0200

    Refactored shared_examples_for_credits
    No more before() blocks.

--- spec/models/cr_credit_spec.rb
+++ spec/models/cr_credit_spec.rb
@@ -3,9 +3,9 @@ require 'shared_examples_for_credits'
 
 RSpec.describe CrCredit, type: :model do
   it_behaves_like 'a credit' do
-    let(:factory)           { :cr_credit }
-    let(:factory_with_job)  { :cr_credit_with_job }
-    let(:factory_with_role) { :cr_credit_with_role }
+    let(:credit)           { FactoryGirl.create(:cr_credit) }
+    let(:credit_with_job)  { FactoryGirl.create(:cr_credit_with_job) }
+    let(:credit_with_role) { FactoryGirl.create(:cr_credit_with_role) }
     let(:owner_setter)      { 'compilation_release=' }
   end
 end
