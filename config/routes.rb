25f92d1154c69f80ea6a23f9fb874343d758a48a
ossi <ossi@example.com>
2017-05-03 21:44:37 +0200

    Re-added api/v01 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v01 do
+      jsonapi_resources :album_heads
+      jsonapi_resources :artist_credits
+      jsonapi_resources :artist_identifiers      
+      jsonapi_resources :artists
+      jsonapi_resources :ch_identifiers
+      jsonapi_resources :compilation_heads
+      jsonapi_resources :episode_heads
+      jsonapi_resources :movie_heads
+      jsonapi_resources :participants
+      jsonapi_resources :piece_heads
+      jsonapi_resources :song_heads
+      jsonapi_resources :sources
+    end
+  end
 end
