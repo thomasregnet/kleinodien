e3fc5b8f343236040e99d055c9a6cfa969e9f7da
ossi <ossi@example.com>
2017-05-03 20:38:16 +0200

    *_credit_spec.rb work

--- app/models/ph_credit.rb
+++ app/models/ph_credit.rb
@@ -1,7 +1,7 @@
 # PieceHead Credit
 class PhCredit < ActiveRecord::Base
   belongs_to :artist_credit
-  belongs_to :job
+  belongs_to :job, required: false
   belongs_to :piece_head
   validates :artist_credit, presence: true
   validates :piece_head, presence: true
