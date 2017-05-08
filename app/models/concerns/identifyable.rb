a82777f4876f0b138bc7b82d7e4adca5cf910205
ossi <ossi@example.com>
2017-05-08 21:56:39 +0200

    Identifyable.identify works for Artist

--- app/models/concerns/identifyable.rb
+++ app/models/concerns/identifyable.rb
@@ -2,8 +2,16 @@ module Identifyable
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def identify(source, value)
-      nil
+    def identify(source_name, value)
+      identifier_class = "#{self.to_s}Identifier".constantize
+
+      source = Source.find_by(name: source_name)
+      return unless source
+
+      identifier = identifier_class.find_by(source: source, value: value)
+      return unless identifier
+
+      return identifier.identified
     end
   end
 end
--- spec/shared_examples_for_identifyable.rb
+++ spec/shared_examples_for_identifyable.rb
@@ -2,4 +2,19 @@ RSpec.shared_examples 'an identifyable model' do
   it 'responds to .identify' do
     expect(identifyable.class).to respond_to(:identify)
   end
+
+  it 'identifies the object' do
+    identifier  = identifyable.identifiers[0]
+    source_name = identifier.source.name
+    value       = identifier.value
+
+    found = identifyable.class.identify(source_name, value)
+
+    expect(found.id).to eq identifyable.id
+  end
+
+  it 'returns nil if the nothing can be identified' do
+    found = identifyable.class.identify('unknown source', 'no such id')
+    expect(found).to be_nil
+  end
 end
