f583e08669193d3adcca595226ce76e466b5d086
ossi <ossi@example.com>
2017-05-09 20:22:37 +0200

    Renamed ch_identifiers* to compilation_head_ide...

--- spec/models/compilation_head_identifier_spec.rb
+++ spec/models/compilation_head_identifier_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 require 'shared_examples_for_identifiers'
-RSpec.describe ChIdentifier, type: :model do
+RSpec.describe CompilationHeadIdentifier, type: :model do
   it_behaves_like 'an identifier' do
-    let(:identifier) { FactoryGirl.build(:ch_identifier) }
+    let(:identifier) { FactoryGirl.build(:compilation_head_identifier) }
   end
 
   it 'is not valid without a CompilationHead' do
-    identifier = FactoryGirl.build(:ch_identifier)
+    identifier = FactoryGirl.build(:compilation_head_identifier)
     identifier.compilation_head = nil
     expect(identifier).not_to be_valid
   end
