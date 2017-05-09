edfd532b544e4d4f0fe58daa462427071a5d7f6d
ossi <ossi@example.com>
2017-05-09 20:34:35 +0200

    Rake general runs with errors ...
    after renaming some JSONAPI-classes from ChIdentifier* to
    CompilationHeadIdentifer*

---
+++ app/controllers/api/v01/compilation_head_identifiers_controller.rb
@@ -0,0 +1,6 @@
+module Api
+  module V01
+    class CompilationHeadIdentifiersController < JSONAPI::ResourceController
+    end
+  end
+end
--- app/resources/api/v01/ch_identifier_resource.rb
+++
@@ -1,8 +0,0 @@
-class Api::V01::ChIdentifierResource < JSONAPI::Resource
-  attributes :compilation_head_id, :source_id, :value
-
-  filters :compilation_head_id, :source_id, :value
-
-  has_one :compilation_head
-  has_one :source
-end
