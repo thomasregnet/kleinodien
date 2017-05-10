528b235081f3af8c02c67b729ac88e7bc7bd8200
ossi <ossi@example.com>
2017-05-10 21:58:36 +0200

    PieceHeadIdentifier: Specs, associations ...

--- app/models/piece_head_identifier.rb
+++ app/models/piece_head_identifier.rb
@@ -1,2 +1,10 @@
 class PieceHeadIdentifier < ApplicationRecord
+  belongs_to :piece_head
+  belongs_to :source
+
+  validates :piece_head, presence: true
+  validates :source, presence: true
+  validates :value, presence: true, blank: false
+
+  alias_attribute :identified, :piece_head
 end
---
+++ spec/factories/piece_head_identifiers.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :piece_head_identifier do
+    sequence(:value) { |n| "album-head-identifier-#{n}" }
+    association :identified, factory: :piece_head
+    association :source, factory: :source
+  end
+end
