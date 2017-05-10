28125383ddb48498a3ca06ee2f64dedf2a56906a
ossi <ossi@example.com>
2017-05-10 21:58:36 +0200

    PieceHeadIdentifier: Specs, associations ...

--- spec/models/piece_head_identifier_spec.rb
+++ spec/models/piece_head_identifier_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
+require 'shared_examples_for_identifiers'
 
 RSpec.describe PieceHeadIdentifier, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like 'an identifier' do
+    let(:identifier) { FactoryGirl.build(:piece_head_identifier) }
+  end
+
+  it 'is not valid without a PieceHead' do
+    identifier = FactoryGirl.build(:piece_head_identifier)
+    identifier.piece_head = nil
+    expect(identifier).not_to be_valid
+  end
 end
