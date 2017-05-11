66f5ed20ea06a3ee7952ee55d4082f22a700310f
ossi <ossi@example.com>
2017-05-11 21:49:51 +0200

    SongRelease is Identifyable

--- app/models/song_release.rb
+++ app/models/song_release.rb
@@ -1,5 +1,7 @@
 # Version of a song
 class SongRelease < PieceRelease
+  include Identifyable
+
   belongs_to :head,
              class_name: 'SongHead',
              foreign_key: :piece_head_id
---
+++ app/models/song_release_identifier.rb
@@ -0,0 +1,2 @@
+class SongReleaseIdentifier < PieceReleaseIdentifier
+end
