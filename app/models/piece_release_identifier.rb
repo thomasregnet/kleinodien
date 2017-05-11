9ae51384053daf26a6983e513d62abe2340a6ff0
ossi <ossi@example.com>
2017-05-11 21:28:38 +0200

    EpisodeRelease is Identifyable

--- app/models/piece_release_identifier.rb
+++ app/models/piece_release_identifier.rb
@@ -1,2 +1,10 @@
 class PieceReleaseIdentifier < ApplicationRecord
+  belongs_to :piece_release
+  belongs_to :source
+
+  validates :piece_release, presence: true
+  validates :source, presence: true
+  validates :value, presence: true, blank: false
+
+  alias_attribute :identified, :piece_release
 end
--- spec/models/episode_release_spec.rb
+++ spec/models/episode_release_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
+require 'shared_examples_for_identifyable'
 
 RSpec.describe EpisodeRelease, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like 'an identifyable model' do
+    before(:each) do
+      @episode_release = FactoryGirl.create(:episode_release_with_identifiers)
+    end
+
+    let(:identifyable) { @episode_release }
+  end
 end
