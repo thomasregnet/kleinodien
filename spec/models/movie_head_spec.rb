5c3713a73b46fd2990c54d1a247fe9b574f2196a
ossi <ossi@example.com>
2017-05-11 20:27:58 +0200

    MovieHead is Identifyable

--- spec/models/movie_head_spec.rb
+++ spec/models/movie_head_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 require 'shared_examples_for_disambiguations'
+require 'shared_examples_for_identifyable'
 
 RSpec.describe MovieHead, type: :model do
   before(:each) do
@@ -14,5 +15,13 @@ RSpec.describe MovieHead, type: :model do
     let(:factory) { :movie_head }
     let(:object) { @movie_head }
     let(:naming) { 'title' }
+  end
+
+  it_behaves_like 'an identifyable model' do
+    before(:each) do
+      @movie_head = FactoryGirl.create(:movie_head_with_identifiers)
+    end
+
+    let(:identifyable) { @movie_head }
   end
 end
