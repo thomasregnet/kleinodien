7033dfd8153eb044d6596140d690daf09d555f9e
ossi <ossi@example.com>
2017-09-26 20:24:48 +0200

    Music_brainz_releases receives some useless data
    ... for testing.

--- app/controllers/api/v01/music_brainz_releases_controller.rb
+++ app/controllers/api/v01/music_brainz_releases_controller.rb
@@ -1,4 +1,12 @@
 class Api::V01::MusicBrainzReleasesController < ApplicationController
   def create
+    x = brainz_params
+  end
+
+  private
+
+  def brainz_params
+    # params.require(:data).permit(:type)
+    params.require(:data).require(:type)
   end
 end
