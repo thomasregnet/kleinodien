de0fcf73b624784e63edd7b2dfc0a37296cbe1f4
ossi <ossi@example.com>
2017-09-26 10:44:24 +0200

    import_a_music_brainz_releases receives data

--- features/step_definitions/import_a_music_brainz_release_steps.rb
+++ features/step_definitions/import_a_music_brainz_release_steps.rb
@@ -1,6 +1,6 @@
 When(/^I send a MusicBrainz id of a release i want to import$/) do
   post(
-    '/api/v01/music_brainz_release',
+    '/api/v01/music_brainz_releases',
     headers: { 'Content-Type' => 'application/json' },
     params:  { foo: 'bar' }.to_json
   )
