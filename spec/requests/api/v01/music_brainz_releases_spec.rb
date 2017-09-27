1e005b0be8c99c9a348d755163a6ed832f266a2a
ossi <ossi@example.com>
2017-09-27 11:32:39 +0200

    Some more on music_brainz_releases_controller

--- spec/requests/api/v01/music_brainz_releases_spec.rb
+++ spec/requests/api/v01/music_brainz_releases_spec.rb
@@ -7,7 +7,14 @@ RSpec.describe Api::V01::MusicBrainzReleasesController do
   it 'accepts a request to import an MusicBraiz-release' do
     api_post '/api/v01/music_brainz_releases',
              Hash[
-               data: { type: 'music-brainz-releases'}
+               data: {
+                 type: 'music-brainz-releases',
+                 attributes: {
+                   wanted: 'b3b2ee43-57d4-4f82-b14a-999fd46821de'
+                 }
+               }
              ]
+    expect(response).to have_http_status(202)
+    expect(response.content_type).to eq 'application/vnd.api+json'
   end
 end
