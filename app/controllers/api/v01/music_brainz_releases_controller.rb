c7fe8b9b4c6d8ac39f1ec6ebc29ba5f94397ba1c
ossi <ossi@example.com>
2017-09-27 10:45:00 +0200

    Added music_brainz_releases_controller_spec.rb
    Also modified config/initializers/mime_types.rb to accept
    application/vnd.api+json.

--- app/controllers/api/v01/music_brainz_releases_controller.rb
+++ app/controllers/api/v01/music_brainz_releases_controller.rb
@@ -1,12 +1,14 @@
 class Api::V01::MusicBrainzReleasesController < ApplicationController
   def create
     x = brainz_params
+    response.status = 200
+    response.content_type = 'application/vnd.api+json'
+    render :json => {}
   end
 
   private
 
   def brainz_params
-    # params.require(:data).permit(:type)
     params.require(:data).require(:type)
   end
 end
