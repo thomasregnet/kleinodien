b548b37ba7b97c6aa9bcec154e179c44a3ad0d47
ossi <ossi@example.com>
2017-09-28 11:56:42 +0200

    ImportBrainzRelease returns required data

--- spec/models/import_brainz_release_spec.rb
+++ spec/models/import_brainz_release_spec.rb
@@ -1,25 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe ImportBrainzRelease, type: :model do
-  it 'returns a response object' do
-    expect(ImportBrainzRelease.perform({}, ActionDispatch::Response.new))
-      .to be_instance_of(ActionDispatch::Response)
-  end
-
   it 'returns required data' do
+    brainz_id = '404e67be-0b5e-47bc-81db-1e8c408e9e3f'
     response = ImportBrainzRelease.perform(
       Hash[
         data: {
           type: 'music-brainz-release',
           attributes: {
-            wanted: '404e67be-0b5e-47bc-81db-1e8c408e9e3f'
+            wanted: brainz_id
           }
         }
       ],
       ActionDispatch::Response.new
     )
 
-    expect(response.body).to match(/foobar/)
+    expect(response).to be_instance_of ActionDispatch::Response
     expect(response.status).to eq 202
+
+    data = JSON.parse(response.body, symbolize_names: true)[:data]
+    required = data[:attributes][:required][:musicbrainz][0]
+    required_attrs = required[:attributes]
+
+    expect(required[:id]). to eq brainz_id
+    expect(required_attrs[:path]). to eq "release/#{brainz_id}"
+    expect(required_attrs[:url])
+      .to eq "http://musicbrainz.org/ws/2/release/#{brainz_id}"
   end
 end
