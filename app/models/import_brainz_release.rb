2223888e336c807bb96cd446f32923058c80a1e4
ossi <ossi@example.com>
2017-09-28 11:56:42 +0200

    ImportBrainzRelease returns required data

--- app/models/import_brainz_release.rb
+++ app/models/import_brainz_release.rb
@@ -1,9 +1,9 @@
+# Post MusicBrainz data to kleinodien
 class ImportBrainzRelease
-
   attr_reader :data, :response
 
   def self.perform(data, response)
-    new(data,response).perform
+    new(data, response).perform
   end
 
   def initialize(data, response)
@@ -14,10 +14,35 @@ class ImportBrainzRelease
   def perform
     response.body = body
     response.status = 202
-    response 
+    response
   end
 
   def body
-    'foobar'
+    {
+      data:
+        {
+          attributes:
+            {
+              required: {
+                musicbrainz: [
+                  required('release', data[:attributes][:wanted])
+                ]
+              }
+            }
+        }
+    }.to_json
+  end
+
+  def required(type, id)
+    path = "#{type}/#{id}"
+    {
+      type: type,
+      id:   id,
+      attributes: {
+        url: "http://musicbrainz.org/ws/2/#{path}",
+        query_string: 'inc=artists+labels+recordings+release-groups',
+        path: path
+      }
+    }
   end
 end
