c3976c52fc3e729b5576c3f11ddbd3e1047b8623
ossi <ossi@example.com>
2017-09-28 12:38:16 +0200

    Started spec 'Sepultura - Arise'
    ... for ImportBrainzRelease.

--- spec/models/import_brainz_release_spec.rb
+++ spec/models/import_brainz_release_spec.rb
@@ -27,4 +27,33 @@ RSpec.describe ImportBrainzRelease, type: :model do
     expect(required_attrs[:url])
       .to eq "http://musicbrainz.org/ws/2/release/#{brainz_id}"
   end
+
+  specify 'Sepultura - Arise' do
+    brainz_id = '7452f8c9-f9bc-3ca7-859e-3220e57e4e4'
+    response = ImportBrainzRelease.perform(
+      Hash[
+        data: {
+          type: 'music-brainz-release',
+          attributes: {
+            wanted: brainz_id,
+            known: {
+              musicbrainz: [
+                {
+                  type: 'release',
+                  id: brainz_id,
+                  attributes: {
+                    query_result: '<xml></xml>'
+                  }
+                }
+              ]
+            }
+          }
+        }
+      ],
+      ActionDispatch::Response.new
+    )
+
+    expect(response.status).to eq 202
+    # TODO: :wanted must not contain the release
+  end
 end
