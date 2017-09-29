c6d5e38822589fb9f60da1d5275cd4906b21152a
ossi <ossi@example.com>
2017-09-29 11:17:05 +0200

    Progress in ImportCache

--- spec/models/import_cache_spec.rb
+++ spec/models/import_cache_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ImportCache, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  specify '.fetch_brainz_artist' do
+    expect(subject).to respond_to(:fetch_brainz_artist)
+  end
+
+  specify '.store_brainz_artist' do
+    expect(subject).to respond_to('store_brainz_artist')
+  end
+
+  specify '.require_brainz_artist' do
+    expect(subject).to respond_to('require_brainz_artist')
+  end
+
+  specify 'write and read known' do
+    cache = ImportCache.new
+    cache.store_discogs_artist('abc', 'foo')
+    expect(cache.fetch_discogs_artist('abc')).to eq 'foo'
+  end
 end
