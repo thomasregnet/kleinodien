85030a60f0ec00ed3e50fc4d705cef64a6529743
ossi <ossi@example.com>
2017-10-04 08:34:31 +0200

    ImportFlatCache is flatter than before

--- spec/models/import_flat_cache_spec.rb
+++ spec/models/import_flat_cache_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 RSpec.describe ImportFlatCache, type: :model do
   specify '.fetch_brainz_artist' do
-    expect(subject).to respond_to(:fetch_brainz_artist)
+    expect(subject).to respond_to(:fetch_brainz)
   end
 
   specify '.store_brainz_artist' do
-    expect(subject).to respond_to('store_brainz_artist')
+    expect(subject).to respond_to(:store_brainz)
   end
 
   specify '.require_brainz_artist' do
-    expect(subject).to respond_to('require_brainz_artist')
+    expect(subject).to respond_to(:require_brainz)
   end
 
   specify 'write and read known' do
     cache = ImportFlatCache.new
-    cache.store_discogs_artist('abc', 'foo')
-    expect(cache.fetch_discogs_artist('abc')).to eq 'foo'
+    cache.store_discogs('abc', 'foo')
+    expect(cache.fetch_discogs('abc')).to eq 'foo'
   end
 
   specify 'write and read requirements' do
     cache = ImportFlatCache.new
-    cache.require_discogs_artist('http://foo/bar')
+    cache.require_discogs('http://foo/bar')
     # TODO: read the requirements
   end
 end
