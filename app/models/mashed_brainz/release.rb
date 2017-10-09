1e9ef49c5bbd3dec82e14de71d58000e14709ea3
ossi <ossi@example.com>
2017-10-09 22:12:07 +0200

    New methods for MashedBrainz::Release
    * #brainz_id
    * #source_id

--- app/models/mashed_brainz/release.rb
+++ app/models/mashed_brainz/release.rb
@@ -3,5 +3,13 @@ module MashedBrainz
     def self.xml(xml_string)
       new(parse_xml(xml_string)['release'])
     end
+    
+    def brainz_id
+      BrainzReleaseId.new(id)
+    end
+
+    def source_id
+      brainz_id.source_id
+    end
   end
 end
