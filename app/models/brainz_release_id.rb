4114deab8674a78867b231a5e79aa7d3ecef9b63
ossi <ossi@example.com>
2017-10-09 21:02:35 +0200

    New class BrainzReleaseId

---
+++ app/models/brainz_release_id.rb
@@ -0,0 +1,15 @@
+class BrainzReleaseId
+  URL_PREFIX = 'https://musicbrainz.org/ws/2/release/'.freeze
+  QUERY_STRING = '?inc=artists+labels+recordings+release-groups'.freeze
+
+  attr_reader :id
+
+  def initialize(uuid)
+    @id = uuid
+  end
+
+  # TODO: source_id must return string without https://musicbrainz.org...
+  def source_id
+    URL_PREFIX + id + QUERY_STRING
+  end
+end
