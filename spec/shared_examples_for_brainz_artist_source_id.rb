7da9064041e9bec16c79e1bac79a8c511dca169e
ossi <ossi@example.com>
2017-10-11 22:24:47 +0200

    Specs are passing. One well konwn cucumber error

--- spec/shared_examples_for_brainz_artist_source_id.rb
+++ spec/shared_examples_for_brainz_artist_source_id.rb
@@ -1,14 +1,6 @@
 RSpec.shared_examples 'a brainz artist source id' do
-  def prefix_regex
-    %r{^ws/2/artist/}
-  end
-
   def query_string_regex
-    /\?inc=currently\+unknown$/
-  end
-
-  it 'contains the prefix' do
-    expect(source_id).to match(prefix_regex)
+    /\?inc=url-rels$/
   end
 
   it 'matches the query_string' do
