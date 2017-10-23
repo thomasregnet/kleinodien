0248c4bac83c678be312c1251fb660d4aa27fe3f
ossi <ossi@example.com>
2017-10-23 21:41:30 +0200

    Add class "BrainzID"
    Base class for other BrinzIds

---
+++ spec/models/brainz_id_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe BrainzId do
+  before(:all) do
+    @foreign_id = BrainzId.new(value: '354f6427-e2d8-47e5-99bc-538992120eb7')
+  end
+
+  specify '#schema' do
+    expect(@foreign_id.schema).to  eq('https')
+  end
+
+  specify '#host' do
+    expect(@foreign_id.host).to eq('musicbrainz.org')
+  end
+
+  specify '#path_prefix' do
+    expect(@foreign_id.path_prefix).to eq('ws/2')
+  end
+end
