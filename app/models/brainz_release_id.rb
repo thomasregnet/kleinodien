f77e48fd9e084af0a29f8414ce1f729008f0df3b
ossi <ossi@example.com>
2017-10-24 09:47:58 +0200

    Use instance variables in BrainzReleaseId
    BrainzReleaseId uses instance variables instead class variables.
    All descendants of ForeignId are using class variables now.

--- app/models/brainz_release_id.rb
+++ app/models/brainz_release_id.rb
@@ -1,20 +1,23 @@
 # Source id for a MusicBrainz Release
 class BrainzReleaseId < BrainzId
-  URL_PREFIX = 'https://musicbrainz.org/ws/2/release/'.freeze
+  URL_PREFIX   = 'https://musicbrainz.org/ws/2/release/'.freeze
   QUERY_STRING = '?inc=artists+labels+recordings+release-groups'.freeze
 
-  attr_reader :id
+  attr_reader :kind, :query_string, :url_prefix
 
   def self.source_id(uuid)
     new(value: uuid).source_id
   end
 
-  # def initialize(uuid)
-  #   @id = uuid
-  # end
+  def initialize(args)
+    super(args)
+    @kind         = args[:kind]         || 'release'
+    @url_prefix   = args[:url_prefix]   || URL_PREFIX
+    @query_string = args[:query_string] || QUERY_STRING
+  end
 
   # TODO: source_id must return string without https://musicbrainz.org...
   def source_id
-    URL_PREFIX + value + QUERY_STRING
+    "#{schema}://#{host}/#{path_prefix}/#{kind}/#{value}#{query_string}"
   end
 end
