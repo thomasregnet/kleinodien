039d7f209e1124d33a408f6251f6848dedf3db0e
ossi <ossi@example.com>
2017-10-24 12:06:20 +0200

    Use cache_key almost work
    One failing scenario in features/import_brainz_release.feature.
    Should be fixed next.

--- app/models/prepare/brainz_artist.rb
+++ app/models/prepare/brainz_artist.rb
@@ -1,28 +1,27 @@
 module Prepare
   # Prapare an Artist from MusicBrainz for persistence
   class BrainzArtist
-    attr_reader :brainz_id, :cache
+    attr_reader :foreign_id, :cache
 
-    def self.using_id(id, cache)
-      new(id, cache).using_id
+    def self.using_id(foreign_id, cache)
+      new(foreign_id, cache).using_id
     end
 
-    def initialize(id, cache)
-      @brainz_id = BrainzArtistId.new(value: id)
-      @cache     = cache
+    def initialize(foreign_id, cache)
+      @foreign_id = foreign_id
+      @cache      = cache
     end
 
     def using_id
       prepare
       return if cache.any_required?
       # persist
-      ::Persist::BrainzArtist.using_id(brainz_id, cache)
+      ::Persist::BrainzArtist.using_id(foreign_id, cache)
     end
 
     def prepare
-      source_id = brainz_id.source_id
-      return if cache.required['brainz'].include? source_id
-      cache.require_brainz source_id unless cache.fetch_brainz(source_id)
+      return if cache.required['brainz'].include? foreign_id # source_id
+      cache.require_brainz foreign_id unless cache.fetch_brainz(foreign_id)
     end
   end
 end
--- spec/models/prepare/brainz_artist_spec.rb
+++ spec/models/prepare/brainz_artist_spec.rb
@@ -3,24 +3,24 @@ require 'ko_test_data'
 
 RSpec.describe Prepare::BrainzArtist do
   before(:each) do
-    @jello_biafra_id = '2280ca0e-6968-4349-8c36-cb0cbd6ee95f'
+    @cache      = Import::Cache.new
+    @foreign_id = BrainzArtistId.new(
+      value: '2280ca0e-6968-4349-8c36-cb0cbd6ee95f'
+    )
+    # @jello_biafra_id = '2280ca0e-6968-4349-8c36-cb0cbd6ee95f'
   end
 
   specify '.perform without cached artist' do
-    artist_importer = Prepare::BrainzArtist.new(
-      @jello_biafra_id,
-      Import::Cache.new
-    )
+    artist_importer = Prepare::BrainzArtist.new(@foreign_id, @cache)
     artist_importer.using_id
     expect(artist_importer.cache.any_required?).to be true
   end
 
   specify '.perform with cached artist' do
-    artist_sid = BrainzArtistId.new(value: @jello_biafra_id)
-    xml = KoTestData.brainz_artist(artist_sid)
-    cache = Import::Cache.new
-    cache.store_brainz(artist_sid.source_id, xml)
-    artist = Prepare::BrainzArtist.using_id(@jello_biafra_id, cache)
+    # artist_sid = BrainzArtistId.new(value: @jello_biafra_id)
+    xml = KoTestData.brainz_artist(@foreign_id)
+    @cache.store_brainz(@foreign_id, xml)
+    artist = Prepare::BrainzArtist.using_id(@foreign_id, @cache)
     expect(artist).to be_instance_of(Artist)
     expect(artist.new_record?).to be false
   end
