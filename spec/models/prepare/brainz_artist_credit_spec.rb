0e5514c7350f28f5ee81f53484475f4c5af7dfc0
ossi <ossi@example.com>
2017-10-25 11:28:04 +0200

    Cleanup ...prepare/brainz_artist_credit_spec.rb

--- spec/models/prepare/brainz_artist_credit_spec.rb
+++ spec/models/prepare/brainz_artist_credit_spec.rb
@@ -3,15 +3,15 @@ require 'ko_test_data'
 
 RSpec.describe Prepare::BrainzArtistCredit do
   before(:each) do
+    @cache = Import::Cache.new
+
     brainz_id = '693748be-7c18-39c3-af2e-2e62092090cf'
     xml = KoTestData.brainz_release(BrainzReleaseId.new(value: brainz_id))
     @artist_credit = MashedBrainz::Release.xml(xml).artist_credit
   end
 
   specify '.perform' do
-    ac_importer = Prepare::BrainzArtistCredit.new(
-      @artist_credit, Import::Cache.new
-    )
+    ac_importer = Prepare::BrainzArtistCredit.new(@artist_credit, @cache)
     ac_importer.using_data
     expect(ac_importer.cache.any_required?).to be true
   end
