2e870e911fc99263dc357690e8eded5c7bd492b6
ossi <ossi@example.com>
2017-10-25 21:35:36 +0200

    Cleanup Persist::BrainzArtistCredit
    Moved building of participants from Persist::BrainzArtistCredit.using_data
    to .build_name_credit_on. Reek thinks that's not enough.

--- app/models/persist/brainz_artist_credit.rb
+++ app/models/persist/brainz_artist_credit.rb
@@ -15,20 +15,25 @@ module Persist
     def using_data
       artist_credit = ArtistCredit.new(source: Source::MusicBrainz)
       original.name_credits.each_with_index do |original_name_credit, position|
-        artist = BrainzArtist.using_id(
-          original_name_credit.artist.brainz_id, cache
-        )
-        join_phrase = original_name_credit.joinphrase
-        join_phrase.strip! if join_phrase
-        artist_credit.participants.build(
-          artist:      artist,
-          join_phrase: join_phrase,
-          position:    position
-        )
+        build_name_credit_on(original_name_credit, artist_credit, position)
       end
 
       artist_credit.save!
       artist_credit
+    end
+
+    def build_name_credit_on(original_name_credit, artist_credit, position)
+      artist = BrainzArtist.using_id(
+        original_name_credit.artist.brainz_id, cache
+      )
+      join_phrase = original_name_credit.joinphrase
+      join_phrase.strip! if join_phrase
+      artist_credit.participants.build(
+        artist:      artist,
+        join_phrase: join_phrase,
+        position:    position
+      )
+
     end
   end
 end
