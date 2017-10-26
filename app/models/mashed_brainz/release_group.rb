ca084aa5e467a2c295a230d4b59d804e6646d1ae
ossi <ossi@example.com>
2017-10-26 21:17:03 +0200

    Implement MashedBrainz::ReleaseGroup.xml
    Currently without specs.

--- app/models/mashed_brainz/release_group.rb
+++ app/models/mashed_brainz/release_group.rb
@@ -1,6 +1,9 @@
 module MashedBrainz
   # MusicBrainz release-group
   class ReleaseGroup < Base
+    def self.xml(xml_string)
+      new(parse_xml(xml_string)['release_group'])
+    end
 
     def brainz_id
       BrainzReleaseGroupId.new(value: id)
