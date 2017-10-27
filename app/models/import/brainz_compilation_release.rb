ab50274de457d81b326d51367b9b64c7e5e143fd
ossi <ossi@example.com>
2017-10-27 11:00:42 +0200

    Fix Import::BrainzCompilationRelease
    Import::BrainzCompilationRelease.perform no longer tries to perist
    if cache holds any requirements.
    Also cleaned up the class.

--- app/models/import/brainz_compilation_release.rb
+++ app/models/import/brainz_compilation_release.rb
@@ -1,40 +1,34 @@
 module Import
   # Post MusicBrainz params to kleinodien
   class BrainzCompilationRelease
-    URL_PREFIX = 'https://musicbrainz.org/ws/2/release/'.freeze
-    # TODO: add '+url-rels+recording-level-rels' to QUERY_STRING?
-    QUERY_STRING = '?inc=artists+labels+recordings+release-groups'.freeze
-    attr_reader :params, :cache
+    attr_reader :cache, :foreign_id, :params
 
     def self.perform(params)
       new(params).perform
     end
 
     def initialize(params)
-      @params = params
-      @cache = Cache.new
+      @params     = params
+      @cache      = Cache.new
+      @foreign_id = BrainzReleaseId.new(
+        value: params[:data][:attributes][:wanted]
+      )
+
+      cache.rebuild_from_params(params)
     end
 
     def perform
-      foreign_id = BrainzReleaseId.new(
-        value: params[:data][:attributes][:wanted]
-      )
-      #byebug
-      cache.rebuild_from_params(params)
-      ::Prepare::BrainzCompilationRelease.using_id(
-        foreign_id,
-        cache
-      )
-      ::Persist::BrainzCompilationRelease.using_id(
-        foreign_id,
-        cache
-      )
+      ::Prepare::BrainzCompilationRelease.using_id(foreign_id, cache)
+
+      return body if cache.any_required?
+
+      ::Persist::BrainzCompilationRelease.using_id(foreign_id, cache)
+
       body
     end
 
     def body
       wanted_id = params[:data][:attributes][:wanted]
-      #brainz_id = BrainzReleaseId.new(value: wanted_id)
       brainz_id = BrainzReleaseId.new(value: wanted_id)
       cache.require_brainz(brainz_id)
       {
