db56e1946f91d384a3d2b096ef6dc2f10e2bc618
ossi <ossi@example.com>
2017-10-30 11:45:58 +0100

    AlbumRelease belongs_to :artist_credit

--- app/models/album_release.rb
+++ app/models/album_release.rb
@@ -2,6 +2,8 @@
 class AlbumRelease < CompilationRelease
   include Identifyable
 
+  belongs_to :artist_credit, required: false
+
   def self.with_discogs_id_exists?(discogs_id)
     with_id_from_data_supplier_exists?(discogs_id, 'Discogs')
   end
--- spec/models/album_release_spec.rb
+++ spec/models/album_release_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe AlbumRelease, type: :model do
   it_behaves_like 'an identifyable model' do
     let(:identifyable) { FactoryGirl.create(:album_release_with_identifiers) }
   end
+
+  it 'has_one artist_credit' do
+    album_release = FactoryGirl.create(:album_release)
+    album_release.artist_credit = FactoryGirl.create(:artist_credit)
+  end
 end
