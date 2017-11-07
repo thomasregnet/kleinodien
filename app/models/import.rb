37ef2cebc2d95665f59590a4df3c121df0dd230c
ossi <ossi@example.com>
2017-11-07 21:23:40 +0100

    Call :prepare in Import::Base#method_missing
    The #method_missing method of Import::Base calls :prepare on other
    Import-classes.
    
    All classes of the "Import" module get preloaded at app/models/import.rb
    to let Import.const_defined? work proper.

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,4 +1,8 @@
-# Classes responsible to import data 
+# We need to preload all classes of "Import" for Import.const_defined?
+# to work proper
+Dir[File.dirname(__FILE__) + '/import/*.rb'].each {|file| require file }
+
+# Classes responsible to import data
 module Import
   # see also
   # - prepare
