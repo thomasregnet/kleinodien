29004077214f5116df38ab9622f3555b213e957c
ossi <ossi@example.com>
2017-11-13 20:45:32 +0100

    More specs for MashedBrainz.from_xml
    MashedBrainz.from_xml gets tested with MusicBrainz Artist, Release
    and ReleaseGroup.
    Added "coerce_key :release" to MashedBrainz::Base.

--- spec/models/mashed_brainz_spec.rb
+++ spec/models/mashed_brainz_spec.rb
@@ -3,7 +3,7 @@ require 'ko_test_data'
 
 RSpec.describe MashedBrainz do
   describe '.from_xml' do
-    context 'with an artist-xml as parameter' do
+    context 'with artist xml as parameter' do
       it 'returns a MashedBrainz::Artist object' do
         reference = BrainzArtistRef.new(
           code: '1d93c839-22e7-4f76-ad84-d27039efc048'
@@ -12,6 +12,30 @@ RSpec.describe MashedBrainz do
         xml = KoTestData.brainz_xml_for(reference)
         expect(MashedBrainz.from_xml(xml))
           .to be_instance_of MashedBrainz::Artist
+      end
+    end
+
+    context 'with release xml as prameter' do
+      it 'returns a MashedBrainz::Release object' do
+        reference = BrainzReleaseRef.new(
+          code: '693748be-7c18-39c3-af2e-2e62092090cf'
+        )
+
+        xml = KoTestData.brainz_xml_for(reference)
+        expect(MashedBrainz.from_xml(xml))
+          .to be_instance_of MashedBrainz::Release
+      end
+    end
+
+    context 'with release-group xml as parameter' do
+      it 'returns a MashedBrainz::ReleaseGroup object' do
+        reference = BrainzReleaseGroupRef.new(
+          code: '7d31891f-b9da-36de-ab08-98b1fdbbb023'
+        )
+
+        xml = KoTestData.brainz_xml_for(reference)
+        expect(MashedBrainz.from_xml(xml))
+          .to be_instance_of MashedBrainz::ReleaseGroup
       end
     end
   end
