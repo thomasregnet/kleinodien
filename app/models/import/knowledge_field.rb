f1953858f6ef3189b1cfa97b9a064663a38dfad2
ossi <ossi@example.com>
2017-11-14 20:41:29 +0100

    KnowledgeField add requested items to required
    If an unknown item is requested via Import::KnowledgeField#about it is
    added to the requested items.
    
    New method Import::KnowledgeField#missing?. It indicates if any item
    was requested but present.

--- app/models/import/knowledge_field.rb
+++ app/models/import/knowledge_field.rb
@@ -1,15 +1,26 @@
 module Import
   # Base class for Import::Knowledge*
   class KnowledgeField
-    attr_reader :known
+    attr_reader :known, :required
 
     def initialize
       @known = {}
-      # TODO: @required
+      @required = Set.new
     end
 
     def about(reference)
-      known[reference.to_key]
+      ref_key = reference.to_key
+
+      response = known[ref_key]
+      return response if response
+
+      required.add(ref_key)
+
+      nil
+    end
+
+    def missing?
+      !required.empty?
     end
 
     def about!(reference)
