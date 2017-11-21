b125871a848f7e1bc7f1f39d411938ffdda666b3
ossi <ossi@example.com>
2017-11-21 22:21:42 +0100

    Remove commented out code related to Cache

--- app/models/import/brainz_compilation_release.rb
+++ app/models/import/brainz_compilation_release.rb
@@ -7,22 +7,14 @@ module Import
 
     def initialize(args)
       super({ reference_class: BrainzReleaseRef }.merge args)
-
-      #cache.rebuild_from_params(params)
     end
 
     def perform
-      # PrepareBrainzCompilationRelease.using_id(
-      #   cache:      cache,
-      #   reference: reference
-      # )
       prepare_brainz_compilation_release(reference: reference)
       # TODO: respond_to_missing?
       # TODO: respond_to_missing?
-      #return body if cache.any_required?
       return body if knowledge.missing?
 
-      #PersistBrainzCompilationRelease.using_id(reference, cache)
       persist_brainz_compilation_release(reference: reference)
 
       body
@@ -31,7 +23,6 @@ module Import
     def body
       wanted_id = wanted
       reference = BrainzReleaseRef.new(code: wanted_id)
-      #cache.require_brainz(reference)
       ask.brainz.about(reference)
       {
         data:
