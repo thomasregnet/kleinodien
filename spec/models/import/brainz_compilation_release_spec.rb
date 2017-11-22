f82f4345f23b4ac05cd31548a42fad596d4644d3
ossi <ossi@example.com>
2017-11-22 23:16:10 +0100

    Refactor import/brainz_compilation_release_spec.rb

--- spec/models/import/brainz_compilation_release_spec.rb
+++ spec/models/import/brainz_compilation_release_spec.rb
@@ -1,54 +1,65 @@
 require 'rails_helper'
+require 'ko_test_data'
 
-RSpec.describe Import::BrainzCompilationRelease, type: :model do
-  it 'returns required params' do
-    reference = '404e67be-0b5e-47bc-81db-1e8c408e9e3f'
-    data = {
-      data: {
-        type: 'music-brainz-release',
-        attributes: {
-          wanted: reference
+# TODO: Why do i need to require this modules
+require 'import/prepare_brainz_compilation_release'
+require 'import/prepare_brainz_artist'
+require 'import/prepare_brainz_artist_credit'
+
+
+RSpec.describe Import::BrainzCompilationRelease do
+  context 'without konwledge' do
+    describe '.perform' do
+      brainz_id = '404e67be-0b5e-47bc-81db-1e8c408e9e3f'
+      data = {
+        data: {
+          type: 'music-brainz-release',
+          attributes: {
+            wanted: brainz_id
+          }
         }
       }
-    }
 
-    response = Import::BrainzCompilationRelease.perform(params: data)
+      response = described_class.perform(params: data)
 
-    expect(response[:data][:attributes][:http_status_code]).to eq(202)
-    #byebug
-    ref_key = response[:data][:attributes][:required][:brainz][0]
-    expected_ref_key = 'release/'\
-                   "#{reference}"\
-                   '?inc=artists+labels+recordings+release-groups'
-    expect(ref_key).to eq(expected_ref_key)
+      it 'returns a http status code of 202' do
+        expect(response.dig(:data, :attributes, :http_status_code)).to eq(202)
+      end
+
+      it 'requires the release data' do
+        ref_key = response.dig(:data, :attributes, :required, :brainz)[0]
+        reference = BrainzReleaseRef.new(code: brainz_id)
+        expect(ref_key).to eq(reference.to_key)
+      end
+    end
   end
 
-  specify 'Sepultura - Arise' do
-    reference = '7452f8c9-f9bc-3ca7-859e-3220e57e4e4'
-    response = Import::BrainzCompilationRelease.perform(params: 
+  context 'with knowledge of the release' do
+    brainz_id = '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a'
+    reference = BrainzReleaseRef.new(code: brainz_id)
+    response = described_class.perform(
+      params:
       Hash[
         data: {
           type: 'music-brainz-release',
           attributes: {
-            wanted: reference,
+            wanted: brainz_id,
             known: {
-              musicbrainz: [
-                {
-                  type: 'release',
-                  id: reference,
-                  attributes: {
-                    query_result: '<xml></xml>'
-                  }
-                }
-              ]
+              brainz: {
+                reference.to_key => KoTestData.brainz_release(reference)
+              }
             }
           }
         }
       ]
     )
-    # TODO: check response status
-    # expect(response.status).to eq 202
+    it 'returns a http status code of 202' do
+      expect(response.dig(:data, :attributes, :http_status_code)).to eq(202)
+    end
 
-    # TODO: :wanted must not contain the release
+    it 'does not require the release data' do
+      required = response.dig(:data, :attributes, :required, :brainz)
+      expect(required).not_to include(reference.to_key)
+    end
   end
 end
