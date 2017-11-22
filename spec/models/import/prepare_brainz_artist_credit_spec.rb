f7f2a71d96449bf1eddbc09d1aba67cd53a14710
ossi <ossi@example.com>
2017-11-22 21:55:39 +0100

    Refactor prepare_brainz_artist_credit_spec.rb

--- spec/models/import/prepare_brainz_artist_credit_spec.rb
+++ spec/models/import/prepare_brainz_artist_credit_spec.rb
@@ -2,19 +2,27 @@ require 'rails_helper'
 require 'ko_test_data'
 
 RSpec.describe Import::PrepareBrainzArtistCredit do
-  before(:each) do
-    #@cache = Import::Cache.new
-
-    reference = '693748be-7c18-39c3-af2e-2e62092090cf'
-    xml = KoTestData.brainz_release(BrainzReleaseRef.new(code: reference))
-    @artist_credit = MashedBrainz.from_xml(xml).artist_credit
+  def reference
+    '693748be-7c18-39c3-af2e-2e62092090cf'
   end
 
-  specify '.perform' do
-    ac_preparer = Import::PrepareBrainzArtistCredit.new(
-      template: @artist_credit
-    )
-    ac_preparer.perform
-    expect(ac_preparer.knowledge.missing?).to be true
+  def xml
+    KoTestData.brainz_release(BrainzReleaseRef.new(code: reference))
+  end
+
+  def artist_credit
+    MashedBrainz.from_xml(xml).artist_credit
+  end
+
+  context 'without knowledge' do
+    describe '#knowledge.missing?' do
+      it 'returns true' do
+        ac_preparer = described_class.new(
+          template: artist_credit
+        )
+        ac_preparer.perform
+        expect(ac_preparer.knowledge.missing?).to be true
+      end
+    end
   end
 end
