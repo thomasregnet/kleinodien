8b43f4bb396be61af910320e63e3c426d559a76d
ossi <ossi@example.com>
2017-11-23 20:15:42 +0100

    Refactor import/prepare_brainz_artist_spec.rb

--- spec/models/import/prepare_brainz_artist_spec.rb
+++ spec/models/import/prepare_brainz_artist_spec.rb
@@ -2,37 +2,41 @@ require 'rails_helper'
 require 'ko_test_data'
 
 RSpec.describe Import::PrepareBrainzArtist do
-  before(:context) do
-    @reference = BrainzArtistRef.new(
-      code: '2280ca0e-6968-4349-8c36-cb0cbd6ee95f'
-    )
+  let(:reference) do
+    BrainzArtistRef.new(code: '2280ca0e-6968-4349-8c36-cb0cbd6ee95f')
   end
 
-  specify '.perform without knowledged artist' do
-    knowledge = Import::Knowledge.new
-    artist_importer = described_class.new(
-      knowledge: knowledge,
-      reference: @reference
-    )
-    artist_importer.perform
-    expect(knowledge.missing?).to be true
+  context 'with an unknown artist' do
+    describe '#knowledge.missing?' do
+      it 'returns true' do
+        artist_importer = described_class.new(
+          reference: reference
+        )
+        artist_importer.perform
+        expect(artist_importer.knowledge.missing?).to be true
+      end
+    end
   end
 
-  specify '.perform with known artist' do
-    xml = KoTestData.brainz_xml_for(@reference)
-    knowledge = Import::Knowledge.new(
-      known: {
-        brainz: {
-          @reference.to_key => xml
-        }
-      }
-    )
+  context 'with a known artist' do
+    describe '#knowledge.missing?' do
+      it 'returns false' do
+        knowledge = Import::Knowledge.new(
+          known: {
+            brainz: {
+              reference.to_key => KoTestData.brainz_xml_for(reference)
+            }
+          }
+        )
 
-    described_class.perform(
-      knowledge: knowledge,
-      reference: @reference
-    )
+        artist_importer = described_class.new(
+          knowledge: knowledge,
+          reference: reference
+        )
 
-    expect(knowledge.missing?).to be false
+        artist_importer.perform
+        expect(artist_importer.knowledge.missing?).to be false
+      end
+    end
   end
 end
