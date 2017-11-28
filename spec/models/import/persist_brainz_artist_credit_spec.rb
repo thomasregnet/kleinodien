e9d6501845076790b88039fd3ddc051e2445f054
ossi <ossi@example.com>
2017-11-28 21:50:14 +0100

    Refactor import/persist_brainz_artist_credit_spec

--- spec/models/import/persist_brainz_artist_credit_spec.rb
+++ spec/models/import/persist_brainz_artist_credit_spec.rb
@@ -4,36 +4,48 @@ require 'ko_test_data'
 RSpec.describe Import::PersistBrainzArtistCredit do
   # TODO: Specs for #find_by
   # TODO: Specs for #create
-  before(:each) do
-    #@cache = Import::Cache.new
-    @knowledge = Import::Knowledge.new
-    reference = BrainzReleaseRef.new(
-      code: '693748be-7c18-39c3-af2e-2e62092090cf'
-    )
-    xml = KoTestData.brainz_xml_for(reference)
-    release = MashedBrainz.from_xml(xml)
-    @brainz_artist_credit = release.artist_credit
-  end
+  describe 'persist an artist_credit' do
+    before(:context) do
+      DatabaseCleaner.start
 
-  it 'persists an artist_credit' do
-    brainz = {}
-    %w[2280ca0e-6968-4349-8c36-cb0cbd6ee95f
-       37e9d7b2-7779-41b2-b2eb-3685351caad3].each do |reference|
-      reference = BrainzArtistRef.new(code: reference)
+      reference = BrainzReleaseRef.new(
+        code: '693748be-7c18-39c3-af2e-2e62092090cf'
+      )
       xml = KoTestData.brainz_xml_for(reference)
-      brainz[reference.to_key] = xml
+      release = MashedBrainz.from_xml(xml)
+      @brainz_artist_credit = release.artist_credit
+
+      brainz = {}
+      %w[2280ca0e-6968-4349-8c36-cb0cbd6ee95f
+         37e9d7b2-7779-41b2-b2eb-3685351caad3].each do |code|
+        reference = BrainzArtistRef.new(code: code)
+        xml = KoTestData.brainz_xml_for(reference)
+        brainz[reference.to_key] = xml
+      end
+
+      knowledge = Import::Knowledge.new(known: { brainz: brainz })
+
+      @artist_credit = Import::PersistBrainzArtistCredit.perform(
+        data_import: FactoryGirl.create(:data_import),
+        knowledge:   knowledge,
+        template:    @brainz_artist_credit
+      )
     end
 
-    knowledge = Import::Knowledge.new(known: { brainz: brainz })
+    it 'is persisted' do
+      expect(@artist_credit.new_record?).to be false
+    end
 
-    artist_credit = Import::PersistBrainzArtistCredit.perform(
-      data_import: FactoryGirl.create(:data_import),
-      knowledge:   knowledge,
-      template:    @brainz_artist_credit
-    )
+    it 'has the righet name' do
+      expect(@artist_credit.name).to eq('Jello Biafra With NoMeansNo')
+    end
 
-    expect(artist_credit.new_record?).to be false
-    expect(artist_credit.name).to eq('Jello Biafra With NoMeansNo')
-    expect(artist_credit.data_import).to be_instance_of(DataImport)
+    it 'has the DataImport set' do
+      expect(@artist_credit.data_import).to be_instance_of(DataImport)
+    end
+
+    after(:context) do
+      DatabaseCleaner.clean
+    end
   end
 end
