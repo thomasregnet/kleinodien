8bb77d119df45ab1d488ca6efe8bd461f3288f2f
ossi <ossi@example.com>
2017-11-28 20:58:16 +0100

    Associations between ArtistCredit and DataImport

--- app/models/artist_credit.rb
+++ app/models/artist_credit.rb
@@ -1,5 +1,6 @@
 # ArtistCredit joins Artists with join_phrases by using Participants
 class ArtistCredit < ActiveRecord::Base
+  belongs_to :data_import, required: false
   belongs_to :source, required: false
 
   has_and_belongs_to_many :tags
