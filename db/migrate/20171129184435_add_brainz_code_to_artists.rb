01adb66b594d5773dd2260d41a55b811901195d2
ossi <ossi@example.com>
2017-11-29 21:01:13 +0100

    Correct migration add_brainz_code_to_artists
    Now an unique index is created on artist.brainz_code.

--- db/migrate/20171129184435_add_brainz_code_to_artists.rb
+++ db/migrate/20171129184435_add_brainz_code_to_artists.rb
@@ -1,5 +1,10 @@
 class AddBrainzCodeToArtists < ActiveRecord::Migration[5.1]
   def change
-    add_column :artists, :brainz_code, :uuid, unique: true
+    add_column :artists, :brainz_code, :uuid
+
+    add_index :artists,
+              :brainz_code,
+              unique: true,
+              name: :index_on_artists_brainz_code
   end
 end
