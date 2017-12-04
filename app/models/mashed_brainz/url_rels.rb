ccecc6115c3389b01b40028250552715670aec21
ossi <ossi@example.com>
2017-12-04 22:01:07 +0100

    Add MashedBrainz::UrlRels#wikidata* methods

--- app/models/mashed_brainz/url_rels.rb
+++ app/models/mashed_brainz/url_rels.rb
@@ -1,6 +1,12 @@
 module MashedBrainz
   # MusicBrainz url-rels
   class UrlRels < Base
+    def discogs
+      relation.each do |rel|
+        return rel if rel.type == 'discogs'
+      end
+    end
+
     def discogs_code
       url = discogs_url
       return unless url
@@ -12,10 +18,20 @@ module MashedBrainz
       rel.target.__content__
     end
 
-    def discogs
+    def wikidata
       relation.each do |rel|
-        return rel if rel.type == 'discogs'
+        return rel if rel.type == 'wikidata'
       end
+    end
+
+    def wikidata_code
+      url = wikidata_url || return
+      %r{\/(Q\d+)$}.match(url)[1]
+    end
+
+    def wikidata_url
+      rel = wikidata || return
+      rel.target.__content__
     end
   end
 end
