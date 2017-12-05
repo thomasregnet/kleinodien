2e1f1a0bf3db8b6f41bd1986f84df141b823dad5
ossi <ossi@example.com>
2017-12-05 21:20:50 +0100

    Start implement TestData

---
+++ spec/models/test_data_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe TestData do
+  describe '.define' do
+    TestData.define(:brainz_arise) do |set| 
+      set.add(:brainz_release, '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a')
+    end
+
+    it 'returns the testset' do
+      expect(TestData.fetch(:brainz_arise)).not_to be nil
+    end
+  end
+end
