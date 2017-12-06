203a2964e9db99a4de38c8797172a7b3aa3b5371
ossi <ossi@example.com>
2017-12-06 22:27:44 +0100

    Save SubSet in TestSet

--- app/models/test_set.rb
+++ app/models/test_set.rb
@@ -1,5 +1,14 @@
 class TestSet < TestSubset
+  attr_reader :subsets
+
+  def initialize
+    super
+    @subsets = []
+  end
+
   def define
-    yield TestSubset.new
+    sub_set = TestSubset.new
+    subsets << sub_set
+    yield sub_set
   end
 end
