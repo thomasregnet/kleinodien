1a2c371e1a45f65125518d7e8490d074a7fda44c
ossi <ossi@example.com>
2017-12-06 21:56:37 +0100

    Let TestData.retrieve return a real TestSet

--- spec/models/test_data_spec.rb
+++ spec/models/test_data_spec.rb
@@ -9,12 +9,23 @@ RSpec.describe TestData do
   end
 
   describe '.retrieve' do
-    described_class.define(:brainz_arise) do |set|
-      set.add(:brainz_release, '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a')
+    described_class.define(:brainz_arise) do |test_set|
+      # TODO: currently the set.add line does not make sense. Delete it?
+      test_set.add(:brainz_release, '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a')
     end
 
-    it 'returns the testset' do
-      expect(described_class.retrieve(:brainz_arise)).not_to be nil
+    context 'with a valid test-set name' do
+      it 'returns the test set' do
+        expect(described_class.retrieve(:brainz_arise))
+          .to be_instance_of(TestSet)
+      end
+    end
+
+    context 'with an  invalid test-set name' do
+      it 'raises an ArguemtnError' do
+        expect { described_class.retrieve(:unknown_stuff) }
+          .to raise_error(ArgumentError)
+      end
     end
   end
 end
