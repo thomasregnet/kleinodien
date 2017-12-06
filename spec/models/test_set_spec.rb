87b3daf0617304518e08aa2a3b1c50f2ae83c39f
ossi <ossi@example.com>
2017-12-06 23:12:33 +0100

    Add specs for TestSet#retrieve

--- spec/models/test_set_spec.rb
+++ spec/models/test_set_spec.rb
@@ -9,5 +9,18 @@ RSpec.describe TestSet do
     end
   end
 
-  specify '#retrieve'
+  describe 'retrieve' do
+    context 'with a subset defined' do
+      it 'returns that subset' do
+        test_set.define {}
+        expect(test_set.retrieve(0)).to be_instance_of(TestSubset)
+      end
+    end
+
+    context 'with a wrong subset_no' do
+      it 'raises an ArgumentError' do
+        expect { test_set.retrieve(0) }.to raise_error(ArgumentError)
+      end
+    end
+  end
 end
