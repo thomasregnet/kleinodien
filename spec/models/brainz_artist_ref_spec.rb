1da33e687f14ead237bf21ff458a069991035d3c
ossi <ossi@example.com>
2017-12-07 21:51:37 +0100

    Implement BrainzRef#eql?
    This is the first step to use BrainzRef's as hash keys.
    This will be useful for TestData and maybe for Import::Knowledge.
    Next step is to implement BrainzRef#hash. See
    https://ruby-doc.org/core-2.4.2/Hash.html#class-Hash-label-Hash+Keys

--- spec/models/brainz_artist_ref_spec.rb
+++ spec/models/brainz_artist_ref_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 require 'shared_examples_for_brainz_artist_source_id'
+require 'shared_examples_for_brainz_references'
 
 RSpec.describe BrainzArtistRef do
   before(:all) do
@@ -17,5 +18,11 @@ RSpec.describe BrainzArtistRef do
   it 'returns the ref_key' do
     expected = "artist/#{uuid}#{query_string}"
     expect(@reference.to_key).to eq(expected)
+  end
+
+  it_behaves_like 'a MusicBrainz reference' do
+    let(:reference)  do
+      described_class.new(code: 'ff11eada-b5fa-4b8b-a1bc-17a9cccff1c4')
+    end 
   end
 end
