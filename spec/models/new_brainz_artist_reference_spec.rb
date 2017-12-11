95aa9e149936b21fb1854ac3acd6aeceb4a8aa3a
ossi <ossi@example.com>
2017-12-11 12:25:28 +0100

    Devided shared_examples_for_new_references.rb

--- spec/models/new_brainz_artist_reference_spec.rb
+++ spec/models/new_brainz_artist_reference_spec.rb
@@ -2,14 +2,41 @@ require 'rails_helper'
 require 'shared_examples_for_new_references'
 
 RSpec.describe NewBrainzArtistReference do
+  def test_code
+    '4509c51e-b790-41aa-a2b3-e3bbf62cbf3f'
+  end
+
+  def test_key
+    "musicbrainz.org/artist/#{test_code}?#{test_query_string}"
+  end
+
+  def test_query_string
+    'inc=url-rels'
+  end
+
+  def test_uri
+    "https://musicbrainz.org/ws/2/artist/#{test_code}?#{test_query_string}"
+  end
+
   it_behaves_like 'a new reference' do
     let(:code) { '4509c51e-b790-41aa-a2b3-e3bbf62cbf3f' }
-    let(:uri) do
-      'https://musicbrainz.org/ws/2/artist/' \
-      + '4509c51e-b790-41aa-a2b3-e3bbf62cbf3f?inc=url-rels'
-    end
-    let(:key) do
-      'musicbrainz.org/artist/4509c51e-b790-41aa-a2b3-e3bbf62cbf3f?inc=url-rels'
-    end
+  end
+
+  it_behaves_like 'a reference initialized from_code' do
+    let(:code) { test_code }
+    let(:uri) { test_uri }
+    let(:key) { test_key }
+  end
+
+  it_behaves_like 'a reference initialized from_key' do
+    let(:code) { test_code }
+    let(:uri) { test_uri }
+    let(:key) { test_key }
+  end
+
+  it_behaves_like 'a reference initialized from_uri' do
+    let(:code) { test_code }
+    let(:uri) { test_uri }
+    let(:key) { test_key }
   end
 end
