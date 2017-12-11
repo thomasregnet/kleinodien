78bb549891ca6f08eb6ca67bb62b9ed49978ef8e
ossi <ossi@example.com>
2017-12-11 10:39:23 +0100

    Polish NewBrainzArtistReference#to_uri

--- app/models/new_brainz_artist_reference.rb
+++ app/models/new_brainz_artist_reference.rb
@@ -1,7 +1,18 @@
-class NewBrainzArtistReference < NewReference
-  def to_uri
-    return uri if uri
-    "https://musicbrainz.org/ws/2/artist/#{to_code}?inc=url-rels"
+# A reference to a MusicBrainz Artist
+class NewBrainzArtistReference < NewBrainzReference
+  URI_KIND         = 'artist'.freeze
+  URI_QUERY_STRING = 'inc=url-rels'.freeze
+
+  def kind
+    URI_KIND
   end
 
+  def query_string
+    URI_QUERY_STRING
+  end
+
+  def to_uri
+    return uri if uri
+    "#{scheme}://#{host}/#{path_prefix}/#{kind}/#{to_code}?#{query_string}"
+  end
 end
---
+++ spec/models/new_brainz_reference_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe NewBrainzReference do
+  let(:brainz) do
+    described_class.from_code('07ba5f4d-af2e-4c17-8901-79fa2d5bcfa9')
+  end
+
+  describe '#host' do
+    it 'returns the default host' do
+      expect(brainz.host).to eq('musicbrainz.org')
+    end
+  end
+
+  describe '#path_prefix' do
+    it 'returns the path_prefix' do
+      expect(brainz.path_prefix).to eq('ws/2')
+    end
+  end
+
+  describe 'scheme' do
+    it 'returns the scheme' do
+      expect(brainz.scheme).to eq('https')
+    end
+  end
+end
