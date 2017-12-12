971a7810f1ea352d16127da3958cee80d76011d0
ossi <ossi@example.com>
2017-12-12 20:16:45 +0100

    Add BrainzReference#category

--- spec/models/brainz_release_reference.rb
+++ spec/models/brainz_release_reference.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 require 'shared_examples_for_new_references'
+require 'shared_examples_for_brainz_references'
 
 RSpec.describe BrainzReleaseReference do
   def test_code
@@ -18,31 +19,38 @@ RSpec.describe BrainzReleaseReference do
     "https://musicbrainz.org/ws/2/release/#{test_code}?#{test_query_string}"
   end
 
-  it_behaves_like 'a new reference' do
-    let(:code) { '4509c51e-b790-41aa-a2b3-e3bbf62cbf3f' }
-  end
+  # it_behaves_like 'a new reference' do
+  #   let(:code) { '4509c51e-b790-41aa-a2b3-e3bbf62cbf3f' }
+  # end
 
-  it_behaves_like 'a reference initialized from_code' do
+  it_behaves_like 'a MusicBrainz reference' do
+    let(:reference) { described_class.from_code(test_code) }
     let(:code) { test_code }
     let(:uri) { test_uri }
     let(:key) { test_key }
   end
 
-  it_behaves_like 'a reference initialized from_key' do
-    let(:code) { test_code }
-    let(:uri) { test_uri }
-    let(:key) { test_key }
-  end
+  # it_behaves_like 'a reference initialized from_code' do
+  #   let(:code) { test_code }
+  #   let(:uri) { test_uri }
+  #   let(:key) { test_key }
+  # end
 
-  it_behaves_like 'a reference initialized from_uri' do
-    let(:code) { test_code }
-    let(:uri) { test_uri }
-    let(:key) { test_key }
-  end
+  # it_behaves_like 'a reference initialized from_key' do
+  #   let(:code) { test_code }
+  #   let(:uri) { test_uri }
+  #   let(:key) { test_key }
+  # end
 
-  it_behaves_like 'a hash key' do
-    let(:code) { test_code }
-    let(:uri) { test_uri }
-    let(:key) { test_key }
-  end
+  # it_behaves_like 'a reference initialized from_uri' do
+  #   let(:code) { test_code }
+  #   let(:uri) { test_uri }
+  #   let(:key) { test_key }
+  # end
+
+  # it_behaves_like 'a hash key' do
+  #   let(:code) { test_code }
+  #   let(:uri) { test_uri }
+  #   let(:key) { test_key }
+  # end
 end
