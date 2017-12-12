6b043c1d400d891e588bdc9455282dc4e7bad391
ossi <ossi@example.com>
2017-12-12 10:21:57 +0100

    Implement TestDataFetch

--- features/step_definitions/test_data_steps.rb
+++ features/step_definitions/test_data_steps.rb
@@ -2,7 +2,6 @@ When(/^I define test data$/) do
   TestData.define(:brainz_arise) do |testset|
     testset.add(:brainz_release, '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a')
 
-    #byebug
     testset.define do |subset|
       subset.add(:brainz_release, '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a')
     end
