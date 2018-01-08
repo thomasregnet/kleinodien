1dfd02e766bf62695740a2bd0c6c7bd1615ce3c2
ossi <ossi@example.com>
2018-01-08 21:35:20 +0100

    Extend UriToReference as far as possible
    All Brainz*Reference types that exists until now can be detected and returned.
    UriToReference raises an exception if the host of the given uri is unknown
    or the type (eg. artist, release...) is unknown.

--- spec/models/uri_to_reference_spec.rb
+++ spec/models/uri_to_reference_spec.rb
@@ -1,16 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe UriToReference do
-  describe 'with a MusicBrainz artist uri' do
-    let(:uri) do
-      'https://musicbrainz.org/ws/2/artist/' \
-      + 'd1346194-325a-4ea3-826f-e0c68ce38c5b' \
-      + '?inc=url-rels'
+  def uri_for(type)
+    "https://musicbrainz.org/ws/2/artist/#{type}/fake-uuid?inc=foo-bar"
+  end
+
+  describe '.perform MusicBrainz uri' do
+    context 'of an artist' do
+      let(:uri) { uri_for(:artist) }
+
+      it 'returns a BrainzArtistReference' do
+        expect(described_class.perform(uri))
+          .to be_instance_of(BrainzArtistReference)
+      end
     end
 
-    it 'returns a BrainzArtistReference' do
-      expect(described_class.perform(uri))
-        .to be_instance_of(BrainzArtistReference)
+    context 'of a release-group' do
+      let(:uri) { uri_for('release-group') }
+
+      it 'returns a BrainzArtistReference' do
+        expect(described_class.perform(uri))
+          .to be_instance_of(BrainzReleaseGroupReference)
+      end
+    end
+
+    context 'of a release-group' do
+      let(:uri) { uri_for(:release) }
+
+      it 'returns a BrainzArtistReference' do
+        expect(described_class.perform(uri))
+          .to be_instance_of(BrainzReleaseReference)
+      end
+    end
+
+    context 'of a unknown type' do
+      it 'raises an error' do
+        expect { described_class.perform(uri_for(:bad_type)) }
+          .to raise_error(/bad_type/)
+      end
+    end
+  end
+
+  describe '.perform' do
+    describe 'with a bad host' do
+      it 'raises an error' do
+        expect { described_class.perform('https://evil-host.org/foo/bar') }
+          .to raise_error(/evil-host/)
+      end
     end
   end
 end
