655a9929707fb188ef8d5c01a22c62fa860ced44
ossi <ossi@example.com>
2018-01-08 21:35:20 +0100

    Extend UriToReference as far as possible
    All Brainz*Reference types that exists until now can be detected and returned.
    UriToReference raises an exception if the host of the given uri is unknown
    or the type (eg. artist, release...) is unknown.

--- app/models/uri_to_reference.rb
+++ app/models/uri_to_reference.rb
@@ -13,20 +13,34 @@ class UriToReference
   end
 
   def perform
-    case uri.host
+    host = uri.host
+    case host
     when 'musicbrainz.org'
       perform_brainz
+    else
+      raise "can't create reference for #{host} of #{self}"
     end
   end
 
   def perform_brainz
-    case path_to_array[-2]
+    type = path_to_array[-2]
+    case type
     when 'artist'
-      BrainzArtistReference.from_uri(uri.to_s)
+      BrainzArtistReference.from_uri(to_s)
+    when 'release-group'
+      BrainzReleaseGroupReference.from_uri(to_s)
+    when 'release'
+      BrainzReleaseReference.from_uri(to_s)
+    else
+      raise "can't create reference for #{type} of #{self}"
     end
   end
 
   def path_to_array
     uri.path.split('/').reject { |element| element == '' }
+  end
+
+  def to_s
+    uri.to_s
   end
 end
