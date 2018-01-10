148bff61ff2b8b6fb9f5decd71225db3852e622b
ossi <ossi@example.com>
2018-01-10 21:02:39 +0100

    Start to make Import::Knowledge "flat"
    Tests fail!
    
    Since References can act as a Hash key there is no more need for
    complex data structures.

--- app/models/import/knowledge.rb
+++ app/models/import/knowledge.rb
@@ -1,45 +1,62 @@
 module Import
   # Known and required data to import
   class Knowledge
-    KNOWLEDGE_FIELDS = {
-      brainz: Import::BrainzKnowledge
-      # TODO: discogs
-      # TODO: tmdb
-    }.freeze
-
-    attr_reader :fields
-
-    KNOWLEDGE_FIELDS.each_key do |field|
-      define_method field do
-        fields[field]
-      end
-    end
+    attr_reader :having, :missing
 
     def initialize(args = {})
-      args ||= {}
-      @fields = {}
-      known = args.fetch(:known, {})
-      KNOWLEDGE_FIELDS.each do |field, klass|
-        raw = known[field] || {}
-        fields[field] = klass.new(known: raw)
-      end
+      @having  = args[:having] || {}
+      @missing = Set.new
     end
 
-    def collect
-      response = { known: {}, required: {} }
-      fields.each do |name, field|
-        collected = field.collect
-        response[:known][name] = collected[:known]
-        response[:required][name] = collected[:required]
-      end
+    def about(reference)
+      response = having[reference]
+      missing.add(reference) unless response
       response
-    end
-
-    def missing?
-      fields.each_value do |intance|
-        return true if intance.missing?
-      end
-      false
     end
   end
 end
+# module Import
+#   # Known and required data to import
+#   class Knowledge
+#     KNOWLEDGE_FIELDS = {
+#       brainz: Import::BrainzKnowledge
+#       # TODO: discogs
+#       # TODO: tmdb
+#     }.freeze
+
+#     attr_reader :fields
+
+#     KNOWLEDGE_FIELDS.each_key do |field|
+#       define_method field do
+#         fields[field]
+#       end
+#     end
+
+#     def initialize(args = {})
+#       args ||= {}
+#       @fields = {}
+#       known = args.fetch(:known, {})
+#       KNOWLEDGE_FIELDS.each do |field, klass|
+#         raw = known[field] || {}
+#         fields[field] = klass.new(known: raw)
+#       end
+#     end
+
+#     def collect
+#       response = { known: {}, required: {} }
+#       fields.each do |name, field|
+#         collected = field.collect
+#         response[:known][name] = collected[:known]
+#         response[:required][name] = collected[:required]
+#       end
+#       response
+#     end
+
+#     def missing?
+#       fields.each_value do |intance|
+#         return true if intance.missing?
+#       end
+#       false
+#     end
+#   end
+# end
