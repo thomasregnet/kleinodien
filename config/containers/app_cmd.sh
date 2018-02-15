92ab95fc166d4f49854bbeb099b8fd0a111a0a88
ossi <ossi@example.com>
2018-02-15 21:24:22 +0100

    Clear unicorn pid-file on container start

--- config/containers/app_cmd.sh
+++ config/containers/app_cmd.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Removing unicorn pidfile"
+rm -rf tmp/pids/unicorn.pid
+
 # http://chrisstump.online/2016/02/20/docker-existing-rails-application/
 # Prefix `bundle` with `exec` so unicorn shuts down
 # gracefully on SIGTERM (i.e. `docker stop`)
