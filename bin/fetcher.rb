619a692c75c02ddbcabcaa4b9c8d95cfc8a00c84
ossi <ossi@example.com>
2018-02-26 11:13:47 +0100

    Subscribe to redis
    For now just a simple test.

--- bin/fetcher.rb
+++ bin/fetcher.rb
@@ -1,8 +1,18 @@
 #!/usr/bin/env ruby
 
+require 'redis'
 $stdout.sync = true
 puts 'starting fetcher'
 
-while 1 == 1
-  sleep 9999
+# while 1 == 1
+#   sleep 9999
+# end
+
+redis = Redis.new(host: 'redis', timeout: 3)
+
+redis.subscribe('kleinodien:test') do |on|
+  puts 'subscribing'
+  on.message do |chanel, message|
+    puts "#{chanel}: #{message}"
+  end
 end
