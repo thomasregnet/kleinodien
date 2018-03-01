42640bcb20eae167f8f366cfcd89067e4943329d
ossi <ossi@example.com>
2018-03-01 11:20:13 +0100

    Create method Fetcher::Conventions.key_name_for
    Already used by Fetcher::WorkingQueue.

--- app/models/fetcher/conventions.rb
+++ app/models/fetcher/conventions.rb
@@ -1,7 +1,12 @@
 module Fetcher
   # Naming conventions for fetchers
   module Conventions
+    KEY_NAME_PREFIX      = 'data'.freeze
     MESSAGE_QUEUE_SUFFIX = 'messages'.freeze
+
+    def self.key_name_for(suffix)
+      "#{KEY_NAME_PREFIX}:#{suffix}"
+    end
 
     def self.message_queue_name_for(fetcher_name)
       "#{fetcher_name}:#{MESSAGE_QUEUE_SUFFIX}"
