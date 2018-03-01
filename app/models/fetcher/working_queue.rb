8f0cd11c969fc4815ba68c5bf64529f0b8915f6e
ossi <ossi@example.com>
2018-03-01 10:10:57 +0100

    Store fetcher data to redis

--- app/models/fetcher/working_queue.rb
+++ app/models/fetcher/working_queue.rb
@@ -1,19 +1,38 @@
 module Fetcher
   class WorkingQueue
-    attr_reader :redis, :working_queue_name
+    attr_reader :getter_class, :redis, :working_queue_name
 
     def self.perform(args)
       new(args).perform
     end
 
     def initialize(args)
+      @getter_class       = args[:getter_class]
       @redis              = args[:redis]
       @working_queue_name = args[:working_queue_name]
     end
 
     def perform
-      return true if redis.llen(working_queue_name) > 0
-      false
+      return false unless redis.llen(working_queue_name).positive?
+      retrieve_and_store
+    end
+
+    private
+
+    def retrieve_and_store
+      uri = redis.lindex(working_queue_name, 0)
+      # TODO: error handling
+      data = getter_class.perform(uri)
+      store_data(data, uri)
+    end
+
+    def store_data(data, uri)
+      key = "#{data}:#{uri}"
+      redis.multi do |multi| # transaction
+        # TODO: set expiration seconds for key
+        multi.set(key, data)
+      end
+      true
     end
   end
 end
