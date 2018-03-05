b1549263e7abab60691793bcc3c70f75cf4b6a35
ossi <ossi@example.com>
2018-03-05 20:16:10 +0100

    Reorganize config/containers

--- config/containers/app_cmd.sh
+++ config/containers/app_cmd.sh
@@ -6,4 +6,4 @@ rm -rf tmp/pids/unicorn.pid
 # http://chrisstump.online/2016/02/20/docker-existing-rails-application/
 # Prefix `bundle` with `exec` so unicorn shuts down
 # gracefully on SIGTERM (i.e. `docker stop`)
-exec bundle exec unicorn -c config/containers/unicorn.rb -E $RAILS_ENV;
+exec bundle exec unicorn -c config/containers/unicorn/unicorn.rb -E $RAILS_ENV;
