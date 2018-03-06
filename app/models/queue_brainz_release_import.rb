bc1e6c96570542ce5d00a7ec283ae5c4d2b6abe0
ossi <ossi@example.com>
2018-03-06 21:18:14 +0100

    Queue BrainzRelease import to redis
    Bad code for now, but it works.

--- app/models/queue_brainz_release_import.rb
+++ app/models/queue_brainz_release_import.rb
@@ -3,4 +3,13 @@ class QueueBrainzReleaseImport
 
   attr_accessor :code
   validates :code, presence: true
+
+  def self.perform(params)
+    new(params).perform
+  end
+
+  def perform
+    redis = Redis.new(host: 'redis', timeout: 3)
+    redis.rpush('brainz:wait', code)
+  end
 end
--- spec/models/queue_brainz_release_import_spec.rb
+++ spec/models/queue_brainz_release_import_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe QueueBrainzReleaseImport, type: :model do
   it { is_expected.to respond_to(:code) }
+
+  describe '.perform' do
+    before { DatabaseCleaner.start }
+
+    let(:redis) { Redis.new(host: 'redis', timeout: 3) }
+
+    it 'queues to redis' do
+      described_class.perform(code: 'abc')
+      expect(redis.lindex('brainz:wait', 0)).to eq('abc')
+    end
+
+    after { DatabaseCleaner.clean }
+  end
 end
