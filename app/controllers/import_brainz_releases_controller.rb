b73f148dac191b13fbb161db1006871faaad3934
ossi <ossi@example.com>
2018-03-06 10:28:15 +0100

    Form for import_brainz_releases/new.html.erb
    Created form to submit an UUID.
    Also created the model Import::BrainzRelease.

--- app/controllers/import_brainz_releases_controller.rb
+++ app/controllers/import_brainz_releases_controller.rb
@@ -1,2 +1,15 @@
 class ImportBrainzReleasesController < ApplicationController
+  def new
+    @import_brainz_release = Import::BrainzRelease.new
+  end
+
+  def create
+    Import::BrainzRelease.perform(import_params)
+  end
+
+  private
+
+  def import_params
+    params.require(:import_brainz_release).permit(:code)
+  end
 end
