ef72f8c7a902743cce46d5aa9951130f180571aa
ossi <ossi@example.com>
2018-03-07 20:42:56 +0100

    Fix fetcher
    The brainz_fetcher, which is currently the only fetcher that is started,
    could not run. The reason was using `Rails.application.config_for`
    in the ImportConnection module.
    
    I resolved the problem by loading config/application and requiring
    all files under app/models in `config/containers/import_queue/queue_cmd.rb`.
    
    Another solution might be to use a rake-task to start a fetcher.
    This should set the environment correctly.

--- config/containers/import_queue/queue_cmd.rb
+++ config/containers/import_queue/queue_cmd.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/env ruby
 
+# Load config/application to benefit from rails environment
+require File.expand_path('../../../../config/application', __FILE__)
+
+# require everything under app/models
+Dir[File.expand_path('../../../../app/modeles', __FILE__)]
+  .each { |file| require file }
+
+# adopted from Rakefile
 $LOAD_PATH.unshift File.expand_path('../../../../app/models', __FILE__)
 
 require 'redis'
