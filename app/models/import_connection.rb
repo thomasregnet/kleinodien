3fe2a413e4e8eb172f5b5a322b5e74d304860f48
ossi <ossi@example.com>
2018-03-07 12:29:06 +0100

    Add method ImportConnection.uri
    Added ImportConnection.uri, which returns obviously an uri to connect
    to redis. Modified DatabaseCleaner settings in spec/rails_halper.rb
    to use this uri.

--- app/models/import_connection.rb
+++ app/models/import_connection.rb
@@ -3,5 +3,10 @@ module ImportConnection
     def redis
       @redis ||= Redis.new(Rails.application.config_for(:import_connection))
     end
+
+    def uri
+      config = Rails.application.config_for(:import_connection)
+      "#{config['url']}/#{config['db']}"
+    end
   end
 end
--- spec/models/import_connection_spec.rb
+++ spec/models/import_connection_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe ImportConnection do
       expect(described_class.redis).to be_instance_of(Redis)
     end
   end
+
+  describe '.uri' do
+    it 'returns the redis uri' do
+      expect(described_class.uri).to match(%r{^redis://[a-z]+})
+    end
+  end
 end
