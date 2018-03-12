f58ceee6191411820d3c20946bb69703a187dfa8
ossi <ossi@example.com>
2018-03-12 22:02:05 +0100

    Let the service generator do something useful

--- lib/generators/service/service_generator.rb
+++ lib/generators/service/service_generator.rb
@@ -1,3 +1,11 @@
 class ServiceGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+
+  def create_service_class
+    template 'service.rb.erb', "app/services/#{file_name}_service.rb"
+  end
+
+  def create_service_spec
+    template 'service_spec.rb.erb', "spec/services/#{file_name}_service_spec.rb"
+  end
 end
