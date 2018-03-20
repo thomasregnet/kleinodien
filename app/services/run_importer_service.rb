e22a22eff6552451f4a3b4b2f7055ab9076e4764
ossi <ossi@example.com>
2018-03-20 21:29:25 +0100

    Add private method RunImporterService#subscribe

--- app/services/run_importer_service.rb
+++ app/services/run_importer_service.rb
@@ -1,19 +1,29 @@
 class RunImporterService
   include CallWithArgs
+  include ImportStoreCommons
 
   private
 
-  attr_reader :importer_name, :prohibit_subscription
+  attr_reader :importer_name, :subscription_prohibited
 
   def initialize(args)
-    @importer_name         = args[:importer_name]
-    @prohibit_subscription = args[:prohibit_subscription]
+    @importer_name           = args[:importer_name]
+    @subscription_prohibited = args[:subscription_prohibited]
   end
 
   def private_call
-    redis = ImportConnection.redis
+    subscribe
+  end
 
-    redis.subscribe(importer_name) do |on|
+  def subscription_prohibited?
+    # TODO: check if subscription is prohibited by a redis key
+    subscription_prohibited
+  end
+
+  def subscribe
+    return if subscription_prohibited?
+
+    import_store.subscribe(importer_name) do |on|
       puts "subscribing to #{importer_name}"
       on.message do |channel, message|
         puts "#{channel}: #{message}"
