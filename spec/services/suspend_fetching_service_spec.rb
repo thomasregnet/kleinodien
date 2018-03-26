bac9ecad04cfbebd84e1693cc8698e2a3fd80a75
ossi <ossi@example.com>
2018-03-26 11:52:51 +0200

    Introduce SuspendFetchingService
    The SuspendFetchingService for no does nothing at all.
    It is called by the FetchDataService.

---
+++ spec/services/suspend_fetching_service_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+require 'shared_examples_for_services'
+
+RSpec.describe SuspendFetchingService do
+  it_behaves_like 'a service'
+
+  # the specs for the service belongs here
+end
