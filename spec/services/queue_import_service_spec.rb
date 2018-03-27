5983fcdd1cd78330fbce2431959674473f13acd5
ossi <ossi@example.com>
2018-03-27 20:24:31 +0200

    Add QueueImportService

---
+++ spec/services/queue_import_service_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+require 'redis_helper'
+require 'shared_examples_for_services'
+
+RSpec.describe QueueImportService do
+  it_behaves_like 'a service'
+
+  before do
+    DatabaseCleaner.start
+  end
+
+  it 'queues an ImportRequest' do
+    described_class.call(
+      importer_name: 'test',
+      request: 'just a string'
+    )
+    expect(RedisHelper.import_store.llen('test:requests')).to eq(1)
+  end
+
+  after do
+    DatabaseCleaner.clean
+  end
+end
