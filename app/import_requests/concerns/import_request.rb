ec2be953104459693e5abe43d80f435e71834c2d
ossi <ossi@example.com>
2018-03-27 20:28:11 +0200

    Buffer before delete module ImportRequest

--- app/import_requests/concerns/import_request.rb
+++ app/import_requests/concerns/import_request.rb
@@ -1,10 +1,15 @@
 module ImportRequest
   extend ActiveSupport::Concern
+  include ActiveModel::Serializers::JSON
 
   included do
     attr_accessor :code
 
     validates :code, presence: true
+
+    define_method :class_name do
+      self.class.name
+    end
 
     define_singleton_method :importer_class do |value|
       define_method(:importer_class) { value }
@@ -13,5 +18,15 @@ module ImportRequest
     define_singleton_method :reference_class do |value|
       define_method(:reference_class) { value }
     end
+  end
+
+  def attributes=(hash)
+    hash.each do |key, value|
+      send("#{key}=", value)
+    end
+  end
+
+  def attributes
+    instance_values
   end
 end
