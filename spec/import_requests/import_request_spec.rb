5cdbcd46f7d9f76e1da53963715c89365d80c4b3
ossi <ossi@example.com>
2018-03-28 22:16:10 +0200

    Refactor ImportRequest
    Removed attributes
    - importer_class
    - reference_class
    Added attributes
    - type
    Added methods
    - importer_class  -> derived from type
    - reference_class -> derived from type

--- spec/import_requests/import_request_spec.rb
+++ spec/import_requests/import_request_spec.rb
@@ -2,24 +2,43 @@ require 'rails_helper'
 
 RSpec.describe ImportRequest do
   it { is_expected.to respond_to(:code) }
-  it { is_expected.to respond_to(:importer_class) }
-  it { is_expected.to respond_to(:reference_class) }
-  it { is_expected.to respond_to(:requested) }
+  it { is_expected.to respond_to(:type) }
 
-  describe '.brainz_release' do
-    let(:import_request) { described_class.brainz_release(code: 'abc') }
-
-    it 'has the importer_class set' do
-      expect(import_request.importer_class).to eq('ImportBrainzReleaseService')
+  describe '#requested' do
+    context 'when not given' do
+      it 'returns the current time' do
+        # Using .to_s to prevent errors caused by a difference of milliseconds
+        expect(described_class.new.requested.to_s).to eq(Time.now.to_s)
+      end
     end
 
-    it 'has the reference_class set' do
-      expect(import_request.reference_class)
-        .to eq('BrainzReleaseReference')
+    context 'when given' do
+      before do
+        @time = Time.now
+        @import_request = ImportRequest.new(requested: @time)
+      end
+      it 'returns the given time' do
+        expect(@import_request.requested).to eq(@time)
+      end
+    end
+  end
+
+  context 'with type "brainz_release"' do
+    let(:import_request) do
+      described_class.new(code: 'abc', type: 'brainz_release')
     end
 
-    it 'has the code set' do
-      expect(import_request.code).to eq('abc')
+    describe '#importer_class' do
+      it 'returns "ImportBrainzReleaseService"' do
+        expect(import_request.importer_class)
+          .to eq('ImportBrainzReleaseService')
+      end
+    end
+
+    describe '#reference_class' do
+      it 'returns "BrainzReleaseReference"' do
+        expect(import_request.reference_class).to eq('BrainzReleaseReference')
+      end
     end
   end
 end
