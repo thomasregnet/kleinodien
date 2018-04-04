bd368861d988f0ae3c6e6c2d3cc3e99e931397e0
ossi <ossi@example.com>
2018-04-04 20:52:09 +0200

    Begin specs for PrepareBrainzReleaseService

--- app/services/prepare_brainz_release_service.rb
+++ app/services/prepare_brainz_release_service.rb
@@ -3,13 +3,16 @@ class PrepareBrainzReleaseService
 
   private
 
-  # attr_reader
+  attr_reader :importer_name, :reference
 
   def initialize(args)
-
+    @importer_name = args[:importer_name]
+    @reference     = args[:reference]
   end
 
   def private_call
-    # the code for the service belongs here
+    # TODO: Check if that release does not already exists in the database
+    # TODO: is the release already cached?
+    # TODO: if the release is already cached get the ArtistCredit
   end
 end
