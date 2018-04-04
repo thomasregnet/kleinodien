e984e4bb95224cc63949c23d8f3b8ac358ca9135
ossi <ossi@example.com>
2018-04-04 20:52:09 +0200

    Begin specs for PrepareBrainzReleaseService

--- spec/services/prepare_brainz_release_service_spec.rb
+++ spec/services/prepare_brainz_release_service_spec.rb
@@ -1,8 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+require 'redis_helper'
 require 'shared_examples_for_services'
 
 RSpec.describe PrepareBrainzReleaseService do
   it_behaves_like 'a service'
 
-  # the specs for the service belongs here
+  before { DatabaseCleaner.start }
+
+  let(:args) do
+    {
+      code:          '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a',
+      importer_name: 'test'
+    }
+  end
+
+  context 'when nothing is cached' do
+    it 'returns nil' do
+      expect(described_class.call(args)).to be_nil
+    end
+  end
+
+  after { DatabaseCleaner.clean }
 end
