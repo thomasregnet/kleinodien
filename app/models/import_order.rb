5bfc4da96a8d1242695ffc0f726cff89610561f4
ossi <ossi@asgard.example.com>
2018-10-24 22:52:02 +0200

    Let ImportOrder extract params from uri

--- app/models/import_order.rb
+++ app/models/import_order.rb
@@ -1,3 +1,22 @@
 class ImportOrder < ApplicationRecord
   belongs_to :user
+
+  after_initialize :set_default_state
+  after_initialize :try_to_assign_missing_params
+
+  private
+
+  def set_default_state
+    return if state
+
+    self.state = 'placed'
+  end
+  def try_to_assign_missing_params
+    return if !uri || code || kind || type
+
+    params = ExtractImportOrderParamsService.call(uri)
+    self.code = params[:code]
+    self.kind = params[:kind]
+    self.type = params[:type]
+  end
 end
