5143d86fdcb423f89900f0ccacf727bc927fb0bc
ossi <ossi@asgard.example.com>
2018-10-30 21:45:42 +0100

    Add ImportOrderParamsFromUriService

---
+++ app/services/import_order_params_from_uri_service.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+# Base for *ImportOrderParamsFromUriService classes
+class ImportOrderParamsFromUriService
+  DEFAULT_OFFSET = 0
+
+  def self.call(uri)
+    new(uri).call
+  end
+
+  attr_reader :uri
+
+  def initialize(uri)
+    @uri = uri
+  end
+
+  def call
+    return unless path_items
+
+    {
+      code: path_items[offset + 1],
+      kind: path_items[offset]
+    }
+  end
+
+  private
+
+  def offset
+    DEFAULT_OFFSET
+  end
+
+  def path_items
+    return unless path_string
+
+    path_string.split('/')
+  end
+
+  def path_string
+    # an path returned from the uri-object will look like
+    # '/foo/bar'. The leading slash ('/') distrubs so remove it
+    with_leading_slash = uri.path
+    return unless with_leading_slash
+
+    with_leading_slash.sub(%r{^/}, '')
+  end
+end
---
+++ spec/services/import_order_params_from_uri_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require 'shared_examples_for_services'
+
+RSpec.describe ImportOrderParamsFromUriService do
+  it_behaves_like 'a service'
+
+  context 'with a valid uri' do
+    let(:params) do
+      described_class.call(URI('https://test.example.com/some-kind/123'))
+    end
+
+    it 'sets the right code' do
+      expect(params[:code]).to eq '123'
+    end
+
+    it 'sets the right kind' do
+      expect(params[:kind]).to eq 'some-kind'
+    end
+  end
+end
