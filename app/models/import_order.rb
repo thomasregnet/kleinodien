c9791baca9c8a753dc00d268c7989847769db4fc
ossi <ossi@asgard.example.com>
2018-10-31 21:46:26 +0100

    Remove ImportOrder after_initialize hook
    ImportOrder does no longer try to extract it's parameters from
    a given uri.

--- app/models/import_order.rb
+++ app/models/import_order.rb
@@ -5,7 +5,6 @@ class ImportOrder < ApplicationRecord
   belongs_to :user
 
   after_initialize :set_default_state
-  after_initialize :try_to_assign_missing_params
 
   validates :code, :kind, :user, presence: true
 
@@ -15,14 +14,5 @@ class ImportOrder < ApplicationRecord
     return if state
 
     self.state = 'placed'
-  end
-
-  def try_to_assign_missing_params
-    return if !uri || code || kind || type
-
-    params = ExtractImportOrderParamsService.call(uri)
-    self.code = params[:code]
-    self.kind = params[:kind]
-    self.type = params[:type]
   end
 end
--- spec/models/import_order_spec.rb
+++ spec/models/import_order_spec.rb
@@ -1,45 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'shared_examples_for_import_orders'
 
-
 RSpec.describe ImportOrder, type: :model do
-
   include_examples 'for ImportOrders', :import_order
-
-  context 'only called with an uri' do
-    let(:user) { FactoryBot.create(:user) }
-    def expected_code
-      '57961a97-3796-4bf7-9f02-a985a8979ae9'
-    end
-
-    def expected_kind
-      'artist'
-    end
-
-    def expected_type
-      'MusicBrainzImportOrder'
-    end
-
-    def uri_string
-      ['https://musicbrainz.org', expected_kind, expected_code].join('/')
-    end
-
-    let(:order) { described_class.new(uri: uri_string, user: user) }
-
-    it 'has the expected code set' do
-      expect(order.code).to eq expected_code
-    end
-
-    it 'has the expected kind set' do
-      expect(order.kind).to eq expected_kind
-    end
-
-    it 'has the expected type set' do
-      expect(order.type).to eq expected_type
-    end
-
-    it 'can be saved' do
-      expect(order.save).to be true
-    end
-  end
 end
