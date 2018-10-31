edf247713a9061527fc34ef74bc6a04360044cfc
ossi <ossi@asgard.example.com>
2018-10-31 21:32:55 +0100

    Add specs and validations for BrainzImportOrder

--- app/models/brainz_import_order.rb
+++ app/models/brainz_import_order.rb
@@ -1,2 +1,22 @@
+# frozen_string_literal: true
+
+# ImportOrder for MusicBrainz
 class BrainzImportOrder < ImportOrder
+  validates :code,
+            format: {
+              with: %r{
+                \A
+                [a-z0-9]{8}
+                -
+                [a-z0-9]{4}
+                -
+                [a-z0-9]{4}
+                -
+                [a-z0-9]{4}
+                -
+                [a-z0-9]{12}
+                \z
+                }x,
+              message: 'must be an uuid'
+            }
 end
