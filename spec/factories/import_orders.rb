c743e6a82407bc36a7f259fb37665d3d09afcc98
ossi <ossi@asgard.example.com>
2018-10-31 21:32:55 +0100

    Add specs and validations for BrainzImportOrder

--- spec/factories/import_orders.rb
+++ spec/factories/import_orders.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :import_order do
     sequence(:code) { |n| "code-no-#{n}" }
-    kind { 'some-kind'}
+    kind { 'some-kind' }
     association :user, factory: :user
+
+    factory :brainz_import_order, class: 'BrainzImportOrder' do
+      code { SecureRandom.uuid.to_s }
+    end
   end
 end
--- spec/models/brainz_import_order_spec.rb
+++ spec/models/brainz_import_order_spec.rb
@@ -1,5 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+require 'shared_examples_for_import_orders'
 
 RSpec.describe BrainzImportOrder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include_examples 'for ImportOrders', :brainz_import_order
+
+  context 'no uuid as code' do
+    let(:import_order) { FactoryBot.build(:brainz_import_order, code: 'abc') }
+
+    it 'is not valid' do
+      expect(import_order).not_to be_valid
+    end
+  end
 end
