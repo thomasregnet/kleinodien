ca338956a127d9d90277c1bf26a632b465720562
ossi <ossi@asgard.example.com>
2018-11-05 09:54:47 +0100

    Add checks and validations for ImportOrder.state

--- spec/factories/import_orders.rb
+++ spec/factories/import_orders.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :import_order do
     sequence(:code) { |n| "code-no-#{n}" }
     kind { 'some-kind' }
+    state { 'pending' }
     association :user, factory: :user
 
     factory :brainz_import_order, class: 'BrainzImportOrder' do
--- spec/shared_examples_for_import_orders.rb
+++ spec/shared_examples_for_import_orders.rb
@@ -25,6 +25,34 @@ RSpec.shared_examples 'for ImportOrders' do |model|
     end
   end
 
+  describe '#state' do
+    context 'pending, processing, done, failed' do
+      let(:import_order) { FactoryBot.build(model) }
+      let(:valid_states) { %w(pending processing done failed) }
+
+      it 'is valid' do
+        expect(import_order)
+          .to validate_inclusion_of(:state).in_array(valid_states)
+      end
+    end
+
+    context 'invalid' do
+      let(:import_order) { FactoryBot.build(model, state: 'invalid') }
+
+      it 'is not valid' do
+        expect(import_order).not_to be_valid
+      end
+    end
+
+    context 'nil' do
+      let(:import_order) { FactoryBot.build(model, state: nil) }
+
+      it 'is not valid' do
+        expect(import_order).not_to be_valid
+      end
+    end
+  end
+
   context 'without a user' do
     let(:import_order) { FactoryBot.build(model, user: nil) }
 
