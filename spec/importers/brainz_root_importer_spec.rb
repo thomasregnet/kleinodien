70bf87091b74e711baf84cb33d0a73cfb1a71a9b
ossi <ossi@asgard.example.com>
2018-11-06 20:29:05 +0100

    Save interim status

--- spec/importers/brainz_root_importer_spec.rb
+++ spec/importers/brainz_root_importer_spec.rb
@@ -3,5 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe BrainzRootImporter do
-  
+  context 'release' do
+    it 'foo' do
+      # TODO: make a real test of this
+      allow(BrainzReleaseImporter).to receive(:call)
+    end
+  end
 end
