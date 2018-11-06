c1cae187764cd6994ef7047947d80a89f00b3ef3
ossi <ossi@asgard.example.com>
2018-11-06 10:39:12 +0100

    Save interim status of BaseImportWorker

--- app/import_workers/base_import_worker.rb
+++ app/import_workers/base_import_worker.rb
@@ -1,7 +1,32 @@
 # frozen_string_literal: true
 
 class BaseImportWorker
-  def self.run(importer_class)
-    true
+  def self.run(args)
+    new(args).run
+  end
+
+  def initialize(args)
+    @import_order_class = args[:import_order_class]
+    @importer_class     = args[:import_service_class]
+  end
+
+  attr_reader :importer_class, :import_order_class
+
+  def run
+    import_order = next_pending_order
+    subscribe unless import_order
+    import_order
+  end
+
+  def next_pending_order
+    orders = import_order_class.where(state: 'pending')
+                               .order('created_at asc')
+                               .limit(1)
+    return unless orders
+
+    orders.first
+  end
+
+  def subscribe
   end
 end
