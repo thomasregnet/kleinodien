06421e6cab8cca9686c1fc0109cae4198ffb52ff
ossi <ossi@asgard.example.com>
2018-11-06 20:29:05 +0100

    Save interim status

--- app/importers/brainz_root_importer.rb
+++ app/importers/brainz_root_importer.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
+# Choose the right class for MusicBrainz imports and call it
 class BrainzRootImporter
   IMPORTER_FOR = {
     'releae' => 'BrainzReleaseImporter'
   }.freeze
+
+  def self.run(import_order)
+    importer_class = IMPORTER_FOR[importorder.kind]
+    importer_class.call(import_order)
+  end
 end
