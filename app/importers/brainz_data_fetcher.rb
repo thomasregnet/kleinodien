6d1af1c3353a333e498ff20976abc00e7494d4e2
ossi <ossi@asgard.example.com>
2018-11-08 12:02:36 +0100

    Fix test-data handling of file-suffixes
    .xml is appended by the classes and modules that read the data
    from the filesystem.

---
+++ app/importers/brainz_data_fetcher.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Get data from MusicBrainz
+class BrainzDataFetcher
+  class << self; attr_reader :last_request end
+  @last_request = 0
+
+  def initialize
+    @store = {}
+  end
+
+  attr_reader :store
+
+  def get(uri)
+    uri = 'https://musicbrainz.org/ws/2/release/7452f8c9-f9bc-3ca7-859e-3220e57e4e4a?inc=artists+labels+recordings+release-groups'
+
+    result = Faraday.get(uri)
+    # byebug
+    result
+  end
+
+  def last_request
+    self.class.last_request
+  end
+end
---
+++ spec/importers/brainz_data_fetcher_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BrainzDataFetcher do
+  let(:fetcher) { described_class.new }
+
+  describe '.last_request' do
+    it 'is initial set to 0' do
+      expect(described_class.last_request).to be(0)
+    end
+  end
+
+  describe '#last_request' do
+    it 'is initial set to 0' do
+      expect(fetcher.last_request).to be(0)
+    end
+  end
+
+  # describe '#get' do
+  #   it 'returns a Brainz blueprint' do
+  #     expect(fetcher.get(:foo)).to be_instance_of BrainzReleaseBlueprint
+  #   end
+  # end
+end
