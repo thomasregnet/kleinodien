9f920761dd23b94c2aafe795c2523fda6df3b196
ossi <ossi@asgard.example.com>
2018-11-08 08:30:39 +0100

    Save interim status

--- app/importers/brainz_release_importer.rb
+++ app/importers/brainz_release_importer.rb
@@ -6,14 +6,29 @@ class BrainzReleaseImporter
     new(import_order).call
   end
 
+  # TODO: initialize with args to make it possible to hand over the queue
   def initialize(import_order)
     @import_order = import_order
+    @queue        = []
   end
 
   attr_reader :import_order
 
   def call
     # TODO: add real code here
+    release = find_persisted
+    return release if release
+
+    request
     AlbumRelease.new
   end
+
+  def find_persisted
+    CompilationRelease.find_by(brainz_code: import_order.code)
+  end
+
+  def request
+
+  end
 end
+
