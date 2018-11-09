bbd9bfd71ade037de21c54bc30f2e5939434c152
ossi <ossi@asgard.example.com>
2018-11-09 09:07:42 +0100

    Add spec for imports when an Brainz-release exists
    Spec fails for now.

--- spec/importers/brainz_release_importer_spec.rb
+++ spec/importers/brainz_release_importer_spec.rb
@@ -7,22 +7,31 @@ RSpec.describe BrainzReleaseImporter do
 
   after { DatabaseCleaner.clean }
 
+  let(:import_order) do
+    BrainzImportOrder.new(
+      code: '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a',
+      kind: 'release',
+      state: 'pending',
+      user: FactoryBot.build(:user)
+    )
+  end
+
   describe '.from_import_order' do
     context 'when the release does not exist' do
-      let(:import_order) do
-        BrainzImportOrder.new(
-          code: '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a',
-          kind: 'release',
-          state: 'pending',
-          user: FactoryBot.build(:user)
-        )
+      it 'returns an AlbumRelease' do
+        expect(described_class.from_import_order(import_order))
+          .to be_instance_of AlbumRelease
+      end
+    end
+
+    context 'when the release is already stored in the database' do
+      before do
+        FactoryBot.create!(brainz_code: '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a')
       end
 
-      describe '.from_import_order' do
-        it 'returns an AlbumRelease' do
-          expect(described_class.from_import_order(import_order))
-            .to be_instance_of AlbumRelease
-        end
+      it 'raises an exception' do
+        expect(described_class.from_import_order(import_order))
+          .to raise_exception
       end
     end
   end
