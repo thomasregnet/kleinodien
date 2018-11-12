8c9c2463331818d602bac90fcd278cd9e72ad5a2
Ossi <ossi@example.com>
2018-11-12 21:46:30 +0100

    Add BrainzReleaseImportRequest#kind & #query_string

--- app/models/brainz_release_import_request.rb
+++ app/models/brainz_release_import_request.rb
@@ -1,5 +1,15 @@
-# frozen_string_literal true
+# frozen_string_literal: true
 
 # ImportRequest for MusicBrainz Releases
 class BrainzReleaseImportRequest < BrainzImportRequest
+  QUERY_KIND   = 'release'
+  QUERY_STRING = 'inc=artists+labels+recordings+release-groups'
+
+  def kind
+    QUERY_KIND
+  end
+
+  def query_string
+    QUERY_STRING
+  end
 end
--- spec/models/brainz_release_import_request_spec.rb
+++ spec/models/brainz_release_import_request_spec.rb
@@ -5,4 +5,27 @@ require 'shared_examples_for_import_requests'
 
 RSpec.describe BrainzReleaseImportRequest, type: :model do
   include_examples 'for ImportRequests', :brainz_release_import_request
+
+  def uuid
+    @uuid ||= SecureRandom.uuid.to_s
+  end
+
+  describe '#to_uri' do
+    let(:import_request) do
+      FactoryBot.build(
+        :brainz_release_import_request,
+        code: uuid
+      )
+    end
+
+    let(:uri) do
+      'https://musicbrainz.org/ws/2/release/' \
+        + uuid \
+        + 'inc=artists+labels+recordings+release-groups'
+    end
+
+    it 'returns the uri' do
+      expect(import_request.to_uri).to eq(uri)
+    end
+  end
 end
