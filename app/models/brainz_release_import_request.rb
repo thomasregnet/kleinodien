628b758e106878d64b38f8d43b013e3eec35f3f8
Ossi <ossi@example.com>
2018-11-12 21:16:50 +0100

    Add model BrainzImportRequest

--- app/models/brainz_release_import_request.rb
+++ app/models/brainz_release_import_request.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal true
 
 # ImportRequest for MusicBrainz Releases
-class BrainzReleaseImportRequest < ImportRequest
+class BrainzReleaseImportRequest < BrainzImportRequest
 end
