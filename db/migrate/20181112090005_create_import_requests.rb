85c12b3137b45047b5b003fc7d9c831745e36b71
Ossi <ossi@example.com>
2018-11-12 19:53:07 +0100

    Add "NOT NUll" constraint to import_requests.type

--- db/migrate/20181112090005_create_import_requests.rb
+++ db/migrate/20181112090005_create_import_requests.rb
@@ -4,7 +4,7 @@ class CreateImportRequests < ActiveRecord::Migration[5.2]
       t.integer :attempts_cache, null: false, default: 0
       t.text :code, null: false
       t.text :state, null: false, default: :pending
-      t.text :type
+      t.text :type, null: false
       t.references :import_order, foreign_key: true, null: false
 
       t.timestamps
