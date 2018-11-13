26d54dcfa8653e72972bceab11597f572c4245d6
Ossi <ossi@example.com>
2018-11-13 10:59:44 +0100

    Add empty specs for ImportBrainzRelease

--- spec/importers/import_brainz_release_spec.rb
+++ spec/importers/import_brainz_release_spec.rb
@@ -5,4 +5,13 @@ require 'shared_examples_for_services'
 
 RSpec.describe ImportBrainzRelease do
   it_behaves_like 'a service'
+
+  it 'raises without a BrainzImportOrder'
+  it 'raises if the kind is not "release"'
+
+  it 'initializes the store'
+  it 'calls PrepareBrainzRelease'
+  it 'calls PersistBrainzRelease'
+
+  it 'persists within a transaction'
 end
