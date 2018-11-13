1996839fc121847bd7e4a736de3f8973b20113d8
Ossi <ossi@example.com>
2018-11-13 12:27:42 +0100

    TransformBrainzOrder... raise error with wrong arg

--- spec/services/transform_brainz_order_to_request_service_spec.rb
+++ spec/services/transform_brainz_order_to_request_service_spec.rb
@@ -3,12 +3,17 @@
 require 'rails_helper'
 require 'shared_examples_for_services'
 
+class TestWrongImportOrder
+end
 RSpec.describe TransformBrainzOrderToRequestService do
   it_behaves_like 'a service'
 
   # the specs for the service belongs here
 
-  it 'raises with a wrong ImportOrder class'
+  it 'raises with a wrong ImportOrder class' do
+    expect { described_class.call(TestWrongImportOrder.new) }
+      .to raise_error ArgumentError
+  end
 
   context 'when the kind is "release"' do
     it 'returns a BrainzReleaseImportRequest object'
