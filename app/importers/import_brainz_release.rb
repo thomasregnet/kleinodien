e2a030a5407e486dd29509f2af8baaa87c8e43cd
Ossi <ossi@example.com>
2018-11-13 21:13:11 +0100

    Clean up ImportBrainzRelease

--- app/importers/import_brainz_release.rb
+++ app/importers/import_brainz_release.rb
@@ -13,31 +13,11 @@ class ImportBrainzRelease
   attr_reader :import_order
 
   def call
-    # validate
-
     import_request = TransformBrainzOrderToRequestService.call(
       expected_kind: :release,
       import_order:  import_order
     )
     # TODO: call PrepareBrainzRelease with useful args
     PrepareBrainzRelease.call(:some_thing)
-  end
-
-  def validate
-    validate_import_order_class
-    validate_kind
-  end
-
-  def validate_import_order_class
-    return if import_order.class == BrainzImportOrder
-
-    raise ArgumentError, 'Must be called with a BrainzImportOrder'
-  end
-
-  def validate_kind
-    kind = import_order.kind
-    return if kind == 'release'
-
-    raise ArgumentError, "Expected \"release\" as kind, got \"#{kind}\""
   end
 end
