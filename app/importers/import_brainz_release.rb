92661eadf07b4adfa9dfee64fedcef5b669bd461
Ossi <ossi@example.com>
2018-11-13 11:56:27 +0100

    Fill empty specs of ImportBrainzRelease

--- app/importers/import_brainz_release.rb
+++ app/importers/import_brainz_release.rb
@@ -7,10 +7,32 @@ class ImportBrainzRelease
   end
 
   def initialize(import_order)
+    @import_order = import_order
   end
 
   attr_reader :import_order
 
   def call
+    validate
+
+    PrepareBrainzRelease.call
+  end
+
+  def validate
+    validate_import_order_class
+    validate_kind
+  end
+
+  def validate_import_order_class
+    return if import_order.class == BrainzImportOrder
+
+    raise ArgumentError, 'Must be called with a BrainzImportOrder'
+  end
+
+  def validate_kind
+    kind = import_order.kind
+    return if kind == 'release'
+
+    raise ArgumentError, "Expected \"release\" as kind, got \"#{kind}\""
   end
 end
--- spec/importers/import_brainz_release_spec.rb
+++ spec/importers/import_brainz_release_spec.rb
@@ -3,14 +3,44 @@
 require 'rails_helper'
 require 'shared_examples_for_services'
 
+# For testing
+class TestImportClass
+end
+
+class PrepareBrainzRelease
+  def self.call
+  end
+end
 RSpec.describe ImportBrainzRelease do
   it_behaves_like 'a service'
 
-  it 'raises without a BrainzImportOrder'
-  it 'raises if the kind is not "release"'
+  context 'when it is called with an object of the wrong class' do
+    it 'raises without a BrainzImportOrder' do
+      expect { described_class.call(TestImportClass.new) }
+        .to raise_error ArgumentError
+    end
+  end
+
+  context 'when the kind is wrong' do
+    let(:import_order) do
+      FactoryBot.build(:brainz_import_order, kind: 'artist')
+    end
+
+    it 'raises if the kind is not "release"' do
+      expect { described_class.call(import_order) }
+        .to raise_error ArgumentError
+    end
+  end
 
   it 'initializes the store'
-  it 'calls PrepareBrainzRelease'
+
+  it 'calls PrepareBrainzRelease' do
+    allow(PrepareBrainzRelease).to receive(:call)
+    described_class.call(
+      FactoryBot.build(:brainz_import_order, kind: 'release')
+    )
+  end
+
   it 'calls PersistBrainzRelease'
 
   it 'persists within a transaction'
