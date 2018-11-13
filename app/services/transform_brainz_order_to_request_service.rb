2fccb6108acc0a8a7108d5f6211bd1de096bee84
Ossi <ossi@example.com>
2018-11-13 12:44:53 +0100

    Let Transform...Service return the right object

--- app/services/transform_brainz_order_to_request_service.rb
+++ app/services/transform_brainz_order_to_request_service.rb
@@ -2,6 +2,10 @@
 
 # Transform a BrainzImportOrder object to a BrainzImport*Request
 class TransformBrainzOrderToRequestService
+  BRAINZ_REQUEST_CLASS_FOR = {
+    'release' => BrainzReleaseImportRequest
+  }.freeze
+
   def self.call(import_order)
     new(import_order).call
   end
@@ -11,15 +15,33 @@ class TransformBrainzOrderToRequestService
   end
 
   def call
-    validate_import_order
+    validate
+
+    request_class = BRAINZ_REQUEST_CLASS_FOR[import_order.kind]
+    request_class.new(
+      code:         import_order.code,
+      import_order: import_order
+    )
   end
 
   attr_reader :import_order
 
+  def validate
+    validate_import_order
+    validate_kind
+  end
+
   def validate_import_order
-    klass = import_order.class
+    klass = import_order.class.to_s
     return if klass == 'BrainzImportOrder'
 
     raise ArgumentError, "Expected a BrainzImportOrder, not #{klass}"
+  end
+
+  def validate_kind
+    kind = import_order.kind
+    return if BRAINZ_REQUEST_CLASS_FOR[kind]
+
+    raise ArgumentError, "Can't handle kind \"#{kind}\""
   end
 end
