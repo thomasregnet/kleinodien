6fcd1e05c7399398c6021908860bdfc747ab28e0
Ossi <ossi@example.com>
2018-11-13 10:03:42 +0100

    Delete commented code

--- app/models/brainz_import_order.rb
+++ app/models/brainz_import_order.rb
@@ -3,21 +3,4 @@
 # ImportOrder for MusicBrainz
 class BrainzImportOrder < ImportOrder
   include CodeUuidValidation
-  # validates :code,
-  #           format: {
-  #             with: %r{
-  #               \A
-  #               [a-z0-9]{8}
-  #               -
-  #               [a-z0-9]{4}
-  #               -
-  #               [a-z0-9]{4}
-  #               -
-  #               [a-z0-9]{4}
-  #               -
-  #               [a-z0-9]{12}
-  #               \z
-  #               }x,
-  #             message: 'must be an uuid'
-  #           }
 end
