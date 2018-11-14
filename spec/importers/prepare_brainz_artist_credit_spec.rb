3c6829aa9a441422e50890c004b803b89f9e121f
Ossi <ossi@example.com>
2018-11-14 10:31:23 +0100

    Try to spec PrepareBrainzArtistCredit

--- spec/importers/prepare_brainz_artist_credit_spec.rb
+++ spec/importers/prepare_brainz_artist_credit_spec.rb
@@ -5,4 +5,11 @@ require 'shared_examples_for_services'
 
 RSpec.describe PrepareBrainzArtistCredit do
   it_behaves_like 'a service'
+
+  describe '.call' do
+    it 'requests the artist' do
+      proxy = instance_double('proxy')
+      # expect(proxy).to have_received(:get)
+    end
+  end
 end
