e7b3b545fcf45996e57df0cb248c0492fe72717f
Ossi <ossi@example.com>
2018-11-14 09:28:23 +0100

    Continue PrepareBrainzRelease

--- app/importers/prepare_brainz_release.rb
+++ app/importers/prepare_brainz_release.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Prepare a MusicBrainz release for import
 class PrepareBrainzRelease
   def self.call(args)
@@ -5,8 +7,29 @@ class PrepareBrainzRelease
   end
 
   def initialize(args)
+    @import_request = args[:import_request]
+    @proxy          = args[:proxy]
   end
 
+  attr_reader :import_request, :proxy
+
   def call
+    prepare
+    persist
+  end
+
+  def prepare
+    PrepareBrainzArtistCredit.call(
+      blueprint: blueprint,
+      proxy:     proxy
+    )
+  end
+
+  def persist
+    # TODO: implement persist
+  end
+
+  def blueprint
+    @blueprint ||= proxy.get(import_request)
   end
 end
