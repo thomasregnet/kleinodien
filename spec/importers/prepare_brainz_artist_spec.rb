444fa2874179f8e545dcee2f1aa1139580d81e15
Ossi <ossi@example.com>
2018-11-14 12:53:03 +0100

    Add specs for PrepareBrainzArtist

---
+++ spec/importers/prepare_brainz_artist_spec.rb
@@ -0,0 +1,25 @@
+
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'ko_test_data'
+
+RSpec.describe PrepareBrainzArtist do
+  context 'when the artist does not exist in the database' do
+    let(:blueprint) do
+      KoTestData::GetBrainzBlueprintFor.path(
+        'artist/1d93c839-22e7-4f76-ad84-d27039efc048?inc=url-rels.xml'
+      )
+    end
+
+    it 'calls the proxy' do
+      proxy = spy
+      described_class.call(
+        blueprint: blueprint,
+        proxy:     proxy
+      )
+      expect(proxy).to have_received(:get)
+    end
+
+  end
+end
