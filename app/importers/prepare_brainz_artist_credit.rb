d0cbf987d00192c935a97532ab9ced942a04062d
Ossi <ossi@example.com>
2018-11-14 11:38:53 +0100

    Save interim status of PrepareBrainzArtistCredit
    The next step is the implementation of PrepareBrainzArtist.

--- app/importers/prepare_brainz_artist_credit.rb
+++ app/importers/prepare_brainz_artist_credit.rb
@@ -14,6 +14,21 @@ class PrepareBrainzArtistCredit
   attr_reader :blueprint, :proxy
 
   def call
-    artists = proxy.get(:foo)
+    return if already_have
+
+    blueprint.name_credits.each do |name_credit| 
+      prepare_brainz_artist(name_credit)
+    end
+  end
+
+  def already_have
+    # TODO: implement already_have
+  end
+
+  def prepare_brainz_artist(name_credit)
+    PrepareBrainzArtist.call(
+      blueprint: name_credit,
+      proxy:     proxy
+    )
   end
 end
