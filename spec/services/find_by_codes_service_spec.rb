7b2d2e4593fe05060b6a6d235fa564680b26add3
Ossi <ossi@example.com>
2018-11-15 09:04:20 +0100

    Clean up find_by_codes_service_spec.rb

--- spec/services/find_by_codes_service_spec.rb
+++ spec/services/find_by_codes_service_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 require 'shared_examples_for_services'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe FindByCodesService do
   it_behaves_like 'a service'
 
@@ -10,20 +11,24 @@ RSpec.describe FindByCodesService do
     'fdfea6f4-6c8e-4200-ba46-c797831cedae'
   end
 
-  before { DatabaseCleaner.start }
-
-  let(:args) do
+  def default_args
     {
       model_class: Artist,
       attributes: {
-        unused: nil,
-        brainz_code: brainz_code,
+        unused:       nil,
+        brainz_code:  brainz_code,
         discogs_code: 123
       }
     }
   end
 
   context 'when nothing can be found by codes' do
+    before { DatabaseCleaner.start }
+
+    after { DatabaseCleaner.clean }
+
+    let(:args) { default_args }
+
     it 'returns nil' do
       expect(described_class.call(args)).to be_nil
     end
@@ -31,12 +36,17 @@ RSpec.describe FindByCodesService do
 
   context 'when an entity can be found by codes' do
     before do
+      DatabaseCleaner.start
       FactoryBot.create(:artist, brainz_code: brainz_code)
     end
+
+    after { DatabaseCleaner.clean }
+
+    let(:args) { default_args }
 
     it 'returns the model' do
       expect(described_class.call(args)).to be_instance_of(Artist)
     end
   end
-  after { DatabaseCleaner.clean }
 end
+# rubocop:enable Metrics/BlockLength
