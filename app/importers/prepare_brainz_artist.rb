7e1772fd4b482cada9cb85323f043a527db6f217
Ossi <ossi@example.com>
2018-11-19 20:37:30 +0100

    Save interim status

--- app/importers/prepare_brainz_artist.rb
+++ app/importers/prepare_brainz_artist.rb
@@ -14,34 +14,59 @@ class PrepareBrainzArtist
   attr_reader :blueprint, :proxy
 
   def call
-    proxy.get(request) unless complete_blueprint?
-
     artist = find_already_existing
     return artist if artist
+
+    return if blueprint_complete?
+
+    find_already_existing(full_blueprint)
   end
+
+  # def call
+  #   proxy.get(request) unless complete_blueprint?
+
+  #   artist = find_already_existing
+  #   return artist if artist
+  # end
 
   # <artist
   #     type-id="e431f5f6-b5d2-343d-8b36-72607fffb74b"
   #     type="Group"
   #     id="1d93c839-22e7-4f76-ad84-d27039efc048">
-  def complete_blueprint?
+  def blueprint_complete?
+    return false unless blueprint
     return true if blueprint.type
 
     false
   end
 
-  def find_already_existing
-    find_already_existing_by_brainz_code unless complete_blueprint?
-
-    find_already_existing_by_codes
+  def find_already_existing(codes_hash = blueprint.codes_hash)
+    FindByCodesService.call(model_class: Artist, attributes: codes_hash)
   end
 
-  def find_already_existing_by_brainz_code
-    Artist.find_by(brainz_code: blueprint.id)
+  def full_blueprint
   end
+  # def find_already_existing
+  #   # find_already_existing_by_brainz_code unless complete_blueprint?
 
-  def find_already_existing_by_codes
-  end
+  #   # find_already_existing_by_codes
+  #   code_for = {
+  #     brainz_code:   blueprint.id,
+  #     discogs_code:  blueprint.discogs_code,
+  #     tmdb_code:     blueprint.tmdb_code,
+  #     wikidata_code: blueprint.wikidata_code
+  #   }
+
+  #   FindByCodesService.call(model_class: Artist, attributes: code_for)
+  # end
+
+  # def find_already_existing_by_brainz_code
+  #   Artist.find_by(brainz_code: blueprint.id)
+    
+  # end
+
+  # def find_already_existing_by_codes
+  # end
 
   def request
     # TODO: implement request
--- spec/importers/prepare_brainz_artist_spec.rb
+++ spec/importers/prepare_brainz_artist_spec.rb
@@ -8,63 +8,82 @@ RSpec.describe PrepareBrainzArtist do
     123
   end
 
-  context 'without a complete dataset' do
-    let(:blueprint) do
-      release_blueprint = KoTestData::GetBrainzBlueprintFor.path(
-        'release/7452f8c9-f9bc-3ca7-859e-3220e57e4e4a?' \
-          'inc=artists+labels+recordings+release-groups.xml'
-      )
-      release_blueprint.artist_credit.name_credit.first
-    end
-
-    it 'calls the proxy' do
-      proxy = spy
-      described_class.call(
-        blueprint: blueprint,
-        proxy:     proxy
-      )
-
-      expect(proxy).to have_received(:get)
-    end
+  def brainz_code
+    'f9ea7be4-ebee-11e8-b919-d7e490fb38cb'
   end
 
-  context 'with a complete dataset' do
-    let(:blueprint) do
-      KoTestData::GetBrainzBlueprintFor.path(
-        'artist/2280ca0e-6968-4349-8c36-cb0cbd6ee95f?inc=url-rels.xml'
-      )
-    end
-
-    it 'does not call the proxy' do
-      proxy = spy
-      described_class.call(
-        blueprint: blueprint,
-        proxy:     proxy
-      )
-
-      expect(proxy).not_to have_received(:get)
-    end
+  def code_for
+    {
+      brainz_code:  brainz_code,
+      discogs_code: discogs_code
+    }
   end
+
+  # context 'without a complete dataset' do
+  #   let(:blueprint) do
+  #     release_blueprint = KoTestData::GetBrainzBlueprintFor.path(
+  #       'release/7452f8c9-f9bc-3ca7-859e-3220e57e4e4a?' \
+  #         'inc=artists+labels+recordings+release-groups.xml'
+  #     )
+  #     release_blueprint.artist_credit.name_credit.first
+  #   end
+
+  #   it 'calls the proxy' do
+  #     proxy = spy
+  #     described_class.call(
+  #       blueprint: blueprint,
+  #       proxy:     proxy
+  #     )
+
+  #     expect(proxy).to have_received(:get)
+  #   end
+  # end
+
+  # context 'with a complete dataset' do
+  #   let(:blueprint) do
+  #     KoTestData::GetBrainzBlueprintFor.path(
+  #       'artist/2280ca0e-6968-4349-8c36-cb0cbd6ee95f?inc=url-rels.xml'
+  #     )
+  #   end
+
+  #   it 'does not call the proxy' do
+  #     proxy = spy
+  #     described_class.call(
+  #       blueprint: blueprint,
+  #       proxy:     proxy
+  #     )
+
+  #     expect(proxy).not_to have_received(:get)
+  #   end
+  # end
 
   context 'when the artist already exists in the database' do
     before do
       DatabaseCleaner.start
-      FactoryBot.create(:artist, discogs_code: discogs_code)
+      FactoryBot.create(
+        :artist,
+        brainz_code: '1d93c839-22e7-4f76-ad84-d27039efc048'
+      )
     end
 
     after { DatabaseCleaner.clean }
 
     let(:blueprint) do
-      KoTestData::GetBrainzBlueprintFor.path(
+      xml_string = KoTestData::GetBrainzXmlFor.path(
         'artist/1d93c839-22e7-4f76-ad84-d27039efc048?inc=url-rels.xml'
       )
+      BrainzBlueprint.from_xml(xml_string)
     end
 
     it 'returns the artist' do
       proxy = double
-      allow(proxy).to receive(:get).and_return(:foo)
+      # allow(proxy).to receive(:get).and_return(:foo)
+      # blueprint = double('Brainz Blueprint').as_null_object
+      # allow(blueprint).to receive(:type).and_return('artist')
+      # allow(blueprint).to receive(:id).and_return(brainz_code)
+
       expect(described_class.call(blueprint: blueprint, proxy: proxy))
-        .not_to be_nil
+        .to be_instance_of(Artist)
     end
   end
 
