2fa6be23c8fbc8702ea0dc855b373eda8715afbe
Ossi <ossi@example.com>
2018-11-19 21:14:27 +0100

    Let PrepareBrainzArtist call the proxy
    PrepareBrainzArtist calls the proxy, but #request must still be implemented.

--- app/importers/prepare_brainz_artist.rb
+++ app/importers/prepare_brainz_artist.rb
@@ -22,17 +22,6 @@ class PrepareBrainzArtist
     find_already_existing(full_blueprint)
   end
 
-  # def call
-  #   proxy.get(request) unless complete_blueprint?
-
-  #   artist = find_already_existing
-  #   return artist if artist
-  # end
-
-  # <artist
-  #     type-id="e431f5f6-b5d2-343d-8b36-72607fffb74b"
-  #     type="Group"
-  #     id="1d93c839-22e7-4f76-ad84-d27039efc048">
   def blueprint_complete?
     return false unless blueprint
     return true if blueprint.type
@@ -45,28 +34,8 @@ class PrepareBrainzArtist
   end
 
   def full_blueprint
+    proxy.get(request)
   end
-  # def find_already_existing
-  #   # find_already_existing_by_brainz_code unless complete_blueprint?
-
-  #   # find_already_existing_by_codes
-  #   code_for = {
-  #     brainz_code:   blueprint.id,
-  #     discogs_code:  blueprint.discogs_code,
-  #     tmdb_code:     blueprint.tmdb_code,
-  #     wikidata_code: blueprint.wikidata_code
-  #   }
-
-  #   FindByCodesService.call(model_class: Artist, attributes: code_for)
-  # end
-
-  # def find_already_existing_by_brainz_code
-  #   Artist.find_by(brainz_code: blueprint.id)
-    
-  # end
-
-  # def find_already_existing_by_codes
-  # end
 
   def request
     # TODO: implement request
