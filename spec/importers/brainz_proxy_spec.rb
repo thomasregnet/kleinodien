e930a249f28fb8c7cdef52c1f4dd72fb6ec11b61
Ossi <ossi@example.com>
2018-11-20 10:14:57 +0100

    In brainz_proxy_spec.rb change "fetcher" to "proxy"

--- spec/importers/brainz_proxy_spec.rb
+++ spec/importers/brainz_proxy_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe BrainzProxy do
-  let(:fetcher) { described_class.new }
+  let(:proxy) { described_class.new }
   let(:uri) do
     'https://musicbrainz.org/ws/2/release/' \
       '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a' \
@@ -18,13 +18,13 @@ RSpec.describe BrainzProxy do
 
   describe '#last_request' do
     it 'is initial set to 0' do
-      expect(fetcher.last_request).to be(0)
+      expect(proxy.last_request).to be(0)
     end
   end
 
   describe '#get' do
     it 'returns a Brainz blueprint' do
-      expect(fetcher.get(uri)).to be_instance_of BrainzBlueprint
+      expect(proxy.get(uri)).to be_instance_of BrainzBlueprint
     end
   end
 end
