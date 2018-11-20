6569d5f5e1377e7215c70aa893758828d42ad1d1
Ossi <ossi@example.com>
2018-11-20 21:16:37 +0100

    Start BrainzFetcher

---
+++ app/importers/brainz_fetcher.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+# Fetch data from the MusicBrainz web-service
+class BrainzFetcher
+  def self.call(args)
+    new(args).call
+  end
+
+  def initialize(args)
+    @import_request = args[:import_request]
+  end
+
+  attr_reader :import_request, :response
+
+  def call
+    start_processing
+    fetch
+
+    done_processing
+    BrainzBlueprint.from_xml(response.body)
+  end
+
+  def start_processing
+    import_request.processing
+    import_request.save!
+  end
+
+  def done_processing
+    import_request.done
+    import_request.save!
+  end
+
+  def fetch
+    max_tries.times do
+      take_a_nap
+      @response = Faraday.get(import_request.to_uri)
+      return response if good_response?
+    end
+  end
+
+  def max_tries
+    3
+  end
+
+  def good_response?
+    return false unless response
+    return true if response.status == 200
+
+    false
+  end
+
+  def take_a_nap
+    # TODO: implement take_a_nap
+  end
+end
