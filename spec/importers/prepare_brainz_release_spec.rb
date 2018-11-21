23160d6e0b162476fc993ec7b486688d56ca781d
Ossi <ossi@example.com>
2018-11-21 21:43:04 +0100

    Create mock-class for PrepareBrainzRelease

--- spec/importers/prepare_brainz_release_spec.rb
+++ spec/importers/prepare_brainz_release_spec.rb
@@ -3,6 +3,24 @@
 require 'rails_helper'
 require 'shared_examples_for_services'
 
+class MockPrepareBrainzRelease < PrepareBrainzRelease
+  def initialize(args)
+    @prepare_artist_credit_spy = args[:prepare_artist_credit_spy]
+    @prepare_release_group_spy = args[:prepare_release_group_spy]
+    super(args)
+  end
+
+  attr_reader :prepare_artist_credit_spy, :prepare_release_group_spy
+
+  def prepare_artist_credit
+    prepare_artist_credit_spy.call
+  end
+
+  def prepare_release_group
+    prepare_release_group_spy.call
+  end
+end
+
 RSpec.describe PrepareBrainzRelease do
   it_behaves_like 'a service'
 
@@ -16,9 +34,18 @@ RSpec.describe PrepareBrainzRelease do
       end
 
       it 'returns nil' do
-        proxy = spy
-        args = { import_request: import_request, proxy: proxy }
-        expect(described_class.call(args)).to be_nil
+        prepare_artist_credit_spy = spy
+        prepare_release_group_spy = spy
+        proxy                     = spy
+        args = {
+          prepare_artist_credit_spy: prepare_artist_credit_spy,
+          prepare_release_group_spy: prepare_release_group_spy,
+          import_request:            import_request,
+          proxy:                     proxy
+        }
+        expect(MockPrepareBrainzRelease.call(args)).to be_nil
+        expect(prepare_artist_credit_spy).to have_received(:call)
+        expect(prepare_release_group_spy).to have_received(:call)
       end
     end
   end
