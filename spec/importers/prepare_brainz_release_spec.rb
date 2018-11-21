0de74cfedf8b7471eec2b3b28be9ace9ca8c1ebd
Ossi <ossi@example.com>
2018-11-21 21:16:38 +0100

    Refine prepare_brainz_release_spec.rb

--- spec/importers/prepare_brainz_release_spec.rb
+++ spec/importers/prepare_brainz_release_spec.rb
@@ -7,24 +7,32 @@ RSpec.describe PrepareBrainzRelease do
   it_behaves_like 'a service'
 
   describe '.call' do
-    context 'with a valid ImportRequest' do
+    context 'when the release is not already persisted' do
       def brainz_code
         '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a'
       end
 
-      let(:import_request) do
-        FactoryBot.build(:brainz_release_import_request, code: brainz_code)
+      def import_request
+        @import_request ||= FactoryBot.build(
+          :brainz_release_import_request,
+          code: brainz_code
+        )
       end
 
-      it 'foos' do
-        proxy = BrainzProxy.new(import_order: import_request.import_order)
-        described_class.call(
+      def proxy
+        BrainzProxy.new(import_order: import_request.import_order)
+      end
+
+      def args
+        {
           import_request: import_request,
           proxy:          proxy
-        )
-        expect(:foo).to eq(:foo)
+        }
+      end
+
+      it 'returns nil' do
+        expect(described_class.call(args)).to be_nil
       end
     end
   end
 end
-
