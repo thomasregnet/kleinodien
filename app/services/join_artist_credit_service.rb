f0b1ff31d4e45f048c06e303840e47a8aa5c44e2
Ossi <ossi@example.com>
2018-11-22 21:35:10 +0100

    Start JoinArtistCreditService

---
+++ app/services/join_artist_credit_service.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Join names with join-phrases
+class JoinArtistCreditService
+  def self.call(args)
+    new(args).call
+  end
+
+  def initialize(args)
+    @candidates         = args[:candidates]
+    @name_method        = args[:name_method] || :name
+    @join_phrase_method = args[:join_phrase_method] || :join_phrase
+  end
+
+  attr_reader :candidates, :join_phrase_method, :name_method
+
+  def call
+    tokens = candidates.map { |candidate| candidate_tokens_for(candidate) }
+    tokens.flatten.join(' ')
+  end
+
+  def candidate_tokens_for(candidate)
+    tokens = []
+    tokens << candidate.send(name_method)
+    tokens << candidate.send(join_phrase_method) if join_pharse?(candidate)
+    tokens
+  end
+
+  def join_pharse?(candidate)
+    return true if candidate.send(join_phrase_method)
+
+    false
+  end
+end
+
