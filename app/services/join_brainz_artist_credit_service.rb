985febab5a9184db39b0b2abc4d516fe63964b69
Ossi <ossi@example.com>
2018-11-23 10:03:38 +0100

    Add JoinBrainzArtistCreditService

---
+++ app/services/join_brainz_artist_credit_service.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# Takes MusicBrainz blueprint returns the joined artist-credit name
+class JoinBrainzArtistCreditService < JoinArtistCreditService
+
+  # Used as parameter for JoinArtistCreditService
+  class BrainzArtistNameCredit
+    def initialize(args)
+      @name_credit = args[:name_credit]
+    end
+
+    attr_reader :name_credit
+
+    def name
+      name_credit.artist.name
+    end
+
+    def join_phrase
+      joinphrase = name_credit.joinphrase
+      return unless joinphrase
+
+      joinphrase.strip
+    end
+  end
+
+  def initialize(args)
+    @blueprint = args[:blueprint]
+  end
+
+  attr_reader :blueprint
+
+  def call
+    JoinArtistCreditService.call(candidates: candidates)
+  end
+
+  def candidates
+    blueprint.artist_credit.name_credits.map do |name_credit|
+      BrainzArtistNameCredit.new(name_credit: name_credit)
+    end
+  end
+end
