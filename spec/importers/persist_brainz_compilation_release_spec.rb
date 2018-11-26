f4b4914d11610fa807afb067883dcdfdce116db8
Ossi <ossi@example.com>
2018-11-26 12:45:29 +0100

    Progress with PersistBrainzCompilationRelease

--- spec/importers/persist_brainz_compilation_release_spec.rb
+++ spec/importers/persist_brainz_compilation_release_spec.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
+require 'ko_test_data'
 require 'rails_helper'
 require 'shared_examples_for_services'
 
 RSpec.describe PersistBrainzCompilationRelease do
   it_behaves_like 'a service'
+
+  describe '.call' do
+
+  end
 end
