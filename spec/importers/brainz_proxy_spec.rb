81fb8d5e1d47205e78fa1fd78373d3b18672b241
Ossi <ossi@example.com>
2018-11-26 10:47:29 +0100

    Implement BrainzProxy#lock and #locked?

--- spec/importers/brainz_proxy_spec.rb
+++ spec/importers/brainz_proxy_spec.rb
@@ -47,4 +47,31 @@ RSpec.describe BrainzProxy do
       expect { proxy.get(import_request) }.to raise_error ArgumentError
     end
   end
+
+  context 'when locked' do
+    describe '#locked?' do
+      let(:proxy) { described_class.new({}) }
+
+      it 'return true' do
+        proxy.lock
+        expect(proxy.locked?).to be true
+      end
+    end
+
+    describe '#get' do
+      def import_request
+        @import_request ||= FactoryBot.create(:brainz_artist_import_request)
+      end
+
+      let(:proxy) do
+        described_class.new(import_order: import_request.import_order)
+      end
+
+      it 'raises an error if the ImportRequest is not cached' do
+        proxy.lock
+        expect { proxy.get(import_request) }
+          .to raise_error ImportError::ProxyLocked
+      end
+    end
+  end
 end
