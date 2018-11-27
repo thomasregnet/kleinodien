3b556218e3dc199afe6a1a26fbe907917f3cc7aa
Ossi <ossi@example.com>
2018-11-27 21:33:17 +0100

    Change source_root call in ServiceGenerator

--- lib/generators/service/service_generator.rb
+++ lib/generators/service/service_generator.rb
@@ -1,5 +1,5 @@
 class ServiceGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
+  source_root File.expand_path('templates', __dir__)
 
   def create_service_class
     template 'service.rb.erb', "app/services/#{file_name}_service.rb"
