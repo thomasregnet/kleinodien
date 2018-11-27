ca8199c0a2e58620cd71eece544d87072523db02
Ossi <ossi@example.com>
2018-11-27 11:33:36 +0100

    More specs for PersistBrainzArtist

--- spec/importers/persist_brainz_artist_spec.rb
+++ spec/importers/persist_brainz_artist_spec.rb
@@ -6,17 +6,25 @@ require 'rails_helper'
 RSpec.describe PersistBrainzArtist do
   context 'when the Artist does not exist' do
     describe '.call' do
-      let(:blueprint) do
+      let(:artist) do
         xml_string = KoTestData::GetBrainzXmlFor.path(
           'artist/bdacc37b-8633-4bf8-9dd5-4662ee651aec'\
             '?inc=artist-rels+url-rels.xml'
         )
-        BrainzBlueprint.from_xml(xml_string)
+        blueprint = BrainzBlueprint.from_xml(xml_string)
+        described_class.call(blueprint: blueprint)
       end
 
       it 'persists the Artist' do
-        expect(described_class.call(blueprint: blueprint))
-          .to be_instance_of(Artist)
+        expect(artist).to be_instance_of(Artist)
+      end
+
+      it 'sets the name' do
+        expect(artist.name).to eq('Slayer')
+      end
+
+      it 'sets the begin_date' do
+        expect(artist.begin_date.year).to eq(1981)
       end
     end
   end
