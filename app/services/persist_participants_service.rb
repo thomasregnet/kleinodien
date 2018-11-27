db0dd2508032ebf6a27348a5dff254475e92965c
Ossi <ossi@example.com>
2018-11-27 12:41:39 +0100

    Start sketching PersistParticipantsService

--- app/services/persist_participants_service.rb
+++ app/services/persist_participants_service.rb
@@ -1,17 +1,20 @@
 # frozen_string_literal: true
 
+# Persist ArtistCredit-participants
 class PersistParticipantsService
-  include CallWithArgs
-
-  private
-
-  # attr_reader
-
-  def initialize(args)
-
+  def self.call(args)
+    new(args)
   end
 
-  def private_call
+  def initialize(args)
+    @artists       = args[:artists]
+    @artist_credit = args[:artist_credit]
+    @join_phrases  = args[:join_phrases]
+  end
+
+  attr_reader :artists, :artist_credit, :join_phrases
+
+  def call
     # the code for the service belongs here
   end
 end
