da09142238ec8cefecb0ef45cc1f6932b9d1edb2
Ossi <ossi@example.com>
2018-11-27 08:49:45 +0100

    Spec PersistBrainzArtistCredit. Some tests fail!
    Needed to change behaviour of JoinBrainzArtistCreditService.
    Need to adapt other consumers of this service.

--- spec/importers/persist_brainz_artist_credit_spec.rb
+++ spec/importers/persist_brainz_artist_credit_spec.rb
@@ -6,4 +6,25 @@ require 'shared_examples_for_services'
 
 RSpec.describe PersistBrainzArtistCredit do
   it_behaves_like 'a service'
+
+  context 'when the ArtistCredit already exists' do
+    describe '.call' do
+      let!(:artist_credit) do
+        FactoryBot.create(:artist_credit, name: 'Jello Biafra With NoMeansNo')
+      end
+
+      let(:blueprint) do
+        xml_string = KoTestData::GetBrainzXmlFor.path(
+          'release/693748be-7c18-39c3-af2e-2e62092090cf?' \
+            'inc=artists+labels+recordings+release-groups.xml'
+        )
+        BrainzBlueprint.from_xml(xml_string).artist_credit
+      end
+
+      it 'returns the ArtistCredit' do
+        expect(described_class.call(blueprint: blueprint))
+          .to eq(artist_credit)
+      end
+    end
+  end
 end
