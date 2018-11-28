8b818bd3230e100721c0eef72da18e090b21982c
Ossi <ossi@example.com>
2018-11-28 21:24:46 +0100

    Integrate PersistBrainzCompilationHead
    Integrated PersistBrainzCompilationHead in
    PersistBrainzCompilationRelease.

--- app/importers/persist_brainz_compilation_release.rb
+++ app/importers/persist_brainz_compilation_release.rb
@@ -26,6 +26,14 @@ class PersistBrainzCompilationRelease
 
   def persist
     artist_credit = persist_artist_credit
+    compilation_head = persist_compilation_head
+    # TODO: Persist the right type, not always 'album'
+    CompilationRelease.create!(
+      artist_credit: artist_credit,
+      head:          compilation_head,
+      title:         blueprint.title,
+      type:          AlbumRelease
+    )
   end
 
   def persist_artist_credit
@@ -43,5 +51,20 @@ class PersistBrainzCompilationRelease
   end
 
   def persist_compilation_head
+    compilation_head = find_already_existing_compilation_head
+    return compilation_head if compilation_head
+
+    import_request = BrainzReleaseGroupImportRequest.new(
+      code: blueprint.release_group.brainz_code
+    )
+
+    PersistBrainzCompilationHead.call(
+      blueprint: proxy.get(import_request),
+      proxy:     proxy
+    )
+  end
+
+  def find_already_existing_compilation_head
+    CompilationHead.find_by(brainz_code: blueprint.brainz_code)
   end
 end
