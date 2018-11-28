52f087648fdeb4072590c17965afcc196d9225ee
Ossi <ossi@example.com>
2018-11-28 10:29:35 +0100

    Start to implement PersistBrainzCompilationRelease
    First specs. Now there is a bug with import_album_from_brainz_spec.rb!

--- spec/importers/persist_brainz_compilation_release_spec.rb
+++ spec/importers/persist_brainz_compilation_release_spec.rb
@@ -8,6 +8,27 @@ RSpec.describe PersistBrainzCompilationRelease do
   it_behaves_like 'a service'
 
   describe '.call' do
+    context 'when the CompilationRelease already exists' do
+      before do
+        FactoryBot.create(
+          :compilation_release,
+          brainz_code: '693748be-7c18-39c3-af2e-2e62092090cf',
+          title:       'Test Dummy'
+        )
+      end
 
+      let(:blueprint) do
+        xml_string = KoTestData::GetBrainzXmlFor.path(
+          'release/693748be-7c18-39c3-af2e-2e62092090cf' \
+            '?inc=artists+labels+recordings+release-groups.xml'
+        )
+        BrainzBlueprint.from_xml(xml_string)
+      end
+
+      it 'returns the CompilationRelease' do
+        expect(described_class.call(blueprint: blueprint).title)
+          .to eq('Test Dummy')
+      end
+    end
   end
 end
