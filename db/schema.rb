955ca2f1c35a87f6d9cd08e10883a6fb54674a0b
Ossi <ossi@example.com>
2018-11-29 11:35:56 +0100

    Add column import_orders.requests_cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_073406) do
+ActiveRecord::Schema.define(version: 2018_11_29_103344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -421,6 +421,7 @@ ActiveRecord::Schema.define(version: 2018_11_29_073406) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "requests_cache"
     t.index ["user_id"], name: "index_import_orders_on_user_id"
   end
 
