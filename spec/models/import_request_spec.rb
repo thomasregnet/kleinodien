b224a1bd60700d52a6b592eab39c6fd6ec7e1892
Ossi <ossi@example.com>
2018-11-29 09:57:58 +0100

    Modify matcher in import_request_spec.rb
    should -> is_expected

--- spec/models/import_request_spec.rb
+++ spec/models/import_request_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe ImportRequest, type: :model do
   # all specs fail because in rails the "type" of the base-class is nil.
   # https://stackoverflow.com/questions/4858122/rails-sti-and-the-setting-of-the-type-string
   # include_examples 'for ImportRequests', :import_request
-  it { should have_many(:attempts) }
+  it { is_expected.to have_many(:attempts) }
   it { is_expected.to have_one(:body) }
 
   describe '#processing' do
