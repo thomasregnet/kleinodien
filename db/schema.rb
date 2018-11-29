a6f081907e9a71a0f6a2f9d9e39ed92e91dd3be6
Ossi <ossi@example.com>
2018-11-29 12:02:32 +0100

    Import_orders.requests_cache to requests_count
    Renamed import_orders.requests_cache to requests_count.
    Must be _count not _cache for a counter_cache!

--- db/schema.rb
+++ db/schema.rb
@@ -421,7 +421,7 @@ ActiveRecord::Schema.define(version: 2018_11_29_103344) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "requests_cache"
+    t.integer "requests_count"
     t.index ["user_id"], name: "index_import_orders_on_user_id"
   end
 
