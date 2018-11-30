8470af83085dfe8fc0835860f3be9bcba3821496
Ossi <ossi@example.com>
2018-11-30 10:03:56 +0100

    Make a singleton class of CalculateBrainzNap
    Also added specs. Class needs to be improved! May not work correctly!

--- app/importers/calculate_brainz_nap.rb
+++ app/importers/calculate_brainz_nap.rb
@@ -2,30 +2,31 @@
 
 # Calculate interruptions for MusicBrainz import requests
 class CalculateBrainzNap
-  class << self
-    attr_reader :last, :last_multiplier
-  end
-
-  @last            = 0
-  @last_multiplier = 1
+  include Singleton
 
   def self.call(args)
-    new(args).call
+    instance.call(args)
   end
 
-  def intialize(args)
-    @error = args[:error]
-    @now   = Time.now
+  def initialize
+    @last            = Time.new(1970)
+    @last_multiplier = 1
+    @now             = Time.now
   end
 
-  attr_reader :error, :now
+  attr_reader :error, :last, :last_multiplier, :now
 
-  def call
+  def call(error)
     return 0 if min_nap_time <= delta
 
+    @error = error
     nap_time = delta - required_nap_time
     @last = now
     nap_time
+  end
+
+  def default_nap_time
+    1
   end
 
   def delta
@@ -33,7 +34,7 @@ class CalculateBrainzNap
   end
 
   def multiplier
-    CalculateBrainzNapMultiplier.call(
+    @last_multiplier = CalculateBrainzNapMultiplier.call(
       error:           error,
       last_multiplier: last_multiplier
     )
---
+++ app/importers/calculate_brainz_nap_multiplier.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Calculate nap-time multiplier for MusicBrainz
+class CalculateBrainzNapMultiplier < CalculateNapMultiplier
+
+end
