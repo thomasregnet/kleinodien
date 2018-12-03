e13a8e41bd9018fe9862ba0f85a8798ce78be191
Ossi <ossi@example.com>
2018-12-03 22:04:20 +0100

    Spec TestData::Brainz.blueprint adds 'raw_data'

--- spec/lib/test_data/brainz_spec.rb
+++ spec/lib/test_data/brainz_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe TestData::Brainz do
       expect(described_class.blueprint(:artist, code))
         .to be_instance_of(BrainzBlueprint)
     end
+
+    it 'adds #raw_data to the blueprint' do
+      code = 'bdacc37b-8633-4bf8-9dd5-4662ee651aec'
+
+      expect(described_class.blueprint(:artist, code).raw_data)
+        .to match(/\A<\?xml/)
+    end
   end
 end
