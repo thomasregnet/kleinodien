9ee41aad64d2703cfae02d1359c9cab73da30c9d
Ossi <ossi@example.com>
2018-12-04 20:48:37 +0100

    Implement TestData::BrainzResult
    also added "shared examples for test data results".

---
+++ spec/shared_examples_for_test_data_results.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'a test-data result' do |blueprint_class, raw_data|
+  def string
+    'a test string'
+  end
+
+  let(:result) { described_class.new(string) }
+
+  describe '#raw' do
+    it 'returns the given string' do
+      expect(result.raw).to eq string
+    end
+  end
+
+  describe '#to_s' do
+    it 'returns the raw string' do
+      expect(result.to_s).to eq string
+    end
+  end
+
+  describe '#blueprint' do
+    let(:result) { described_class.new(raw_data) }
+
+    it 'returns a blueprint of the expected class' do
+      expect(result.blueprint).to be_instance_of(blueprint_class)
+    end
+  end
+end
