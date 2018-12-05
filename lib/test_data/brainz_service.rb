a43ee2f2f54e5f52de182fdc54e093ac668ed492
Ossi <ossi@example.com>
2018-12-05 20:50:20 +0100

    Remove TestData::Base

--- lib/test_data/brainz_service.rb
+++ lib/test_data/brainz_service.rb
@@ -1,23 +1,30 @@
 # frozen_string_literal: true
 
-require 'test_data/base'
 require 'test_data/brainz_result'
+require 'test_data/path_service'
 
 module TestData
   # Get MusicBrainz test-data
-  class BrainzService < TestData::Base
+  class BrainzService
     INC_FOR = {
       artist:  'artist-rels+url-rels',
       release: 'artists+labels+recordings+release-groups'
     }.freeze
 
     def self.call(args)
-      path = "musicbrainz.org/#{args[:kind]}/#{args[:code]}" \
-        "?inc=#{INC_FOR[args[:kind]]}"
-      new(path: path).call
+      new(args).call
     end
 
+    def initialize(args)
+      @code = args[:code]
+      @kind = args[:kind]
+    end
+
+    attr_reader :code, :kind
+
     def call
+      path = "musicbrainz.org/#{kind}/#{code}?inc=#{INC_FOR[kind]}"
+      raw = TestData::PathService.call(path)
       TestData::BrainzResult.new(raw)
     end
   end
