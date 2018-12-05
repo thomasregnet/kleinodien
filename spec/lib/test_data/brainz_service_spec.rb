d99346dd939eb328ec067ce8b125be5a14a117d8
Ossi <ossi@example.com>
2018-12-05 20:30:05 +0100

    Make TestData::BrainzService a service-class
    Some tests fail!

--- spec/lib/test_data/brainz_service_spec.rb
+++ spec/lib/test_data/brainz_service_spec.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'shared_examples_for_services'
 require 'test_data/brainz_service'
 
 RSpec.describe TestData::BrainzService do
-  describe '.blueprint' do
+  it_behaves_like 'a service'
+
+  describe '.call' do
     it 'returns a BrainzBlueprint' do
-      code = 'bdacc37b-8633-4bf8-9dd5-4662ee651aec'
-      expect(described_class.blueprint(:artist, code))
-        .to be_instance_of(BrainzBlueprint)
-    end
-
-    it 'adds #raw_data to the blueprint' do
-      code = 'bdacc37b-8633-4bf8-9dd5-4662ee651aec'
-
-      expect(described_class.blueprint(:artist, code).raw_data)
-        .to match(/\A<\?xml/)
+      # code = 'bdacc37b-8633-4bf8-9dd5-4662ee651aec'
+      args = {
+        code: 'bdacc37b-8633-4bf8-9dd5-4662ee651aec',
+        kind: :artist
+      }
+      expect(described_class.call(args))
+        .to be_instance_of(TestData::BrainzResult)
     end
   end
 end
