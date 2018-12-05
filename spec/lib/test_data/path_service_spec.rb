1b0565ba2b6bcf2d808c86455d42e03107744775
Ossi <ossi@example.com>
2018-12-05 20:15:17 +0100

    TestData::PathService behaves like a service

--- spec/lib/test_data/path_service_spec.rb
+++ spec/lib/test_data/path_service_spec.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: treu
 
 require 'rails_helper'
+require 'shared_examples_for_services'
 require 'test_data/path_service'
 
 RSpec.describe TestData::PathService do
+  it_behaves_like 'a service'
+
   describe '.call' do
     context 'with a valid path' do
       path = 'musicbrainz.org/artist/bdacc37b-8633-4bf8-9dd5-4662ee651aec?' \
