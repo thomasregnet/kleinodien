28b43c13cc7ccf31d4aeb79720699ffdac75c42a
Ossi <ossi@example.com>
2018-12-17 21:16:15 +0100

    Remove KoTestData from prep_brz_art_credit_spec

--- spec/importers/prepare_brainz_artist_credit_spec.rb
+++ spec/importers/prepare_brainz_artist_credit_spec.rb
@@ -2,32 +2,25 @@
 
 require 'rails_helper'
 require 'shared_examples_for_services'
-require 'ko_test_data'
+require 'test_data'
 
 RSpec.describe PrepareBrainzArtistCredit do
   it_behaves_like 'a service'
 
   # TODO: Add some specs to PrepareBrainzArtistCredit
   describe '.call' do
-    let(:blueprint) do
-      xml_string = KoTestData::GetBrainzXmlFor.path(
-        'release/7452f8c9-f9bc-3ca7-859e-3220e57e4e4a' \
-          '?inc=artists+labels+recordings+release-groups.xml'
-      )
-      BrainzBlueprint.from_xml(xml_string).artist_credit
+    let(:arise) do
+      TestData.by_name(:brainz_release_arise_jp_cd).blueprint.artist_credit
     end
 
-    let(:artist_blueprint) do
-      xml_string = KoTestData::GetBrainzXmlFor.path(
-        'artist/1d93c839-22e7-4f76-ad84-d27039efc048?inc=url-rels.xml'
-      )
-      BrainzBlueprint.from_xml(xml_string)
+    let(:sepultura) do
+      TestData.by_name(:brainz_artist_sepultura).blueprint
     end
 
     it 'requests the artist' do
       proxy = double # spy
-      args  = { blueprint: blueprint, proxy: proxy }
-      allow(proxy).to receive(:get).and_return(artist_blueprint)
+      args  = { blueprint: arise, proxy: proxy }
+      allow(proxy).to receive(:get).and_return(sepultura)
 
       expect(described_class.call(args)).to be_nil
     end
