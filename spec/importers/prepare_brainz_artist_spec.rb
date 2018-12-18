20db32a85afde876b8f0fc5871f5714dd3035d9d
Ossi <ossi@example.com>
2018-12-18 19:55:32 +0100

    Use import_request arg for PrepareBrainzArtist
    Some specs fail!
    
    PrepareBrainzArtist no longer takes "blueprint" as an argument.
    It expects "import_request".

--- spec/importers/prepare_brainz_artist_spec.rb
+++ spec/importers/prepare_brainz_artist_spec.rb
@@ -6,21 +6,6 @@ require 'test_data'
 
 # rubocop:disable Metrics/BlockLength
 RSpec.describe PrepareBrainzArtist do
-  def discogs_code
-    123
-  end
-
-  def brainz_code
-    'f9ea7be4-ebee-11e8-b919-d7e490fb38cb'
-  end
-
-  def code_for
-    {
-      brainz_code:  brainz_code,
-      discogs_code: discogs_code
-    }
-  end
-
   context 'when the artist already exists in the database' do
     before do
       DatabaseCleaner.start
@@ -36,9 +21,17 @@ RSpec.describe PrepareBrainzArtist do
       TestData.by_name(:brainz_artist_slayer).blueprint
     end
 
+    let(:import_request) do
+      BrainzArtistImportRequest.new(
+        code: 'bdacc37b-8633-4bf8-9dd5-4662ee651aec'
+      )
+    end
+
     it 'returns the artist' do
       proxy = double
-      expect(described_class.call(blueprint: blueprint, proxy: proxy))
+      allow(proxy).to receive(:get).and_return(blueprint)
+
+      expect(described_class.call(import_request: import_request, proxy: proxy))
         .to be_instance_of(Artist)
     end
   end
