caf1f57ce93683d205e197fc9ff56276bbd2192a
Ossi <ossi@example.com>
2019-01-07 20:45:30 +0100

    Use constants in TestData

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
-BRAINZ_ARTIST_QUERY_STRING = 'inc=url-rels'
+BRAINZ_ARTIST_QUERY_STRING = 'inc=artist-rels+url-rels'
 BRAINZ_RELEASE_GROUP_QUERY_STRING = \
   'inc=artists+artist-rels+label-rels+url-rels'
 BRAINZ_RELEASE_QUERY_STRING = 'inc=artists+labels+recordings+release-groups'
+
+BRAINZ_INC_FOR = {
+  artist:        BRAINZ_ARTIST_QUERY_STRING,
+  release:       BRAINZ_RELEASE_QUERY_STRING,
+  release_group: BRAINZ_RELEASE_GROUP_QUERY_STRING
+}.freeze
+
