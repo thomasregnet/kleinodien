6de4972ae504cfe0b8452959693ce622a967a29b
Ossi <ossi@example.com>
2019-01-17 20:55:43 +0100

    Add factory, specs and validations for SetHead

--- spec/models/set_head_spec.rb
+++ spec/models/set_head_spec.rb
@@ -1,5 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe SetHead, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:artist_credit) }
+  it { is_expected.to belong_to(:import_order) }
+  it { is_expected.to belong_to(:season) }
+
+  describe '#title' do
+    let(:head) { FactoryBot.build(:set_head) }
+
+    context 'when nil' do
+      it 'is not valid' do
+        head.title = nil
+        expect(head).not_to be_valid
+      end
+    end
+
+    context 'when nil' do
+      it 'is not valid' do
+        head.title = ''
+        expect(head).not_to be_valid
+      end
+    end
+  end
 end
