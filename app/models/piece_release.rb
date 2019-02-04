d3d712f067a0f14cd4a9238bd7ac8632cf423d08
Ossi <ossi@example.com>
2019-02-04 21:35:10 +0100

    Specs, validations and factories for PieceRelease

--- app/models/piece_release.rb
+++ app/models/piece_release.rb
@@ -1,4 +1,25 @@
+# frozen_string_literal: true
+
+# A released Piece, may resist on a Heap
 class PieceRelease < ApplicationRecord
-  belongs_to :heap
-  belongs_to :import_order
+  belongs_to :heap, required: false
+  belongs_to :import_order, required: false
+  belongs_to :piece
+
+  validate :heap_required_if_position_is_present
+  validate :position_required_if_heap_is_present
+
+  def heap_required_if_position_is_present
+    return unless position.present?
+    return if heap.present?
+
+    errors.add(:position, 'heap must be set when position is present')
+  end
+
+  def position_required_if_heap_is_present
+    return unless heap.present?
+    return if position.present?
+
+    errors.add(:heap, 'position must be set when heap is present')
+  end
 end
--- spec/models/piece_release_spec.rb
+++ spec/models/piece_release_spec.rb
@@ -1,5 +1,50 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe PieceRelease, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:heap) }
+  it { is_expected.to belong_to(:import_order) }
+  it { is_expected.to belong_to(:piece) }
+
+  context 'with valid parameters' do
+    let(:piece_release) { FactoryBot.build(:piece_release) }
+
+    it 'is valid' do
+      expect(piece_release).to be_valid
+    end
+  end
+
+  context 'with a heap and a position' do
+    context 'without a heap' do
+      let(:piece_release) { FactoryBot.build(:piece_release_with_heap) }
+
+      it 'is valid' do
+        expect(piece_release).to be_valid
+      end
+    end
+  end
+
+  describe '#position' do
+    context 'without a heap' do
+      let(:piece_release) { FactoryBot.build(:piece_release_with_heap) }
+
+      it 'is not valid' do
+        piece_release.heap = nil
+        expect(piece_release).not_to be_valid
+      end
+    end
+  end
+
+  describe '#heap' do
+    context 'without a position' do
+      let(:piece_release) { FactoryBot.build(:piece_release_with_heap) }
+
+      it 'is not valid' do
+        piece_release.position = nil
+
+        expect(piece_release).not_to be_valid
+      end
+    end
+  end
 end
