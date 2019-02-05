d07ddc0ba21ea40a8bd230863579b7c328f49328
Ossi <ossi@example.com>
2019-02-05 21:41:28 +0100

    Alter piece_releases
    Removed columns
    - position
    - heap_id
    Added columns
    - accuracy
    - milliseconds

--- app/models/piece_release.rb
+++ app/models/piece_release.rb
@@ -2,24 +2,6 @@
 
 # A released Piece, may resist on a Heap
 class PieceRelease < ApplicationRecord
-  belongs_to :heap, required: false
   belongs_to :import_order, required: false
   belongs_to :piece
-
-  validate :heap_required_if_position_is_present
-  validate :position_required_if_heap_is_present
-
-  def heap_required_if_position_is_present
-    return unless position.present?
-    return if heap.present?
-
-    errors.add(:position, 'heap must be set when position is present')
-  end
-
-  def position_required_if_heap_is_present
-    return unless heap.present?
-    return if position.present?
-
-    errors.add(:heap, 'position must be set when heap is present')
-  end
 end
--- spec/factories/piece_releases.rb
+++ spec/factories/piece_releases.rb
@@ -3,10 +3,5 @@
 FactoryBot.define do
   factory :piece_release do
     association :piece, factory: :piece
-
-    factory :piece_release_with_heap do
-      association :heap, factory: :heap
-      sequence(:position) { |n| "position #{n}" }
-    end
   end
 end
