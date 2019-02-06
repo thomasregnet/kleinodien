83133a53b8e4e4adc97c4a7d6a65abebbde5d60b
Ossi <ossi@example.com>
2019-02-06 21:04:32 +0100

    Add specs, factory and validations for HeapTrack

--- spec/models/heap_track_spec.rb
+++ spec/models/heap_track_spec.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe HeapTrack, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryBot.build(:heap_track) }
+
+  it { is_expected.to belong_to(:heap) }
+  it { is_expected.to belong_to(:import_order) }
+  it { is_expected.to belong_to(:piece) }
+
+  it { is_expected.to validate_presence_of(:heap) }
+  it { is_expected.to validate_presence_of(:piece) }
+  it { is_expected.to validate_presence_of(:position) }
 end
