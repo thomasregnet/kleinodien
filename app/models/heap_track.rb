e98f7af335eb5fd518e2ca5269ee86bc03d9ac04
Ossi <ossi@example.com>
2019-02-06 21:36:13 +0100

    Add "composed_of :duration" to HeapTrack

--- app/models/heap_track.rb
+++ app/models/heap_track.rb
@@ -6,6 +6,11 @@ class HeapTrack < ApplicationRecord
   belongs_to :import_order
   belongs_to :piece
 
+  composed_of(
+    :duration,
+    mapping: [%w[milliseconds milliseconds], %w[accuracy accuracy]]
+  )
+
   validates :heap, presence: true
   validates :piece, presence: true
   validates :position, presence: true, blank: false
--- spec/models/heap_track_spec.rb
+++ spec/models/heap_track_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'shared_examples_for_models_with_duration'
 
 RSpec.describe HeapTrack, type: :model do
-  subject { FactoryBot.build(:heap_track) }
+  # subject { FactoryBot.build(:heap_track) }
 
   it { is_expected.to belong_to(:heap) }
   it { is_expected.to belong_to(:import_order) }
@@ -12,4 +13,6 @@ RSpec.describe HeapTrack, type: :model do
   it { is_expected.to validate_presence_of(:heap) }
   it { is_expected.to validate_presence_of(:piece) }
   it { is_expected.to validate_presence_of(:position) }
+
+  it_behaves_like 'a model with duration'
 end
