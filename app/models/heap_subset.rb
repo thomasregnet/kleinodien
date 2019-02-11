d264ed069e8c4a8a39a50301e5823da785df7332
Ossi <ossi@example.com>
2019-02-11 21:04:06 +0100

    Add specs and validations for HeapSubset

--- app/models/heap_subset.rb
+++ app/models/heap_subset.rb
@@ -1,3 +1,7 @@
 class HeapSubset < ApplicationRecord
   belongs_to :heap
+
+  has_many :tracks, class_name: 'HeapTrack'
+
+  validates :no, presence: true, blank: false
 end
--- spec/models/heap_subset_spec.rb
+++ spec/models/heap_subset_spec.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe HeapSubset, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:heap) }
+  it { is_expected.to have_many(:tracks) }
+  it { is_expected.to validate_presence_of(:no) }
 end
