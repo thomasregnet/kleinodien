dfc303ac729242668e69e82e2c41bcfa2a405853
Ossi <ossi@example.com>
2019-02-12 21:32:22 +0100

    HeapTrack#import_order is not required

--- app/models/heap_track.rb
+++ app/models/heap_track.rb
@@ -3,7 +3,7 @@
 # Piece that resides on a Heap
 class HeapTrack < ApplicationRecord
   belongs_to :subset, class_name: 'HeapSubset', foreign_key: :heap_subset_id
-  belongs_to :import_order
+  belongs_to :import_order, required: false
   belongs_to :piece
 
   composed_of(
