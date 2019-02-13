991996f73d4f72b48bb192531a9c4f037c1bb13d
Ossi <ossi@example.com>
2019-02-13 20:18:15 +0100

    Beautify persit_brainz_heap_track_spec.rb

--- spec/importers/persist_brainz_heap_track_spec.rb
+++ spec/importers/persist_brainz_heap_track_spec.rb
@@ -3,17 +3,18 @@
 require 'rails_helper'
 require 'test_data'
 
+# Proxy for testing
 class FakePersistBrainzHeapTrackProxy
   def get(_)
     TestData.by_name(:brainz_recording_arise).blueprint
   end
 end
+
 RSpec.describe PersistBrainzHeapTrack do
-
   context 'with valid arguments' do
+    blueprint = TestData.by_name(:brainz_release_arise_jp_cd)
+                        .blueprint.media[0].track_list.track[0]
 
-    blueprint = TestData.by_name(:brainz_release_arise_jp_cd).blueprint
-    blueprint = blueprint.media[0].track_list.track[0]
     args = {
       blueprint: blueprint,
       no:        3,
