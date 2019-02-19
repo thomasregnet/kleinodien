4f6128cb850de9b8fec50f36341c6e5b2010840f
Ossi <ossi@example.com>
2019-02-19 20:04:45 +0100

    Add attribute PersistBase#import_order
    Currently only used by PersistBrainzPiece

--- app/importers/persist_base.rb
+++ app/importers/persist_base.rb
@@ -2,4 +2,10 @@
 
 # Base class for persisting classes
 class PersistBase < PersistPrepareBase
+  def initialize(args)
+    super(args)
+    @import_order = args[:import_order]
+  end
+
+  attr_reader :import_order
 end
