0eb560df9646e93ac1b4506772b0b86a812e07b8
Ossi <ossi@example.com>
2019-02-20 22:17:06 +0100

    Remove RepositoryFormatDetail

--- app/models/repository_format_detail.rb
+++
@@ -1,7 +0,0 @@
-# More precise information for repository formats
-class RepositoryFormatDetail < ApplicationRecord
-  belongs_to :repository, inverse_of: :format_details
-  belongs_to :detail,
-             class_name: 'FormatDetail',
-             foreign_key: :format_detail_id
-end
