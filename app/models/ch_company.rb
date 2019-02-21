a1a8498eb7b7c0cf941b87c05fb4fbf3852befba
Ossi <ossi@example.com>
2019-02-21 20:30:50 +0100

    Let test pass
    Removed models
    * CompilationHead
    * ChCompany
    * ChCredit
    * ChLabel

--- app/models/ch_company.rb
+++
@@ -1,6 +0,0 @@
-# Companies involved in a CompilationHead
-class ChCompany < ActiveRecord::Base
-  belongs_to :compilation_head
-  belongs_to :company
-  belongs_to :company_role
-end
--- app/models/ch_credit.rb
+++
@@ -1,6 +0,0 @@
-# ArtistCredit of a CompilationHead
-class ChCredit < ActiveRecord::Base
-  belongs_to :artist_credit
-  belongs_to :compilation_head
-  belongs_to :job, required: false
-end
