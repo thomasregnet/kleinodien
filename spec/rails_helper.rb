0336e9d23b6bd6dfd8ef04af5f9acc0c6f75f0f1
Ossi <ossi@example.com>
2019-02-21 21:16:51 +0100

    Exclude lib/generators from coverage
    Simplecov ignores lib/generators.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+
 require 'simplecov'
-SimpleCov.start('rails')
+SimpleCov.start 'rails' do
+  # exclude lib/generators from coverage
+  add_filter 'lib/generators/'
+end
+
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
