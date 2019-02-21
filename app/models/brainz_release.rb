b338f7cde0a8673037aa646002a1b70010af3af5
Ossi <ossi@example.com>
2019-02-21 20:49:33 +0100

    Remove Brainz and BrainzRelease

--- app/models/brainz_release.rb
+++
@@ -1,6 +0,0 @@
-# Caches MusicBrainz data of releases
-class BrainzRelease < ApplicationRecord
-  validates :mbid, presence: true
-  validates :url,  presence: true, blank: false
-  validates :data, presence: true, blank: false
-end
