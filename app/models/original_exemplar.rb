941dc9b4b8e60d8871adfbcd3dc7b24795210706
Ossi <ossi@example.com>
2019-02-21 20:56:55 +0100

    Delete unused classes and modules from app/models

--- app/models/original_exemplar.rb
+++
@@ -1,7 +0,0 @@
-# An original in an users collection
-class OriginalExemplar < ApplicationRecord
-  belongs_to :compilation_release
-  belongs_to :user
-
-  validates :disambiguation, presence: true
-end
--- app/models/persist.rb
+++
@@ -1,6 +0,0 @@
-# Classes responsible to persist imports
-module Persist
-  # see also
-  # - import
-  # - prepare
-end
