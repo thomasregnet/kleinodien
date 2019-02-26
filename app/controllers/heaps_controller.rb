a45e1acd0840642bdf1c9498aff83105124e8de5
Ossi <ossi@example.com>
2019-02-26 11:41:20 +0100

    Show heap

--- app/controllers/heaps_controller.rb
+++ app/controllers/heaps_controller.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
+# Controller for Heaps, Albums, Singles...
 class HeapsController < ApplicationController
   def index
     @heaps = Heap.all
   end
 
   def show
+    @heap = Heap.find(params[:id])
   end
 end
--- spec/controllers/heaps_controller_spec.rb
+++ spec/controllers/heaps_controller_spec.rb
@@ -1,19 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe HeapsController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
+  describe 'GET #show' do
+    let(:heap) { FactoryBot.create(:heap) }
+
+    it 'returns http success' do
+      get :show, params: { id: heap.id }
       expect(response).to have_http_status(:success)
     end
   end
-
 end
