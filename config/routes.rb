773fc03539678b817e0588ff1edc93fc0643aa44
Ossi <ossi@example.com>
2019-02-26 10:52:55 +0100

    Generate controller Heaps
    $(rails generate controller Heaps index show)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'heaps/index'
+  get 'heaps/show'
   # get 'import_orders/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'import_requests/new'
