b78426b33fda964bebce1749666c90008dc0c596
Ossi <ossi@example.com>
2019-02-27 11:07:12 +0100

    Beautify FlattenBrainzTrackListService

--- app/services/flatten_brainz_track_list_service.rb
+++ app/services/flatten_brainz_track_list_service.rb
@@ -20,12 +20,14 @@ class FlattenBrainzTrackListService
 
   def data_track_list
     return unless data_track_list?
+
     blueprint.data_track_list.track
   end
 
   def data_track_list?
-    return false unless blueprint.data_track_list
-    return false unless blueprint.data_track_list.track
+    data_tl = blueprint.data_track_list
+    return false unless data_tl
+    return false unless data_tl.track
 
     true
   end
