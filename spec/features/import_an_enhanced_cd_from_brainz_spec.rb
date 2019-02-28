4ae5fb46c67167bb0b7ccc7e91b6a754ab30f251
Ossi <ossi@example.com>
2019-02-28 10:50:16 +0100

    Inspect the page when importing an Enhanced CD

--- spec/features/import_an_enhanced_cd_from_brainz_spec.rb
+++ spec/features/import_an_enhanced_cd_from_brainz_spec.rb
@@ -3,24 +3,44 @@
 require 'rails_helper'
 
 RSpec.feature 'import an enhanced CD from MusicBrainz', type: :feature do
-  describe 'import' do
-    before { DatabaseCleaner.start }
+  # rubocop:disable RSpec/BeforeAfterAll
+  # rubocop:disable RSpec/InstanceVariable
+  before(:all) do
+    DatabaseCleaner.start
+    WebMock.stub_request(:any, /musicbrainz.org/).to_rack(FakeMusicBrainz)
 
-    after { DatabaseCleaner.clean }
+    # Create an ImportOrder for "Iron Maiden - Powerslave"
+    import_order = BrainzImportOrder.create!(
+      code:  '58e6a3d6-bbbd-4864-983b-e468a5a1a71c',
+      kind:  'release',
+      state: 'pending',
+      user:  FactoryBot.create(:user)
+    )
 
-    let(:import_order) do
-      # Create an ImportOrder for "Iron Maiden - Powerslave"
-      BrainzImportOrder.create!(
-        code:  '58e6a3d6-bbbd-4864-983b-e468a5a1a71c',
-        kind:  'release',
-        state: 'pending',
-        user:  FactoryBot.create(:user)
-      )
+    @result = BrainzRootImporter.call(import_order: import_order)
+  end
+
+  after(:all) { DatabaseCleaner.clean }
+  # rubocop:enable RSpec/BeforeAfterAll
+
+  before { visit(album_path(@result.id)) }
+  # rubocop:enable RSpec/InstanceVariable
+
+  it 'contains the title' do
+    expect(page).to have_content('Powerslave')
+  end
+
+  it 'contains the band-name' do
+    expect(page).to have_content('Iron Maiden')
+  end
+
+  describe 'pieces' do
+    it 'contains "Aces High"' do
+      expect(page).to have_content('Aces High')
     end
 
-    scenario 'run the import' do
-      result = BrainzRootImporter.call(import_order: import_order)
-      expect(result).to be_created
+    it 'content "2 Minutes to Midnight (video)"' do
+      expect(page).to have_content('2 Minutes to Midnight (video)')
     end
   end
 end
