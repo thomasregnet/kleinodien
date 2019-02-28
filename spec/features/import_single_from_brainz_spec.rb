b40d3290f038ce30ea2e2caca47cee078aa4a976
Ossi <ossi@example.com>
2019-02-28 10:25:47 +0100

    Inspect the page when importing a single
    Now visiting the page of "Sepultura - Arise" to check if the import succeeded.

--- spec/features/import_single_from_brainz_spec.rb
+++ spec/features/import_single_from_brainz_spec.rb
@@ -3,23 +3,37 @@
 require 'rails_helper'
 
 RSpec.feature 'import a single from MusicBrainz', type: :feature do
-  describe 'import' do
-    before { DatabaseCleaner.start }
+  # rubocop:disable RSpec/BeforeAfterAll
+  # rubocop:disable RSpec/InstanceVariable
+  before(:all) do
+    DatabaseCleaner.start
+    WebMock.stub_request(:any, /musicbrainz.org/).to_rack(FakeMusicBrainz)
 
-    after { DatabaseCleaner.clean }
+    import_order = BrainzImportOrder.create!(
+      code:  '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a', # Sepultura - Arise
+      kind:  'release',
+      state: 'pending',
+      user:  FactoryBot.create(:user)
+    )
 
-    let(:import_order) do
-      BrainzImportOrder.create!(
-        code:  '7452f8c9-f9bc-3ca7-859e-3220e57e4e4a', # Sepultura - Arise
-        kind:  'release',
-        state: 'pending',
-        user:  FactoryBot.create(:user)
-      )
-    end
+    @result = BrainzRootImporter.call(import_order: import_order)
+  end
 
-    scenario 'import' do
-      result = BrainzRootImporter.call(import_order: import_order)
-      expect(result).to be_created
-    end
+  after(:all) { DatabaseCleaner.clean }
+  # rubocop:enable RSpec/BeforeAfterAll
+
+  before { visit(single_path(@result.id)) }
+  # rubocop:enable RSpec/InstanceVariable
+
+  it 'contains the title' do
+    expect(page).to have_text('Arise')
+  end
+
+  it 'contains the band-name' do
+    expect(page).to have_text('Sepultura')
+  end
+
+  it 'contains "Troops of Doom (live)"' do
+    expect(page).to have_content('Troops of Doom (live)')
   end
 end
