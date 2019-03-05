eaaec510928450c757a179cb31b160b3dde0a872
Ossi <ossi@example.com>
2019-03-05 10:21:24 +0100

    Generate model HeapMedium
    $(rails generate model HeapMedium position:integer \
                                      quantity:integer \
                                      medium_format:references)

---
+++ db/migrate/20190305091751_create_heap_media.rb
@@ -0,0 +1,11 @@
+class CreateHeapMedia < ActiveRecord::Migration[5.2]
+  def change
+    create_table :heap_media do |t|
+      t.integer :position, limit: 2, null: false
+      t.integer :quantity, limit: 2, null: false
+      t.references :medium_format, foreign_key: true, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/models/heap_medium_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe HeapMedium, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
