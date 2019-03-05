ad24788e22548381c56d85c7cf5dfc60db023ea1
Ossi <ossi@example.com>
2019-03-05 10:32:23 +0100

    Add heap:references to HeapMedium

--- db/migrate/20190305091751_create_heap_media.rb
+++ db/migrate/20190305091751_create_heap_media.rb
@@ -3,6 +3,7 @@ class CreateHeapMedia < ActiveRecord::Migration[5.2]
     create_table :heap_media do |t|
       t.integer :position, limit: 2, null: false
       t.integer :quantity, limit: 2, null: false
+      t.references :heap, foreign_key: true, null: false
       t.references :medium_format, foreign_key: true, null: false
 
       t.timestamps
--- spec/models/heap_medium_spec.rb
+++ spec/models/heap_medium_spec.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe HeapMedium, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to(:position) }
+  it { is_expected.to respond_to(:quantity) }
+  it { is_expected.to respond_to(:medium_format) }
+  it { is_expected.to respond_to(:heap) }
 end
