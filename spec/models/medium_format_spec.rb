128241c21af4cb2917a8c78f796c91f4dd6d42b2
Ossi <ossi@example.com>
2019-03-05 10:43:20 +0100

    MediumFormat has_many :heap_media

--- spec/models/medium_format_spec.rb
+++ spec/models/medium_format_spec.rb
@@ -11,5 +11,7 @@ RSpec.describe MediumFormat, type: :model do
 
   it { is_expected.to belong_to(:import_order).without_validating_presence }
 
+  it { is_expected.to have_many(:heap_media) }
+
   it { is_expected.to validate_presence_of(:name) }
 end
