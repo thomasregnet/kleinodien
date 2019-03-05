3bac74c5d1bb1cf9c407ffa2063cd63ae19d6847
Ossi <ossi@example.com>
2019-03-05 10:43:20 +0100

    MediumFormat has_many :heap_media

--- app/models/medium_format.rb
+++ app/models/medium_format.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
+# The kind of a medium
 class MediumFormat < ApplicationRecord
   belongs_to :import_order, required: false
+
+  has_many :heap_media
 
   validates :name, presence: true
 end
