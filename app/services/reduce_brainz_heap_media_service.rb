b20308ce6a0f5af1b380bf2ff4bccdfe2a8fc376
Ossi <ossi@example.com>
2019-03-06 10:18:49 +0100

    Start to implement ReduceBrainzHeapMediaService

--- app/services/reduce_brainz_heap_media_service.rb
+++ app/services/reduce_brainz_heap_media_service.rb
@@ -1,17 +1,42 @@
 # frozen_string_literal: true
 
+# Reduce Media to it's formats
 class ReduceBrainzHeapMediaService
-  include CallWithArgs
-
-  private
-
-  # attr_reader
-
-  def initialize(args)
-
+  def self.call(args)
+    new(args).call
   end
 
-  def private_call
-    # the code for the service belongs here
+  def initialize(args)
+    @blueprint    = args[:blueprint]
+    @import_order = args[:import_order]
+    @last_code    = ''
+    @result       = []
+  end
+
+  attr_reader :blueprint, :import_order, :last_code, :result
+
+  def call
+    blueprint.each do |medium|
+      brainz_format_code = medium.format.brain_code
+      if brainz_format_code == last_code
+        result.last[:quantity] += 1
+      else
+        find_or_create_medium_format(medium)
+      end
+    end
+
+    result
+  end
+
+  def find_or_create_medium_format(medium)
+    medium_format = medium.format
+
+    # TODO: use find_or_create
+    result << MediumFormat.find_by(brainz_code: medium_format.id)
+    {
+      medium_format: medium_format,
+      name:          medium_format.__content__,
+      quantity:      1
+    }
   end
 end
--- spec/services/reduce_brainz_heap_media_service_spec.rb
+++ spec/services/reduce_brainz_heap_media_service_spec.rb
@@ -2,9 +2,30 @@
 
 require 'rails_helper'
 require 'shared_examples_for_services'
+require 'test_data'
 
 RSpec.describe ReduceBrainzHeapMediaService do
   it_behaves_like 'a service'
 
-  # the specs for the service belongs here
+  # TODO: do this with a double album
+  describe '.call' do
+    let(:blueprint) do
+      TestData.by_name(:brainz_release_powerslave_enhanced_cd)
+              .blueprint.media
+    end
+
+    # let(:heap) { FactoryBot.create(:heap) }
+    let(:import_order) { FactoryBot.create(:brainz_import_order) }
+
+    it 'returns the created entries' do
+      args = {
+        blueprint:    blueprint,
+        import_order: import_order
+      }
+      # described_class.call(args)
+      # expect(heap.media[0].format.name).to eq('Enhanced CD')
+      expect(described_class.call(args).first.name)
+        .to eq('Enhanced CD')
+    end
+  end
 end
