5806e229eb2fb251c0c146453228a1b105026e9d
Ossi <ossi@example.com>
2019-03-25 22:29:31 +0100

    Implement PublishImportOrderService

--- app/services/publish_import_order_service.rb
+++ app/services/publish_import_order_service.rb
@@ -2,13 +2,28 @@
 
 class PublishImportOrderService < ServiceBase
   def initialize(args)
-
+    @import_order_type = args[:import_order_type]
   end
 
-  # attr_reader
-
+  attr_reader :import_order_type
 
   def call
-    # the code for the service belongs here
+    connection.start
+    exchange.publish('run', routing_key: import_order_type)
+  end
+
+  private
+
+  def exchange
+    channel.direct('import_orders')
+  end
+
+  def channel
+    @channel ||= connection.create_channel
+  end
+
+  def connection
+    # TODO: get RabbitMQ host name by configuration
+    @connection ||= Bunny.new(host: 'rabbit')
   end
 end
