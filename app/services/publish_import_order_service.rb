5db55103925f866793ed06a408693e298e3bcb5c
Ossi <ossi@example.com>
2019-03-25 21:01:17 +0100

    Generate service PublishImportOrderService

---
+++ app/services/publish_import_order_service.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class PublishImportOrderService < ServiceBase
+  def initialize(args)
+
+  end
+
+  # attr_reader
+
+
+  def call
+    # the code for the service belongs here
+  end
+end
