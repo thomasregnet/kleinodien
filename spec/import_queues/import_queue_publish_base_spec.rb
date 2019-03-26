d910b4f431b9db37fc774942f6fed99d4d0da7d7
Ossi <ossi@example.com>
2019-03-26 22:32:53 +0100

    Add more specs for ImportQueuePublishBase

--- spec/import_queues/import_queue_publish_base_spec.rb
+++ spec/import_queues/import_queue_publish_base_spec.rb
@@ -2,17 +2,64 @@
 
 require 'rails_helper'
 
+# Just for test
+class FakeImportQueueConnection
+  attr_reader :close, :start
+end
+
 # Just for testing
-class FakeImpotQueuePublishBase < ImportQueuePublishBase
-  name :fake_import_queue
+# this class smells of :reek:InstanceVariableAssumption
+class FakeImportQueueExchange
+  def publish(message)
+    @message = message
+  end
+
+  def published_message
+    @message
+  end
+end
+
+# Just for testing
+class FakeImportQueuePublishBase < ImportQueuePublishBase
+  name :fake
+
+  def initialize(args)
+    super(args)
+
+    @connection = FakeImportQueueConnection.new
+    @exchange   = FakeImportQueueExchange.new
+  end
+
+  attr_reader :connection, :exchange
+end
+
+# Just for testing
+class FakeImportQueuePublishBaseCall < ImportQueuePublishBase
+  def call
+    "called with \"#{message}\""
+  end
 end
 
 RSpec.describe ImportQueuePublishBase do
   describe '#call' do
-    let(:publisher) { described_class.new('my message') }
+    let(:publisher) { FakeImportQueuePublishBase.new(message: 'my message') }
 
     it 'publishes the message' do
-      
+      publisher.call
+      expect(publisher.exchange.published_message).to eq('my message')
+    end
+  end
+
+  describe '.call' do
+    it 'publishes the message' do
+      expect(FakeImportQueuePublishBaseCall.call(message: 'my message'))
+        .to eq('called with "my message"')
+    end
+  end
+
+  describe '.run' do
+    it 'publishes "run"' do
+      expect(FakeImportQueuePublishBaseCall.run).to eq('called with "run"')
     end
   end
 end
