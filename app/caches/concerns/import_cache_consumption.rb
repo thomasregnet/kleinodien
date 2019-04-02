47c1fed19960afd411ccc4e324da653e082333f5
Ossi <ossi@example.com>
2019-04-02 21:10:14 +0200

    Remove caches
    All tests pass!

--- app/caches/concerns/import_cache_consumption.rb
+++
@@ -1,7 +0,0 @@
-module ImportCacheConsumption
-  extend ActiveSupport::Concern
-
-  def import_cache
-    @import_cache ||= ImportCache.new
-  end
-end
--- app/caches/import_cache.rb
+++
@@ -1,6 +0,0 @@
-class ImportCache
-  include ImportStore
-  include RedisCache
-
-  redis import_store
-end
