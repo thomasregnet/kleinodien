bb835569f50ede6e0b761eb2361d0c16d489488d
Ossi <ossi@example.com>
2019-04-04 21:06:26 +0200

    Implement ImportOrder.next_pending

--- spec/models/import_order_spec.rb
+++ spec/models/import_order_spec.rb
@@ -3,6 +3,15 @@
 require 'rails_helper'
 require 'shared_examples_for_import_orders'
 
+# just for testing
+class FakeImportOrder < ImportOrder
+end
+
+# just for testing
+class OtherFakeImportOrder < ImportOrder
+end
+
+# rubocop:disable Metrics/BlockLength
 RSpec.describe ImportOrder, type: :model do
   include_examples 'for ImportOrders', :import_order
 
@@ -15,6 +24,46 @@ RSpec.describe ImportOrder, type: :model do
   it { is_expected.to have_many(:piece_heads) }
   it { is_expected.to have_many(:pieces) }
 
+  describe '.next_pending' do
+    context 'when no ImportOrder exists' do
+      it 'returns nil' do
+        expect(described_class.next_pending).to be_nil
+      end
+    end
+
+    context 'when an ImportOrder exist' do
+      before do
+        FactoryBot.create(:import_order, type: 'FakeImportOrder')
+      end
+
+      it 'returns that order' do
+        expect(FakeImportOrder.next_pending).to be_instance_of(FakeImportOrder)
+      end
+    end
+
+    context 'when another ImportOrder exists' do
+      before do
+        FactoryBot.create(:import_order, type: 'OtherFakeImportOrder')
+        FactoryBot.create(:import_order, type: 'FakeImportOrder')
+      end
+
+      it 'returns that order' do
+        expect(FakeImportOrder.next_pending)
+          .to be_instance_of(FakeImportOrder)
+      end
+    end
+
+    context 'when only another ImportOrder exists' do
+      before do
+        FactoryBot.create(:import_order, type: 'OtherFakeImportOrder')
+      end
+
+      it 'returns that order' do
+        expect(FakeImportOrder.next_pending).to be_nil
+      end
+    end
+  end
+
   it 'has a counter_cache for import_requests' do
     request_args = {
       type: 'BrainzArtistImportRequest',
@@ -25,3 +74,4 @@ RSpec.describe ImportOrder, type: :model do
     expect(import_order.requests_count).to eq(1)
   end
 end
+# rubocop:enable Metrics/BlockLength
