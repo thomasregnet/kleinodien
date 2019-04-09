3037ad91affde9b5c735d9ffe3565d1495c454ea
Ossi <ossi@example.com>
2019-04-09 20:37:54 +0200

    Implement DeleiverImportOrderService

--- app/services/deliver_import_order_service.rb
+++ app/services/deliver_import_order_service.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
+# Calls the right importer for an ImportOrder
 class DeliverImportOrderService < ServiceBase
   def initialize(args)
-
+    @import_order = args[:import_order]
   end
 
-  # attr_reader
-
+  attr_reader :import_order
 
   def call
-    # the code for the service belongs here
+    importer_class.call(import_order: import_order)
+  end
+
+  private
+
+  def importer_class
+    import_order_class_name = import_order.class.to_s
+    class_name = import_order_class_name.gsub(/ImportOrder$/, 'Importer')
+    class_name.constantize
   end
 end
--- spec/services/deliver_import_order_service_spec.rb
+++ spec/services/deliver_import_order_service_spec.rb
@@ -3,8 +3,39 @@
 require 'rails_helper'
 require 'shared_examples_for_services'
 
+# Just for testing
+class FakeImportOrder < ImportOrder
+end
+
+# Just for testing
+class FakeImporter
+  def self.call(_args); end
+end
+
 RSpec.describe DeliverImportOrderService do
   it_behaves_like 'a service'
 
-  # the specs for the service belongs here
+  describe '#importer_class' do
+    let(:deliverer) do
+      described_class.new(import_order: FakeImportOrder.new)
+    end
+
+    it 'returns the right importer class as constant' do
+      expect(deliverer.send(:importer_class)).to eq(FakeImporter)
+    end
+  end
+
+  describe '#call' do
+    let(:import_order) { FakeImportOrder.new }
+
+    # rubocop:disable RSpec/MessageSpies
+    it 'calls the importer' do
+      importer = class_double('FakeImporter').as_stubbed_const
+
+      expect(importer).to receive(:call).with(import_order: import_order)
+      deliverer = described_class.new(import_order: import_order)
+      deliverer.call
+    end
+    # rubocop:enable RSpec/MessageSpies
+  end
 end
