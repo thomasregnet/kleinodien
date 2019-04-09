f8171771f96fa18af74dd6ef00d5d1a19ebd6f57
Ossi <ossi@example.com>
2019-04-09 19:51:01 +0200

    Generate DeliverImportOrderService

---
+++ app/services/deliver_import_order_service.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class DeliverImportOrderService < ServiceBase
+  def initialize(args)
+
+  end
+
+  # attr_reader
+
+
+  def call
+    # the code for the service belongs here
+  end
+end
