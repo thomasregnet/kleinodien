454539fa846fe42cac079b8403e05b10a17d53b7
Ossi <ossi@example.com>
2019-04-30 21:32:17 +0200

    Implement ImportBrainzBase#import_request

--- spec/importers/import_brainz_base_spec.rb
+++ spec/importers/import_brainz_base_spec.rb
@@ -3,6 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe ImportBrainzBase do
+  describe 'import_request' do
+    let(:importer) { described_class.new({}) }
+
+    # rubocop:disable RSpec/MessageSpies
+    it 'returns an ImportRequest' do
+      transformer = class_double('TransformBrainzOrderToRequestService')
+                    .as_stubbed_const(transfer_nested_constants: true)
+
+      expect(transformer).to receive(:call).with(import_order: nil)
+      importer.import_request
+    end
+  end
+  # rubocop:enable RSpec/MessageSpies
+
   describe '#proxy' do
     let(:importer) { described_class.new({}) }
 
