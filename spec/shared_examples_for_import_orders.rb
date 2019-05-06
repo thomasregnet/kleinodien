7dc887b54ab6ab7a693170ddf942ebdf7ab50434
Ossi <ossi@example.com>
2019-05-06 22:06:54 +0200

    Move ImportOrder#state transition specs
    ... to shared_examples_for_import_orders.rb

--- spec/shared_examples_for_import_orders.rb
+++ spec/shared_examples_for_import_orders.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
 RSpec.shared_examples 'for ImportOrders' do |model|
   context 'with valid parameters' do
     let(:import_order) { FactoryBot.build(model) }
@@ -26,9 +27,9 @@ RSpec.shared_examples 'for ImportOrders' do |model|
   end
 
   describe '#state' do
-    context 'pending, processing, done, failed' do
+    context 'when pending, processing, done, failed' do
       let(:import_order) { FactoryBot.build(model) }
-      let(:valid_states) { %w(pending processing done failed) }
+      let(:valid_states) { %w[pending processing done failed] }
 
       it 'is valid' do
         expect(import_order)
@@ -36,7 +37,7 @@ RSpec.shared_examples 'for ImportOrders' do |model|
       end
     end
 
-    context 'invalid' do
+    context 'with an invalid value' do
       let(:import_order) { FactoryBot.build(model, state: 'invalid') }
 
       it 'is not valid' do
@@ -44,7 +45,7 @@ RSpec.shared_examples 'for ImportOrders' do |model|
       end
     end
 
-    context 'nil' do
+    context 'when nil' do
       let(:import_order) { FactoryBot.build(model, state: nil) }
 
       it 'is not valid' do
@@ -60,5 +61,96 @@ RSpec.shared_examples 'for ImportOrders' do |model|
       expect(import_order).not_to be_valid
     end
   end
-end
 
+  describe 'state transitions' do
+    context 'when pending' do
+      let(:import_order) do
+        FactoryBot.create(:import_order, type: 'FakeImportOrder')
+      end
+
+      it 'transits to processing' do
+        expect(import_order)
+          .to transition_from(:pending).to(:processing).on_event(:process)
+      end
+
+      it 'does not transit to done' do
+        expect(import_order)
+          .not_to allow_event(:done)
+      end
+
+      it 'does not transit to failed' do
+        expect(import_order)
+          .not_to allow_event(:failed)
+      end
+    end
+
+    context 'when processing' do
+      let(:import_order) do
+        FactoryBot.create(
+          :import_order,
+          type:  'FakeImportOrder',
+          state: :processing
+        )
+      end
+
+      it 'transits to done' do
+        expect(import_order)
+          .to transition_from(:processing).to(:done).on_event(:done)
+      end
+
+      it 'transits to failed' do
+        expect(import_order)
+          .to transition_from(:processing).to(:failed).on_event(:failure)
+      end
+
+      it 'does not transit to pending' do
+        expect(import_order).not_to allow_event(:pending)
+      end
+    end
+
+    context 'when done' do
+      let(:import_order) do
+        FactoryBot.create(
+          :import_order,
+          type:  'FakeImportOrder',
+          state: :done
+        )
+      end
+
+      it 'does not transit to pending' do
+        expect(import_order).not_to allow_event(:pending)
+      end
+
+      it 'does not transit to processing' do
+        expect(import_order).not_to allow_event(:processing)
+      end
+
+      it 'does not transit to failed' do
+        expect(import_order).not_to allow_event(:failed)
+      end
+    end
+
+    context 'when failed' do
+      let(:import_order) do
+        FactoryBot.create(
+          :import_order,
+          type:  'FakeImportOrder',
+          state: :done
+        )
+      end
+
+      it 'does not transit to pending' do
+        expect(import_order).not_to allow_event(:pending)
+      end
+
+      it 'does not transit to processing' do
+        expect(import_order).not_to allow_event(:processing)
+      end
+
+      it 'does not transit to done' do
+        expect(import_order).not_to allow_event(:done)
+      end
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength
