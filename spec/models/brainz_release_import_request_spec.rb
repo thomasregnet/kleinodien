8bb37677754594da8304c65aba88dfd7d18883ff
Ossi <ossi@example.com>
2019-05-07 22:05:26 +0200

    Transition specs for Brainz*ImportRequest classes

--- spec/models/brainz_release_import_request_spec.rb
+++ spec/models/brainz_release_import_request_spec.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'shared_examples_for_import_requests'
-require 'shared_examples_for_code_uuid_validations'
 
+require 'shared_examples_for_code_uuid_validations'
+require 'shared_examples_for_import_requests'
+require 'shared_examples_for_import_state_transitions'
+
+# rubocop:disable Metrics/BlockLength
 RSpec.describe BrainzReleaseImportRequest, type: :model do
+  it_behaves_like(
+    'an import state transitions capable object',
+    :brainz_release_import_request
+  )
+
   include_examples 'for ImportRequests', :brainz_release_import_request
   include_examples(
     'for code fields that must be an uuid',
@@ -34,3 +42,4 @@ RSpec.describe BrainzReleaseImportRequest, type: :model do
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
