8493e9dc88275b7ae2001dbe23aa7c2cc3ebc230
Ossi <ossi@example.com>
2019-05-15 20:21:18 +0200

    Generate CalculateBrainzImportInterruptionService

---
+++ app/services/calculate_brainz_import_interruption_service.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Calculate interruption time to comply with MusicBrainz rate limit
+# https://musicbrainz.org/doc/XML_Web_Service/Rate_Limiting
+class CalculateBrainzImportInterruptionService < ServiceBase
+  def initialize(args)
+
+  end
+
+  # attr_reader
+
+
+  def call
+    # the code for the service belongs here
+  end
+end
