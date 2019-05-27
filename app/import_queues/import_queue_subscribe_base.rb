f318d6b9a968c7ca124bd1116f6d655167c4cbc8
Ossi <ossi@example.com>
2019-05-27 21:26:27 +0200

    Remove app/import_queues

--- app/import_queues/import_queue_subscribe_base.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Subscribe to an import queue
-class ImportQueueSubscribeBase < ImportQueueBase
-  define_singleton_method(:importer_class) do |value|
-    define_method(:importer_class) { value.to_s }
-  end
-end
