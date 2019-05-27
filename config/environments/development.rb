42b0c1e99e2779e88466c0ba88d99834a1687919
Ossi <ossi@example.com>
2019-05-27 19:45:26 +0200

    Initialize development Logger

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Ossi 2019-05-23
+  config.logger = Logger.new(STDOUT)
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
