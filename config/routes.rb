481a19e2ae4e83496b21faec4e47ac420b23b811
Ossi <ossi@example.com>
2019-08-19 21:39:13 +0200

    Restore old config/routes.rb
    Still errors, but less.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,54 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'heaps/index'
+  # get 'heaps/show'
+
+  # get 'import_orders/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'import_requests/new'
+
+  # get 'import_requests/create'
+
+  devise_for :users
+
+  resources :compilation_copies, only: [:create, :edit, :index, :new, :show]
+
+  resources :album_heads, only: [:index, :show]
+
+  resources :album_releases, only: [:index, :show]
+
+  resources :uri_import_orders, only: [:create, :new]
+
+  # https://stackoverflow.com/questions/5246767/sti-one-controller
+  resources :heaps, only: [:index, :show]
+  resources :albums, controller: 'heaps', type: 'Album'
+  resources :singles, controller: 'heaps', type: 'Single'
+
+  resources :movie_heads, only: [:index, :show]
+
+  resources :original_exemplars
+
+  resources :repositories
+
+  resources :import_requests, only: [:create, :new]
+
+  get 'pages/index'
+
+  root to: 'pages#index'
+  namespace :api do
+    namespace :import do
+      resources :brainz_releases, only: [:create]
+    end
+    namespace :v01 do
+      resources :brainz_releases, only: [:create]
+      jsonapi_resources :artist_credits
+      jsonapi_resources :artists
+      jsonapi_resources :compilation_heads
+      jsonapi_resources :episode_heads
+      jsonapi_resources :movie_heads
+      jsonapi_resources :participants
+      jsonapi_resources :piece_heads
+      jsonapi_resources :song_heads
+      jsonapi_resources :sources
+    end
+  end
 end
