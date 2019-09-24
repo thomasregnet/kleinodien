2b9be9a4cdfb8a434dd265f2d766c9fc55e7044b
Ossi <ossi@example.com>
2019-09-24 09:00:13 +0200

    Remove redundant methods from ImportBrainzRelease
    The methods are already defined in ImportBase.

--- app/importers/import_brainz_release.rb
+++ app/importers/import_brainz_release.rb
@@ -2,23 +2,15 @@
 
 # Import a Release from MusicBrainz
 class ImportBrainzRelease < ImportBrainzBase
-  def call
-    validate_import_order
-    super
-  end
-
   def find_already_existing
     Release.find_by(brainz_code: import_request.code)
   end
 
+  # OPTIMIZE: move #prepare to ImportBase?
   def prepare
     PrepareBrainzRelease.call(
       blueprint: blueprint,
       proxy:     proxy
     )
-  end
-
-  def validate_import_order
-    raise ArgumentError, 'invalid ImportOrder' unless import_order.valid?
   end
 end
