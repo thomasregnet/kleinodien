6a5718b38755bc1d822b339e53cdc8d9a7251f33
Ossi <ossi@example.com>
2019-09-30 11:50:10 +0200

    Remove ImportBrainzRelease#find_already_existing

--- app/importers/import_brainz_release.rb
+++ app/importers/import_brainz_release.rb
@@ -2,10 +2,6 @@
 
 # Import a Release from MusicBrainz
 class ImportBrainzRelease < ImportBrainzBase
-  def find_already_existing
-    Release.find_by(brainz_code: import_request.code)
-  end
-
   # OPTIMIZE: move #prepare to ImportBase?
   def prepare
     PrepareBrainzRelease.call(
