594c8778bd9cd2e17f829102e298615db687c749
Ossi <ossi@example.com>
2019-10-01 11:29:21 +0200

    Use named parameters for PersistBrainzPiece

--- app/importers/persist_brainz_piece.rb
+++ app/importers/persist_brainz_piece.rb
@@ -2,9 +2,9 @@
 
 # Persist a MusicBrainz recording
 class PersistBrainzPiece < PersistBrainzBase
-  def initialize(args)
+  def initialize(import_request:, **args)
     super(args)
-    @import_request = args[:import_request]
+    @import_request = import_request
   end
 
   attr_reader :import_request
