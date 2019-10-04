9073b8107d3b68f6afb44ae7cbe22c8f4176eba9
Ossi <ossi@example.com>
2019-10-04 11:55:10 +0200

    Clean up factory participants.rb

--- spec/factories/participants.rb
+++ spec/factories/participants.rb
@@ -1,15 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   # TODO: maybe delete this factory
   factory :participant do
     sequence(:position) { |n| n }
-
-    # factory :participant_without_artist_credit do
-    #   artist
-    # end
-
-    # factory :valid_participant do
-    #   association :artist, factory: :artist
-    #   #association :artist_credit, factory: :artist_credit
-    # end
   end
 end
