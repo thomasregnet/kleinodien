9de74bbc7633092ef8abd11cca916021e926dcb7
Ossi <ossi@example.com>
2019-10-07 14:02:57 +0200

    Add app/import_worker/init_import_worker.rb

---
+++ app/import_worker/init_import_worker.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Start up the ImportWorker
+class InitImportWorker
+  def self.call
+    new.call
+  end
+
+  attr_reader :import_queue_name, :timeout
+
+  def initialize
+    @import_queue_name = ENV['IMPORT_QUEUE_NAME']
+    @timeout           = ENV.fetch('IMPORT_SUBSCRIPTION_TIMEOUT', 300).to_i
+
+    raise ArgumentError, 'Environment variable IMPORT_QUEUE_NAME not set' \
+      unless import_queue_name
+  end
+
+  def call
+    ImportWorker.run(
+      import_queue_name: import_queue_name,
+      subscriber:        subscriber
+    )
+  end
+
+  def subscriber
+    ImportSubscriber.new(
+      channel: import_queue_name,
+      timeout: timeout
+    )
+  end
+end
---
+++ spec/import_worker/init_import_worker_spec.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+# rubocop:disable Metrics/BlockLength
+RSpec.describe InitImportWorker do
+  describe '.call' do
+    before { ENV['IMPORT_QUEUE_NAME'] = 'test_import_queue' }
+
+    it 'calls .run on ImportWorker' do
+      allow(ImportWorker).to receive(:run).with(
+        hash_including(
+          import_queue_name: 'test_import_queue',
+          subscriber:        an_instance_of(ImportSubscriber)
+        )
+      ).and_return(:success)
+      expect(described_class.call).to eq(:success)
+    end
+  end
+
+  describe '.new' do
+    context 'when the environment variable IMPORT_QUEUE_NAME is set' do
+      let(:import_queue_name) { 'test_import_queue' }
+
+      before { ENV['IMPORT_QUEUE_NAME'] = import_queue_name }
+
+      it 'sets the instance variable "import_queue_name"' do
+        expect(described_class.new.import_queue_name).to eq(import_queue_name)
+      end
+
+      it 'sets the default timeout' do
+        expect(described_class.new.timeout).to eq(300)
+      end
+    end
+
+    context 'with the environment variable IMPORT_SUBSCRIPTION_TIMEOUT set' do
+      let(:timeout) { '123' } # must be a string
+
+      before do
+        ENV['IMPORT_SUBSCRIPTION_TIMEOUT'] = timeout
+        ENV['IMPORT_QUEUE_NAME']           = 'test_queue_name'
+      end
+
+      it 'sets the instance_variable timeout as an integer' do
+        expect(described_class.new.timeout).to eq(timeout.to_i)
+      end
+    end
+
+    context 'when the environment variable IMPORT_QUEUE_NAME is not set' do
+      before { ENV.delete('IMPORT_QUEUE_NAME') }
+
+      it 'returns the environment variable' do
+        expect { described_class.new }
+          .to raise_error ArgumentError, /IMPORT_QUEUE_NAME not set/
+      end
+    end
+  end
+
+  describe '#subscriber' do
+    let(:initializer) do
+      ENV['IMPORT_QUEUE_NAME'] = 'test_import_queue'
+      described_class.new
+    end
+
+    it 'returns a subscriber' do
+      expect(initializer.subscriber).to be_instance_of(ImportSubscriber)
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength
