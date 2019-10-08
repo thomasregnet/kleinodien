86b41cb89055800582aca8582bec46049ea364ad
Ossi <ossi@example.com>
2019-10-08 12:18:53 +0200

    Move logger message from #initialize to #call

--- app/import_worker/init_import_worker.rb
+++ app/import_worker/init_import_worker.rb
@@ -18,11 +18,11 @@ class InitImportWorker
       unless import_queue_name
 
     @timeout = ENV.fetch('IMPORT_SUBSCRIPTION_TIMEOUT', 300).to_i
-
-    Rails.logger.info("starting ImportWorker for queue #{import_queue_name}")
   end
 
   def call
+    Rails.logger.info(%(starting ImportWorker for queue "#{import_queue_name}"))
+
     ImportWorker.run(
       import_queue_name: import_queue_name,
       subscriber:        subscriber
