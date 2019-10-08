05cc74aa5e1daaebefc8a7d2473c240d98d808bd
Ossi <ossi@example.com>
2019-10-08 09:38:16 +0200

    Include Pundit in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+  # TODO: Ensure Pundit policies and scopes are used
+  # https://github.com/varvet/pundit#ensuring-policies-and-scopes-are-used
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
