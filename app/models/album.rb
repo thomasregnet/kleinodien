df50844e4fbce2ffedaf7fadde61ebe1eaf8b1ee
Ossi <ossi@example.com>
2019-10-10 12:25:58 +0200

    Remove Album.with_discogs_id_exists?

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,8 +3,4 @@
 # Release of a music album
 class Album < Release
   belongs_to :artist_credit, required: false
-
-  def self.with_discogs_id_exists?(discogs_id)
-    with_id_from_data_supplier_exists?(discogs_id, 'Discogs')
-  end
 end
