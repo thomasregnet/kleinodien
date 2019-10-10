9686fa9071876857b076e5e82e30a3fc45973346
Ossi <ossi@example.com>
2019-10-10 12:12:54 +0200

    Enable specs for PrepareBase

--- spec/importers/prepare_base_spec.rb
+++ spec/importers/prepare_base_spec.rb
@@ -3,16 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe PrepareBase do
-  # describe '#call' do
-  #   context 'when an exception is thrown within "prepare"' do
-  #     let(:import_order) { spy }
-  #     let(:pre_base) { described_class.new(proxy: :fake_proxy) }
+  describe '#call' do
+    context 'when an exception is thrown within "prepare"' do
+      let(:import_order) { spy }
+      let(:pre_base) do
+        described_class.new(
+          import_order: import_order,
+          proxy:        :fake_proxy
+        )
+      end
 
-  #     it 'calls "failure" in the ImportOrder' do
-  #       pre_base.define_singleton_method(:prepare) { raise 'wicked' }
-  #       pre_base.call
-  #       expect(import_order).to have_received('failure!')
-  #     end
-  #   end
-  # end
+      it 'calls "failure" in the ImportOrder' do
+        pre_base.define_singleton_method(:prepare) { raise 'wicked' }
+        pre_base.call
+        expect(import_order).to have_received('failure!')
+      end
+    end
+  end
 end
