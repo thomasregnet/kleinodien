3bb3995213d6b1bc40fe58612453f0bd84256f24
Ossi <ossi@example.com>
2019-10-16 22:32:27 +0200

    Add specs and validations for Iso3166Part1Country

--- app/models/iso3166_part3_country.rb
+++ app/models/iso3166_part3_country.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
+# ISO codes for countries deleted form ISO 3166-1
 class Iso3166Part3Country < ApplicationRecord
+  include Iso3166
   belongs_to :area
+
+  validates(
+    :code,
+    format: {
+      with:    /\A[A-Z]{4}\z/,
+      message: 'must consist of four uppercase letters'
+    }
+  )
 end
--- spec/models/iso3166_part3_country_spec.rb
+++ spec/models/iso3166_part3_country_spec.rb
@@ -1,5 +1,20 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+require 'shared_examples_for_iso_3166_models'
 
 RSpec.describe Iso3166Part3Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like 'an ISO-3166 model' do
+    let(:good_code) { 'DDDE' }
+  end
+
+  describe '#code' do
+    context 'when invalid' do
+      let(:iso) { FactoryBot.build(:iso3166_part3_country, code: 'qwerty') }
+
+      it 'is not valid' do
+        expect(iso).not_to be_valid
+      end
+    end
+  end
 end
