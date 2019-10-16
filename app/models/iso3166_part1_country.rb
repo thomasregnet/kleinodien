204a709ecf571f12fce8ac124743ee2f2c6b61a7
Ossi <ossi@example.com>
2019-10-16 21:15:42 +0200

    More specs and validations

--- app/models/iso3166_part1_country.rb
+++ app/models/iso3166_part1_country.rb
@@ -1,5 +1,16 @@
+# frozen_string_literal: true
+
+# ISO-3166 1 Country codes and their Area
 class Iso3166Part1Country < ApplicationRecord
   belongs_to :area
 
-  validates :code, presence: true, blank: false
+  validates(
+    :code,
+    blank:    false,
+    format:   {
+      with:    /\A[A-Z]{2}\z/,
+      message: 'must consist of two capital letters'
+    },
+    presence: true
+  )
 end
--- spec/models/iso3166_part1_country_spec.rb
+++ spec/models/iso3166_part1_country_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe Iso3166Part1Country, type: :model do
   it_behaves_like 'an ISO-3166 model' do
     let(:good_code) { 'ZZ' }
   end
+
+  context 'with a bad code' do
+    let(:iso) { FactoryBot.build(:iso3166_part1_country, code: 'x0%') }
+
+    it 'is not valid' do
+      expect(iso).not_to be_valid
+    end
+  end
 end
