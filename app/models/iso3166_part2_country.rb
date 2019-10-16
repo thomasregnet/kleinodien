62e122280c83d9b087d3cf67ab3a4739e0be561c
Ossi <ossi@example.com>
2019-10-16 21:45:35 +0200

    Start Iso#166Part1Country specs

--- app/models/iso3166_part2_country.rb
+++ app/models/iso3166_part2_country.rb
@@ -1,3 +1,4 @@
 class Iso3166Part2Country < ApplicationRecord
+  # include Iso3166
   belongs_to :area
 end
--- spec/models/iso3166_part2_country_spec.rb
+++ spec/models/iso3166_part2_country_spec.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+require 'shared_examples_for_iso_3166_models'
 
 RSpec.describe Iso3166Part2Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # it_behaves_like 'an ISO-3166 model' do
+  #   let(:good_code) { 'AA-123' }
+  # end
 end
