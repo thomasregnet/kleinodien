106a6dfc0579a090c5d5bf078a6be2269894c1b8
Ossi <ossi@example.com>
2019-10-21 21:39:14 +0200

    Generate model Subdivision

---
+++ app/models/subdivision.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# A main administrative division of a country, e.g. Bavaria
+class Subdivision < Area
+end
---
+++ spec/models/subdivision_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'shared_examples_for_areas'
+
+RSpec.describe Subdivision, type: :model do
+  it_behaves_like 'an area'
+end
