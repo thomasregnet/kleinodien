72f2e6c2f6ee981aec8fff12781301754f013c48
Ossi <ossi@example.com>
2019-11-19 22:26:19 +0100

    Specs and factory for "Language"

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,2 +1,43 @@
+# frozen_string_literal: true
+
+# Languages and their ISO-639 codes
 class Language < ApplicationRecord
+  validates(
+    :iso_code_1,
+    length:    { is: 2 },
+    allow_nil: true,
+    format:    {
+      with:    /\A[a-z][a-z]\z/,
+      message: 'must consist of two lowercase letters'
+    }
+  )
+  validates(
+    :iso_code_2b,
+    length:    { is: 3 },
+    allow_nil: true,
+    format:    {
+      with:    /\A[a-z][a-z][a-z]\z/,
+      message: 'must consist of three lowercase letters'
+    }
+  )
+  validates(
+    :iso_code_2t,
+    length:    { is: 3 },
+    allow_nil: true,
+    format:    {
+      with:    /\A[a-z][a-z][a-z]\z/,
+      message: 'must consist of three lowercase letters'
+    }
+  )
+  validates(
+    :iso_code_3,
+    length:    { is: 3 },
+    allow_nil: true,
+    format:    {
+      with:    /\A[a-z][a-z][a-z]\z/,
+      message: 'must consist of three lowercase letters'
+    }
+  )
+
+  validates :name, presence: true, blank: false
 end
--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -1,5 +1,104 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Language, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_length_of(:iso_code_1).is_equal_to(2) }
+  it { should validate_length_of(:iso_code_2b).is_equal_to(3) }
+  it { should validate_length_of(:iso_code_2t).is_equal_to(3) }
+  it { should validate_length_of(:iso_code_3).is_equal_to(3) }
+
+  it { should validate_presence_of(:name) }
+
+  context 'with valid parameters' do
+    let(:lang) { FactoryBot.build(:language) }
+
+    it 'is valid' do
+      expect(lang).to be_valid
+    end
+  end
+
+  describe '#iso_code_1' do
+    context 'when valid' do
+      let(:lang) { FactoryBot.build(:language, iso_code_1: 'az') }
+
+      it 'is valid' do
+        expect(lang).to be_valid
+      end
+    end
+
+    context 'when format is wrong' do
+      let(:lang) { FactoryBot.build(:language, iso_code_1: 'AZ') }
+
+      it 'is not valid' do
+        # expect(lang).not_to be_valid
+        lang.valid?
+        expect(lang.errors[:iso_code_1])
+          .to include('must consist of two lowercase letters')
+      end
+    end
+  end
+
+  describe '#iso_code_2b' do
+    context 'when valid' do
+      let(:lang) { FactoryBot.build(:language, iso_code_2b: 'abc') }
+
+      it 'is valid' do
+        expect(lang).to be_valid
+      end
+    end
+
+    context 'when format is wrong' do
+      let(:lang) { FactoryBot.build(:language, iso_code_2b: 'ABC') }
+
+      it 'is not valid' do
+        # expect(lang).not_to be_valid
+        lang.valid?
+        expect(lang.errors[:iso_code_2b])
+          .to include('must consist of three lowercase letters')
+      end
+    end
+  end
+
+  describe '#iso_code_2t' do
+    context 'when valid' do
+      let(:lang) { FactoryBot.build(:language, iso_code_2t: 'abc') }
+
+      it 'is valid' do
+        expect(lang).to be_valid
+      end
+    end
+
+    context 'when format is wrong' do
+      let(:lang) { FactoryBot.build(:language, iso_code_2t: 'ABC') }
+
+      it 'is not valid' do
+        # expect(lang).not_to be_valid
+        lang.valid?
+        expect(lang.errors[:iso_code_2t])
+          .to include('must consist of three lowercase letters')
+      end
+    end
+  end
+
+  describe '#iso_code_3' do
+    context 'when valid' do
+      let(:lang) { FactoryBot.build(:language, iso_code_3: 'abc') }
+
+      it 'is valid' do
+        expect(lang).to be_valid
+      end
+    end
+
+    context 'when format is wrong' do
+      let(:lang) { FactoryBot.build(:language, iso_code_3: 'ABC') }
+
+      it 'is not valid' do
+        # expect(lang).not_to be_valid
+        lang.valid?
+        expect(lang.errors[:iso_code_3])
+          .to include('must consist of three lowercase letters')
+      end
+    end
+  end
 end
