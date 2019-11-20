fcc1516169ecdb597fd4550c689dff0ec2a21c23
Ossi <ossi@example.com>
2019-11-20 21:54:04 +0100

    Fix bug in an older migration
    The migration ChangeImportOrderStateCheckConstraint
    did not DROP the old constraint before it created the new one.

--- db/migrate/20191113192318_change_import_order_state_check_constraint.rb
+++ db/migrate/20191113192318_change_import_order_state_check_constraint.rb
@@ -1,6 +1,8 @@
 class ChangeImportOrderStateCheckConstraint < ActiveRecord::Migration[6.0]
   def change
     execute <<~DDL
+      ALTER TABLE import_orders DROP CONSTRAINT import_orders_state_check;
+
       ALTER TABLE import_orders ADD CHECK
         (state IN ('pending', 'preparing', 'persisting', 'done', 'failed'));
     DDL
