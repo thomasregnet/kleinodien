85969ac5bebcafdc4a2981bbabc41b9ae0fe1a82
Ossi <ossi@example.com>
2019-11-26 20:25:36 +0100

    Add CodesForModelService

---
+++ app/services/codes_for_model_service.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Extract the codes useable by a model_class from a codes_hash
+# Optional merge the codes with a given hash
+class CodesForModelService < ServiceBase
+  def initialize(codes_hash:, model_class:, given: {})
+    @codes_hash  = codes_hash
+    @given       = given
+    @model_class = model_class
+  end
+
+  attr_reader :codes_hash, :given, :model_class
+
+  def call
+    codes = codes_hash.select { |key, _| common_codes.include?(key) }
+    codes.merge(given)
+  end
+
+  private
+
+  def common_codes
+    filtered_codes.keys & model_codes
+  end
+
+  # This method smells of :reek:NilCheck
+  def filtered_codes
+    codes_hash.reject { |_, value| value.nil? }
+  end
+
+  def model_codes
+    model_class.column_names.grep(/.+code$/)
+  end
+end
---
+++ spec/services/codes_for_model_service_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'shared_examples_for_services'
+
+# rubocop:disable Metrics/BlockLength
+# rubocop:disable RSpec/VerifiedDoubles
+RSpec.describe CodesForModelService do
+  it_behaves_like 'a service'
+
+  describe '.call' do
+    context 'without "given"' do
+      let(:model_class) do
+        double(column_names: %i[known_code some_column])
+      end
+
+      let(:codes_hash) do
+        described_class.call(
+          codes_hash:  { known_code: 'known', unknown_code: 2 },
+          model_class: model_class
+        )
+      end
+
+      it 'has the exepcted lenth' do
+        expect(codes_hash.length).to eq(1)
+      end
+
+      it 'returns the expected keys' do
+        expect(codes_hash[:known_code]).to eq('known')
+      end
+    end
+
+    context 'with given' do
+      let(:model_class) do
+        double(column_names: %i[known_code some_column])
+      end
+
+      let(:codes_hash) do
+        described_class.call(
+          codes_hash:  { known_code: 'known', unknown_code: 2 },
+          given:       { given_parameter: 'given' },
+          model_class: model_class
+        )
+      end
+
+      it 'has the exepcted lenth' do
+        expect(codes_hash.length).to eq(2)
+      end
+
+      it 'contains the given parameter' do
+        expect(codes_hash[:given_parameter]).to eq('given')
+      end
+    end
+  end
+end
+# rubocop:enable RSpec/VerifiedDoubles
+# rubocop:enable Metrics/BlockLength
