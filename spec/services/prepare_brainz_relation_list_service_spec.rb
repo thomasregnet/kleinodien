e7f4681271c0419d087d06efd90ca60667f058c9
Ossi <ossi@example.com>
2019-12-11 21:48:05 +0100

    Add specs for PrepareBrainzRelationListService

--- spec/services/prepare_brainz_relation_list_service_spec.rb
+++ spec/services/prepare_brainz_relation_list_service_spec.rb
@@ -3,8 +3,62 @@
 require 'rails_helper'
 require 'shared_examples_for_services'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe PrepareBrainzRelationListService do
   it_behaves_like 'a service'
 
-  # the specs for the service belongs here
+  context 'with a relation-key containing a single hash' do
+    let(:relation_list) do
+      {
+        'relation' => {},
+        'target_type' => 'gizmo'
+      }
+    end
+
+    let(:prepared) { described_class.call(relation_list: relation_list) }
+
+    it 'returns a hash with one key' do
+      expect(prepared.length).to eq(1)
+    end
+
+    it 'returns a hash with the expected key' do
+      expect(prepared).to have_key('gizmo_relations')
+    end
+
+    it 'returns a hash-value containing an array' do
+      expect(prepared['gizmo_relations']).to be_instance_of(Array)
+    end
+
+    it 'returns a hash-value containing the expected number of items' do
+      expect(prepared['gizmo_relations'].length).to eq(1)
+    end
+  end
+
+  context 'with a relation-key containing an array of hashes' do
+    let(:relation_list) do
+      {
+        'relation' => [{}, {}],
+        'target_type' => 'gizmo'
+      }
+    end
+
+    let(:prepared) { described_class.call(relation_list: relation_list) }
+
+    it 'returns a hash with one key' do
+      expect(prepared.length).to eq(1)
+    end
+
+    it 'returns a hash with the expected key' do
+      expect(prepared).to have_key('gizmo_relations')
+    end
+
+    it 'returns a hash-value containing an array' do
+      expect(prepared['gizmo_relations']).to be_instance_of(Array)
+    end
+
+    it 'returns a hash-value containing the expected number of items' do
+      expect(prepared['gizmo_relations'].length).to eq(2)
+    end
+  end
 end
+# rubocop:enable Metrics/BlockLength
