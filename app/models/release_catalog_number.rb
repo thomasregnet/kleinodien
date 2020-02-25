30e4b121f2216d59dfc78d0514b46b849c28b2e6
ossi <ossi@asgard.localdomain>
2020-02-25 20:13:46 +0100

    Validate uniqueness of ReleaseCatalogNumber#code

--- app/models/release_catalog_number.rb
+++ app/models/release_catalog_number.rb
@@ -5,4 +5,8 @@ class ReleaseCatalogNumber < ApplicationRecord
   belongs_to :release_company
 
   validates :code, presence: true, blank: false
+  validates(
+    :code,
+    uniqueness: { case_sensitive: false, scope: :release_company_id }
+  )
 end
--- spec/models/release_catalog_number_spec.rb
+++ spec/models/release_catalog_number_spec.rb
@@ -5,6 +5,21 @@ require 'rails_helper'
 RSpec.describe ReleaseCatalogNumber, type: :model do
   it { should validate_presence_of(:code) }
 
+  describe 'uniqueness of code in scope with ReleaseCompany' do
+    subject do
+      described_class.create!(
+        code:            'aaa',
+        release_company: FactoryBot.create(:release_company)
+      )
+    end
+
+    it do
+      should validate_uniqueness_of(:code)
+        .scoped_to(:release_company_id)
+        .case_insensitive
+    end
+  end
+
   it { should belong_to(:release_company) }
   it { should have_db_index([:code, :release_company_id]).unique }
 end
