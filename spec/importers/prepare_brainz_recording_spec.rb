52b4dafd1b455599e63e4222ca05a60068128729
ossi <ossi@asgard.localdomain>
2020-03-03 09:10:10 +0100

    Remove empty example group

--- spec/importers/prepare_brainz_recording_spec.rb
+++ spec/importers/prepare_brainz_recording_spec.rb
@@ -5,25 +5,6 @@ require 'test_data'
 require 'test_data/brainz_service'
 
 RSpec.describe PrepareBrainzRecording do
-  context 'when the recording already exists in the database' do
-    before do
-      FactoryBot.create(
-        :piece,
-        brainz_code: '5935ec91-8124-42ff-937f-f31a20ffe58f'
-      )
-    end
-
-    let(:import_request) do
-      BrainzRecordingImportRequest.new(
-        code: '5935ec91-8124-42ff-937f-f31a20ffe58f'
-      )
-    end
-
-    let(:blueprint) do
-      TestData.by_name(:brainz_recording_highway_to_hell).blueprint
-    end
-  end
-
   context 'when the recording does not exist in the database' do
     let(:import_request) do
       BrainzRecordingImportRequest.new(
