cc0192a1809a0612258b4a56731b866353eec2f5
ossi <ossi@asgard.localdomain>
2020-03-09 12:07:30 +0100

    Improve PrepareBase and it's specs

--- app/importers/prepare_base.rb
+++ app/importers/prepare_base.rb
@@ -10,17 +10,22 @@ class PrepareBase < PersistPrepareBase
     raise
   end
 
-  def method_missing(method, *args)
-    klass = prepare_class_for(method)
+  def prepare_infix
+    raise NoMethodError, "missed to implement #{self.class}#prepare_infix"
+  end
+
+  def method_missing(method_name, *args)
+    klass = prepare_class_for(method_name)
 
     if klass
-      klass.call(merge_arguments(args))
+      klass.call(merge_arguments(args[0]))
     else
       super
     end
   end
 
-  def respond_to_missing?(method_name, _)
+  # This method smells of :reek:BooleanParameter
+  def respond_to_missing?(method_name, _ = false)
     return true if prepare_class_for(method_name)
 
     false
@@ -28,14 +33,25 @@ class PrepareBase < PersistPrepareBase
 
   private
 
-  def merge_arguments(args)
-    { import_order: import_order, proxy: proxy }.merge(args[0])
+  def merge_arguments(args = {})
+    { import_order: import_order, proxy: proxy }.merge(args)
   end
 
-  def prepare_class_for(method)
-    match_data = method.match(/\Aprepare_(.+)\z/)
+  def prepare_class_for(method_name)
+    class_name = prepare_class_name_for(method_name) || return
+
+    begin
+      class_name.constantize
+    rescue NameError => e
+      Rails.logger.error(e)
+      nil
+    end
+  end
+
+  def prepare_class_name_for(method_name)
+    match_data = method_name.match(/\Aprepare_(.+)\z/)
     return unless match_data
 
-    "prepare_#{prepare_infix}_#{match_data[1]}".camelize.constantize
+    "prepare_#{prepare_infix}_#{match_data[1]}".camelize
   end
 end
--- spec/importers/prepare_base_spec.rb
+++ spec/importers/prepare_base_spec.rb
@@ -20,7 +20,6 @@ RSpec.describe PrepareBase do
         expect { preparer.call }.to raise_error(RuntimeError, 'wicked')
       end
 
-
       # rubocop:disable Lint/SuppressedException
       # rubocop:disable Lint/UselessAssignment
       it 'calls "failure" in the ImportOrder' do
@@ -33,6 +32,62 @@ RSpec.describe PrepareBase do
       end
       # rubocop:enable Lint/SuppressedException
       # rubocop:enable Lint/UselessAssignment
+    end
+  end
+
+  describe 'respond_to_missing?' do
+    let(:base) do
+      described_class.new(
+        import_order: :fake_import_order,
+        proxy:        :fake_proxy
+      )
+    end
+
+    context 'when able to respond' do
+      before do
+        stub_const('PrepareTestGizmo', 1)
+        base.define_singleton_method(:prepare_infix) { :test }
+      end
+
+      it 'returns true' do
+        expect(base.send(:respond_to_missing?, :prepare_gizmo)).to be_truthy
+      end
+    end
+
+    context 'when unable to respond' do
+      before { base.define_singleton_method(:prepare_infix) { :test } }
+
+      it 'returns false' do
+        expect(base.send(:respond_to_missing?, :prepare_gizmo)).to be_falsey
+      end
+    end
+  end
+
+  context 'when called with an prepare_* method that can not be called' do
+    let(:preparer) do
+      preparer = described_class.new(
+        import_order: :fake_import_order,
+        proxy:        :fake_proxy
+      )
+      preparer.define_singleton_method(:prepare_infix) { :some_stuff }
+
+      preparer
+    end
+
+    it 'raises a NoMethodError' do
+      expect { preparer.prepare_gizmo }
+        .to raise_error(NoMethodError, /undefined method `prepare_gizmo'/)
+    end
+  end
+
+  describe '#prepare_infix' do
+    it 'raises a NoMethodError' do
+      preparer = described_class.new(
+        import_order: :fake_import_order,
+        proxy:        :fake_proxy
+      )
+      expect { preparer.prepare_infix }
+        .to raise_error(NoMethodError, /PrepareBase#prepare_infix\z/)
     end
   end
 end
