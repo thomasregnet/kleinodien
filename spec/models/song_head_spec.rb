21098967dcb91b8eefa0bfd883ba6fe3f42381d0
thomasregnet <ossi@asgard.localdomain>
2020-09-28 14:10:33 +0200

    Make song_head_spec rubocop compliant

--- spec/models/song_head_spec.rb
+++ spec/models/song_head_spec.rb
@@ -4,22 +4,18 @@ require 'rails_helper'
 require 'shared_examples_for_disambiguations'
 
 RSpec.describe SongHead, type: :model do
-  before do
-    @song_head = FactoryBot.create(:song_head)
-  end
-
   it 'is valid with valid attributes' do
-    expect(@song_head).to be_valid
+    expect(FactoryBot.build(:song_head)).to be_valid
   end
 
   it 'is not valid without an artist_credit' do
-    @song_head.artist_credit = nil
-    expect(@song_head).not_to be_valid
+    song_head = FactoryBot.build(:song_head, artist_credit: nil)
+    expect(song_head).not_to be_valid
   end
 
   it_behaves_like 'a model with disambiguations' do
     let(:factory) { :song_head }
-    let(:object) { @song_head }
+    let(:object) { FactoryBot.build(:song_head) }
     let(:naming) { 'title' }
   end
 end
