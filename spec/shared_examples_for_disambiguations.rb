940ae7573f77c22a1bc54059ac1d8a1e646abd94
thomasregnet <ossi@asgard.localdomain>
2020-10-05 15:42:00 +0200

    Make shar_...diesambiguation.rb rubocop compliant

--- spec/shared_examples_for_disambiguations.rb
+++ spec/shared_examples_for_disambiguations.rb
@@ -2,15 +2,18 @@
 
 RSpec.shared_examples 'a model with disambiguations' do
   let(:get_name) { naming }
-  let(:set_name) { naming + '=' }
+  # let(:set_name) { naming + '=' }
+  let(:set_name) { "#{naming}=" }
   let(:clone) { FactoryBot.build(factory) }
   let(:disambiguation) { 'another one' }
 
   it 'is not valid without a naming' do
     object.send set_name, nil
     expect(object).not_to be_valid
-    expect { object.save! validate: false }.to raise_error do |error|
-      expect(error).to be_a(ActiveRecord::StatementInvalid)
-    end
+  end
+
+  it 'can not be saved without a naming' do
+    object.send set_name, nil
+    expect { object.save! validate: false }.to raise_error(ActiveRecord::StatementInvalid)
   end
 end
