86c9473a002f23564773c6b9ba1e3c38685e90ba
thomasregnet <ossi@asgard.localdomain>
2020-10-06 10:13:14 +0200

    Fix Rubocop issue in BrainzTestHelper

--- spec/brainz_test_helper.rb
+++ spec/brainz_test_helper.rb
@@ -2,7 +2,7 @@
 
 module BrainzTestHelper
   def self.get_release(mbid)
-    file_name = File.join('fixtures', 'music_brainz', 'release', mbid + '.xml')
+    file_name = File.join('fixtures', 'music_brainz', 'release', "#{mbid}.xml")
     xml = File.open(file_name)
     KleinodienBrainz::Model::Release.xml(xml)
   end
