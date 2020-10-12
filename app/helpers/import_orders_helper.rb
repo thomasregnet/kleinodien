cb023be890b90a929e0359c53fd01365a93e07f0
thomasregnet <ossi@asgard.localdomain>
2020-10-12 10:09:38 +0200

    Add colors for import_order.state

--- app/helpers/import_orders_helper.rb
+++ app/helpers/import_orders_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ImportOrdersHelper
+  def import_order_state_class_for(state)
+    return 'tag is-dark' if state == 'pending'
+    return 'tag is-primary is-light' if state == 'preparing'
+    return 'tag is-primary' if state == 'persisting'
+    return 'tag is-success' if state == 'done'
+
+    'tag is-danger'
+  end
 end
