26768717b6ed90b0e6cdcf8d2d54bb6d8127bf9a
thomasregnet <ossi@asgard.localdomain>
2020-10-22 20:50:43 +0200

    Upgrade to PostgreSQL 13.0

--- config/containers/app_cmd.sh
+++ config/containers/app_cmd.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+if [ ! -d tmp/pids ]; then
+  echo "mkdir tmp/pids"
+  mkdir tmp/pids
+fi
+
 echo "Removing unicorn pidfile"
 rm -rf tmp/pids/unicorn.pid
 
 # http://chrisstump.online/2016/02/20/docker-existing-rails-application/
 # Prefix `bundle` with `exec` so unicorn shuts down
 # gracefully on SIGTERM (i.e. `docker stop`)
+# exec bundle exec unicorn -c config/containers/unicorn/unicorn.rb -E $RAILS_ENV;
 exec bundle exec unicorn -c config/containers/unicorn/unicorn.rb -E $RAILS_ENV;
--- db/migrate/20201020172310_rename_release_data_mask_to_date_mask.rb
+++ db/migrate/20201020172310_rename_release_data_mask_to_date_mask.rb
@@ -1,5 +1,5 @@
 class RenameReleaseDataMaskToDateMask < ActiveRecord::Migration[6.0]
   def change
-    rename_column :releases, :date_mask, :date_mask
+    rename_column :releases, :data_mask, :date_mask
   end
 end
