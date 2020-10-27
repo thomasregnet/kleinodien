ae7b4643e20b2fd9f4e656b091602106dd350b37
thomasregnet <ossi@asgard.localdomain>
2020-10-27 22:22:17 +0100

    Spec "add an original release to my collection"

---
+++ spec/features/add_an_original_release_to_my_collection_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'add an OriginalRelease to my collection', type: :feature do
+  let(:release) { FactoryBot.create(:release) }
+  let(:user) { FactoryBot.create(:user) }
+
+  before do
+    sign_in user
+  end
+
+  scenario 'user adds a OriginalRelease to his collection' do
+    visit release_path(release)
+    click_link('Add to my collection')
+    click_button('Create Release copy')
+
+    expect(page).to have_content('Release copy was successfully created.')
+  end
+end
