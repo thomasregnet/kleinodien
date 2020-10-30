3245ef7b261bc239ca23daa7583ed21224d52b53
thomasregnet <ossi@asgard.localdomain>
2020-10-30 11:18:11 +0100

    Add specs for release_copies/show

--- spec/views/release_copies/show.html.erb_spec.rb
+++ spec/views/release_copies/show.html.erb_spec.rb
@@ -1,16 +1,36 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "release_copies/show", type: :view do
-  before(:each) do
-    release_copy = FactoryBot.create(:release_copy)
+RSpec.describe 'release_copies/show', type: :view do
+  let(:artist_credit) { FactoryBot.build(:artist_credit, name: 'wonderful artist') }
+  let(:release_copy) { FactoryBot.create(:release_copy) }
+
+  before do
     assign(:release_copy, release_copy)
   end
 
-  it "renders attributes in <p>" do
+  it 'links to the release' do
+    allow(release_copy).to receive(:title).and_return('my title')
     render
-    # expect(rendered).to match(/MyText/)
-    expect(rendered).to match(//)
-    expect(rendered).to match(//)
-    expect(rendered).to match(//)
+    expect(rendered).to have_link('my title')
+  end
+
+  it 'renders the #artist_credit' do
+    allow(release_copy).to receive(:artist_credit).and_return(artist_credit)
+    render
+    expect(rendered).to match(/by wonderful artist/)
+  end
+
+  it 'renders the #designation as title' do
+    allow(release_copy).to receive(:designation).and_return('my designation')
+    render
+    expect(rendered).to match(/<h1 class="title">\s*my designation/)
+  end
+
+  it 'renders the #title as subtitle' do
+    allow(release_copy).to receive(:title).and_return('my title')
+    render
+    expect(rendered).to match(/<h2 class="subtitle">\s*my title/)
   end
 end
