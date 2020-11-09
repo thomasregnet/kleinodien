c6c147bf5738f29bba19d56fa6bfe86317afe9bf
thomasregnet <ossi@asgard.localdomain>
2020-11-09 10:06:01 +0100

    Mv ImportCoverArt -> ImportCoverArtBase

--- app/importers/import_cover_art_base.rb
+++ app/importers/import_cover_art_base.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Import covers from coverartarchive.org
-class ImportCoverArt < ImportBase
+class ImportCoverArtBase < ImportBase
   def find_existing
   end
 
--- spec/importers/import_cover_art_base_spec.rb
+++ spec/importers/import_cover_art_base_spec.rb
@@ -3,6 +3,6 @@
 require 'rails_helper'
 require 'shared_examples_for_services'
 
-RSpec.describe ImportCoverArt do
+RSpec.describe ImportCoverArtBase do
   it_behaves_like 'a service'
 end
