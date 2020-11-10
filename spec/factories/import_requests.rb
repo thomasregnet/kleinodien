be444f1ab27827d82c1a8b61ecb097227999106b
thomasregnet <ossi@asgard.localdomain>
2020-11-10 21:31:39 +0100

    Refine spec/factories/import_requests.rb

--- spec/factories/import_requests.rb
+++ spec/factories/import_requests.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/BlockLength
 FactoryBot.define do
   factory :import_request do
     sequence(:code) { |n| }
@@ -9,35 +8,11 @@ FactoryBot.define do
       sequence(:code) { SecureRandom.uuid.to_s }
       association :import_order, factory: :brainz_import_order
 
-      factory(
-        :brainz_area_import_request,
-        class: 'BrainzAreaImportRequest'
-      ) do
-      end
-
-      factory(
-        :brainz_artist_import_request,
-        class: 'BrainzArtistImportRequest'
-      ) do
-      end
-
-      factory(
-        :brainz_label_import_request,
-        class: 'BrainzLabelImportRequest'
-      ) do
-      end
-
-      factory(
-        :brainz_release_group_import_request,
-        class: 'BrainzReleaseGroupImportRequest'
-      ) do
-      end
-
-      factory(
-        :brainz_release_import_request,
-        class: 'BrainzReleaseImportRequest'
-      ) do
-      end
+      factory :brainz_area_import_request, class: 'BrainzAreaImportRequest'
+      factory :brainz_artist_import_request, class: 'BrainzArtistImportRequest'
+      factory :brainz_label_import_request, class: 'BrainzLabelImportRequest'
+      factory :brainz_release_group_import_request, class: 'BrainzReleaseGroupImportRequest'
+      factory :brainz_release_import_request, class: 'BrainzReleaseImportRequest'
 
       factory :cover_art_import_request, class: 'CoverArtImportRequest' do
         sequence(:code) { SecureRandom.uuid.to_s }
@@ -49,4 +24,3 @@ FactoryBot.define do
     end
   end
 end
-# rubocop:enable Metrics/BlockLength
