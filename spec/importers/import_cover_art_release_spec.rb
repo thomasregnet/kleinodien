ef57d1a16417581db4350b952fe362882a02493d
thomasregnet <ossi@asgard.localdomain>
2020-11-10 21:27:32 +0100

    Refine ImportCoverArtRelease specs

--- spec/importers/import_cover_art_release_spec.rb
+++ spec/importers/import_cover_art_release_spec.rb
@@ -6,14 +6,18 @@ require 'shared_examples_for_services'
 RSpec.describe ImportCoverArtRelease do
   it_behaves_like 'a service'
 
-  it 'foos' do
-    import_order = FactoryBot.create(
-      :cover_art_release_import_order,
-      code: '58e6a3d6-bbbd-4864-983b-e468a5a1a71c'
-    )
-   
-    FactoryBot.create(:release, brainz_code: '58e6a3d6-bbbd-4864-983b-e468a5a1a71c')
+  context 'when a front-cover is available' do
+    let(:brainz_code) { '58e6a3d6-bbbd-4864-983b-e468a5a1a71c' }
+    let(:import_order) { FactoryBot.create(:cover_art_release_import_order, code: brainz_code) }
 
-    described_class.call(import_order: import_order)
+    before do
+      FactoryBot.create(:release, brainz_code: '58e6a3d6-bbbd-4864-983b-e468a5a1a71c')
+      described_class.call(import_order: import_order)
+    end
+
+    it 'attaches the front-cover to the release' do
+      release = Release.find_by(brainz_code: brainz_code)
+      expect(release.front_cover).to be_attached
+    end
   end
 end
