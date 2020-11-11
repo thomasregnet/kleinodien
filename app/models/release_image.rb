2d2856b08238a59d7be7504a2fb52b3f407eb786
thomasregnet <ossi@asgard.localdomain>
2020-11-11 22:19:55 +0100

    Add concern FileAttachable

--- app/models/release_image.rb
+++ app/models/release_image.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# An image of a Release
 class ReleaseImage < ApplicationRecord
+  include FileAttachable
+
   belongs_to :release
 end
--- spec/shared_examples_for_images.rb
+++ spec/shared_examples_for_images.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.shared_examples 'an image' do
   it { should respond_to(:archive_org_code) }
   it { should respond_to(:back) }
+  it { should respond_to(:file) }
   it { should respond_to(:front) }
   it { should respond_to(:note) }
 end
