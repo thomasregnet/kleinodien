880b2111e9e7cba625db166d6cea0829c6bd1668
thomasregnet <ossi@asgard.localdomain>
2020-11-20 11:22:34 +0100

    Add TestData::GetEmptyImage

---
+++ lib/test_data/get_empty_image.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module TestData
+  # Get an empty image
+  class GetEmptyImage
+    def self.call
+      PathService.call(path: 'empty.jpg')
+    end
+
+    def self.as_io
+      StringIO.new(call)
+    end
+  end
+end
--- spec/views/release_images/show.html.erb_spec.rb
+++ spec/views/release_images/show.html.erb_spec.rb
@@ -1,24 +1,17 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'test_data/get_empty_image'
 
 RSpec.describe 'release_images/show', type: :view do
   let(:release_image) { FactoryBot.create(:release_image) }
 
   before do
-    image = TestData::PathService.call(path: 'empty.jpg')
-    io = StringIO.new(image)
+    io = TestData::GetEmptyImage.as_io
     release_image.file.attach(io: io, filename: 'an image')
     assign(:release_image, release_image)
-    
+
     render
-  end
-
-
-  it 'renders attributes in <p>' do
-    expect(rendered).to match(/false/)
-    expect(rendered).to match(/false/)
-    expect(rendered).to match(/Note/)
   end
 
   it 'shows the image' do
