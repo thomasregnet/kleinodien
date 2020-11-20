a8dae5eb3e0059ae7596cea6c7f2ccebbdeb6c27
thomasregnet <ossi@asgard.localdomain>
2020-11-20 11:08:51 +0100

    Show image in release_images/show.html.erb

--- spec/views/release_images/show.html.erb_spec.rb
+++ spec/views/release_images/show.html.erb_spec.rb
@@ -5,12 +5,23 @@ require 'rails_helper'
 RSpec.describe 'release_images/show', type: :view do
   let(:release_image) { FactoryBot.create(:release_image) }
 
-  before { assign(:release_image, release_image) }
+  before do
+    image = TestData::PathService.call(path: 'empty.jpg')
+    io = StringIO.new(image)
+    release_image.file.attach(io: io, filename: 'an image')
+    assign(:release_image, release_image)
+    
+    render
+  end
+
 
   it 'renders attributes in <p>' do
-    render
     expect(rendered).to match(/false/)
     expect(rendered).to match(/false/)
     expect(rendered).to match(/Note/)
+  end
+
+  it 'shows the image' do
+    expect(rendered).to match(/<img src="http/)
   end
 end
