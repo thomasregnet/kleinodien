f21720e159bd4f966b0450df309e53db23444431
thomasregnet <ossi@asgard.localdomain>
2020-11-23 13:52:45 +0100

    Add #file to Image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+# Image
 class Image < ApplicationRecord
+  include FileAttachable
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,34 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+require 'test_data/get_empty_image_service'
 
 RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should respond_to(:coverartarchive_code) }
+
+  describe '#file' do
+    let(:image) { described_class.new }
+
+    context 'when attached' do
+      before { image.file.attach(io: TestData::GetEmptyImageService.as_io, filename: 'an_image') }
+
+      it 'returns an instance of ActiveStorage::Attached::One' do
+        expect(image.file).to be_instance_of(ActiveStorage::Attached::One)
+      end
+
+      it 'is attached' do
+        expect(image.file).to be_attached
+      end
+    end
+
+    context 'when not attached' do
+      it 'returns an instance of ActiveStorage::Attached::One' do
+        expect(image.file).to be_instance_of(ActiveStorage::Attached::One)
+      end
+
+      it 'is attached' do
+        expect(image.file).not_to be_attached
+      end
+    end
+  end
 end
