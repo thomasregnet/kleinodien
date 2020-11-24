4bbcde89ed5bcf05b2bfb2e1d363d79e5b45273b
thomasregnet <ossi@asgard.localdomain>
2020-11-24 21:40:27 +0100

    Find existing Image

--- spec/importers/import_cover_art_release_spec.rb
+++ spec/importers/import_cover_art_release_spec.rb
@@ -26,4 +26,26 @@ RSpec.describe ImportCoverArtRelease do
       end
     end
   end
+
+  context 'when a front-cover already exists' do
+    let(:brainz_code) { '58e6a3d6-bbbd-4864-983b-e468a5a1a71c' }
+    let(:import_order) { FactoryBot.create(:cover_art_release_import_order, code: brainz_code) }
+    let(:release) { Release.find_by(brainz_code: brainz_code) }
+
+    before do
+      allow(Image).to receive(:find_by).and_return(Image.new)
+      FactoryBot.create(:release, brainz_code: '58e6a3d6-bbbd-4864-983b-e468a5a1a71c')
+      described_class.call(import_order: import_order)
+    end
+
+    it 'attaches cover images to the release' do
+      expect(release.images.length).to be > 0
+    end
+
+    it "saves it's ImportOrder" do
+      release.images.each do |release_image|
+        expect(release_image.import_order).to be_instance_of(CoverArtReleaseImportOrder)
+      end
+    end
+  end
 end
