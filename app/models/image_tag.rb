3f30df6296044fab6bbc8c99e2bd5a0c7420be68
thomasregnet <ossi@asgard.localdomain>
2020-11-30 11:58:59 +0100

    Add ImageTag.find_or_create_by_name

--- app/models/image_tag.rb
+++ app/models/image_tag.rb
@@ -4,4 +4,8 @@
 class ImageTag < ApplicationRecord
   has_and_belongs_to_many :release_images
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  def self.find_or_create_by_name(name)
+    where('LOWER(name) = ?', name.downcase).first || create(name: name)
+  end
 end
