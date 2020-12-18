84521ddddea1c3c619f85ff7992df9cbc8fd7a8d
thomasregnet <ossgewalt@gmail.com>
2020-12-18 11:45:49 +0100

    Test CoverArtReleaseImportOrder#item_designation

--- spec/models/cover_art_release_import_order_spec.rb
+++ spec/models/cover_art_release_import_order_spec.rb
@@ -2,13 +2,14 @@
 
 require 'rails_helper'
 require 'shared_examples_for_active_import_orders'
+require 'shared_examples_for_concrete_import_orders'
 require 'shared_examples_for_import_orders'
 
 RSpec.describe CoverArtReleaseImportOrder, type: :model do
   it { should respond_to(:item_designation) }
 
   it_behaves_like 'an active ImportOrder', :cover_art_release_import_order
-
+  it_behaves_like 'a concrete ImportOrder'
   it_behaves_like 'an ImportOrder', :cover_art_release_import_order
 
   describe '#item' do
@@ -23,17 +24,24 @@ RSpec.describe CoverArtReleaseImportOrder, type: :model do
         end
       end
     end
+  end
 
-    context 'when done' do
-      let(:cover_art_code) { '50cdde32-1ed4-11eb-be0c-08606e75dc17' }
-      let(:import_order) { described_class.new(code: :cover_art_code, state: :done) }
+  context 'when state is "done"' do
+    let(:code) { '50cdde32-1ed4-11eb-be0c-08606e75dc17' }
+    let(:import_order) { described_class.new(code: code, state: :done) }
 
-      before do
-        import_order.code = cover_art_code
-        FactoryBot.create(:release, brainz_code: cover_art_code)
-      end
+    before do
+      FactoryBot.create(:release, brainz_code: code, title: 'my title')
+    end
 
+    describe '#item' do
       it 'returns a release' do
+        expect(import_order.item).to be_instance_of(Release)
+      end
+    end
+
+    describe '#item_designation' do
+      it 'returns a title of the release' do
         expect(import_order.item).to be_instance_of(Release)
       end
     end
