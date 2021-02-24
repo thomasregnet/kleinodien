94ba5b2d9805ba30f1a642c7585be7e526a3ce1c
thomasregnet <ossgewalt@gmail.com>
2021-02-24 21:44:23 +0100

    Use Docker-Rails-Template

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,6 @@
 # https://stackoverflow.com/questions/24962319/is-there-a-more-direct-way-to-do-a-pub-sub-pattern-in-rails-than-observers
 
+# config/initializers/redis.rb
+uri = URI.parse ENV.fetch("REDISTOGO_URL", 'http://127.0.0.1:6379')
+REDIS_CONFIG = { host: uri.host, port: uri.port, password: uri.password }
 REDIS = Redis.new(url: ENV.fetch('REDIS_URL', 'redis://redis:6379'))
