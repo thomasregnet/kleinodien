4629b71a4602b8c70724bd29f5fa5fc18839e6f4
thomasregnet <ossgewalt@gmail.com>
2021-03-09 12:38:02 +0100

    Add shared_examples for access by a curator

--- spec/requests/artists_spec.rb
+++ spec/requests/artists_spec.rb
@@ -3,9 +3,10 @@
 require 'rails_helper'
 require 'support/shared_examples_for_a_curated_model_accessed_by_an_unauthorized_user'
 require 'support/shared_examples_for_a_curated_model_accessed_by_an_user_without_curator_rights'
+require 'support/shared_examples_for_a_curated_model_accessed_by_a_curator'
 
 RSpec.describe '/artists', type: :request do
   it_behaves_like 'a curated model accessed by an unauthorized user', 'artist'
   it_behaves_like 'a curated model accessed by an user without curator rights', 'artist'
-  # it_behaves_like 'a curated model accessed by a curator', 'artist'
+  it_behaves_like 'a curated model accessed by a curator', 'artist'
 end
