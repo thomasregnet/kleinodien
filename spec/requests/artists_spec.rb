e52326049b04527a391ba1fdf9e1a71d7889c3f7
thomasregnet <ossgewalt@gmail.com>
2021-03-10 11:46:27 +0100

    Add specs for GET /index and GET /show

--- spec/requests/artists_spec.rb
+++ spec/requests/artists_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe '/artists', type: :request do
   it_behaves_like 'a curated model accessed by an unauthorized user', 'artist'
   it_behaves_like 'a curated model accessed by an user without curator rights', 'artist'
   it_behaves_like 'a curated model accessed by a curator', 'artist'
+
+  describe 'GET /index' do
+    it 'renders a successful response' do
+      FactoryBot.create(:artist)
+      get artists_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /show' do
+    it 'renders a successful response' do
+      artist = FactoryBot.create(:artist)
+      get artist_url(artist)
+      expect(response).to be_successful
+    end
+  end
 end
