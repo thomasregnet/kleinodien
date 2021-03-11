48a334c7a433588a34950773859a151a28889696
thomasregnet <ossgewalt@gmail.com>
2021-03-11 11:30:39 +0100

    Implement IncompleteDate(year, month, day)

--- app/models/incomplete_date.rb
+++ app/models/incomplete_date.rb
@@ -4,42 +4,63 @@ require 'date'
 
 # Representation of a date which must not consist of all
 # parameters year, month and day.
-class IncompleteDate < Delegator
-  VALID_MASKS = [4, 6, 7].freeze
-
-  attr_reader :date, :mask
-
+# class IncompleteDate < Delegator
+class IncompleteDate
   def self.from_string(date_string)
-    digits = date_string.split('-').map(&:to_i)
-    mask   = [4, 6, 7].fetch(digits.length - 1)
-    date   = Date.new(*digits)
-    new(date, mask)
+    year, month, day = date_string.split('-').map(&:to_i)
+    new(year, month, day)
   end
 
-  # rubocop:disable Lint/MissingSuper
-  def initialize(date, mask)
-    raise(ArgumentError, "invalid mask: #{mask}") \
-      unless VALID_MASKS.include?(mask)
+  def initialize(year = nil, month = nil, day = nil)
+    init_date(year, month, day)
 
-    @date = date
-    @mask = mask
+    @year  = year
+    @month = month
+    @day   = day
+  rescue Date::Error => e
+    Rails.logger.error("Bad value for IncompleteDate: #{e.message}")
   end
-  # rubocop:enable Lint/MissingSuper
+
+  attr_reader :year, :month, :day
+
+  def accuracy
+    return unless year
+
+    return :day if day
+    return :month if month
+
+    :year
+  end
 
   def to_s
-    case mask
-    when 4
+    return unless year
+
+    case accuracy
+    when :year
       year.to_s
-    when 6
-      strftime('%Y-%m')
-    when 7
-      strftime('%F')
+    when :month
+      date.strftime('%Y-%m')
     else
-      raise "invalid mask: #{mask}"
+      date.strftime('%F')
     end
   end
 
-  def __getobj__
-    date
+  def valid?
+    date ? true : false
+  end
+
+  private
+
+  attr_reader :date
+
+  def init_date(year, month, day)
+    raise Date::Error, 'if day is set month needs a value' if day && !month
+    raise Date::Error, 'if month is set year needs a value' if month && !year
+
+    @date = date_for(year, month, day)
+  end
+
+  def date_for(year, month, day)
+    Date.new(year || 1, month || 1, day || 1)
   end
 end
--- spec/models/incomplete_date_spec.rb
+++ spec/models/incomplete_date_spec.rb
@@ -3,135 +3,150 @@
 require 'rails_helper'
 
 RSpec.describe IncompleteDate, type: :model do
-  describe '.new' do
-    let(:incomplete_date) do
-      described_class.new(Date.new(2015, 12, 13), 7)
-    end
-
-    specify '#date' do
-      expect(incomplete_date.date).to be_instance_of Date
-    end
-
-    specify '#mask' do
-      expect(incomplete_date.mask).to eq 7
-    end
-  end
-
   describe '.from_string' do
     context 'with a complete date' do
-      let(:incomplete_date) do
-        described_class.from_string('2015-12-13')
+      let(:incomplete_date) { described_class.from_string('2021-03-11') }
+
+      it 'is valid' do
+        expect(incomplete_date).to be_valid
       end
 
-      it 'returns 7 when it receives #mask' do
-        expect(incomplete_date.mask).to eq 7
+      it 'has set the correct year' do
+        expect(incomplete_date.year).to eq(2021)
       end
 
-      it 'returns the year' do
-        expect(incomplete_date.year).to eq(2015)
+      it 'has set the correct month' do
+        expect(incomplete_date.month).to eq(3)
       end
 
-      it 'returns the month' do
-        expect(incomplete_date.month).to eq(12)
-      end
-
-      it 'returns the dey' do
-        expect(incomplete_date.day).to eq(13)
+      it 'has set the correct day' do
+        expect(incomplete_date.day).to eq(11)
       end
     end
 
     context 'with year and month' do
-      let(:incomplete_date) do
-        described_class.from_string('2015-12')
+      let(:incomplete_date) { described_class.from_string('2021-03') }
+
+      it 'is valid' do
+        expect(incomplete_date).to be_valid
       end
 
-      it 'returns 7 when it receives #mask' do
-        expect(incomplete_date.mask).to eq 6
+      it 'has set the correct year' do
+        expect(incomplete_date.year).to eq(2021)
       end
 
-      it 'returns the year' do
-        expect(incomplete_date.year).to eq(2015)
+      it 'has set the correct month' do
+        expect(incomplete_date.month).to eq(3)
       end
 
-      it 'returns the month' do
-        expect(incomplete_date.month).to eq(12)
+      it 'has set the day to nil' do
+        expect(incomplete_date.day).to be_nil
+      end
+    end
+
+    context 'with only a year' do
+      let(:incomplete_date) { described_class.from_string('2021') }
+
+      it 'is valid' do
+        expect(incomplete_date).to be_valid
       end
 
-      it 'returns 1 for the dey' do
-        expect(incomplete_date.day).to eq(1)
+      it 'has set the correct year' do
+        expect(incomplete_date.year).to eq(2021)
+      end
+
+      it 'has set the month to nil' do
+        expect(incomplete_date.month).to be_nil
+      end
+
+      it 'has set the day to nil' do
+        expect(incomplete_date.day).to be_nil
+      end
+    end
+
+    context 'with an invalid string' do
+      let(:incomplete_date) { described_class.from_string('2021-13-44') }
+
+      it 'is not valid' do
+        expect(incomplete_date).not_to be_valid
+      end
+
+      it 'has set the year to nil' do
+        expect(incomplete_date.year).to be_nil
+      end
+    end
+  end
+
+  describe '.new' do
+    context 'with valid values' do
+      let(:incomplete_date) { described_class.new(2021, 3, 11) }
+
+      it 'is valid' do
+        expect(incomplete_date).to be_valid
+      end
+    end
+
+    context 'without any values' do
+      let(:incomplete_date) { described_class.new(nil, nil, nil) }
+
+      it 'is valid' do
+        expect(incomplete_date).to be_valid
+      end
+    end
+  end
+
+  describe '#accuracy' do
+    context 'with complete date' do
+      it 'returns :day' do
+        expect(described_class.new(2021, 3, 11).accuracy).to eq(:day)
+      end
+    end
+
+    context 'with year and month' do
+      it 'returns :month' do
+        expect(described_class.new(2021, 3).accuracy).to eq(:month)
       end
     end
 
     context 'with year only' do
-      let(:incomplete_date) do
-        described_class.from_string('2015')
+      it 'returns :year' do
+        expect(described_class.new(2021).accuracy).to eq(:year)
       end
+    end
 
-      it 'returns 7 when it receives #mask' do
-        expect(incomplete_date.mask).to eq 4
-      end
-
-      it 'returns the year' do
-        expect(incomplete_date.year).to eq(2015)
-      end
-
-      it 'returns 1 for the month' do
-        expect(incomplete_date.month).to eq(1)
-      end
-
-      it 'returns 1 for the dey' do
-        expect(incomplete_date.day).to eq(1)
+    context 'without values' do
+      it 'returns nil' do
+        expect(described_class.new().accuracy).to be_nil
       end
     end
   end
 
   describe '#to_s' do
     context 'with a complete date' do
-      let(:incomplete_date) do
-        described_class.new(Date.new(2019, 3, 2), 7)
-      end
-
       it 'returns the right date-string' do
-        expect(incomplete_date.to_s).to eq('2019-03-02')
+        expect(described_class.new(2019, 3, 2).to_s).to eq('2019-03-02')
       end
     end
 
     context 'with year and month' do
       let(:incomplete_date) do
-        described_class.new(Date.new(2019, 3, 2), 6)
+        described_class.new(2019, 3)
       end
 
       it 'returns the right date-string' do
-        expect(incomplete_date.to_s).to eq('2019-03')
+        expect(described_class.new(2019, 3).to_s).to eq('2019-03')
       end
     end
 
     context 'with year only' do
-      let(:incomplete_date) do
-        described_class.new(Date.new(2019, 3, 2), 4)
-      end
-
       it 'returns the right date-string' do
-        expect(incomplete_date.to_s).to eq('2019')
-      end
-    end
-  end
-
-  context 'with an invalid mask' do
-    describe '#to_s' do
-      let(:incomplete_date) { described_class.new(Date.new(2019, 3, 2), 7) }
-
-      it 'raises a RuntimeError' do
-        allow(incomplete_date).to receive(:mask).and_return(:wtf)
-        expect { incomplete_date.to_s }
-          .to raise_error(RuntimeError, /invalid mask/)
+        expect(described_class.new(2019).to_s).to eq('2019')
       end
     end
 
-    describe '.new' do
-      it 'raises an ArgumentError' do
-        expect { described_class.new(Date.new(2019, 3, 2), :wrong) }
-          .to raise_error(ArgumentError, /invalid mask/)
+    context 'without any values' do
+      it 'returns nil' do
+        expect(described_class.new.to_s).to be_nil
       end
     end
   end
