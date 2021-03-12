dd72772dcd7f45c6e9d3e7e9175da321cd055e16
thomasregnet <ossgewalt@gmail.com>
2021-03-12 10:21:28 +0100

    Delete comments

--- app/models/area_alias.rb
+++ app/models/area_alias.rb
@@ -5,19 +5,6 @@ class AreaAlias < ApplicationRecord
   include IncompletePeriodable
   belongs_to :area
 
-  # composed_of(
-  #   :begin_date,
-  #   allow_nil:  true,
-  #   class_name: 'IncompleteDate',
-  #   mapping:    [%w[begin_date date], %w[begin_date_mask mask]]
-  # )
-  # composed_of(
-  #   :end_date,
-  #   allow_nil:  true,
-  #   class_name: 'IncompleteDate',
-  #   mapping:    [%w[end_date date], %w[end_date_mask mask]]
-  # )
-
   before_validation :ensure_sort_name_has_a_value
 
   validates :name, :sort_name, presence: true
