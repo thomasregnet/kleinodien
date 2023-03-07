6b9144dd1c6e33183c5c38682a84f951797dceef
thomasregnet <ossgewalt@gmail.com>
2023-03-07 11:34:24 +0100

    Add tests and validations

--- app/models/music_brainz_import_order.rb
+++ app/models/music_brainz_import_order.rb
@@ -1,2 +1,6 @@
 class MusicBrainzImportOrder < ImportOrder
+  validates :code, presence: true
+  validates :kind, presence: true
+  validates :state, presence: true
+  validates :user, presence: true
 end
--- test/models/music_brainz_import_order_test.rb
+++ test/models/music_brainz_import_order_test.rb
@@ -4,4 +4,35 @@ class MusicBrainzImportOrderTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @import_order = music_brainz_import_orders(:music_brainz_one)
+  end
+
+  test "it schould be valid" do
+    assert_predicate(@import_order, :valid?)
+  end
+
+  test "without a code it is not valid" do
+    @import_order.code = nil
+
+    assert_not @import_order.valid?
+  end
+
+  test "without a kind it is not valid" do
+    @import_order.kind = nil
+
+    assert_not @import_order.valid?
+  end
+
+  test "without a state it is not valid" do
+    @import_order.state = nil
+
+    assert_not @import_order.valid?
+  end
+
+  test "without a user it is not valid" do
+    @import_order.user = nil
+
+    assert_not @import_order.valid?
+  end
 end
