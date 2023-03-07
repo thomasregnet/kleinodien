a1c9dc136d75b060c52352f4f4e750ea34cdd6be
thomasregnet <ossgewalt@gmail.com>
2023-03-07 13:57:43 +0100

    Share tests between ImportOrder-descendants

--- test/models/music_brainz_import_order_test.rb
+++ test/models/music_brainz_import_order_test.rb
@@ -1,38 +1,10 @@
 require "test_helper"
+require "shared_import_order_tests"
 
 class MusicBrainzImportOrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include SharedImportOrderTests
+
   setup do
     @import_order = music_brainz_import_orders(:music_brainz_one)
-  end
-
-  test "it schould be valid" do
-    assert_predicate(@import_order, :valid?)
-  end
-
-  test "without a code it is not valid" do
-    @import_order.code = nil
-
-    assert_not @import_order.valid?
-  end
-
-  test "without a kind it is not valid" do
-    @import_order.kind = nil
-
-    assert_not @import_order.valid?
-  end
-
-  test "without a state it is not valid" do
-    @import_order.state = nil
-
-    assert_not @import_order.valid?
-  end
-
-  test "without a user it is not valid" do
-    @import_order.user = nil
-
-    assert_not @import_order.valid?
   end
 end
---
+++ test/shared_import_order_tests.rb
@@ -0,0 +1,31 @@
+# require "test_helper"
+
+module SharedImportOrderTests
+  def test_import_order_is_valid
+    assert_predicate(@import_order, :valid?)
+  end
+
+  def test_without_a_code_it_is_not_valid
+    @import_order.code = nil
+
+    assert_not @import_order.valid?
+  end
+
+  def test_without_a_kind_it_is_not_valid
+    @import_order.kind = nil
+
+    assert_not @import_order.valid?
+  end
+
+  def test_without_a_state_it_is_not_valid
+    @import_order.state = nil
+
+    assert_not @import_order.valid?
+  end
+
+  def test_without_a_user_it_is_not_valid
+    @import_order.user = nil
+
+    assert_not @import_order.valid?
+  end
+end
