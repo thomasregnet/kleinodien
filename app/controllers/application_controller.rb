a375f838031dffcbd4021d7ab3ddf4ae93526c43
thomasregnet <ossgewalt@gmail.com>
2023-03-09 13:48:30 +0100

    Fix one RuboCop issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate
-    if session = Session.find_by(id: cookies.signed[:session_token])
+    if (session = Session.find_by(id: cookies.signed[:session_token]))
       Current.session = session
     else
       redirect_to sign_in_path
