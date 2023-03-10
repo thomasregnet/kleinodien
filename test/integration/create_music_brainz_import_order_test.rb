89800770c6b77dae96fc4d8ff416483203278c84
thomasregnet <ossgewalt@gmail.com>
2023-03-10 11:49:40 +0100

    Test creation of a MusicBrainzImportOrder

--- test/integration/create_music_brainz_import_order_test.rb
+++ test/integration/create_music_brainz_import_order_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class CreateMusicBrainzImportOrderTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = sign_in_as(users(:kim))
+  end
+
+  def test_can_create_a_music_brainz_import_order
+    get new_import_order_path
+
+    assert_response :success
+
+    post "/import_orders", params: {import_order: {code: "c0f4b036-e215-43f7-970d-9506e053ea82", kind: "release", state: "pending", type: "MusicBrainzImportOrder", user_id: @user.id}}
+
+    assert_response :redirect
+    follow_redirect!
+
+    assert_response :success
+    assert_select "p", "Import order was successfully created."
+  end
 end
