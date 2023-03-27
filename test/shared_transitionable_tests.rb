57056a8d0fdd4102b853d581c649aac43a72fa96
thomasregnet <ossgewalt@gmail.com>
2023-03-27 20:43:02 +0200

    Remove dead code

--- test/shared_transitionable_tests.rb
+++ test/shared_transitionable_tests.rb
@@ -3,10 +3,6 @@ module SharedTransitionableTests
     assert_respond_to @subject, :state=
   end
 
-  def test_respond_to_can_change_state_to?
-    assert_respond_to @subject, :state_can_be_changed_to?
-  end
-
   def test_with_an_illegal_state
     assert_raises(ArgumentError) { @subject.state = :illegal_state }
   end
