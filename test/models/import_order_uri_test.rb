e29c718c978a80cdc4af695c35514a04fdf42925
thomasregnet <ossgewalt@gmail.com>
2023-04-26 19:45:05 +0200

    Test ImportOrderUri::* with an OpenStruct

--- test/models/import_order_uri_test.rb
+++ test/models/import_order_uri_test.rb
@@ -17,4 +17,30 @@ class ImportOrderUriTest < ActiveSupport::TestCase
     assert_equal "23cbaa49-1e24-4550-88c9-d07de78b5b89", iou.code
     assert_equal "release", iou.kind
   end
+
+  def test_common
+    iou = OpenStruct.new(path: "/foo/bar")
+
+    iou.extend ImportOrderUri::Common
+
+    assert_nil iou.code
+    assert_nil iou.kind
+  end
+
+  def test_musicbrainz_with_a_valid_path
+    iou = OpenStruct.new(path: "/release/23cbaa49-1e24-4550-88c9-d07de78b5b89")
+
+    iou.extend ImportOrderUri::MusicBrainz
+
+    assert_equal "23cbaa49-1e24-4550-88c9-d07de78b5b89", iou.code
+    assert_equal "release", iou.kind
+  end
+
+  def test_musicbrainz_with_an_invalid_path
+    iou = OpenStruct.new(path: "/wrong/not-a-uuid")
+    iou.extend ImportOrderUri::MusicBrainz
+
+    assert_nil iou.code
+    assert_nil iou.kind
+  end
 end
