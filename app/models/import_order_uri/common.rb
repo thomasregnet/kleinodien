fbc4c2a245892691916f26bc60e9ce98f8618282
thomasregnet <ossgewalt@gmail.com>
2023-04-26 20:10:52 +0200

    Let #kind_and_code return a Data-object

--- app/models/import_order_uri/common.rb
+++ app/models/import_order_uri/common.rb
@@ -1,15 +1,19 @@
 module ImportOrderUri
+  KindAndCode = Data.define(:kind, :code)
+
   module Common
     def kind_and_code = nil
 
     def import_order_class = ImportOrder
 
     def code
-      kind_and_code&.named_captures&.dig("code")
+      # kind_and_code&.named_captures&.dig("code")
+      kind_and_code&.code
     end
 
     def kind
-      kind_and_code&.named_captures&.dig("kind")
+      # kind_and_code&.named_captures&.dig("kind")
+      kind_and_code&.kind
     end
   end
 end
--- app/models/import_order_uri/music_brainz.rb
+++ app/models/import_order_uri/music_brainz.rb
@@ -4,7 +4,9 @@ module ImportOrderUri
     KIND_AND_CODE_REGEX = %r{/(?<kind>[a-z-]+)/(?<code>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})}
 
     def kind_and_code
-      path&.match KIND_AND_CODE_REGEX
+      path
+        &.match(KIND_AND_CODE_REGEX)
+        &.then { |match_data| KindAndCode.new(*match_data.values_at(1, 2)) }
     end
   end
 end
