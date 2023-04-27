f941df295470e6e09386693566ec163222f02e58
thomasregnet <ossgewalt@gmail.com>
2023-04-27 20:52:05 +0200

    Reestablish ImportOrderUri.build

--- app/models/import_order_uri.rb
+++ app/models/import_order_uri.rb
@@ -1,2 +1,19 @@
 module ImportOrderUri
+  EXTENSION_FOR = {
+    "musicbrainz.org" => ::ImportOrderUri::MusicBrainz
+  }.freeze
+
+  def self.build(uri_candidate)
+    return uri_candidate if uri_candidate.is_a? ImportOrderUri
+
+    uri_object = URI(uri_candidate)
+    host = uri_object.host
+    extension_key = uri_object
+      &.host
+      &.then { EXTENSION_FOR.keys.find { |key| key.ends_with?(host) } }
+
+    extension = EXTENSION_FOR.fetch(extension_key, ImportOrderUri::Common)
+
+    uri_object.extend extension
+  end
 end
--- test/models/import_order_uri_test.rb
+++ test/models/import_order_uri_test.rb
@@ -2,27 +2,25 @@ require "test_helper"
 
 class ImportOrderUriTest < ActiveSupport::TestCase
   def test_common
-    iou = OpenStruct.new(path: "/foo/bar")
+    iou = ImportOrderUri.build("https://example.com/some/stuff")
 
-    iou.extend ImportOrderUri::Common
-
+    assert_kind_of ImportOrderUri::Common, iou
     assert_nil iou.code
     assert_nil iou.kind
   end
 
   def test_musicbrainz_with_a_valid_path
-    iou = OpenStruct.new(path: "/release/23cbaa49-1e24-4550-88c9-d07de78b5b89")
+    iou = ImportOrderUri.build("https://musicbrainz.org/release/23cbaa49-1e24-4550-88c9-d07de78b5b89")
 
-    iou.extend ImportOrderUri::MusicBrainz
-
+    assert_kind_of ImportOrderUri::MusicBrainz, iou
     assert_equal "23cbaa49-1e24-4550-88c9-d07de78b5b89", iou.code
     assert_equal "release", iou.kind
   end
 
   def test_musicbrainz_with_an_invalid_path
-    iou = OpenStruct.new(path: "/wrong/not-a-uuid")
-    iou.extend ImportOrderUri::MusicBrainz
+    iou = ImportOrderUri.build("https://musicbrainz.org/this/is/not-an-uuid")
 
+    assert_kind_of ImportOrderUri::MusicBrainz, iou
     assert_nil iou.code
     assert_nil iou.kind
   end
