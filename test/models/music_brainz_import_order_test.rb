78ae90cbf36bafcd73a3f9e4ae7f57964bb391d1
thomasregnet <ossgewalt@gmail.com>
2023-05-02 21:45:07 +0200

    Add tests with an uri as attribute

--- test/models/music_brainz_import_order_test.rb
+++ test/models/music_brainz_import_order_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class MusicBrainzImportOrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_with_a_valid_uri
+    import_order = MusicBrainzImportOrder.new(uri: "https://musicbrainz.org/release/41362dd7-0665-4e09-8158-9ad8109d47bc", user: users(:kim))
+
+    assert_predicate import_order, :valid?
+    assert_equal "release", import_order.kind
+    assert_equal "41362dd7-0665-4e09-8158-9ad8109d47bc", import_order.code
+  end
+
+  def test_with_an_invalid_uri
+    import_order = MusicBrainzImportOrder.new(uri: "https://musicbrainz.org/non/sense", user: users(:kim))
+
+    assert_not_predicate import_order, :valid?
+    assert_nil import_order.kind
+    assert_nil import_order.code
+  end
 end
