78134ac36c0207843312e1df7e8c70b49d5c10e4
thomasregnet <ossgewalt@gmail.com>
2023-05-02 21:19:12 +0200

    Add tests for ImportOrderUri#import_order_type

--- test/models/import_order_uri_test.rb
+++ test/models/import_order_uri_test.rb
@@ -4,7 +4,13 @@ class ImportOrderUriTest < ActiveSupport::TestCase
   def test_common
     iou = ImportOrderUri.build("https://example.com/some/stuff")
 
+    assert_equal "ImportOrder", iou.import_order_type
     assert_kind_of ImportOrderUri::Common, iou
+  end
+
+  def test_common_kind_and_code
+    iou = ImportOrderUri.build("https://example.com/some/stuff")
+
     assert_nil iou.code
     assert_nil iou.kind
   end
@@ -12,7 +18,13 @@ class ImportOrderUriTest < ActiveSupport::TestCase
   def test_musicbrainz_with_a_valid_path
     iou = ImportOrderUri.build("https://musicbrainz.org/release/23cbaa49-1e24-4550-88c9-d07de78b5b89")
 
+    assert_equal "MusicBrainzImportOrder", iou.import_order_type
     assert_kind_of ImportOrderUri::MusicBrainz, iou
+  end
+
+  def test_musicbrainz_kind_and_kode_with_a_valid_path
+    iou = ImportOrderUri.build("https://musicbrainz.org/release/23cbaa49-1e24-4550-88c9-d07de78b5b89")
+
     assert_equal "23cbaa49-1e24-4550-88c9-d07de78b5b89", iou.code
     assert_equal "release", iou.kind
   end
