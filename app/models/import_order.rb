8679dfff63a62780eadc37a4ed54767ae00b8837
thomasregnet <ossgewalt@gmail.com>
2023-05-02 21:01:17 +0200

    Set kind and code from uri

--- app/models/import_order.rb
+++ app/models/import_order.rb
@@ -1,3 +1,17 @@
 class ImportOrder < ApplicationRecord
   include Importable
+
+  before_validation :set_kind_and_code
+
+  private
+
+  def set_kind_and_code
+    return if kind
+    return if code
+    return unless uri
+
+    import_order_uri = ImportOrderUri.build(uri)
+    self.kind = import_order_uri.kind
+    self.code = import_order_uri.code
+  end
 end
