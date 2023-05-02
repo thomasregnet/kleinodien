36aa0af9b8440de45b8ea0eb3d1d9f770f375273
thomasregnet <ossgewalt@gmail.com>
2023-05-02 21:50:48 +0200

    Add tests with #uri and #kind or #code

--- test/models/music_brainz_import_order_test.rb
+++ test/models/music_brainz_import_order_test.rb
@@ -16,4 +16,20 @@ class MusicBrainzImportOrderTest < ActiveSupport::TestCase
     assert_nil import_order.kind
     assert_nil import_order.code
   end
+
+  def test_with_a_valid_uri_and_a_kind
+    import_order = MusicBrainzImportOrder.new(kind: "release", uri: "https://musicbrainz.org/non/sense", user: users(:kim))
+
+    assert_not_predicate import_order, :valid?
+    assert_equal "release", import_order.kind
+    assert_nil import_order.code
+  end
+
+  def test_with_a_valid_uri_and_code
+    import_order = MusicBrainzImportOrder.new(code: "41362dd7-0665-4e09-8158-9ad8109d47bc", uri: "https://musicbrainz.org/non/sense", user: users(:kim))
+
+    assert_not_predicate import_order, :valid?
+    assert_nil import_order.kind
+    assert_equal "41362dd7-0665-4e09-8158-9ad8109d47bc", import_order.code
+  end
 end
