4a62a9f835de7c840ed1eb906b5c70486fdabccb
thomasregnet <ossgewalt@gmail.com>
2023-05-08 21:39:14 +0200

    Fix formatting and typo

--- test/shared_bufferable_import_order_tests.rb
+++ test/shared_bufferable_import_order_tests.rb
@@ -1,6 +1,7 @@
 module SharedBufferableImportOrderTests
-  def test_illegal_transigion_from_open_to_persisting
+  def test_illegal_transition_from_open_to_persisting
     assert_equal @subject.state, "open"
+
     assert_raises(RuntimeError) { @subject.persisting! }
   end
 end
