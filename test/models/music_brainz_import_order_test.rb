341ed9849fef67981d64ed151a96c73ec22ac3f3
thomasregnet <ossgewalt@gmail.com>
2023-05-08 20:22:24 +0200

    Take advantage of SharedImportOrderTests

--- test/models/music_brainz_import_order_test.rb
+++ test/models/music_brainz_import_order_test.rb
@@ -1,12 +1,27 @@
 require "test_helper"
+require "shared_import_order_tests"
 
 class MusicBrainzImportOrderTest < ActiveSupport::TestCase
+  include SharedImportOrderTests
+
+  def setup
+    @subject = MusicBrainzImportOrder.new(kind: "release", code: "41362dd7-0665-4e09-8158-9ad8109d47bc", user: users(:kim))
+  end
+
   def test_with_a_valid_uri
     import_order = MusicBrainzImportOrder.new(uri: "https://musicbrainz.org/release/41362dd7-0665-4e09-8158-9ad8109d47bc", user: users(:kim))
 
     assert_predicate import_order, :valid?
     assert_equal "release", import_order.kind
     assert_equal "41362dd7-0665-4e09-8158-9ad8109d47bc", import_order.code
+  end
+
+  def test_import_order_uri_type
+    assert_nil @subject.uri
+
+    @subject.uri = "https://musicbrainz.org/release/41362dd7-0665-4e09-8158-9ad8109d47bc"
+
+    assert_kind_of ImportOrderUri::MusicBrainz, @subject.uri
   end
 
   def test_with_an_invalid_uri
