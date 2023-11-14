3e8c7b9f78d7d9d8f056dccfeb0699c17ff523c3
thomasregnet <ossgewalt@gmail.com>
2023-11-14 11:10:54 +0100

    Add ArtistCredit#name
    - ArtistCreditParticipant delegates #name to #participant

--- app/models/artist_credit.rb
+++ app/models/artist_credit.rb
@@ -1,3 +1,16 @@
 class ArtistCredit < ApplicationRecord
   has_many :artist_credit_participants, dependent: :destroy
+
+  def name
+    joinables = artist_credit_participants.map { |acp| [acp.name, join_phrase_for(acp)] }
+    joinables = joinables.flatten
+    joinables.pop # ignore the last join_phrase
+    joinables.join
+  end
+
+  private
+
+  def join_phrase_for(artist_credit_participant)
+    artist_credit_participant.join_phrase || " "
+  end
 end
--- test/models/artist_credit_test.rb
+++ test/models/artist_credit_test.rb
@@ -4,4 +4,18 @@ class ArtistCreditTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "join names" do
+    artist_credit = ArtistCredit.new
+
+    participant = Participant.create!(name: "first", sort_name: "first")
+    acp = ArtistCreditParticipant.new(participant: participant, join_phrase: " feat. ", position: 0)
+    artist_credit.artist_credit_participants << acp
+
+    participant = Participant.create!(name: "second", sort_name: "second")
+    acp = ArtistCreditParticipant.new(participant: participant, join_phrase: " bad ", position: 1)
+    artist_credit.artist_credit_participants << acp
+
+    assert_equal "first feat. second", artist_credit.name
+  end
 end
