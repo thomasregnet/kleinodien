f389d9ae44374732c379a924aa9df4cbc718765a
thomasregnet <ossgewalt@gmail.com>
2023-11-14 11:36:24 +0100

    Test ArtistCreditParticipant delegates name

--- test/models/artist_credit_participant_test.rb
+++ test/models/artist_credit_participant_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class ArtistCreditParticipantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "delegates name to Participant" do
+    artist_credit = ArtistCredit.create!(name: "C-List celebrity")
+    participant = Participant.create!(name: "Superstar", sort_name: "Superstar")
+
+    artist_credit_participant = ArtistCreditParticipant.new(artist_credit: artist_credit, participant: participant, position: 0)
+
+    assert_equal "Superstar", artist_credit_participant.name
+  end
 end
