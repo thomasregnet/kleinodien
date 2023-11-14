fd6e9043b87cb7ba0b82ff84d93d4df06674d123
thomasregnet <ossgewalt@gmail.com>
2023-11-14 11:19:16 +0100

    Rename ArtistCredit#artist_credit_participants
    .. to ArtistCredit#participants

--- app/models/artist_credit.rb
+++ app/models/artist_credit.rb
@@ -1,8 +1,8 @@
 class ArtistCredit < ApplicationRecord
-  has_many :artist_credit_participants, dependent: :destroy
+  has_many :participants, class_name: "ArtistCreditParticipant", dependent: :destroy
 
   def name
-    joinables = artist_credit_participants.map { |acp| [acp.name, join_phrase_for(acp)] }
+    joinables = participants.map { |acp| [acp.name, join_phrase_for(acp)] }
     joinables = joinables.flatten
     joinables.pop # ignore the last join_phrase
     joinables.join
--- test/models/artist_credit_test.rb
+++ test/models/artist_credit_test.rb
@@ -10,11 +10,11 @@ class ArtistCreditTest < ActiveSupport::TestCase
 
     participant = Participant.create!(name: "first", sort_name: "first")
     acp = ArtistCreditParticipant.new(participant: participant, join_phrase: " feat. ", position: 0)
-    artist_credit.artist_credit_participants << acp
+    artist_credit.participants << acp
 
     participant = Participant.create!(name: "second", sort_name: "second")
     acp = ArtistCreditParticipant.new(participant: participant, join_phrase: " bad ", position: 1)
-    artist_credit.artist_credit_participants << acp
+    artist_credit.participants << acp
 
     assert_equal "first feat. second", artist_credit.name
   end
