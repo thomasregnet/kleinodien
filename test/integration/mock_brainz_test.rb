b76aaedd2a370d0240ebb3b0ca4631a3144d0573
thomasregnet <ossgewalt@gmail.com>
2023-11-21 11:35:41 +0100

    Add WebMockExternalApis
    WebMockExternalApis - module to mock http and https requests
    
    Does nothing usefull for now.

--- test/integration/mock_brainz_test.rb
+++ test/integration/mock_brainz_test.rb
@@ -1,8 +1,11 @@
 require "test_helper"
-require "webmock/minitest"
-require "support/web_mock_music_brainz"
+require "support/web_mock_external_apis"
 
 class MockBrainzTest < ActionDispatch::IntegrationTest
+  setup do
+    WebMockExternalApis.setup
+  end
+
   def connection
     @connection ||= Faraday.new do |connection|
       connection.adapter Faraday.default_adapter
@@ -10,7 +13,6 @@ class MockBrainzTest < ActionDispatch::IntegrationTest
   end
 
   test "foobar" do
-    WebMock.stub_request(:any, /musicbrainz.org/).to_rack(WebMockMusicBrainz)
     response = connection.get("https://musicbrainz.org/ws/2/hello/world")
 
     assert_predicate response, :success?
