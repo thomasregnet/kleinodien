7babf7e6b6524217e27616692c04fdb6c6d4c526
thomasregnet <ossgewalt@gmail.com>
2023-11-23 08:58:59 +0100

    Generate Import::Buffer
    bin/rails generate model Import::Buffer --no-migration --no-fixture

---
+++ app/models/import/buffer.rb
@@ -0,0 +1,2 @@
+class Import::Buffer < ApplicationRecord
+end
---
+++ test/models/import/buffer_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Import::BufferTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
