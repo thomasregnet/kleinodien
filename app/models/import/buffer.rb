e9a1e326eaa4b9a5764bec7e91a61e1d4abee6e3
thomasregnet <ossgewalt@gmail.com>
2023-11-23 09:36:35 +0100

    Implement Import::Buffer#fetch and #musicbrainz

--- app/models/import/buffer.rb
+++ app/models/import/buffer.rb
@@ -1,2 +1,36 @@
-class Import::Buffer < ApplicationRecord
+class Import::Buffer
+  class DomainBuffer
+    def initialize(buffer, domain_key)
+      @buffer = buffer
+      @domain_key = domain_key
+    end
+
+    attr_reader :buffer, :domain_key
+
+    def fetch(*cache_key_parts, &block)
+      buffer.fetch("musicbrainz.org", cache_key_parts, &block)
+    end
+  end
+
+  def fetch(domain_key, *cache_key_parts, &block)
+    cache_key = cache_key_for(cache_key_parts)
+
+    cache[cache_key] = block.call if block
+    cache[cache_key]
+  end
+
+  def musicbrainz
+    DomainBuffer.new(self, "musicbrainz.org")
+  end
+
+  private
+
+  def cache
+    @cache ||= {}
+  end
+
+  def cache_key_for(*cache_key_parts)
+    raise ArgumentError, "can't build a cache_key without values" if cache_key_parts.empty?
+    cache_key_parts.map(&:to_s).join("/")
+  end
 end
--- test/models/import/buffer_test.rb
+++ test/models/import/buffer_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class Import::BufferTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @buffer = Import::Buffer.new
+  end
+
+  test "fetch" do
+    assert_nil @buffer.fetch(:foo, "bar")
+
+    assert_equal @buffer.fetch("foo", :bar) { :baz }, :baz
+    assert_equal @buffer.fetch("foo", :bar), :baz
+  end
+
+  test "musicbrainz" do
+    assert_nil @buffer.musicbrainz.fetch(:foo, "bar")
+
+    assert_equal @buffer.musicbrainz.fetch("foo", :bar) { :baz }, :baz
+    assert_equal @buffer.musicbrainz.fetch(:foo, "bar") { :baz }, :baz
+  end
 end
