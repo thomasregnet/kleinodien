30f52b7ba199463fa9a53d7ea0620c777e247a0e
thomasregnet <ossgewalt@gmail.com>
2023-11-23 10:05:20 +0100

    Use module Import class Buffer
    From
    
        class Import::Buffer
    
    to
    
        module Import
          class Buffer

--- app/models/import/buffer.rb
+++ app/models/import/buffer.rb
@@ -1,23 +1,25 @@
-class Import::Buffer
-  def fetch(domain_key, *cache_key_parts, &block)
-    cache_key = cache_key_for(cache_key_parts)
+module Import
+  class Buffer
+    def fetch(domain_key, *cache_key_parts, &block)
+      cache_key = cache_key_for(cache_key_parts)
 
-    cache[cache_key] = block.call if block
-    cache[cache_key]
-  end
+      cache[cache_key] = block.call if block
+      cache[cache_key]
+    end
 
-  def musicbrainz
-    Import::DomainBuffer.new(self, "musicbrainz.org")
-  end
+    def musicbrainz
+      Import::DomainBuffer.new(self, "musicbrainz.org")
+    end
 
-  private
+    private
 
-  def cache
-    @cache ||= {}
-  end
+    def cache
+      @cache ||= {}
+    end
 
-  def cache_key_for(*cache_key_parts)
-    raise ArgumentError, "can't build a cache_key without values" if cache_key_parts.empty?
-    cache_key_parts.map(&:to_s).join("/")
+    def cache_key_for(*cache_key_parts)
+      raise ArgumentError, "can't build a cache_key without values" if cache_key_parts.empty?
+      cache_key_parts.map(&:to_s).join("/")
+    end
   end
 end
