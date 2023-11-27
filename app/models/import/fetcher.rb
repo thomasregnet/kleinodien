abfc7d362aa4d2674b0336e3c13cbe2ca6b88fc0
thomasregnet <ossgewalt@gmail.com>
2023-11-27 12:18:47 +0100

    Let Fetcher call an attempt

--- app/models/import/fetcher.rb
+++ app/models/import/fetcher.rb
@@ -1,20 +1,14 @@
 module Import
   class Fetcher
-    # def self.get(factory:, uri:)
-    #   new(factory: factory, uri: uri).get
-    # end
-
     def initialize(factory:, uri:)
       @factory = factory
-      @ur = uri
+      @uri = uri
     end
 
     def get
       max_tries.times do
-        # TODO: response must accept uri
-        response = connection.get # (uri)
-        # TODO: don't return the body, instead call factory.purify(response)
-        return response.body if response.success?
+        response = attempt.get(uri)
+        return response if response
       end
 
       raise "can't get #{uri}"
--- test/models/import/fetcher_test.rb
+++ test/models/import/fetcher_test.rb
@@ -1,31 +1,45 @@
 require "test_helper"
 require "minitest/mock"
 
-class FakeFactory
+class FakeFetcherFactory
   def max_tries = 1
 
-  attr_accessor :connection
+  attr_accessor :attempt
+
+  def purify(response)
+    response
+  end
 end
 
 class Import::FetcherTest < ActiveSupport::TestCase
   setup do
-    @factory = FakeFactory.new
+    @factory = FakeFetcherFactory.new
     @fetcher = Import::Fetcher.new(factory: @factory, uri: "https://example.com")
   end
 
-  test "get" do
+  test "attempt.get returns a response" do
     response = Minitest::Mock.new
-    response.expect :success?, true
-    response.expect :body, {test: "success"}.to_json
+    response.expect :==, response, [response]
 
-    connection = Minitest::Mock.new
-    connection.expect :get, response
+    attempt = Minitest::Mock.new
+    attempt.expect :get, response, ["https://example.com"]
 
-    @factory.connection = connection
+    @factory.attempt = attempt
 
-    assert_equal @fetcher.get, {test: "success"}.to_json
+    assert_equal @fetcher.get, response
 
-    connection.verify
+    attempt.verify
     response.verify
+  end
+
+  test "attempt.get doesn't return a response" do
+    attempt = Minitest::Mock.new
+    attempt.expect :get, nil, ["https://example.com"]
+
+    @factory.attempt = attempt
+
+    assert_raises(RuntimeError) { @fetcher.get }
+
+    attempt.verify
   end
 end
