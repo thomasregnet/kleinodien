2cbdd263d3bf482f3c7dc96a5db1bf771bcc933e
thomasregnet <ossgewalt@gmail.com>
2023-11-29 10:05:22 +0100

    Add #errors

--- app/models/import/musicbrainz_interrupter.rb
+++ app/models/import/musicbrainz_interrupter.rb
@@ -1,9 +1,27 @@
 module Import
   class MusicbrainzInterrupter
+    def initialize
+      @errors = 0
+    end
+
+    attr_reader :errors
+
     def analyze?(response)
-      true
+      if response.success?
+        reset
+        return true
+      end
+
+      @errors += 1
+      false
     end
 
     def perform = nil
+
+    private
+
+    def reset
+      @errors = 0
+    end
   end
 end
--- test/models/import/musicbrainz_interrupter_test.rb
+++ test/models/import/musicbrainz_interrupter_test.rb
@@ -1,13 +1,37 @@
 require "test_helper"
+require "minitest/mock"
 
 class Import::MusicbrainzInterrupterTest < ActiveSupport::TestCase
   setup do
+    @response = Minitest::Mock.new
     @interrupter = Import::MusicbrainzInterrupter.new
   end
 
-  # TODO: Add more meaningful tests
-  test "methods" do
-    assert_respond_to @interrupter, :analyze?
-    assert_respond_to @interrupter, :perform
+  test "#analze? with a failed response" do
+    @response.expect :success?, false
+    assert_not @interrupter.analyze? @response
+    assert_equal @interrupter.errors, 1
+
+    @response.verify
+  end
+
+  test "#analze? with a failed response followed by a successful response" do
+    @response.expect :success?, false
+    @interrupter.analyze? @response
+
+    @response.expect :success?, true
+    assert @interrupter.analyze? @response
+    assert_equal @interrupter.errors, 0, "errors is set to 0"
+
+    @response.verify
+  end
+
+  test "with failed responses" do
+    3.times do
+      @response.expect(:success?, false)
+      @interrupter.analyze?(@response)
+    end
+
+    assert_equal @interrupter.errors, 3
   end
 end
