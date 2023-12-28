d4f6cc5e441cd7e27dde162fb1118a89c9bdefef
thomasregnet <ossgewalt@gmail.com>
2023-12-28 19:47:19 +0100

    Remove comment

--- test/models/import/import_musicbrainz_artist_test.rb
+++ test/models/import/import_musicbrainz_artist_test.rb
@@ -5,8 +5,6 @@ class Import::ImportMusicbrainzArtistTest < ActiveSupport::TestCase
   setup do
     WebMockExternalApis.setup
 
-    # @from = Import::From.new(:fake_import_order)
-    # @buffer_fill = Import::BufferMusicbrainzArtist.new(@from)
     factory = Import::MusicbrainzFactory.new(:fake_import_order)
     @intermediate = factory.build_intermediate(Participant, "66c662b6-6e2f-4930-8610-912e24c63ed1")
   end
