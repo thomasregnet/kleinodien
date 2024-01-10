f12342d36bd2a805d04c873b7fbbe0280ffd61cd
thomasregnet <ossgewalt@gmail.com>
2024-01-10 21:27:31 +0100

    Refactor Retrieve::Musicbrainz

--- test/support/retrieve/musicbrainz.rb
+++ test/support/retrieve/musicbrainz.rb
@@ -1,24 +1,37 @@
 require "pathname"
 
 module Retrieve
+  # MusicBrainz data by kind and code.
+  # Lookup data under test/webmock/musicbrainz.org/ws/2/#{kind}.
+  # We assume that the file with the longest name contains the most data.
+  # When more then one file "matches" code than return data form the file with
+  # the longest filename.
   class Musicbrainz
     def initialize(kind, code)
       @kind = kind.to_s
       @code = code.to_s
     end
 
+    def retrieve
+      file = dir.join(longest_filename)
+      file.read
+    end
+
+    private
+
     attr_reader :kind, :code
 
-    def retrieve
-      dir_path = Pathname.new("test/webmock/musicbrainz.org/ws/2/#{kind}")
-      dir = dir_path.opendir
+    def base_path
+      File.join("test", "webmock", "musicbrainz.org", "ws", "2", kind)
+    end
 
-      file_name = dir
-        .grep(%r{#{code}})
-        .max_by(&:length)
+    def dir
+      @dir ||= Pathname.new(base_path)
+    end
 
-      path = dir_path.join(file_name)
-      File.read(path)
+    def longest_filename
+      regex = %r{#{code}}
+      dir.opendir.grep(regex).max_by(&:length)
     end
   end
 end
