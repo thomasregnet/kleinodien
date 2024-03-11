b2f56413e82e853fca47fb0dea5c270eb63de116
thomasregnet <ossgewalt@gmail.com>
2024-03-11 09:17:55 +0100

    Always return nil on #call
    The ArtistCredit model does not have any codes, so the methods
    
    - `#find_by_intrinsic_code`
    - `#find_by_all_codes`
    
    have been removed.
    Maybe search for an ArtistCredit with the same `#name` in the future.

--- app/models/import/find_artist_credit.rb
+++ app/models/import/find_artist_credit.rb
@@ -7,28 +7,6 @@ module Import
 
     attr_reader :facade, :session
 
-    # model ????
-
-    def call
-      find_by_intrinsic_code || find_by_all_codes
-    end
-
-    def find_by_intrinsic_code
-      attrs = facade.intrinsic_code
-      return unless attrs
-
-      model.record_class.find(attrs)
-    end
-
-    def find_by_all_codes
-      attrs = facade.all_codes
-      return unless attrs
-
-      result = model.record_class.find(attrs)
-      return unless result.any?
-      raise "Too much reslts" if result.length != 1
-
-      result.first
-    end
+    def call = nil
   end
 end
