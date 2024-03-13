8042915d629f1c35722afb867918f30c3b9a22b9
thomasregnet <ossgewalt@gmail.com>
2024-03-13 09:11:09 +0100

    Add tests for ArtistCreditParticipantProperties

---
+++ test/models/import/artist_credit_participant_properties_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+require "support/shared_import_properties_tests"
+
+class Import::ArtistCreditParticipantPropertiesTest < ActiveSupport::TestCase
+  include SharedImportPropertiesTests
+
+  setup do
+    @subject = Import::ArtistCreditParticipantProperties.new
+  end
+
+  test "#belongs_to_associations" do
+    # assert_empty @subject.belongs_to_associations
+    assert_equal 1, @subject.belongs_to_associations.length
+    assert_equal @subject.belongs_to_associations.map(&:name), [:participant]
+  end
+
+  test "#has_many_associations" do
+    assert_empty @subject.has_many_associations
+  end
+
+  test "#has_and_belongs_to_many_associations" do
+    assert_empty @subject.has_and_belongs_to_many_associations
+  end
+end
