5d4e0f15d94b517671d19fe0cc51c4a059173df8
thomasregnet <ossgewalt@gmail.com>
2024-03-13 12:01:12 +0100

    Remove method #properties

--- app/models/import/musicbrainz_artist_credit_participant_facade.rb
+++ app/models/import/musicbrainz_artist_credit_participant_facade.rb
@@ -11,17 +11,13 @@ module Import
     attr_reader :data, :options, :session
     alias_method :position, :consecutive_number
 
+    def model_class = ArtistCreditParticipant
+
     # FIXME: implement #join_phrase
     def join_phrase = nil
 
     def participant
       session.build_facade(Import::MusicbrainzParticipantFacade, code: data.artist.id)
-    end
-
-    def model_class = ArtistCreditParticipant
-
-    def properties
-      @properties ||= session.build_properties(model_class)
     end
   end
 end
