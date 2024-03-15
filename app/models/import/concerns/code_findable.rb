d0afa227b6a98e8b9fd176cc924382f6eaf0e773
thomasregnet <ossgewalt@gmail.com>
2024-03-15 08:47:01 +0100

    Create concern `CodeFindable`

---
+++ app/models/import/concerns/code_findable.rb
@@ -0,0 +1,23 @@
+module Import::Concerns
+  module CodeFindable
+    extend ActiveSupport::Concern
+
+    def find_by_intrinsic_code
+      return unless intrinsic_codes.any?
+
+      model_class.find_by(intrinsic_codes)
+    end
+
+    def find_by_all_codes
+      return unless all_codes.any?
+
+      result = OrWithPresentValues
+        .query(attributes: all_codes, model_class: model_class)
+        .load
+      return unless result.any?
+      raise "Too much reslts" if result.length != 1
+
+      result.first
+    end
+  end
+end
--- app/models/import/find_participant.rb
+++ app/models/import/find_participant.rb
@@ -1,5 +1,7 @@
 module Import
   class FindParticipant
+    include Concerns::CodeFindable
+
     def initialize(session, facade:)
       @session = session
       @facade = facade
@@ -15,24 +17,6 @@ module Import
 
     def call
       find_by_intrinsic_code || find_by_all_codes
-    end
-
-    def find_by_intrinsic_code
-      return unless intrinsic_codes.any?
-
-      model_class.find_by(intrinsic_codes)
-    end
-
-    def find_by_all_codes
-      return unless all_codes.any?
-
-      result = OrWithPresentValues
-        .query(attributes: all_codes, model_class: model_class)
-        .load
-      return unless result.any?
-      raise "Too much reslts" if result.length != 1
-
-      result.first
     end
 
     def all_codes
