772c9657a6aff6dc0429d6896d9a64d5902a6641
thomasregnet <ossgewalt@gmail.com>
2024-03-15 10:23:47 +0100

    Remove #properties

--- app/models/import/find_participant.rb
+++ app/models/import/find_participant.rb
@@ -10,10 +10,6 @@ module Import
     attr_reader :facade, :session
     delegate :model_class, to: :facade
 
-    def properties
-      @properties ||= session.build_properties(model_class)
-    end
-
     def call
       find_by_cheap_codes || find_by_codes
     end
