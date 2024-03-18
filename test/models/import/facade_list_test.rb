cf79b98390f2f7b14f0a5afd8babd4406a7dfb80
thomasregnet <ossgewalt@gmail.com>
2024-03-18 22:23:02 +0100

    Cleanup tests

--- test/models/import/facade_list_test.rb
+++ test/models/import/facade_list_test.rb
@@ -1,41 +1,29 @@
 require "test_helper"
 require "minitest/mock"
-# require "support/web_mock_external_apis"
-
-class Import::TestFacade
-  def initialize(*)
-  end
-end
 
 class Import::FacadeListTest < ActiveSupport::TestCase
   setup do
     @session = Minitest::Mock.new
+    @f_mocks = 2.times.map { |idx| mock_facade(idx) }
 
-    # @facade_0 = Import::TestFacade.new
-    @f_mocks = [Import::TestFacade.new, Import::TestFacade.new]
-    @session.expect :build_facade, @f_mocks[0] do |consecutive_number: 1, data: :@facade_0, model: Import::TestFacade|
-      true
-    end
-
-    @session.expect :build_facade, @f_mocks[1] do |consecutive_number: 2, data: :facede_1, model: Import::TestFacade|
-      true
-    end
-
-    @facade_list = Import::FacadeList.new(@session, data: @f_mocks, model: Import::TestFacade)
+    @facade_list = Import::FacadeList.new(@session, data: @f_mocks, model: Object)
   end
 
   test "#each" do
     enumerator = @facade_list.each
-    assert_instance_of Import::TestFacade, enumerator.next
-    assert_instance_of Import::TestFacade, enumerator.next
+    assert_instance_of Object, enumerator.next
+    assert_instance_of Object, enumerator.next
 
     @session.verify
   end
 
   test "#map" do
-    classes = @facade_list.map { |item| item.class }
-    assert_equal classes.length, 2
-    assert_equal classes.count { |klass| klass == Import::TestFacade }, 2
+    facades = @facade_list.map { |item| item.class }
+
+    @f_mocks.each do |f_mock|
+    end
+    assert_equal facades.length, 2
+    assert_equal facades.count { |klass| klass == Object }, 2
 
     @session.verify
   end
@@ -61,5 +49,14 @@ class Import::FacadeListTest < ActiveSupport::TestCase
     assert_equal persisters, [:persister_0, :persister_1]
 
     @session.verify
+  end
+
+  def mock_facade(idx)
+    facade = Object.new
+    @session.expect :build_facade, facade do |consecutive_number: idx, data: idx, model: Object|
+      true
+    end
+
+    facade
   end
 end
