e602745b000c4d032ed85de550df2e9a5854f69f
thomasregnet <ossgewalt@gmail.com>
2024-03-19 20:46:32 +0100

    Move methods from MusicbrainzFactory to Factory

--- app/models/import/musicbrainz_factory.rb
+++ app/models/import/musicbrainz_factory.rb
@@ -1,51 +1,13 @@
 module Import
   class MusicbrainzFactory < Factory
-    def initialize(session, buffer: nil)
-      @buffer = buffer || Buffer.new
-      @session = session
-    end
-
-    attr_reader :buffer, :session
-
     def build_attempt
       Import::FaradayAttempt.new(self)
-    end
-
-    def build_fetcher(uri_string)
-      Import::Fetcher.new(factory: self, uri: uri_string)
-    end
-
-    def build_finder(facade)
-      model_name = facade.model_class.name
-
-      class_name = "Import::Find#{model_name}"
-
-      klass = class_name.constantize
-      klass.new(session, facade: facade)
-    end
-
-    def build_collector(facade)
-      Import::Collector.new(session, facade: facade)
     end
 
     def build_facade(model, **)
       name = model.name
       facade_class = "Import::Musicbrainz#{name}Facade".constantize
       facade_class.new(session, **)
-    end
-
-    def build_facade_list(model:, **)
-      Import::FacadeList.new(session, model: model, **)
-    end
-
-    def build_persister(facade, **)
-      Import::Persist.new(session, facade: facade, **)
-    end
-
-    def build_properties(model_class)
-      name = model_class.name
-      properties_class = "Import::#{name}Properties".constantize
-      properties_class.new
     end
 
     def build_uri_string(kind, code)
