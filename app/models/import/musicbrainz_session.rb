83d620c5622560882c01b74a1712147bb025646d
thomasregnet <ossgewalt@gmail.com>
2024-04-03 19:56:53 +0200

    Delete unused code

--- app/models/import/musicbrainz_session.rb
+++ app/models/import/musicbrainz_session.rb
@@ -13,20 +13,9 @@ module Import
       buffer.deep_dup
     end
 
-    # def get(kind, code)
-    #   buffer.fetch(kind, code) || fetch(kind, code)
-    # end
     def get(*params)
       buffer.fetch(*params) { build_fetcher(*params).get }
       # buffer.fetch(kind, code)#  { build_fetcher(kind, code).get }
-    end
-
-    private
-
-    def fetch(kind, code)
-      uri_string = build_uri_string(kind, code)
-      fetcher = build_fetcher(uri_string)
-      buffer.fetch(kind, code) { fetcher.get }
     end
   end
 end
