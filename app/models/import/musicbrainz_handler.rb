78d61b5c462403cecd1061284bbec031bee698c4
thomasregnet <ossgewalt@gmail.com>
2024-09-25 10:58:57 +0200

    Add logging to the MusicbrainzHandler

--- app/models/import/musicbrainz_handler.rb
+++ app/models/import/musicbrainz_handler.rb
@@ -7,6 +7,7 @@ module Import
     attr_reader :import_order
 
     def call
+      Rails.logger.info("starting import from MusicBrainz")
       record = find || collect
       return record if record && record.class.is_a?(ApplicationRecord)
 
@@ -26,14 +27,17 @@ module Import
     end
 
     def find
+      Rails.logger.info("trying to find excisting record")
       finder.call
     end
 
     def collect_action
+      Rails.logger.info("collecting data")
       session.build_collect_action(facade: facade)
     end
 
     def create_action
+      Rails.logger.info("persisting data")
       session.build_create_action(facade: facade)
     end
 
@@ -51,7 +55,11 @@ module Import
       facade_class.new(session, code: import_order.code)
     end
 
-    delegate :lock, to: :session
+    # delegate :lock, to: :session
+    def lock
+      Rails.logger.info("locking session")
+      session.lock
+    end
 
     def session
       @session ||= Import::MusicbrainzSession.new(import_order)
