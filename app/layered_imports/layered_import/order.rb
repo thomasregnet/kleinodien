6ba1ef7f5206f7fb6e68aa48ecc4c4972f05feb7
thomasregnet <ossgewalt@gmail.com>
2024-12-11 19:54:18 +0000

    Add method #create_facade_layer

--- app/layered_imports/layered_import/order.rb
+++ app/layered_imports/layered_import/order.rb
@@ -12,6 +12,12 @@ module LayeredImport
       type.delete_suffix("ImportOrder")
     end
 
+    def create_facade_layer
+      embed_class_name_component("LayeredImport::", "Facade")
+        .constantize
+        .new(self)
+    end
+
     def embed_class_name_component(prefix, suffix)
       prefix.concat(class_name_component).concat(suffix)
     end
--- test/layered_imports/layered_import/order_test.rb
+++ test/layered_imports/layered_import/order_test.rb
@@ -1,6 +1,14 @@
 require "test_helper"
 require "minitest/mock"
 
+class LayeredImport::TestFooBarFacade
+  def initialize(import_order)
+    @import_order = import_order
+  end
+
+  attr_reader :import_order
+end
+
 class LayeredImport::OrderTest < ActiveSupport::TestCase
   setup do
     @import_order = Minitest::Mock.new
@@ -16,10 +24,21 @@ class LayeredImport::OrderTest < ActiveSupport::TestCase
   end
 
   test "#embed_class_name_component" do
-    @import_order.expect :nil?, false # delegate_missing_to uses `#nil?`
+    @import_order.expect :nil?, false
     @import_order.expect :type, "FooImportOrder"
 
     assert_equal "Blubber::FooBar", @order.embed_class_name_component("Blubber::", "Bar")
+
+    @import_order.verify
+  end
+
+  test "#create_facade_layer" do
+    @import_order.expect :nil?, false
+    @import_order.expect :type, "TestFooBarImportOrder"
+
+    facade = @order.create_facade_layer
+
+    assert_kind_of LayeredImport::TestFooBarFacade, facade
 
     @import_order.verify
   end
