1f1f334c4fae41a7877f59d9f9c79c5d6275079f
thomasregnet <ossgewalt@gmail.com>
2024-12-11 19:29:24 +0000

    Add LayeredImports::Order

---
+++ app/layered_imports/layered_import/order.rb
@@ -0,0 +1,19 @@
+module LayeredImport
+  class Order
+    def initialize(import_order)
+      @import_order = import_order
+    end
+
+    attr_reader :import_order
+
+    delegate_missing_to :import_order
+
+    def class_name_component
+      type.delete_suffix("ImportOrder")
+    end
+
+    def embed_class_name_component(prefix, suffix)
+      prefix.concat(class_name_component).concat(suffix)
+    end
+  end
+end
---
+++ test/layered_imports/layered_import/order_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+require "minitest/mock"
+
+class LayeredImport::OrderTest < ActiveSupport::TestCase
+  setup do
+    @import_order = Minitest::Mock.new
+    @order = LayeredImport::Order.new(@import_order)
+  end
+  test "#class_name_component" do
+    @import_order.expect :nil?, false # delegate_missing_to uses `#nil?`
+    @import_order.expect :type, "FooImportOrder"
+
+    assert_equal "Foo", @order.class_name_component
+
+    @import_order.verify
+  end
+
+  test "#embed_class_name_component" do
+    @import_order.expect :nil?, false # delegate_missing_to uses `#nil?`
+    @import_order.expect :type, "FooImportOrder"
+
+    assert_equal "Blubber::FooBar", @order.embed_class_name_component("Blubber::", "Bar")
+
+    @import_order.verify
+  end
+end
