fdce14e2f0289dbf7a09229f3b8d65991eafe3f3
thomasregnet <ossgewalt@gmail.com>
2025-01-16 18:58:23 +0000

    Delete unnecessary methods

--- app/layered_imports/layered_import/order.rb
+++ app/layered_imports/layered_import/order.rb
@@ -12,12 +12,6 @@ module LayeredImport
       type.delete_suffix("ImportOrder")
     end
 
-    def create_facade_layer
-      embed_class_name_component("LayeredImport::", "Facade")
-        .constantize
-        .new(self)
-    end
-
     def create_fetch_layer
       MusicbrainzFetchLayer.new(self)
     end
@@ -27,13 +21,10 @@ module LayeredImport
     end
 
     def create_workflow
-      embed_class_name_component("LayeredImport::", "Workflow")
+      ["LayeredImport::", class_name_component, "Workflow"]
+        .join
         .constantize
         .new(self)
-    end
-
-    def embed_class_name_component(prefix, suffix)
-      prefix.concat(class_name_component).concat(suffix)
     end
   end
 end
--- test/layered_imports/layered_import/order_test.rb
+++ test/layered_imports/layered_import/order_test.rb
@@ -1,14 +1,6 @@
 require "test_helper"
 require "minitest/mock"
 
-class LayeredImport::TestFooBarFacade
-  def initialize(import_order)
-    @import_order = import_order
-  end
-
-  attr_reader :import_order
-end
-
 class LayeredImport::OrderTest < ActiveSupport::TestCase
   setup do
     @import_order = Minitest::Mock.new
@@ -19,26 +11,6 @@ class LayeredImport::OrderTest < ActiveSupport::TestCase
     @import_order.expect :type, "FooImportOrder"
 
     assert_equal "Foo", @order.class_name_component
-
-    @import_order.verify
-  end
-
-  test "#embed_class_name_component" do
-    @import_order.expect :nil?, false
-    @import_order.expect :type, "FooImportOrder"
-
-    assert_equal "Blubber::FooBar", @order.embed_class_name_component("Blubber::", "Bar")
-
-    @import_order.verify
-  end
-
-  test "#create_facade_layer" do
-    @import_order.expect :nil?, false
-    @import_order.expect :type, "TestFooBarImportOrder"
-
-    facade = @order.create_facade_layer
-
-    assert_kind_of LayeredImport::TestFooBarFacade, facade
 
     @import_order.verify
   end
