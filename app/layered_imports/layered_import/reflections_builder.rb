1eacae5eb2d9058a8aec69ec7e594c62ba985929
thomasregnet <ossgewalt@gmail.com>
2025-01-23 20:20:36 +0000

    Rename variable "infix" to "record_class_name"

--- app/layered_imports/layered_import/reflections_builder.rb
+++ app/layered_imports/layered_import/reflections_builder.rb
@@ -14,8 +14,8 @@ module LayeredImport
     attr_reader :kind
 
     def build_reflection
-      infix = kind.to_s.classify
-      class_name = [CLASS_PREFIX, infix, CLASS_SUFFIX].join
+      record_class_name = kind.to_s.classify
+      class_name = [CLASS_PREFIX, record_class_name, CLASS_SUFFIX].join
       class_name.constantize.new
     end
   end
