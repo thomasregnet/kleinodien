43681622fcc34b6fd79faad24b2697766e086779
thomasregnet <ossgewalt@gmail.com>
2025-02-04 19:19:55 +0000

    Take advantage of new Reflections methods

--- app/layered_imports/layered_import/archetype_reflections.rb
+++ app/layered_imports/layered_import/archetype_reflections.rb
@@ -4,15 +4,14 @@ module LayeredImport
 
     delegate_missing_to Archetype
 
-    def inherent_attribute_names
-      attribute_names
-        .without("id", "created_at", "updated_at")
-        .reject { |attr| attr.end_with? "_id" }
+    def after_belongs_to_associations(associations)
+      associations
+        .reject { |association| association.name == :archetypeable }
+        # TODO: stop rejecting ArtistCredit
+        .reject { |association| association.name == :artist_credit }
     end
 
-    def belong_to_associations = []
-
-    def foreign_attribute_names = []
+    # def foreign_attribute_names = []
 
     def has_many_associations = []
   end
