a3875056102bf24871ca802e35b2231078a9fcc4
thomasregnet <ossgewalt@gmail.com>
2025-02-04 19:03:55 +0000

    Remove #foreign_attribute_names

--- app/layered_imports/layered_import/artist_credit_reflections.rb
+++ app/layered_imports/layered_import/artist_credit_reflections.rb
@@ -4,10 +4,6 @@ module LayeredImport
 
     delegate_missing_to ArtistCredit
 
-    def foreign_attribute_names
-      attribute_names.select { |attr| attr.end_with? "_id" }
-    end
-
     def has_many_associations = reflect_on_all_associations(:has_many)
   end
 end
