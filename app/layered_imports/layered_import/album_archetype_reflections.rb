b12c95c2b6615f25607e480798d69e9bac0985e6
thomasregnet <ossgewalt@gmail.com>
2025-02-04 19:27:40 +0000

    Take advantage of new Reflectable methods

--- app/layered_imports/layered_import/album_archetype_reflections.rb
+++ app/layered_imports/layered_import/album_archetype_reflections.rb
@@ -3,17 +3,5 @@ module LayeredImport
     include Concerns::Reflectable
 
     delegate_missing_to AlbumArchetype
-
-    def inherent_attribute_names
-      attribute_names
-        .without("id", "created_at", "updated_at")
-        .reject { |attr| attr.end_with? "_id" }
-    end
-
-    def belong_to_associations = []
-
-    def foreign_attribute_names = []
-
-    def has_many_associations = []
   end
 end
