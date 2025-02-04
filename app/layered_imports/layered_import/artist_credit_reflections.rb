7fb9dc3eb42ec74ef7e770df5419384866592a5c
thomasregnet <ossgewalt@gmail.com>
2025-02-04 19:01:52 +0000

    Remove #inherent_attribute_names

--- app/layered_imports/layered_import/artist_credit_reflections.rb
+++ app/layered_imports/layered_import/artist_credit_reflections.rb
@@ -4,15 +4,6 @@ module LayeredImport
 
     delegate_missing_to ArtistCredit
 
-    def inherent_attribute_names
-      attribute_names
-        .without("id", "created_at", "updated_at")
-        .reject { |attr| attr.end_with? "_id" }
-        .reject { |attr| attr.end_with? "_accuracy" }
-        .map { |attr| (attr == "begin_date") ? "begins_at" : attr }
-        .map { |attr| (attr == "end_date") ? "ends_at" : attr }
-    end
-
     def belong_to_associations = []
 
     def foreign_attribute_names
