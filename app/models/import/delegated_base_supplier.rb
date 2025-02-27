921db1f1056d163b859822efdc3d63b693266dfc
thomasregnet <ossgewalt@gmail.com>
2025-02-27 19:22:30 +0000

    Don't try to find a delegated_base

--- app/models/import/delegated_base_supplier.rb
+++ app/models/import/delegated_base_supplier.rb
@@ -9,7 +9,11 @@ module Import
     end
 
     def supply_delegated_base
-      find || build
+      assign_foreign_attributes
+      assign_foreign_bases
+      build_has_many_records
+
+      record
     end
 
     private
@@ -17,23 +21,6 @@ module Import
     attr_reader :adapter_layer, :facade, :delegated_type_reflections
     delegate :delegated_base_class, to: :delegated_type_reflections
     delegate_missing_to :adapter_layer
-
-    def find
-      # TODO: do we really need to lookup for a delegated_base?
-      # finder_class.new(order, facade: facade).find
-    end
-
-    def finder_class
-      @finder_class ||= "Import::#{delegated_base_class.name}Finder".constantize
-    end
-
-    def build
-      assign_foreign_attributes
-      assign_foreign_bases
-      build_has_many_records
-
-      record
-    end
 
     def record
       @record ||= delegated_base_class.new(inherent_attributes)
