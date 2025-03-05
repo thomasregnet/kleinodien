debc9892436e4b280dab4dbd8332c93fde18a2f6
thomasregnet <ossgewalt@gmail.com>
2025-03-05 09:51:19 +0000

    Make SongArchetype a delegate of Archetype

--- app/models/archetype.rb
+++ app/models/archetype.rb
@@ -1,5 +1,5 @@
 class Archetype < ApplicationRecord
-  delegated_type :archetypeable, types: %w[AlbumArchetype]
+  delegated_type :archetypeable, types: %w[AlbumArchetype SongArchetype]
 
   belongs_to :artist_credit, optional: true
   has_many :editions
--- app/models/song_archetype.rb
+++ app/models/song_archetype.rb
@@ -1,2 +1,4 @@
 class SongArchetype < ApplicationRecord
+  # TODO: are there really codes for discogs, musicbrainz and wikidata?
+  include Archetypeable
 end
