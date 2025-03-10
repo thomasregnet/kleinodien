71d0c7a62ae30143a4b775ecbf82eff7ca75f3d7
thomasregnet <ossgewalt@gmail.com>
2025-03-10 19:56:59 +0000

    Import actual MusicBrainz recordings

--- app/models/import/musicbrainz_song_archetype_facade.rb
+++ app/models/import/musicbrainz_song_archetype_facade.rb
@@ -12,13 +12,13 @@ module Import
     delegate_missing_to :facade_layer
 
     def data
-      options
+      @data ||= request_layer.get(:recording, options[:id])
     end
 
     def scraper_builder
       @@scraper_builder ||= Import::ScraperArchitect.build do
         define :title
-        define :artist_credit, callback: ->(facade) { facade.artist_credit }
+        define :artist_credit
         define :archetypeable_type, always: "SongArchetype"
         define :discogs_code, always: nil
         define :wikidata_code, always: nil
@@ -29,21 +29,6 @@ module Import
     def all_codes = {}
 
     def cheap_codes = {}
-
-    def artist_credit
-      [{
-        joinphrase: "",
-        name: "AC/DC",
-        artist: {
-          disambiguation: "Australian hard rock band",
-          type: "Group",
-          id: "66c662b6-6e2f-4930-8610-912e24c63ed1",
-          sort_name: "AC/DC",
-          name: "AC/DC",
-          type_id: "e431f5f6-b5d2-343d-8b36-72607fffb74b"
-        }
-      }]
-    end
 
     def musicbrainz_code = options[:musicbrainz_code] || options[:id]
   end
