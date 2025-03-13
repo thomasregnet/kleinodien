584c45cae01194b3fb6ccc9c86dc2960c069abd4
thomasregnet <ossgewalt@gmail.com>
2025-03-13 08:04:11 +0000

    Actual find an AlbumEdition

---
+++ test/models/import/album_edition_finder_test.rb
@@ -0,0 +1,46 @@
+require "test_helper"
+require "minitest/mock"
+
+class Import::AlbumEditionFinderTest < ActiveSupport::TestCase
+  setup do
+    @order = Minitest::Mock.new
+    @facade = Minitest::Mock.new
+  end
+
+  test "#find returns nil if AlbumArchetype does not exist" do
+    @facade.expect :cheap_codes, {discogs_code: 333}
+    @facade.expect :all_codes, {discogs_code: 333, musicbrainz_code: "5ec4b261-1afe-4588-9ff7-90b1b32c9dec"}
+
+    assert_nil Import::AlbumEditionFinder.call(@order, @facade)
+
+    @facade.verify
+    @order.verify
+  end
+
+  test "#find by cheap_codes returns if AlbumEdition does exist" do
+    musicbrainz_code = "18c8ecb4-c9fd-4b14-9b2e-249ea3e8c821"
+    album_edition = album_editions(:one)
+    album_edition.update!(musicbrainz_code: musicbrainz_code)
+
+    @facade.expect :cheap_codes, {musicbrainz_code: musicbrainz_code}
+
+    assert_equal album_edition, Import::AlbumEditionFinder.call(@order, @facade)
+
+    @facade.verify
+    @order.verify
+  end
+
+  test "#find by all_codes returns if AlbumEdition does exist" do
+    musicbrainz_code = "18c8ecb4-c9fd-4b14-9b2e-249ea3e8c821"
+    album_edition = album_editions(:one)
+    album_edition.update!(musicbrainz_code: musicbrainz_code)
+
+    @facade.expect :cheap_codes, {discogs_code: 333}
+    @facade.expect :all_codes, {musicbrainz_code: musicbrainz_code}
+
+    assert_equal album_edition, Import::AlbumEditionFinder.call(@order, @facade)
+
+    @facade.verify
+    @order.verify
+  end
+end
