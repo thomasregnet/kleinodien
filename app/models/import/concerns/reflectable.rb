798542fbd7f8feaed1e3b7856657afb46ce9f0c7
thomasregnet <ossgewalt@gmail.com>
2025-03-27 18:32:48 +0000

    Remove comments

--- app/models/import/concerns/reflectable.rb
+++ app/models/import/concerns/reflectable.rb
@@ -2,9 +2,7 @@ module Import::Concerns
   module Reflectable
     extend ActiveSupport::Concern
 
-    # def has_one_associations = reflect_on_all_associations(:has_one)
     def has_one_associations
-      # TODO: how to handle "central" in imports
       reflect_on_all_associations(:has_one).reject { it.name == :central }
     end
 
