7df789b12264f76dae9386d9b5350d3fca3e9f3c
thomasregnet <ossgewalt@gmail.com>
2025-03-31 18:35:10 +0000

    Test deriving kind and code from MusicBrainz-uri

--- test/models/import_order_test.rb
+++ test/models/import_order_test.rb
@@ -2,4 +2,15 @@ require "test_helper"
 # require "support/shared_import_order_tests"
 
 class ImportOrderTest < ActiveSupport::TestCase
+  test "derive kind and code from a MusicBrainz uri" do
+    import_order = ImportOrder.new(
+      state: "open",
+      uri: "https://musicbrainz.org/release/45eb569a-da08-49d5-8735-21a3a169fdee",
+      user: users(:kim)
+    )
+
+    assert import_order.valid?
+    assert_equal "release", import_order.kind
+    assert_equal "45eb569a-da08-49d5-8735-21a3a169fdee", import_order.code
+  end
 end
