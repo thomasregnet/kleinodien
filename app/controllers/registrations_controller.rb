5c0628b74e42b4798eea9555abaab4a1624ccf80
thomasregnet <ossgewalt@gmail.com>
2025-04-15 18:48:25 +0000

    Add sing_up functionality

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,25 @@
+class RegistrationsController < ApplicationController
+  allow_unauthenticated_access only: %i[new create]
+  before_action :resume_session, only: %i[new]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      start_new_session_for @user
+      redirect_to root_path, notice: "You've successfully signed up to Joblister. Welcome!"
+    else
+      flash[:alert] = @user.errors.full_messages.join(", ")
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: [:email_address, :password, :password_confirmation])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resource :registrations, only: [:new, :create]
   resource :session
   resources :passwords, param: :token
   resources :urls
