6e544c59bf002f837decdb486258ff2c28295b92
thomasregnet <ossgewalt@gmail.com>
2025-04-16 18:21:32 +0000

    Create tests for RegistrationsController

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,7 +10,7 @@ class RegistrationsController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       start_new_session_for @user
-      redirect_to root_path, notice: "You've successfully signed up to Joblister. Welcome!"
+      redirect_to root_path, notice: "You've successfully signed up to Kleinodien"
     else
       flash[:alert] = @user.errors.full_messages.join(", ")
       render :new, status: :unprocessable_entity
