1f4feb41c2b9921e1bfeefc26bae9fa66011375d
thomasregnet <ossgewalt@gmail.com>
2025-04-28 18:55:56 +0000

    Delete #set_kind_and_code

--- app/models/import_order.rb
+++ app/models/import_order.rb
@@ -7,19 +7,5 @@ class ImportOrder < ApplicationRecord
 
   VALID_INFERRED_TYPE_REGEX = %r{\A[A-Z][a-z]+ImportOrder\z}
 
-  # before_validation :set_kind_and_code
-
   delegate_missing_to :import_orderable
-
-  private
-
-  def set_kind_and_code
-    return if kind.present?
-    return if code.present?
-    return if uri.blank?
-
-    import_order_uri = ImportOrderUri.build(uri)
-    self.kind = import_order_uri.kind
-    self.code = import_order_uri.code
-  end
 end
