02999f7605012e3b4dbfd94599160426d8536ee6
thomasregnet <ossgewalt@gmail.com>
2025-04-28 18:57:10 +0000

    Delete unused constant

--- app/models/import_order.rb
+++ app/models/import_order.rb
@@ -5,7 +5,5 @@ class ImportOrder < ApplicationRecord
   delegated_type :import_orderable, types: %w[MusicbrainzImportOrder]
   accepts_nested_attributes_for :import_orderable
 
-  VALID_INFERRED_TYPE_REGEX = %r{\A[A-Z][a-z]+ImportOrder\z}
-
   delegate_missing_to :import_orderable
 end
