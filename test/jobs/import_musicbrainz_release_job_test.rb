e643d967f373eddc15f3b6ba364cd4e1a67cf084
thomasregnet <ossgewalt@gmail.com>
2025-04-29 17:56:20 +0000

    Add test for ImportMusicbrainzReleaseJob

--- test/jobs/import_musicbrainz_release_job_test.rb
+++ test/jobs/import_musicbrainz_release_job_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
+require "support/web_mock_external_apis"
 
 class ImportMusicbrainzReleaseJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    WebMockExternalApis.setup
+  end
+
+  test "imports the release" do
+    code = "8866e226-7cd6-414e-b7d2-6ae0b0df6715"
+    user = users(:kim)
+    import_order = ImportOrder.create!(
+      user: user,
+      import_orderable_type: "MusicbrainzImportOrder",
+      import_orderable_attributes: {code: code, kind: "album-edition"}
+    )
+
+    perform_enqueued_jobs do
+      ImportMusicbrainzReleaseJob.perform_later(import_order)
+    end
+
+    assert Archetype.find_by(title: "Beating Around the Bush")
+    # assert import_order.done?
+  end
 end
